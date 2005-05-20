00e1afd1e19a1742819787e8084f5722cefd8076
athomas <athomas@7322e99d-02ea-0310-aa39-e9a107903beb>
2005-05-20 17:18:53 +0000

    Thanks to Michel Jouvin:   * Correct handling of parameters (like ''inline'') when no page is explicitly specified (means current page)   * Suppress level 1 header when the TOC for the current page (no page specified in parameters) is displayed.
    git-svn-id: https://trac-hacks.org/svn@32 7322e99d-02ea-0310-aa39-e9a107903beb

--- trunk/macros/TOC.py
+++ trunk/macros/TOC.py
@@ -7,9 +7,10 @@ from trac.util import escape
 rules_re = re.compile(r"""(?P<heading>^\s*(?P<hdepth>=+)\s(?P<header>.*)\s(?P=hdepth)\s*$)""")
 anchor_re = re.compile('[^\w\d]+')
 
-def parse_toc(env, out, page, body, max_depth = 999):
-    current_depth = 1
+def parse_toc(env, out, page, body, max_depth = 999, min_depth = 1):
+    current_depth = min_depth
     in_pre = False
+    first_li = True
     seen_anchors = []
 
     for line in body.splitlines():
@@ -30,21 +31,23 @@ def parse_toc(env, out, page, body, max_depth = 999):
         if match:
             header = match.group('header')
             new_depth = len(match.group('hdepth'))
-            if new_depth < current_depth:
+            if new_depth < min_depth:
+                continue
+            elif new_depth < current_depth:
                 while new_depth < current_depth:
                     current_depth -= 1
-                    if current_depth == max_depth:
-                        out.write("</li><li>\n")
-                    elif current_depth < max_depth:
-                        out.write("</li></ol><li>\n")
-            elif new_depth > current_depth:
-                while new_depth > current_depth:
-                    current_depth += 1
+                    if current_depth < max_depth:
+                        out.write("</ol>")
+                out.write("<li>")
+            elif new_depth >= current_depth:
+                i = current_depth
+                while new_depth > i or first_li  :
+                    i += 1
+                    first_li = False
                     if current_depth <= max_depth:
-                        out.write("<ol><li>\n")
-            else:
-                if current_depth <= max_depth:
-                    out.write("</li><li>\n")
+                        out.write("<ol>")
+                current_depth = new_depth
+                out.write("<li>")
             default_anchor = anchor = anchor_re.sub("", header)
             anchor_n = 1
             while anchor in seen_anchors:
@@ -53,17 +56,16 @@ def parse_toc(env, out, page, body, max_depth = 999):
             seen_anchors.append(anchor)
             link = page + "#" + anchor
             if current_depth <= max_depth:
-                out.write('<a href="%s">%s</a>' % (env.href.wiki(link), header))
-    while current_depth > 1:
+                out.write('<a href="%s">%s</a></li>\n' % (env.href.wiki(link), header))
+    while current_depth >= min_depth and not first_li:
         if current_depth <= max_depth:
-            out.write("</li></ol>\n")
+            out.write("</ol>\n")
         current_depth -= 1
 
 def execute(hdf, args, env):
     db = env.get_db_cnx()
-    # Has the user supplied a list of pages?
     if not args:
-        args = hdf.getValue("args.page", "WikiStart")
+        args = ''
     pre_pages = re.split('\s*,\s*', args)
     # Options
     inline = False
@@ -79,12 +81,22 @@ def execute(hdf, args, env):
             heading = None
         elif page[:8] == 'heading=':
             heading = page[8:]
+        elif page == '':
+            continue
+        elif page[:6] == 'depth=':
+            params['max_depth'] = int(page[6:])
+        elif page[:5] == 'root=':
+            root = page[5:]
         else:
             pages.append(page)
+    # Has the user supplied a list of pages?
+    if not pages:
+        pages.append(hdf.getValue("args.page", "WikiStart"))
+        root = ''
+        params['min_depth'] = 2     # Skip page title
     out = StringIO()
     if not inline:
         out.write("<div class='wiki-toc'>\n")
-    out.write("<ol>\n")
     if heading:
         out.write("<h4>%s</h4>\n" % heading)
     for page in pages:
@@ -102,7 +114,6 @@ def execute(hdf, args, env):
                 parse_toc(env, out, page, row[0], **params)
             else:
                 out.write('<div class="system-message"><strong>Error: Page %s does not exist</strong></div>' % page)
-    out.write("</ol>\n")
     if not inline:
         out.write("</div>\n")
     return out.getvalue()
