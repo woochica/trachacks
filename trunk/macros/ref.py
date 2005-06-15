70624456b885ab2f73788f841aff61776d4ae98e
root <root@7322e99d-02ea-0310-aa39-e9a107903beb>
2005-06-15 13:00:06 +0000

    * Pathetic attempt at escaping.
    git-svn-id: https://trac-hacks.org/svn@40 7322e99d-02ea-0310-aa39-e9a107903beb

--- trunk/macros/ref.py
+++ trunk/macros/ref.py
@@ -3,4 +3,4 @@ import re
 anchor_re = re.compile('[^\w\d]+')
 
 def execute(hdf, args, env):
-	return "<a href='#%s' title='Go to %s'>%s</a>" % (anchor_re.sub("", args), args, args)
+	return "<a href='#%s' title='Jump to section %s'>%s</a>" % (anchor_re.sub("", args), args.replace("'", "\\'"), args)
