65ca665091da13d05a7287f54d573299db83f39b
vgough <vgough@7322e99d-02ea-0310-aa39-e9a107903beb>
2005-07-27 11:07:00 +0000

    LatexFormulaMacro:
    Fix known bug of latex output causing apache (or python, or trac?) to complain of internal error. Lots of improvements. Can now render general latex in addition to display-math mode.
    
    git-svn-id: https://trac-hacks.org/svn@87 7322e99d-02ea-0310-aa39-e9a107903beb

--- latexformulamacro/stable/formula.py
+++ latexformulamacro/stable/formula.py
@@ -2,9 +2,40 @@
 Convert a latex formula into an image.
 by Valient Gough <vgough@pobox.com>
 
-Usage:  [[formula([density|] latex-formula )]]
+Changes:
+    2005-07-27:
+	* figured out how to get rid of the annoying internal error after latex
+	was run.  Redirected latex output to /dev/null..
+	* found out that {{{#!figure ... }}} runs wiki macro, and doesn't have
+	the problem of not being able to use paranthesis.  So this is the
+	default usage now.  Can still use [[formula(...)]] for simple formula.
+	* add "nomode" command, which can be used to turn off automatic
+	  enclosure of commands in display-math mode ("$$ ... $$")
+    2005-07-26: first release
+
+Usage:
+
+{{{
+#!formula
+[latex code]
+}}}
+
+or, a density can be specified:
+{{{
+#!formula
+#density=100
+[latex code]
+}}}
+
+Options:
     Density defaults to 100.  Larger values produces larger images.
     Density is optional, but if it is include, it must be included first.
+
+Notes:
+    A matrix macro is included in the tex code.  This allows you to do things
+    like:
+      \mat{1&2\\3&4}  to get a 2x2 matrix.  The "\\" separates rows, and "&"
+      separates columns.  Any size up to around 25? will work..
 
 Images are automatically named based on a sha1 hash of the formula, the
 density, and the script version.  This way the image doesn't have to be
@@ -17,16 +48,17 @@ be considered as cached files.  You can safely let the tmp file cleaner process
 remove old files from these directories.
 
 PS.  This is my first python program, so it is probably pretty ugly by python
-standards (whatever those may be).
+standards (whatever those may be).  Feedback is welcome, but complaints about
+ugliness will be redirected to /dev/null.
 """
 
 # TODO: Paths to change for your system:
 # directory where temporary files are created
-tmpdir = "/tmp/trac-tex/"
+tmpdir = "/var/www/html/trac/tex/"
 # directory where images are written
-imagePath = "/var/www/html/figures/"
+imagePath = "/var/www/html/formula/"
 # URL base for accessing images
-displayPath = "http://arg0.net/figures/"
+displayPath = "http://arg0.net/formula/"
 
 # if the version string changes, then images will be regenerated
 version = "0.1"
@@ -38,7 +70,7 @@ import os
 import sha
 
 
-def render(hdf, env, texData, density):
+def render(hdf, env, texData, density, mathMode):
     path = tmpdir # + hdf.getValue("project.name.encoded", "default") + "/"
     # create temporary directory if necessary
     try:
@@ -63,9 +95,9 @@ def render(hdf, env, texData, density):
 	os.chdir(path)
 
 	texFile = name + ".tex"
-	makeTexFile(texFile, texData)
+	makeTexFile(texFile, texData, mathMode)
 
-	cmd = "latex %s" % texFile
+	cmd = "latex %s > /dev/null" % texFile
 	# TODO: this causes error, even though the command appears to run to
 	# completion
 	log += execprog( cmd )
@@ -89,7 +121,7 @@ def execprog(cmd):
     os.system( cmd )
     return cmd + "<br>"
 
-def makeTexFile(texFile, texData):
+def makeTexFile(texFile, texData, mathMode):
     tex = "\\batchmode\n"
     tex += "\\documentclass{article}\n"
     tex += "\\usepackage{epsfig}\n"
@@ -100,9 +132,11 @@ def makeTexFile(texFile, texData):
     tex += "  \\end{array}\n"
     tex += "  \\right]}\n"
     tex += "\\begin{document}\n"
-    tex += "$$\n"
+    if mathMode:
+	tex += "$$\n"
     tex += "%s\n" % texData
-    tex += "$$\n"
+    if mathMode:
+	tex += "$$\n"
     tex += "\\pagebreak\n"
     tex += "\\end{document}\n"
 	
@@ -110,20 +144,34 @@ def makeTexFile(texFile, texData):
     FILE.write( tex )
     FILE.close()
 
-# first argument is the image name, second argument is the formula
+# arguments start with "#" on the beginning of a line
 def execute(hdf, text, env):
-    args = text.split('|') 
-    if len(args) < 1 or len(args) > 2:
-	return "Usage: [[formula([density|] tex-data)]]\n"
-
+    # TODO: unescape all html escape codes
+    text = text.replace("&amp;", "&")
+    
     # defaults
     density = 100
-    if len(args) == 1:
-	formula = args[0]
-    elif len(args) == 2:
-	density = int(args[0])
-	formula = args[1]
+    mathMode = 1 #default to using display-math mode
 
-    return render(hdf, env, formula, density) 
+    # find some number of arguments, followed by the formula
+    command = re.compile('^#([^=]+)=?(.*)')
+    formula = ""
+    errors = ""
+    for line in text.split("\n"):
+	m = command.match(line)
+	if m:
+	    if m.group(1) == "density":
+		density = int(m.group(2))
+	    elif m.group(1) == "nomode":
+		mathMode = 0
+	    else:
+		errors = '<br>Unknown <i>formula</i> command "%s"<br>' % m.group(1)
+	else:
+	    formula += line + "\n"
+
+    result = errors + render(hdf, env, formula, density, mathMode) 
+    #result += "<br> density = %d" % density
+    #result += "<br>text: %s" % formula
+    return result;
 
 
