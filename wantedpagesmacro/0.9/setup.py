e273d03dd9c815ffa7b980e949d91597068ae53d
jfrancis <jfrancis@7322e99d-02ea-0310-aa39-e9a107903beb>
2005-12-06 15:42:24 +0000

    WantedPagesMacro:
    fixed setup.py
    
    git-svn-id: https://trac-hacks.org/svn@164 7322e99d-02ea-0310-aa39-e9a107903beb

--- wantedpagesmacro/0.9/setup.py
+++ wantedpagesmacro/0.9/setup.py
@@ -1,5 +1,3 @@
 from setuptools import setup
 
-setup(name="PivotalPayments", version="0.1", 
-      packages=['effort_stats', 'phpdoc_link', 'wiki_restrict', 'wanted_pages'],
-      package_data={'wiki_restrict' : ['htdocs/css/*.css']})
+setup(name="WantedPages", version="0.1",  packages=['wanted_pages'])
