5d22018ce1e1149d2ad2d0b3667fada5aaf0938d
pkropf <pkropf@7322e99d-02ea-0310-aa39-e9a107903beb>
2005-12-16 22:20:36 +0000

    GraphvizPlugin:
    updated the setup.py and release notes with the v0.6 release number.
    
    git-svn-id: https://trac-hacks.org/svn@214 7322e99d-02ea-0310-aa39-e9a107903beb

--- graphvizplugin/branches/v0.6/setup.py
+++ graphvizplugin/branches/v0.6/setup.py
@@ -18,7 +18,7 @@ from setuptools import setup, find_packages
 
 setup (
     name = 'graphviz',
-    version = "0.5",
+    version = "0.6",
     packages = find_packages(),
     package_data = { 'graphviz' : ['examples/*',],
     },
