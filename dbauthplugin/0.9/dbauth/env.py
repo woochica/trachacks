8579b9f619632475a47d24d09d162139a2a51b43
brad <brad@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-02-03 15:49:01 +0000

    DbAuthPlugin:
    initial checkin of DbAuthPlugin
    
    git-svn-id: https://trac-hacks.org/svn@428 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ dbauthplugin/0.9/dbauth/env.py
@@ -0,0 +1,29 @@
+# -*- coding: iso8859-1 -*-
+#
+# Copyright (C) 2005-2006 The Sankaty Group, Inc.
+# Copyright (C) 2005-2006 Brad Anderson <brad@dsource.org>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution. The terms
+# are also available at http://trac.edgewall.com/license.html.
+#
+# This software consists of voluntary contributions made by many
+# individuals. For the exact contribution history, see the revision
+# history and logs, available at http://projects.edgewall.com/trac/.
+#
+# Author: Brad Anderson <brad@dsource.org>
+
+from trac.db.sqlite_backend import *
+
+def get_db(env):
+    """Return a database connection"""
+    path = env.config.get('central', 'database')
+    return SQLiteConnection(path)
+
+def get_envname(env):
+    envroot = env.config.get('central', 'envroot')
+    if envroot[-1] != "/":
+        envroot += "/"
+    envname = env.path.replace(envroot, "")
+    return envname
