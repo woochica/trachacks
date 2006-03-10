20026b674780216bc8f1edea09a4b15945f076be
coderanger <coderanger@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-03-10 15:31:23 +0000

    TracCasPlugin:
    Initial commit.
    
    git-svn-id: https://trac-hacks.org/svn@472 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ traccasplugin/0.9/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+
+from setuptools import setup
+
+setup(
+    name = 'TracCAS',
+    version = '0.1',
+    packages = ['traccas'],
+
+    author = "Noah Kantrowitz",
+    author_email = "coderanger@yahoo.com",
+    description = "A modified authentication plugin to use the Yale CAS system.",
+    license = "BSD",
+    keywords = "trac cas authentication plugin",
+    url = "http://trac-hacks.org/wiki/TracCASPlugin",
+
+)
