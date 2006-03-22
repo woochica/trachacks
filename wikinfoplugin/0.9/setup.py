fe11ebdd061b5fc2ce0c1da221fb6aac4d717de9
coderanger <coderanger@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-03-22 18:53:22 +0000

    WikinfoPlugin:
    Switching to entry points.
    
    git-svn-id: https://trac-hacks.org/svn@541 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ wikinfoplugin/0.9/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+
+from setuptools import setup
+
+setup(
+    name = 'Wikinfo',
+    version = '0.1',
+    packages = ['wikinfo'],
+    package_data = {  },
+
+    author = "Noah Kantrowitz",
+    author_email = "coderanger@yahoo.com",
+    description = "Provides a few useful macros to get data about wiki pages",
+    license = "BSD",
+    keywords = "trac wiki info macro",
+    url = "http://trac-hacks.org/",
+
+    entry_points = {
+        'trac.plugins': [
+            'wikinfo.wikinfo = wikinfo.wikinfo'
+        ]
+    }
+)
+
