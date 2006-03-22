d60ceac8f6c4e9baff461df9a2e17df9d2ed31a1
coderanger <coderanger@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-03-22 18:41:27 +0000

    AuthFormPlugin:
    Switching to entry points.
    
    git-svn-id: https://trac-hacks.org/svn@536 7322e99d-02ea-0310-aa39-e9a107903beb

--- authformplugin/0.9/setup.py
+++ authformplugin/0.9/setup.py
@@ -1,7 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+
 from setuptools import setup
 
-PACKAGE = 'AuthForm'
-VERSION = '0.1'
+setup(
+    name = 'AuthForm',
+    version = '0.1',
+    packages = ['authform'],
+    package_data = { 'authform': ['templates/*.cs' ] },
 
-setup(name=PACKAGE, version=VERSION, packages=['authform'],
-        package_data={'authform' : ['templates/*.cs',]})
+    author = "Noah Kantrowitz",
+    author_email = "coderanger@yahoo.com",
+    description = "Implements form-based login against HTTP authentication",
+    license = "BSD",
+    keywords = "trac authentication form",
+    url = "http://trac-hacks.org/",
+
+    entry_points = {
+        'trac.plugins': [
+            'authform.web_ui = authform.web_ui'
+        ]
+    }
+)
