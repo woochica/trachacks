5b9a65a88335aa378d8ac695d34c7fbe4b85e97d
muness <muness@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-04-06 15:40:33 +0000

    git-svn-id: https://trac-hacks.org/svn@1798 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ tagsplugin/tags/0.3rc2/tractags/ticket.py
@@ -0,0 +1,84 @@
+from trac.core import *
+from tractags.api import ITaggingSystemProvider, TaggingSystem, sorted, set
+from trac.ticket.query import Query
+from trac.ticket import model
+import re
+
+class TicketTaggingSystem(TaggingSystem):
+    _keyword_split = re.compile(r'''[\w.-]+''')
+
+    def __init__(self, env):
+        self.env = env
+
+    def _ticket_tags(self, ticket):
+        return set(self._keyword_split.findall(ticket['keywords']))
+
+    def walk_tagged_names(self, names, tags, predicate):
+        db = self.env.get_db_cnx()
+        cursor = db.cursor()
+        tags = set(tags)
+        names = set(names)
+        args = []
+        sql = "SELECT id, keywords FROM ticket WHERE keywords IS NOT NULL AND keywords != ''"
+        if names:
+            sql += " AND id IN (" + ', '.join(['%s' for n in names]) + ")"
+            args += [unicode(n) for n in names]
+        if tags:
+            sql += " AND (" + ' OR '.join(["keywords LIKE %s" for t in tags]) + ")"
+            args += ['%' + t + '%' for t in tags]
+        sql += " ORDER BY id"
+        cursor.execute(sql, args)
+        for id, ttags in cursor:
+            ticket_tags = set(self._keyword_split.findall(ttags))
+            if not tags or ticket_tags.intersection(tags):
+                if predicate(id, ticket_tags):
+                    yield (id, ticket_tags)
+                
+    def get_name_tags(self, name):
+        db = self.env.get_db_cnx()
+        cursor = db.cursor()
+        cursor.execute("SELECT keywords FROM ticket WHERE id=%s AND keywords IS NOT NULL AND keywords != ''", (name,))
+        for row in cursor:
+            return set(self._keyword_split.findall(row[0]))
+
+    def add_tags(self, req, name, tags):
+        ticket = model.Ticket(self.env, name)
+        ticket_tags = self._ticket_tags(ticket)
+        ticket_tags.update(tags)
+        ticket['keywords'] = ' '.join(sorted(ticket_tags))
+        ticket.save_changes(req.authname, None)
+
+    def replace_tags(self, req, name, tags):
+        ticket = model.Ticket(self.env, name)
+        ticket['keywords'] = ' '.join(sorted(tags))
+        ticket.save_changes(req.authname, None)
+
+    def remove_tags(self, req, name, tags):
+        ticket = model.Ticket(self.env, name)
+        ticket_tags = self._ticket_tags(ticket)
+        ticket_tags.symmetric_difference_update(tags)
+        ticket['keywords'] = ' '.join(sorted(ticket_tags))
+        ticket.save_changes(req.authname, None)
+
+    def remove_all_tags(self, req, name):
+        ticket = model.Ticket(self.env, name)
+        ticket['keywords'] = ''
+        ticket.save_changes(req.authname, None)
+
+    def name_details(self, name):
+        ticket = model.Ticket(self.env, name)
+        href = self.env.href.ticket(name)
+        from trac.wiki.formatter import wiki_to_oneliner
+        return (href, wiki_to_oneliner('#%s' % name, self.env),
+                ticket.exists and ticket['summary'] or '')
+
+class TicketTags(Component):
+    """ Export a ticket tag interface, using the "keywords" field of tickets. """
+    implements(ITaggingSystemProvider)
+
+    def get_tagspaces_provided(self):
+        yield 'ticket'
+
+    def get_tagging_system(self, tagspace):
+        return TicketTaggingSystem(self.env)
+
---
+++ tagsplugin/tags/0.3rc2/tractags/wiki.py
@@ -0,0 +1,73 @@
+from trac.core import *
+from tractags.api import DefaultTaggingSystem, ITaggingSystemProvider, TagEngine
+from trac.wiki.api import IWikiChangeListener, IWikiSyntaxProvider
+from trac.util import Markup
+import re
+
+class WikiTaggingSystem(DefaultTaggingSystem):
+    """ Subclass of DefaultTaggingSystem that knows how to retrieve wiki page
+        titles. """
+    def __init__(self, env):
+        DefaultTaggingSystem.__init__(self, env, 'wiki')
+
+    def page_info(self, page):
+        from trac.wiki import model
+        """ Return tuple of (model.WikiPage, title) """
+        page = model.WikiPage(self.env, page)
+
+        title = ''
+
+        if page.exists:
+            text = page.text
+            ret = re.search('=\s+([^=]*)=',text)
+            title = ret and ret.group(1) or ''
+
+        return (page, title)
+
+    def name_details(self, name):
+        """ Return a tuple of (href, wikilink, title). eg. ("/ticket/1", "#1", "Broken links") """
+        page, title = self.page_info(name)
+        href = self.env.href.wiki(name)
+        defaults = DefaultTaggingSystem.name_details(self, name)
+        return defaults[0:2] + (title,)
+
+class WikiTags(Component):
+    """ Implement tags in the Wiki system, a tag:<tag> link resolver and
+        correctly deletes tags from Wiki pages that are removed. """
+
+    implements(ITaggingSystemProvider, IWikiChangeListener, IWikiSyntaxProvider)
+
+    # ITaggingSystemProvider methods
+    def get_tagspaces_provided(self):
+        yield 'wiki'
+
+    def get_tagging_system(self, tagspace):
+        return WikiTaggingSystem(self.env)
+
+    # IWikiChangeListener methods
+    def wiki_page_added(self, page):
+        pass
+
+    def wiki_page_changed(self, page, version, t, comment, author, ipnr):
+        pass
+
+    def wiki_page_deleted(self, page):
+        # No point having tags on a non-existent page.
+        self.env.log.debug("Removing all tags from 'wiki:%s'" % page.name)
+        TagEngine(self.env).tagspace.wiki.remove_all_tags(None, page.name)
+
+    def wiki_page_version_deleted(self, page):
+        # Wiki tags are not versioned. If they were, we'd delete them here.
+        pass
+
+    # IWikiSyntaxProvider methods
+    def get_wiki_syntax(self):
+        return []
+
+    def get_link_resolvers(self):
+        yield ('tag', self._tag_formatter)
+
+    def _tag_formatter(self, formatter, ns, target, label):
+        href, title = TagEngine(self.env).get_tag_link(target)
+        return Markup('<a href="%s" title="%s">%s</a>' % (href, title, label))
+
