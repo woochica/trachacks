160a74ab784574b182287046c3fb3807d99532c8
Team5 <Team5@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-04-07 06:20:15 +0000

    PeerReviewPlugin:
    git-svn-id: https://trac-hacks.org/svn@630 7322e99d-02ea-0310-aa39-e9a107903beb

--- peerreviewplugin/0.9/Code/codereview-plugin/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-PACKAGE = 'TracCodeReview'
-VERSION = '0.2'
-
-setup(name=PACKAGE, version=VERSION, packages=['codereview'],
-        package_data={'codereview' : ['templates/*.cs', 'htdocs/images/*.*']})
