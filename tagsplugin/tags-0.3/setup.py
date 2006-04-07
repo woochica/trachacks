3f8426e2338fef336c6c7e3822cfcbc3f34cd120
muness <muness@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-04-07 02:07:28 +0000

    Calling this 0.3.  seems stable enough...
    git-svn-id: https://trac-hacks.org/svn@1804 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ tagsplugin/tags-0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='TracTags',
+    version='0.3',
+    packages=['tractags'],
+	package_data={'tractags' : ['templates/*.cs', 'htdocs/js/*.js', 'htdocs/css/*.css']},
+    author='Muness Alrubaie',
+    url='http://dev.muness.textdriven.com/trac.cgi/wiki/tags',
+    description='Tag plugin for Trac',
+    entry_points = {'trac.plugins': ['tractags = tractags']}
+    )
