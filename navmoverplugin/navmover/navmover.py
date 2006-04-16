4d9a2572f81dcf0a69bf7cb2557ed88808008c22
athomas <athomas@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-04-16 08:12:08 +0000

    NavMoverPlugin:
    Added ability to add custom nav items.
    
    git-svn-id: https://trac-hacks.org/svn@667 7322e99d-02ea-0310-aa39-e9a107903beb

--- navmoverplugin/navmover/navmover.py
+++ navmoverplugin/navmover/navmover.py
@@ -1,5 +1,6 @@
 from trac.core import *
 from trac.web.chrome import INavigationContributor, ITemplateProvider, add_stylesheet
+from trac.util import Markup
 
 class NavMover(Component):
     """ Moves meta navigation items into the main navigation bar. """
@@ -22,6 +23,7 @@ class NavMover(Component):
             move_from = 'metanav'
         else:
             move_from = 'mainnav'
+
         # Hide the appropriate nav block
         add_stylesheet(req, 'navmover/css/hide_%s.css' % move_from)
         # Find meta nav items
@@ -30,9 +32,17 @@ class NavMover(Component):
             if contributor == self: continue
             for item in contributor.get_navigation_items(req):
                 if (not move_items or item[1] in move_items) and item[0] == move_from \
-                        and not unicode(item[2]).startswith('logged in as'):
+                        and unicode(item[2]).startswith('<a '):
                     meta_items.append((move_to, item[1], item[2]))
 
+        # Add custom items
+        for item, title in [o for o in self.env.config.options('navmover')
+                            if '.' not in o[0] and o[0] not in ('move_items', 'move_to')]:
+            url = self.env.config.get('navmover', '%s.url' % item)
+            perm = self.env.config.get('navmover', '%s.permission' % item)
+            if perm and not req.perm.has_permission(perm):
+                continue
+            meta_items.append((move_to, item, Markup('<a href="%s">%s</a>' % (url, title))))
         return meta_items
 
     # ITemplateProvider methods
