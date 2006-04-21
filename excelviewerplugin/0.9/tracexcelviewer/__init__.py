3fa69742e10e3db0f414c7da91bee045027dbf50
cmlenz <cmlenz@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-04-21 12:18:58 +0000

    ExcelViewerPlugin:
    Copy for Trac 0.9 branch.
    
    git-svn-id: https://trac-hacks.org/svn@683 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ excelviewerplugin/0.9/tracexcelviewer/__init__.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2006 Christopher Lenz <cmlenz@gmx.de>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution. The terms
+# are also available at http://trac.edgewall.com/license.html.
+
+from trac.core import *
+from trac.mimeview.api import IHTMLPreviewRenderer
+from trac.util import escape
+
+__all__ = ['ExcelRenderer']
+
+try:
+    import xlrd
+    have_xrld = True
+except ImportError:
+    have_xrld = False
+
+
+class ExcelRenderer(Component):
+    """Syntax highlighting using GNU Enscript."""
+
+    implements(IHTMLPreviewRenderer)
+
+    # IHTMLPreviewRenderer methods
+
+    def get_quality_ratio(self, mimetype):
+        if have_xrld and mimetype == 'application/vnd.ms-excel':
+            return 2
+        return 0
+
+    def render(self, req, mimetype, content, filename=None, url=None):
+        if hasattr(content, 'read'):
+            content = content.read()
+        book = xlrd.open_workbook(file_contents=content)
+        buf = []
+        for sheet_name in book.sheet_names():
+            sheet = book.sheet_by_name(sheet_name)
+            buf.append(u'<table class="listing"><caption>%s</caption>\n' %
+                       escape(sheet.name))
+            buf.append(u'<tbody>')
+            for ridx in range(sheet.nrows):
+                row = sheet.row(ridx)
+                if not filter(lambda cell: unicode(cell.value), row):
+                    continue
+                buf.append(u'<tr class="%s">' % (ridx % 2 and 'odd' or 'even'))
+                for cidx, cell in enumerate(row):
+                    buf.append(u'<td>%s</td>' % sheet.cell_value(ridx, cidx))
+                buf.append(u'</tr>\n')
+            buf.append(u'</tbody>')
+            buf.append(u'</table>\n')
+        return u''.join(buf)
