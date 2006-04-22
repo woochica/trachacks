4bbdcbbe661ac44f06938ea5afd5464c3df16f03
pkropf <pkropf@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-04-22 00:43:09 +0000

    BuildbotPlugin:
    Removed the extra references to graphviz. Ah, the perils of copying files...
    
    git-svn-id: https://trac-hacks.org/svn@693 7322e99d-02ea-0310-aa39-e9a107903beb

--- buildbotplugin/branches/v0.1/setup.py
+++ buildbotplugin/branches/v0.1/setup.py
@@ -4,7 +4,7 @@ $HeadURL$
 
 Copyright (c) 2006 Peter Kropf. All rights reserved.
 
-Python egg setup file for the graphviz trac wiki processor.
+Python egg setup file for the buildbot trac wiki processor.
 """
 
 
@@ -31,10 +31,8 @@ setup(name=PACKAGE,
       keywords = "trac buildbot",
       url = "http://trac-hacks.swapoff.org/wiki/BuildbotPlugin",
       description = "Buildbot plugin for Trac",
-      long_description = """The graphviz wiki processor is a plugin for Trac that allows the the
-dynamic generation of diagrams by the various graphviz programs. The
-text of a wiki page can contain the source text for graphviz and the
-web browser will show the resulting image.""",
+      long_description = """The buildbot plugin for Trac provides access
+to buildbot master and slave information from within a Trac environment.""",
     license = """Copyright (C) 2006 Peter Kropf
 All rights reserved.
 
