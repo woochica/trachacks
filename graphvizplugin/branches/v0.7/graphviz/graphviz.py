53c10fc4cce1b8d50e0685542ec5128363196ed7
pkropf <pkropf@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-05-08 03:58:05 +0000

    GraphvizPlugin:
    Created the v0.7 release branch.
    
    git-svn-id: https://trac-hacks.org/svn@751 7322e99d-02ea-0310-aa39-e9a107903beb

--- graphvizplugin/branches/v0.7/graphviz/graphviz.py
+++ graphvizplugin/branches/v0.7/graphviz/graphviz.py
@@ -13,7 +13,7 @@ __revision__  = '$LastChangedRevision$'
 __id__        = '$Id$'
 __headurl__   = '$HeadURL$'
 __docformat__ = 'restructuredtext'
-__version__   = '0.6.4'
+__version__   = '0.6.3'
 
 
 try:
