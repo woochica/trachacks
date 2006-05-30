c48a54a8c835dd7b1b8a7669597c008a934622c3
coderanger <coderanger@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-05-30 17:42:49 +0000

    TracRewritePlugin:
     * Lock out local rewrites
     * Force rule ordering
    
    git-svn-id: https://trac-hacks.org/svn@795 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracrewriteplugin/0.9/tracrewrite/web_ui.py
+++ tracrewriteplugin/0.9/tracrewrite/web_ui.py
@@ -30,37 +30,48 @@ class RewritePlugin(Component):
         """Note: I am loading and compiling all regexs here,
         so any changes to the config will require a reload."""
         
-        self.rewrites = {}
+        self.rewrites = []
         # Config format: tag = /match/rewrite/[options]
         # Data format: { tag: [ match, rewrite, options ], ... }
         for k,v in self.config.options('rewrites'):
             md = config_re.match(v)
             if md:
+                tag = int(k.strip())
                 try:
-                    match = re.compile(md.group('match'))
+                    delim = md.group('delim')
+                    match_pat = re.sub('\\'+delim,delim,md.group('match'))
+                    match = re.compile(match_pat)                                        
                 except re.error:
-                    self.log.warn("TracRewrite: Unable to compile pattern '%s'"%md.group(1))
+                    self.log.warn("TracRewrite: Unable to compile pattern '%s'"%md.group('match'))
                     continue
                 rewrite = md.group('rewrite')
-                options = md.group('opts') or 'l'
+                options = md.group('opts') or 'r'
                 self.log.debug("TracRewrite: Loaded rewrite ('%s', '%s', %s')"%( match.pattern, rewrite, options.lower() ) )
-                self.rewrites[k.strip()] = ( match, rewrite, options.lower() )
+                self.rewrites.append( (tag, match, rewrite, options.lower()) )
             else:
                 self.log.warn("TracRewrite: Unable to parse value '%s'"%v)
+        self.rewrites.sort(lambda a,b: cmp(a[0],b[0]))
+        
+        # Generate tag location cache
+        i = 0
+        self.tag_index = {}
+        for t,_,_,_ in self.rewrites:
+            self.tag_index[t] = i
+            i += 1
                 
     # IRequestHandler methods
     def match_request(self, req):
         path = req.path_info
-        for k,v in self.rewrites.iteritems():
-            md = v[0].match(path)
+        for t,m,_,_ in self.rewrites:
+            md = m.match(path)
             if md:
-                req.tag = k
+                req.tag = t
                 req.md = md
                 return True
         return False
         
     def process_request(self, req):
-        _, rewrite, options = self.rewrites[req.tag]
+        _, _, rewrite, options = self.rewrites[self.tag_index[req.tag]]
         try:
             new_path = req.md.expand(rewrite)
         except re.error:
@@ -71,6 +82,7 @@ class RewritePlugin(Component):
         elif 'p' in options:
             raise RewriteError, "TracRewrite doesn't support proxying yet. Sorry"
         elif 'l' in options:
+            raise RewriteError, "Internal rewrites don't actually work yet. Check back after 0.10 get finalized a bit more"
             dispatch_request(new_path, req, self.env)
             return False # This indicates that a response has already been sent
         else:
