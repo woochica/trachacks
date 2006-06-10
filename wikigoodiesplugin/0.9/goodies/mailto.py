2e40b86a6ce6f0850ec0805bc2723586a2205052
cboos <cboos@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-06-10 14:06:07 +0000

    WikiGoodiesPlugin:
    Compatibility fix for trac >= 0.9.3. Closes #428.
    
    git-svn-id: https://trac-hacks.org/svn@840 7322e99d-02ea-0310-aa39-e9a107903beb

--- wikigoodiesplugin/0.9/goodies/mailto.py
+++ wikigoodiesplugin/0.9/goodies/mailto.py
@@ -10,8 +10,14 @@
 # Author: Christian Boos <cboos@neuf.fr>
 
 from trac.core import implements, Component
-from trac.util.markup import html
 from trac.wiki import IWikiSyntaxProvider
+
+try:
+    from trac.util.markup import html
+    trac_0_9 = False
+except ImportError:
+    trac_0_9 = True
+    from trac.util import escape
 
 
 class MailToLink(Component):
@@ -20,9 +26,14 @@ class MailToLink(Component):
 
     # IWikiSyntaxProvider methods
 
-    def get_wiki_syntax(self):
-        yield (r"<[^@]+@[^>]+>", 
-               lambda x, y, z: html.A(y, href="mailto:%s" % y[1:-1]))
+    if trac_0_9:
+        def get_wiki_syntax(self):
+            yield (r"<[^@]+@[^>]+>", lambda x, y, z:
+                   '<a href="%s">%s</a>' % ("mailto:%s" % y[1:-1], escape(y)))
+    else:
+        def get_wiki_syntax(self):
+            yield (r"<[^@]+@[^>]+>", 
+                   lambda x, y, z: html.A(y, href="mailto:%s" % y[1:-1]))
 
     def get_link_resolvers(self):
         return []
