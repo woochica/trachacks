3353316fc32f1486e008a178612d1e5f862d0f51
coderanger <coderanger@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-06-13 06:20:58 +0000

    CtxtnavAddPlugin:
    More setup.py files, yay!
    
    git-svn-id: https://trac-hacks.org/svn@851 7322e99d-02ea-0310-aa39-e9a107903beb

--- ctxtnavaddplugin/0.10/setup.py
+++ ctxtnavaddplugin/0.10/setup.py
@@ -4,23 +4,23 @@
 from setuptools import setup
 
 setup(
-    name = 'CtxtnavAdd',
-    version = '0.01',
+    name = 'TracCtxtnavAdd',
+    version = '1.0',
     packages = ['ctxtnavadd'],
-    package_data={
-        'ctxtnavadd' : [ 'templates/*.cs' , 'htdocs/js/*.js' ],
-    },
-
+    package_data={ 'ctxtnavadd' : [ 'templates/*.cs' , 'htdocs/js/*.js' ] },
     author = "Noah Kantrowitz",
     author_email = "coderanger@yahoo.com",
     description = "Allow adding to the ctxtnav bar of other plugins.",
+    long_description = "Exposes an extension point interface for manipulating the ctxtnav bar (the little bar below the main navigation bar) of other plugins",
     license = "BSD",
-    keywords = "ctxtnav add ",
-    url = "http://trac-hacks.org",
+    keywords = "trac plugin ctxtnav",
+    url = "http://trac-hacks.org/wiki/CtxtnavAddPlugin",
 
     entry_points = {
         'trac.plugins': [
             'ctxtnavadd.web_ui = ctxtnavadd.web_ui',
         ],
-    }
+    },
+
+    install_requires = [ 'trac==dev' ],
 )
