d7642e14b445e03014eeb133ed228328127a357a
sambloomquist <sambloomquist@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-06-14 15:51:12 +0000

    ScrumBurndownPlugin:
    git-svn-id: https://trac-hacks.org/svn@867 7322e99d-02ea-0310-aa39-e9a107903beb

--- scrumburndownplugin/burndown/pycron_burndown_job.py
+++ scrumburndownplugin/burndown/pycron_burndown_job.py
@@ -9,3 +9,25 @@
 
 import time
 from trac.core import *
+from trac.util import format_datetime, http_date
+
+    def display_rss(self, req, query):
+        query.verbose = True
+        db = self.env.get_db_cnx()
+        results = query.execute(db)
+        for result in results:
+            result['href'] = self.env.abs_href.ticket(result['id'])
+            if result['reporter'].find('@') == -1:
+                result['reporter'] = ''
+            if result['description']:
+                # str() cancels out the Markup() returned by wiki_to_html
+                descr = wiki_to_html(result['description'], self.env, req, db,
+                                     absurls=True)
+                result['description'] = str(descr)
+            if result['time']:
+                result['time'] = http_date(result['time'])
+        req.hdf['query.results'] = results
+        req.hdf['query.href'] = self.env.abs_href.query(group=query.group,
+                groupdesc=query.groupdesc and 1 or None,
+                verbose=query.verbose and 1 or None,
+                **query.constraints)
