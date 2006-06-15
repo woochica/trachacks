65ff2fc39c21a934d7b8172f10598caeb5a82e84
sambloomquist <sambloomquist@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-06-15 20:11:51 +0000

    ScrumBurndownPlugin:
    git-svn-id: https://trac-hacks.org/svn@878 7322e99d-02ea-0310-aa39-e9a107903beb

--- scrumburndownplugin/burndown/templates/burndown.cs
+++ scrumburndownplugin/burndown/templates/burndown.cs
@@ -5,6 +5,17 @@
  <h1>Burndown Chart</h1>
 </div>
 
+<form action="<?cs var:burndown.href ?>" method="post">
+    <select id="milestone" name="milestone">
+        <option></option>
+        <?cs /if ?>
+            <?cs each:option = field.options ?><option<?cs if:option == newticket[name(field)] ?> selected="selected"<?cs /if ?>>
+                <?cs var:option ?>
+            </option>
+        <?cs /each ?>
+    </select>
+</form>
+
 <!-- graph code begins here-->
 <script src="/cgi-bin/trac.cgi/chrome/hw/js/line.js" type="text/javascript"></script>
 <script src="/cgi-bin/trac.cgi/chrome/hw/js/wz_jsgraphics.js" type="text/javascript"></script>
@@ -35,32 +46,8 @@ g.add('19', 125);
 g.add('20', 125);
 g.add('21', 135);
 g.add('22', 125);
-g.add('23', 145);
-g.add('24', 0);
-g.add('25', 175);
-g.add('26', 130);
-g.add('27', 150);
-g.add('28', 175);
-g.add('29', 205);
-g.add('30', 125);
-g.add('31', 125);
-g.add('32', 135);
-g.add('33', 125);
-g.add('34', 145);
-g.add('35', 0);
-g.add('36', 175);
-g.add('37', 130);
-g.add('38', 150);
-g.add('39', 175);
-g.add('40', 205);
-g.add('41', 125);
-g.add('42', 125);
-g.add('43', 135);
-g.add('44', 125);
 
 //If called without a height parameter, defaults to 250
-//g.render("myCanvas", "test graph");
-
 g.render("myCanvas", "test graph", 400);
 
 </script>
