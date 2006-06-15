8f568f9915b9aa6578f71b475a0142675dd4c823
sambloomquist <sambloomquist@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-06-15 19:43:22 +0000

    ScrumBurndownPlugin:
    Got a basic chart drawing with the wz_jsgraphics library from http://www.sbmkpm.com/
    
    git-svn-id: https://trac-hacks.org/svn@873 7322e99d-02ea-0310-aa39-e9a107903beb

--- scrumburndownplugin/burndown/templates/burndown.cs
+++ scrumburndownplugin/burndown/templates/burndown.cs
@@ -1,21 +1,69 @@
-<?cs include "burndown_header.cs" ?>
+<?cs include "header.cs" ?>
 <?cs include "macros.cs" ?>
 
-<script type="text/javascript">
-    function drawGraph() {
-        var layout = new PlotKit.Layout("bar", {});
-        layout.addDataset("sqrt", [[0, 0], [1, 1], [2, 1.414], [3, 1.73], [4, 2]]);
-        layout.evaluate();
-        var canvas = MochiKit.DOM.getElement("graph");
-        var plotter = new PlotKit.SweetCanvasRenderer(canvas, layout, {});
-        plotter.render();
-    }
-    MochiKit.DOM.addLoadEvent(drawGraph);
-</script>
-
 <div id="content" class="burndown">
- <h1>Kip Dynamite</h1>
- <img src="<?cs var:chrome.href ?>/hw/images/kip.jpg">
+ <h1>Burndown Chart</h1>
 </div>
+
+<!-- graph code begins here-->
+<script src="/cgi-bin/trac.cgi/chrome/hw/js/line.js" type="text/javascript"></script>
+<script src="/cgi-bin/trac.cgi/chrome/hw/js/wz_jsgraphics.js" type="text/javascript"></script>
+
+<div id="myCanvas" style="overflow: auto; position:relative;height:480px;width:640px;"></div>
+
+<script>
+var g = new line_graph();
+g.add('1', 145);
+g.add('2', 0);
+g.add('3', 175);
+g.add('4', 130);
+g.add('5', 150);
+g.add('6', 175);
+g.add('7', 205);
+g.add('8', 125);
+g.add('9', 125);
+g.add('10', 135);
+g.add('11', 125);
+g.add('12', 145);
+g.add('13', 0);
+g.add('14', 175);
+g.add('15', 130);
+g.add('16', 150);
+g.add('17', 175);
+g.add('18', 205);
+g.add('19', 125);
+g.add('20', 125);
+g.add('21', 135);
+g.add('22', 125);
+g.add('23', 145);
+g.add('24', 0);
+g.add('25', 175);
+g.add('26', 130);
+g.add('27', 150);
+g.add('28', 175);
+g.add('29', 205);
+g.add('30', 125);
+g.add('31', 125);
+g.add('32', 135);
+g.add('33', 125);
+g.add('34', 145);
+g.add('35', 0);
+g.add('36', 175);
+g.add('37', 130);
+g.add('38', 150);
+g.add('39', 175);
+g.add('40', 205);
+g.add('41', 125);
+g.add('42', 125);
+g.add('43', 135);
+g.add('44', 125);
+
+//If called without a height parameter, defaults to 250
+//g.render("myCanvas", "test graph");
+
+g.render("myCanvas", "test graph", 400);
+
+</script>
+<!-- graph code ends here-->
 
 <?cs include "footer.cs" ?>
--- scrumburndownplugin/setup.py
+++ scrumburndownplugin/setup.py
@@ -6,6 +6,6 @@ VERSION = '0.1'
 setup(name=PACKAGE,
       version=VERSION,
       packages=['burndown'],
-      package_data={'burndown' : ['htdocs/css/*.css', 'htdocs/images/*.jpg' ,'templates/*.cs']},
+      package_data={'burndown' : ['htdocs/js/*.js', 'htdocs/css/*.css', 'htdocs/images/*.jpg' ,'templates/*.cs']},
       entry_points={'trac.plugins': '%s = burndown' % PACKAGE},
 )
