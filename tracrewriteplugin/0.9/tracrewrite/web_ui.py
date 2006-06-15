c686463ce09f3f4f3b18ba7a5e5bee245949ce8c
coderanger <coderanger@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-06-15 18:54:16 +0000

    TracRewritePlugin:
    Moving development to a new machine.
    
    git-svn-id: https://trac-hacks.org/svn@872 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracrewriteplugin/0.9/tracrewrite/web_ui.py
+++ tracrewriteplugin/0.9/tracrewrite/web_ui.py
@@ -8,13 +8,11 @@ import re
 
 config_re = re.compile("""^
                           \s*
-                          (?P<delim>\S)      # Opening delim
-                          (?P<match>.*?)     # Match RE
-                          (?<!\\\)(?P=delim) # Middle delim
-                          (?P<rewrite>.*?)   # Rewrite string
-                          (?<!\\\)(?P=delim) # End delim
-                          \s*
-                          (?P<opts>\S*)      # Possible options
+                          (?P<match>\S*?)        # Match RE
+                          \s+
+                          (?P<rewrite>\S*?)      # Rewrite string
+                          \s+
+                          (?:\[(?P<opts>\S*)\])? # Possible options
                           \s* 
                           $""",re.X)
 
@@ -27,40 +25,12 @@ class RewritePlugin(Component):
     implements(IRequestHandler)
     
     def __init__(self):
-        """Note: I am loading and compiling all regexs here,
-        so any changes to the config will require a reload."""
-        
-        self.rewrites = []
-        # Config format: tag = /match/rewrite/[options]
-        # Data format: { tag: [ match, rewrite, options ], ... }
-        for k,v in self.config.options('rewrites'):
-            md = config_re.match(v)
-            if md:
-                tag = int(k.strip())
-                try:
-                    delim = md.group('delim')
-                    match_pat = re.sub('\\'+delim,delim,md.group('match'))
-                    match = re.compile(match_pat)                                        
-                except re.error:
-                    self.log.warn("TracRewrite: Unable to compile pattern '%s'"%md.group('match'))
-                    continue
-                rewrite = md.group('rewrite')
-                options = md.group('opts') or 'r'
-                self.log.debug("TracRewrite: Loaded rewrite ('%s', '%s', %s')"%( match.pattern, rewrite, options.lower() ) )
-                self.rewrites.append( (tag, match, rewrite, options.lower()) )
-            else:
-                self.log.warn("TracRewrite: Unable to parse value '%s'"%v)
-        self.rewrites.sort(lambda a,b: cmp(a[0],b[0]))
-        
-        # Generate tag location cache
-        i = 0
-        self.tag_index = {}
-        for t,_,_,_ in self.rewrites:
-            self.tag_index[t] = i
-            i += 1
+        self.mtime = 0
+        self._load_config()
                 
     # IRequestHandler methods
     def match_request(self, req):
+        self._load_config_if_needed()
         path = req.path_info
         for t,m,_,_ in self.rewrites:
             md = m.match(path)
@@ -79,8 +49,8 @@ class RewritePlugin(Component):
         self.log.debug("TracRewrite: New path is '%s'"%new_path)
         if 'r' in options:
             req.redirect(new_path)
-        elif 'p' in options:
-            raise RewriteError, "TracRewrite doesn't support proxying yet. Sorry"
+        elif 'n' in options:
+            pass
         elif 'l' in options:
             raise RewriteError, "Internal rewrites don't actually work yet. Check back after 0.10 get finalized a bit more"
             dispatch_request(new_path, req, self.env)
@@ -88,3 +58,46 @@ class RewritePlugin(Component):
         else:
             raise RewriteError, "No valid options found in '%s'. Please specify a valid rewrite type."%options
                 
+
+    def _load_config(self):
+        """(Re)load config from trac.ini"""        
+        self.rewrites = []
+        # Config format: tag = /match/rewrite/[options]
+        # Data format: { tag: [ match, rewrite, options ], ... }
+        for k,v in self.config.options('rewrites'):
+            md = config_re.match(v)
+            if md:
+                tag = k.strip()
+                try:
+                    match = re.compile(md.group('match'))
+                except re.error:
+                    self.log.warn("TracRewrite: Unable to compile pattern '%s'"%md.group('match'))
+                    continue
+                rewrite = md.group('rewrite')
+                opt_string = md.group('opts') or 'r'
+                options = []
+                for opt in opt_string.split(','):
+                    n = opt.split('=')
+                    if len(n) == 1:
+                        n = (n, None)
+                    options.append(n[0:2])
+                self.log.debug("TracRewrite: Loaded rewrite ('%s', '%s', %s')"%( match.pattern, rewrite, options ) )
+                self.rewrites.append( (tag, match, rewrite, options) )
+            else:
+                self.log.warn("TracRewrite: Unable to parse value '%s'"%v)
+        self.rewrites.sort(lambda a,b: cmp(a[0],b[0]))
+        
+        # Generate tag location cache
+        i = 0
+        self.tag_index = {}
+        for t,_,_,_ in self.rewrites:
+            self.tag_index[t] = i
+            i += 1
+            
+    def _load_config_if_needed(self):
+        """Reload the config only if needed."""
+        config_mtime = max(self.config._lastmtime,self.config._lastsitemtime)
+        if self.mtime < config_mtime:
+            self.log.info('TracRewrite: Config mtime is newer than ours; reloading config.')
+            self._load_config()
+            self.mtime = config_mtime
