cf49b751f8244e2ebafae2ac935df60274594714
coderanger <coderanger@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-06-17 15:40:39 +0000

    CtxtnavAddPlugin:
    Same fix as on the 0.10 branch.
    
    git-svn-id: https://trac-hacks.org/svn@903 7322e99d-02ea-0310-aa39-e9a107903beb

--- ctxtnavaddplugin/0.9/setup.py
+++ ctxtnavaddplugin/0.9/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name = 'TracCtxtnavAdd',
-    version = '0.1',
+    version = '0.1-r1',
     packages = ['ctxtnavadd'],
     package_data={ 'ctxtnavadd' : [ 'templates/*.cs' , 'htdocs/js/*.js' ] },
     author = "Noah Kantrowitz",
