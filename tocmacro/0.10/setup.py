c8205b97efd38f111100a29d90583023dfa3e6d6
coderanger <coderanger@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-06-20 14:50:58 +0000

    TocMacro:
    Add more fields for PyPI.
    
    git-svn-id: https://trac-hacks.org/svn@928 7322e99d-02ea-0310-aa39-e9a107903beb

--- tocmacro/0.10/setup.py
+++ tocmacro/0.10/setup.py
@@ -4,17 +4,21 @@
 from setuptools import setup
 
 setup(
-    name = 'Toc',
+    name = 'TracTocMacro',
     version = '1.0',
     packages = ['tractoc'],
     #package_data = { 'toc': ['templates/*.cs','htdocs/*' ] },
 
     author = "Alec Thomas",
     #author_email = "",
+    maintainer = "Noah Kantrowitz",
+    maintainer_email = "coderanger@yahoo.com",
     description = "A macro to create tables of contents.",
+    long_description = """A macro to create a table of contents for either a single page, \
+                        or a collection of pages."""
     license = "BSD",
-    keywords = "trac table of content macro",
-    url = "http://trac-hacks.org/",
+    keywords = "trac plugin table of content macro",
+    url = "http://trac-hacks.org/wiki/TocMacro",
 
     entry_points = {
         'trac.plugins': [
