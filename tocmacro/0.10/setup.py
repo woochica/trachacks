90e6403dac66c0441ee61be38e87c4af436ff7e6
coderanger <coderanger@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-06-20 14:57:32 +0000

    TocMacro:
    Typo
    
    git-svn-id: https://trac-hacks.org/svn@929 7322e99d-02ea-0310-aa39-e9a107903beb

--- tocmacro/0.10/setup.py
+++ tocmacro/0.10/setup.py
@@ -15,7 +15,7 @@ setup(
     maintainer_email = "coderanger@yahoo.com",
     description = "A macro to create tables of contents.",
     long_description = """A macro to create a table of contents for either a single page, \
-                        or a collection of pages."""
+                        or a collection of pages.""",
     license = "BSD",
     keywords = "trac plugin table of content macro",
     url = "http://trac-hacks.org/wiki/TocMacro",
