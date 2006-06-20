f20ca31efb5dd3424bc7e02d88b2bc4daf39133e
coderanger <coderanger@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-06-20 15:03:54 +0000

    TocMacro:
    Making the long_description not wrap for those silly people still using 80 col terminals :P
    
    git-svn-id: https://trac-hacks.org/svn@930 7322e99d-02ea-0310-aa39-e9a107903beb

--- tocmacro/0.10/setup.py
+++ tocmacro/0.10/setup.py
@@ -14,8 +14,8 @@ setup(
     maintainer = "Noah Kantrowitz",
     maintainer_email = "coderanger@yahoo.com",
     description = "A macro to create tables of contents.",
-    long_description = """A macro to create a table of contents for either a single page, \
-                        or a collection of pages.""",
+    long_description = """A macro to create a table of contents for either a \
+                          single page, or a collection of pages.""",
     license = "BSD",
     keywords = "trac plugin table of content macro",
     url = "http://trac-hacks.org/wiki/TocMacro",
