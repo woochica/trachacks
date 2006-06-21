739993de33f8ddd932877b0c3b8c0aa2ac98a98e
coderanger <coderanger@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-06-21 16:25:11 +0000

    TracForgePlugin:
    Use {{{section.getlist()}}}.
    
    git-svn-id: https://trac-hacks.org/svn@939 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracforgeplugin/0.10/tracforge/config.py
+++ tracforgeplugin/0.10/tracforge/config.py
@@ -32,10 +32,13 @@ class DictOption(Option):
 class ListDictOption(DictOption):
     """Pretty self explanatory."""
     
-    def __init__(self, section, sep=',', doc=''):
+    def __init__(self, section, sep=',', keep_empty=True, doc=''):
         DictOption.__init__(self, section, doc)
         self.sep = sep
+        self.keep_empty = keep_empty
         
     def accessor(self, section):
-        opts = section.options()
-        return dict(map(lambda x: (x[0],x[1].split(self.sep)), list(opts)))
+        ret = {}
+        for name in section:
+            ret[name] = section.getlist(name,sep=self.sep,keep_empty=self.keep_empty)
+        return ret
