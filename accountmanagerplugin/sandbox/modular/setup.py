754888576eb3640d88a7e192d4b55ba337582dcc
coderanger <coderanger@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-07-02 18:23:43 +0000

    AccountManagerPlugin:
    Aaaaand branching completed.
    
    git-svn-id: https://trac-hacks.org/svn@989 7322e99d-02ea-0310-aa39-e9a107903beb

--- accountmanagerplugin/sandbox/modular/setup.py
+++ accountmanagerplugin/sandbox/modular/setup.py
@@ -7,7 +7,7 @@ setup(
     version = '0.1.2',
     author = 'Matthew Good',
     author_email = 'trac@matt-good.net',
-    url = 'http://trac-hacks.swapoff.org/wiki/AccountManagerPlugin',
+    url = 'http://trac-hacks.org/wiki/AccountManagerPlugin',
     description = 'User account management plugin for Trac',
 
     license = '''
@@ -17,6 +17,15 @@ can do whatever you want with this stuff. If we meet some day, and you think
 this stuff is worth it, you can buy me a beer in return.   Matthew Good''',
 
     zip_safe=True,
-    packages=['acct_mgr'],
-    package_data={'acct_mgr': ['templates/*.cs']})
+    packages=['acct_mgr', 'acct_mgr/stores'],
+    package_data={'acct_mgr': ['templates/*.cs']},
+    
+    entry_points = {
+        'trac.plugins': [
+            'acct_mgr.api = acct_mgt.api',
+            'acct_mgr.web_ui = acct_mgr.web_ui',
+            'acct_mgr.stores = acct_mgr.stores',
+        ],
+    },
+)
 
