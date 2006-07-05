a83c94d3e01af0f6a0d5e243fbd991b62d975dc6
coderanger <coderanger@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-07-05 03:07:36 +0000

    TracForgePlugin:
    Render the links correctly.
    
    git-svn-id: https://trac-hacks.org/svn@1000 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracforgeplugin/0.10/tracforge/subscriptions/changeset.py
+++ tracforgeplugin/0.10/tracforge/subscriptions/changeset.py
@@ -5,15 +5,30 @@
 from trac.core import *
 from trac.Timeline import ITimelineEventProvider
 from trac.versioncontrol.web_ui.changeset import ChangesetModule
+from trac.web.href import Href
 
 from api import ISubscribable
 from util import open_env
 from manager import SubscriptionManager
+from config import DictOption
 
-import traceback, sys
+import traceback, sys, os
+
+class PseudoRequest(object):
+    """A wrapper to redirect req.href elsewhere."""
+    # TODO: Alter req.abs_href too
+    
+    def __init__(self, req, href):
+        self.req = req
+        self.href = href
+        
+    def __getattr__(self, name):
+        return getattr(self.req, name)
 
 class ChangesetSubscribable(Component):
     """Provide remote changeset events to the local timeline."""
+    
+    env_map = DictOption('tracforge-envs', doc="Map of the form '/path/to/env = /url/base'")
     
     implements(ISubscribable, ITimelineEventProvider)
     
@@ -29,9 +44,29 @@ class ChangesetSubscribable(Component):
         if 'remote_changeset' in filters:
             for source in SubscriptionManager(self.env).get_subscriptions('changeset'):
                 env = open_env(source)
+                
+                # Try to isolate the base URL to the other Trac
+                base = ''
+                if env in self.env_map: # First use our own mapping
+                    base = self.env_map[base]
+                else:
+                    env_basename = os.path.split(env.path.rstrip(os.sep))[1]
+                    if self.config.get('intertrac', env_basename+'.url'): # Then try using an intertrac link
+                        base = self.config.get('intertrac', env_basename+'.url')
+                    else: # And finally look for a sibling path
+                        self.log.debug('ChangesetSubscribable: Falling back to sibling computation, base is %s'%env_basename)
+                        url = req.href().rstrip('/').split('/')
+                        url.pop()
+                        url.append(env_basename)
+                        base = '/'.join(url)
+
+                self.log.debug('ChangesetSubscribable: Computed base URL of %s to be %s'%(env.path, base))
+                new_href = Href(base)
+                env.href = new_href
+                new_req = PseudoRequest(req, new_href)
                 cset = ChangesetModule(env)
                 try:
-                    for ret in cset.get_timeline_events(req, start, stop, ['changeset']):
+                    for ret in cset.get_timeline_events(new_req, start, stop, ['changeset']):
                         yield ret
                 except Exception, e:
                     self.log.debug('ChangesetSubscribable: %s'%traceback.format_exc())
