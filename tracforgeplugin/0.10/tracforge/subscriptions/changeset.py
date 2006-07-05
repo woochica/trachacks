7fd590e4afa982a733da6f71a717aa60bc7ba763
coderanger <coderanger@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-07-05 00:56:26 +0000

    TracForgePlugin:
    Initial code work
    
    git-svn-id: https://trac-hacks.org/svn@999 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracforgeplugin/0.10/tracforge/subscriptions/changeset.py
+++ tracforgeplugin/0.10/tracforge/subscriptions/changeset.py
@@ -7,11 +7,32 @@ from trac.Timeline import ITimelineEventProvider
 from trac.versioncontrol.web_ui.changeset import ChangesetModule
 
 from api import ISubscribable
+from util import open_env
+from manager import SubscriptionManager
+
+import traceback, sys
 
 class ChangesetSubscribable(Component):
     """Provide remote changeset events to the local timeline."""
     
-    implements(ISubscribable)
+    implements(ISubscribable, ITimelineEventProvider)
     
+    # ISubscribable methods
     def subscribable_types(self):
         yield 'changeset'
+
+    # ITimelineEventProvider methods
+    def get_timeline_filters(self, req):
+        yield ('remote_changeset', 'External repository checkins')
+
+    def get_timeline_events(self, req, start, stop, filters):
+        if 'remote_changeset' in filters:
+            for source in SubscriptionManager(self.env).get_subscriptions('changeset'):
+                env = open_env(source)
+                cset = ChangesetModule(env)
+                try:
+                    for ret in cset.get_timeline_events(req, start, stop, ['changeset']):
+                        yield ret
+                except Exception, e:
+                    self.log.debug('ChangesetSubscribable: %s'%traceback.format_exc())
+                    raise e
