15cf7b656f10ecf7f0d1368783b88080c58e183a
coderanger <coderanger@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-08-10 20:38:02 +0000

    ModAuthAcctmgrScript:
    Making this setuptoolized.
    
    git-svn-id: https://trac-hacks.org/svn@1108 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ modauthacctmgrscript/0.9/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+
+from setuptools import setup
+
+setup(
+    name = 'TracModAuthAcctmgr',
+    version = '0.1',
+    packages = ['mod_auth_acctmgr'],
+    #package_data={ 'mod_auth_acctmgr' : [ ] },
+    author = "Noah Kantrowitz",
+    author_email = "coderanger@yahoo.com",
+    description = "mod_python authentication handler for Trac's AccountManager",
+    long_description = "Provide HTTP authentication using the AccountManager plugin through mod_python.",
+    license = "BSD",
+    keywords = "trac authentication mod_python",
+    url = "http://trac-hacks.org/wiki/ModAuthAcctmgrScript",
+    zip_safe = False,
+)
