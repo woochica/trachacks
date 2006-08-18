612ad49c225ec38aa75684f19f01ac42df92b846
coderanger <coderanger@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-08-18 03:27:08 +0000

    SimpleTicketPlugin:
    Branching for 0.10.
    
    git-svn-id: https://trac-hacks.org/svn@1147 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ simpleticketplugin/0.10/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+
+from setuptools import setup
+
+setup(
+    name = 'TracSimpleTicket',
+    version = '0.1',
+    packages = ['simpleticket'],
+
+    author = "Noah Kantrowitz",
+    author_email = "coderanger@yahoo.com",
+    description = "Restricted ticket entry form",
+    license = "BSD",
+    keywords = "trac plugin restricted ticket",
+    url = "http://trac-hacks.org/wiki/SimpleTicketPlugin",
+
+    entry_points = {
+        'trac.plugins': [
+            'simpleticket.web_ui = simpleticket.web_ui',
+        ],
+    }
+)
