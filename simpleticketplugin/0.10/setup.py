8f25ca3acf73296362636fbcfd8c0fd0ee04eded
coderanger <coderanger@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-08-18 03:35:49 +0000

    SimpleTicketPlugin:
    Up the version number.
    
    git-svn-id: https://trac-hacks.org/svn@1149 7322e99d-02ea-0310-aa39-e9a107903beb

--- simpleticketplugin/0.10/setup.py
+++ simpleticketplugin/0.10/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name = 'TracSimpleTicket',
-    version = '0.1',
+    version = '1.0',
     packages = ['simpleticket'],
 
     author = "Noah Kantrowitz",
