8a0aa47d002c7929607b035a7f4e8e4dd0169b4d
coderanger <coderanger@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-08-22 04:33:58 +0000

    SimileTimelinePlugin:
    New setup.py file that should handle all package files correctly. Ref #631.
    
    git-svn-id: https://trac-hacks.org/svn@1176 7322e99d-02ea-0310-aa39-e9a107903beb

--- similetimelineplugin/0.10/setup.py
+++ similetimelineplugin/0.10/setup.py
@@ -3,11 +3,37 @@
 
 from setuptools import setup
 
+simile_dirs = [
+    '/',
+    '/ext',
+    '/ext/geochrono',
+    '/ext/geochrono/images',
+    '/ext/geochrono/scripts',
+    '/ext/geochrono/scripts/l10n',
+    '/ext/geochrono/scripts/l10n/en',
+    '/ext/geochrono/styles',
+    '/images',
+    '/scripts',
+    '/scripts/ext',
+    '/scripts/l10n',
+    '/scripts/l10n/en',
+    '/scripts/l10n/es',
+    '/scripts/l10n/fr',
+    '/scripts/l10n/it',
+    '/scripts/l10n/ru',
+    '/scripts/l10n/se',
+    '/scripts/l10n/vi',
+    '/scripts/l10n/zh',
+    '/scripts/util',
+    '/styles'
+]
+
+
 setup(
     name = 'TracSimileTimeline',
     version = '0.1',
     packages = ['stimeline'],
-    package_data={ 'stimeline' : [ 'templates/*.cs', 'htdocs/js/simile/*' ] },
+    package_data={ 'stimeline' : [ 'templates/*.cs', 'htdocs/js/*.js' ] + [ 'htdocs/js/simile'+d+'/*.*' for d in simile_dirs ] },
     author = "Noah Kantrowitz",
     author_email = "coderanger@yahoo.com",
     description = "Enhanced timeline using the Simile Timeline.",
