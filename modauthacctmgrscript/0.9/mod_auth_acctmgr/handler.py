5f424fef9563a3241228d1d4f6983ae6305da5ba
coderanger <coderanger@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-08-31 02:17:33 +0000

    ModAuthAcctmgrScript:
    Add a global environment cache to improve performance. 
    
    git-svn-id: https://trac-hacks.org/svn@1214 7322e99d-02ea-0310-aa39-e9a107903beb

--- modauthacctmgrscript/0.9/mod_auth_acctmgr/handler.py
+++ modauthacctmgrscript/0.9/mod_auth_acctmgr/handler.py
@@ -4,20 +4,34 @@
 from mod_python import apache
 from trac.env import Environment
 
+envs = {}
+acct_mgr = None
+
 def authenhandler(req):
+    global envs, acct_mgr
     pw = req.get_basic_auth_pw()
     user = req.user
     
+    # Find the env_path from the Apache config
     options = req.get_options()
     if 'TracEnv' not in options:
         print 'Must specify a Trac environment'
         return apache.HTTP_FORBIDDEN
     env_path = options['TracEnv']
-    env = Environment(env_path)
-    
-    from acct_mgr.api import AccountManager
 
-    if AccountManager(env).check_password(user, pw):
+    # Try loading the env from the global cache, addit it if needed
+    env = None
+    if env_path not in envs:
+        env = Environment(env_path)
+        envs[env_path] = env
+    else:
+        env = envs[env_path]
+    
+    if acct_mgr is None:
+        from acct_mgr.api import AccountManager
+        acct_mgr = AccountManager
+
+    if acct_mgr(env).check_password(user, pw):
         return apache.OK
     else:
         return apache.HTTP_UNAUTHORIZED
