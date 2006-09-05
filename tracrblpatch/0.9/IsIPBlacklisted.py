cbf94b3c788545b1ed3770a92696b5d8740e1650
wmark <wmark@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-09-05 12:25:04 +0000

    TracRblPatch:
     * fixed: typo
    
    git-svn-id: https://trac-hacks.org/svn@1246 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracrblpatch/0.9/IsIPBlacklisted.py
+++ tracrblpatch/0.9/IsIPBlacklisted.py
@@ -1,4 +1,4 @@
 from trac import rbl
 
 def execute(hdf, args, env):
-	return "%s ist blacklisted: %s (%s)" % (args, rbl.is_blacklisted(args.strip(), env.config.get), env.config.get("trac", "rbl"))
+	return "%s is blacklisted: %s (%s)" % (args, rbl.is_blacklisted(args.strip(), env.config.get), env.config.get("trac", "rbl"))
