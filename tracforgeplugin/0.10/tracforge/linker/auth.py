1fdebcb6b5856090946dfd15489f8728834876fe
coderanger <coderanger@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-09-08 23:52:57 +0000

    TracForgePlugin:
    Adding cookie munger.
    
    git-svn-id: https://trac-hacks.org/svn@1258 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracforgeplugin/0.10/tracforge/linker/auth.py
+++ tracforgeplugin/0.10/tracforge/linker/auth.py
@@ -4,9 +4,11 @@
 from trac.core import *
 from trac.config import Option
 from trac.web.auth import LoginModule
+from trac.web.main import _open_environment
 from trac.env import Environment
 from trac.web.href import Href
 from trac.util.html import escape, html
+from trac.web.api import IRequestFilter
 
 class TracForgeLoginModule(LoginModule):
     """Replacement for LoginModule to slave to another environment."""
@@ -14,7 +16,7 @@ class TracForgeLoginModule(LoginModule):
     master_path = Option('tracforge', 'master_path',
                          doc='Path to master Trac')
 
-    master_env = property(lambda self: Environment(self.master_path))
+    master_env = property(lambda self: _open_environment(self.master_path))
     master_href = property(lambda self: Href(self.master_env.base_url))
             
     # INavigationContributor methods
@@ -33,3 +35,31 @@ class TracForgeLoginModule(LoginModule):
     # Internal methods
     def _get_name_for_cookie(self, req, cookie):
         return LoginModule(self.master_env)._get_name_for_cookie(req, cookie)
+
+class TracForgeCookieMunger(Component):
+    
+    uri_root = Option('tracforge', 'uri_root', default='/',
+                      doc='The smallest common URI for the whole TracForge setup')
+                      
+    implements(IRequestFilter)
+
+    def pre_process_request(self, req, handler):
+        self.log.debug('TracForgeCookieMunger: Running')
+        if req.path_info.startswith('/login'):
+            self.log.debug('TracForgeCookieMunger: Path match')
+            def my_redirect(path):
+                self.log.debug('TracForgeCookieMunger: Captured redirect!')
+                if 'trac_auth' in req.outcookie:
+                    req.outcookie['trac_auth']['path'] = self.uri_root
+                req.real_redirect(path)
+            
+            req.real_redirect = req.redirect
+            req.redirect = my_redirect
+            
+        return handler
+        
+    def post_process_request(self, req, template, content_type):
+        #if req.path_info.startswith('/login'):
+        #    if 'trac_auth' in req.outcookie:
+        #        req.outcookie['trac_auth']['path'] = self.uri_root
+        return (template, content_type)
