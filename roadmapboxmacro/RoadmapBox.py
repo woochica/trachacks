58dbd4a8fcfade8a778c449a1cacaeabbc9c459f
frido <frido@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-10-02 18:21:24 +0000

    RoadmapBoxMacro:
    forgot the comments
    
    git-svn-id: https://trac-hacks.org/svn@1330 7322e99d-02ea-0310-aa39-e9a107903beb

--- roadmapboxmacro/RoadmapBox.py
+++ roadmapboxmacro/RoadmapBox.py
@@ -1,30 +1,20 @@
 """
-Display list of ticket numbers in a box on the right side of the page.
-The purpose of this macro is show related tickets compactly.
-You can specify ticket number or report number which would be expanded
-as ticket numbers. Tickets will be displayed as sorted and uniq'ed.
 
-Example:
-{{{
-[[TicketBox(#1,#7,#31)]]               ... list of tickets
-[[TicketBox(1,7,31)]]                  ... '#' char can be omitted
-[[TicketBox({1})]]                     ... expand report result as ticket list
-[[TicketBox([report:1])]]              ... alternate format of report
-[[TicketBox([report:9?name=val])]]     ... report with dynamic variable
-[[TicketBox({1),#50,{2},100)]]         ... convination of above
-[[TicketBox(500pt,{1})]]               ... with box width as 50 point
-[[TicketBox(200px,{1})]]               ... with box width as 200 pixel
-[[TicketBox(25%,{1})]]                 ... with box width as 25%
-[[TicketBox('Different Title',#1,#2)]] ... Specify title
-[[TicketBox(\"Other Title\",#1,#2)]]     ... likewise
-[[TicketBox('%d tickets',#1,#2)]]      ... embed ticket count in title
-}}}
+Display a box with tickets concercing a certain Milestone AND Keyword.
+We use tagging to group certain keywords together based on featureset, 
+since they might fall in different components. This is ideal in the
+Roadmap display, since it shows nice summary boxes of all open/closed 
+tickets by keyword. 
 
-[wiki:TracReports#AdvancedReports:DynamicVariables Dynamic Variables] 
-is supported for report. Variables can be specified like
-{{{[report:9?PRIORITY=high&COMPONENT=ui]}}}. Of course, the special
-variable '{{{$USER}}}' is available. The login name (or 'anonymous)
-is used as $USER if not specified explicitly.
+Syntax:
+
+[[RoadmapBox(keyword:Quickies;milestone:02-corecleanup)]]
+[[RoadmapBox(keyword:Bugfixes;milestone:02-corecleanup)]]
+[[RoadmapBox(keyword:Other;milestone:02-corecleanup)]]
+
+Created by: frido.ferdinand@gmail.com
+Based on: TicketBox
+
 """
 
 ## NOTE: CSS2 defines 'max-width' but it seems that only few browser
