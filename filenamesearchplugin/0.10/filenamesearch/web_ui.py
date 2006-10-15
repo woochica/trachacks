69a455d11cb945fa4b7dcbea00efd8c9014f45a2
coderanger <coderanger@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-10-15 22:18:31 +0000

    FilenameSearchPlugin:
     * Add config options to control "wrong" matches
     * Allow for globbing against each component if the term doesn't contain '/'
    
    git-svn-id: https://trac-hacks.org/svn@1385 7322e99d-02ea-0310-aa39-e9a107903beb

--- filenamesearchplugin/0.10/filenamesearch/web_ui.py
+++ filenamesearchplugin/0.10/filenamesearch/web_ui.py
@@ -2,11 +2,17 @@
 from trac.core import *
 from trac.Search import ISearchSource
 from trac.perm import IPermissionRequestor
+from trac.config import BoolOption
 
 from fnmatch import fnmatchcase
 
 class FilenameSearchModule(Component):
     """Search source for filenames in the repository."""
+
+    check_gone = BoolOption('filenamesearch', 'check_gone', default=True,
+                            doc='Check if files are present in the youngest rev before returning.')    
+    show_gone = BoolOption('filenamesearch', 'show_gone', default=True,
+                           doc='Show files that are in the DB, but not in the youngest rev. (These may indicate moved files)')
     
     implements(ISearchSource, IPermissionRequestor)
     
@@ -21,7 +27,7 @@ class FilenameSearchModule(Component):
         db = self.env.get_db_cnx()
         cursor = db.cursor()
         repo = self.env.get_repository(req.authname)
-        #youngest_rev = repo.get_youngest_rev()        
+        youngest_rev = repo.get_youngest_rev()        
 
         cursor.execute("""SELECT max("""+db.cast('rev','int')+"""), path FROM node_change
                           WHERE node_type = %s AND change_type != 'D'
@@ -32,9 +38,20 @@ class FilenameSearchModule(Component):
         
         for term in terms:
             for rev, path in all_files:
-                if fnmatchcase(path, term):
+                match = None
+                if '/' in term:
+                    match = fnmatchcase(path, term.lstrip('/'))
+                else:
+                    match = sum([fnmatchcase(x, term) for x in path.split('/')])
+                if match:
                     cset = cset_cache.setdefault(rev, repo.get_changeset(rev))
-                    yield (req.href.browser(path, rev=rev), path, cset.date, cset.author, '')
+                    msg = ''
+                    if self.check_gone and not repo.has_node(path, youngest_rev):
+                        if self.show_gone:
+                            msg = 'Not in the youngest revision, file has possibly been moved.'
+                        else:
+                            continue
+                    yield (req.href.browser(path, rev=rev), path, cset.date, cset.author, msg)
         
     # IPermissionRequestor methods
     def get_permission_actions(self):
