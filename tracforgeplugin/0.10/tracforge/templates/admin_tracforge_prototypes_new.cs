5106dea178bac0d66982883b3090bb498bbc3552
coderanger <coderanger@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-10-29 07:48:34 +0000

    TracForgePlugin:
    Use the plugin function for both swaps.
    
    git-svn-id: https://trac-hacks.org/svn@1456 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracforgeplugin/0.10/tracforge/templates/admin_tracforge_prototypes_new.cs
+++ tracforgeplugin/0.10/tracforge/templates/admin_tracforge_prototypes_new.cs
@@ -16,14 +16,7 @@
 <?cs call:teststep(1, "one", "Foo") ?>
 <?cs call:teststep(2, "two", "Bar") ?>
 <?cs call:teststep(3, "three", "Baz") ?>
-
-<style type="text/css">
-.transferer {
-    border: 1px solid #000;
-    background-color: #ffc;
-}
-</style>    
-   
+<?cs call:teststep(4, "four", "Blah") ?>
 
 <script type="text/javascript">
     jQuery.fn.animatedSwap = function(next, duration) {
@@ -80,7 +73,7 @@
 
     $(function() {
         $("img[@alt=Remove]").click(function() { 
-            $(this).parents(".step").animate({height:'hide',opacity:'hide'},'slow', function() {
+            $(this).parents(".step").animate({height:'hide',opacity:'hide'}, 400, function() {
                 $(this).remove()
             });
         });
@@ -89,62 +82,14 @@
             var me = $(this).parents(".step");
             var prev = $(me).prev(".step");
             if(prev.length == 0) { return; } // Top item
-            prev.animatedSwap(me, 500);
+            prev.animatedSwap(me, 400);
         });
 
         $("img[@alt=Down]").click(function() {
             var me = $(this).parents(".step");
             var next = $(me).next(".step");
             if(next.length == 0) { return; } // Bottom item
-            
-            // Find positions and sizes
-            var my_pos = jQuery.extend(
-                jQuery.iUtil.getPosition(me.get(0)),
-                jQuery.iUtil.getSize(me.get(0))
-            );
-            var next_pos = jQuery.extend(
-                jQuery.iUtil.getPosition(next.get(0)),
-                jQuery.iUtil.getSize(next.get(0))
-            );
-            
-            var me_copy = $(me).clone().css('display', 'block')
-                                       .css('position', 'absolute') 
-                                       .css('width', my_pos.w + "px")
-                                       .css("height", my_pos.h + "px")
-                                       .css("left", my_pos.x + "px")
-                                       .css("top", my_pos.y - (my_pos.wb-my_pos.w) + "px")
-                                       .appendTo('body');
-
-            var next_copy = $(next).clone().css('display', 'block')
-                                           .css('position', 'absolute') 
-                                           .css('width', next_pos.w + "px")
-                                           .css("height", next_pos.h + "px")
-                                           .css("left", next_pos.x +"px")
-                                           .css("top", next_pos.y - (next_pos.wb-next_pos.w) + "px")
-                                           .appendTo('body');
-                                       
-            
-            me.css('visibility', 'hidden');
-            next.css('visibility', 'hidden');
-            
-            jQuery['swapper_store_'+next[0].id] = {
-                me: me,
-                next: next,
-                me_copy: me_copy,
-                next_copy: next_copy
-            };
-            
-            me_copy.animate({top:(1.0*next_copy.top().replace("px",""))}, 500);
-            next_copy.animate({top:(1.0*me_copy.top().replace("px",""))}, 500, function() {
-                var store = jQuery['swapper_store_'+this.id];
-                store.me.css('visibility', 'visible');
-                store.next.css('visibility', 'visible');
-                store.next.after(store.me);
-                store.me_copy.hide().remove();
-                store.next_copy.fadeOut(1, function() {
-                    this.parentNode.removeChild(this);
-                });
-            });
+            me.animatedSwap(next, 400);            
         });
     });
 </script>
