f316875edaf3dcdf8584bd6bcc0ef872eb32af9f
coderanger <coderanger@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-11-13 12:59:02 +0000

    HierWikiPlugin:
     * Relative links in the wiki.
     * Hierarchy enforcement.
    
    git-svn-id: https://trac-hacks.org/svn@1552 7322e99d-02ea-0310-aa39-e9a107903beb

--- hierwikiplugin/0.10/setup.py
+++ hierwikiplugin/0.10/setup.py
@@ -5,19 +5,24 @@ from setuptools import setup
 
 setup(
     name = 'TracHierWiki',
-    version = '0.1-r1',
+    version = '1.0',
     packages = ['hierwiki', 'hierwiki/macros' ],
-    package_data={ 'hierwiki' : [ ] },
+    #package_data={ 'hierwiki' : [ ] },
     author = "Noah Kantrowitz",
     author_email = "coderanger@yahoo.com",
-    description = "Hierarchical wiki macros.",
-    long_description = "Various things related to using the wiki in hierarchical way.",
+    description = "Hierarchical wiki utilities for Trac.",
+    long_description = "Various things related to using the Trac wiki in hierarchical way.",
     license = "BSD",
     keywords = "trac plugin macros wiki hierarchy",
     url = "http://trac-hacks.org/wiki/HierWikiPlugin",
+    classifiers = [
+        'Framework :: Trac',
+    ],
 
     entry_points = {
         'trac.plugins': [
+            'hierwiki.filter = hierwiki.filter',
+            'hierwiki.formatter = hierwiki.formatter',
             'hierwiki.macros = hierwiki.macros',
         ],
     },
