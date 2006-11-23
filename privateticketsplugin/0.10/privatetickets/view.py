15e84d10df00074f209676da932ea98bcb1fc01c
coderanger <coderanger@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-11-23 04:35:33 +0000

    PrivateTicketsPlugin:
    Allow viewing attachments.
    
    git-svn-id: https://trac-hacks.org/svn@1591 7322e99d-02ea-0310-aa39-e9a107903beb

--- privateticketsplugin/0.10/privatetickets/view.py
+++ privateticketsplugin/0.10/privatetickets/view.py
@@ -4,6 +4,7 @@ from trac.ticket.web_ui import TicketModule
 from trac.ticket.query import QueryModule
 from trac.Search import SearchModule
 from trac.ticket.report import ReportModule
+from trac.attachment import AttachmentModule
 
 from api import PrivateTicketsSystem
 
@@ -27,6 +28,9 @@ class PrivateTicketsViewModule(Component):
         if not req.perm.has_permission('TICKET_VIEW'):
             if TicketModule(self.env).match_request(req):
                 if PrivateTicketsSystem(self.env).check_ticket_access(req, req.args['id']):
+                    self._grant_view(req)
+            elif AttachmentModule(self.env).match_request(req):
+                if req.args['type'] == 'ticket' and PrivateTicketsSystem(self.env).check_ticket_access(req, req.args['path'].split('/')[0]):
                     self._grant_view(req)
             elif QueryModule(self.env).match_request(req):
                 req.args['DO_PRIVATETICKETS_FILTER'] = 'query'
