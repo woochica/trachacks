d797514b8b778d7231ca3276d263cae61ff0c781
coderanger <coderanger@7322e99d-02ea-0310-aa39-e9a107903beb>
2006-12-05 05:58:30 +0000

    CombineWikiPlugin:
    Starting work on the TiddlyWiki output.
    
    git-svn-id: https://trac-hacks.org/svn@1638 7322e99d-02ea-0310-aa39-e9a107903beb

--- combinewikiplugin/0.10/combinewiki/formats.py
+++ combinewikiplugin/0.10/combinewiki/formats.py
@@ -1,19 +1,24 @@
 from trac.core import *
 from trac.web.api import RequestDone
 from trac.wiki.api import WikiSystem
-from trac.wiki.formatter import wiki_to_html
+from trac.wiki.formatter import wiki_to_html, Formatter, WikiProcessor
 from trac.wiki.model import WikiPage
 from trac.mimeview.api import Mimeview
+from trac.util.text import shorten_line, to_unicode
+from trac.util.html import Markup
 
 from tempfile import mkstemp
+from StringIO import StringIO
 import os
 import re
+import time
 
 from api import ICombineWikiFormat
 
 EXCLUDE_RES = [
-    re.compile('\[\[PageOutline([^]]*)\]\]'),
-    re.compile('----(\r)?$\n^Back up: \[\[ParentWiki\]\]', re.M|re.I)
+    re.compile(r'\[\[PageOutline([^]]*)\]\]'),
+    re.compile(r'\[\[TracGuideToc([^]]*)\]\]'),
+    re.compile(r'----(\r)?$\n^Back up: \[\[ParentWiki\]\]', re.M|re.I)
 ]
 
 class PDFOutputFormat(Component):
@@ -100,4 +105,185 @@ class TiddlyWikiOutputFormat(Component):
         yield 'tiddlywiki', 'TiddlyWiki'
         
     def process_combinewiki(self, req, format, title, pages):
-        pass
+        tiddlers = []
+        #formatter = Formatter(self.env, req)
+        formatter = TiddlyWikiFormatter(self.env, req)
+
+        for name in pages:
+            tiddler = {}
+            tiddler['name'] = name
+            page = WikiPage(self.env, name)
+            tiddler['author'] = page.author
+            tiddler['modtime'] = self._convert_time(page.time)
+            page_1 = WikiPage(self.env, name, version=1)
+            tiddler['ctime'] = self._convert_time(page_1.time)
+                        
+            out = StringIO()
+            text = page.text
+            for r in EXCLUDE_RES:
+                    text = r.sub('', text)
+            formatter.format(text, out)
+            tiddler['content'] = out.getvalue()
+
+            tiddlers.append(tiddler)
+
+
+        def default_page(t, c):
+            if t not in pages:
+                tiddlers.append({
+                    'name': t,
+                    'modtime': self._convert_time(),
+                    'ctime': self._convert_time(),
+                    'content': c,
+                })
+        default_page('MainMenu', '\n'.join(pages))
+        default_page('SiteTitle', title)
+            
+        req.hdf['combinewiki.tiddlers'] = tiddlers
+        req.display('combinewiki_tiddlywiki.cs', 'text/html')
+        
+    def _convert_time(self, secs=None):
+        if secs is not None:
+            secs = int(secs)
+        return time.strftime('%Y%m%d%H%M', time.localtime(secs))
+
+class TiddlyWikiFormatter(Formatter):
+    """Converter to go from Trac wiki to TiddlyWiki markup."""
+
+    # Heading formatting    
+    def _heading_formatter(self, match, fullmatch):
+        self.close_table()
+        self.close_paragraph()
+        self.close_indentation()
+        self.close_list()
+        self.close_def_list()
+        depth, heading, anchor = self._parse_heading(match, fullmatch, False)
+        self.out.write('%s%s' %
+                       (depth*'!', heading))
+        
+    def _parse_heading(self, match, fullmatch, shorten):
+        match = match.strip()
+
+        depth = min(len(fullmatch.group('hdepth')), 5)
+        anchor = fullmatch.group('hanchor') or ''
+        heading_text = match[depth+1:-depth-1-len(anchor)]
+        out = StringIO()
+        TiddlyWikiFormatter(self.env, self.req).format(heading_text, out)
+        heading = Markup(out.getvalue().strip())
+        if anchor:
+            anchor = anchor[1:]
+        else:
+            sans_markup = heading.plaintext(keeplinebreaks=False)
+            anchor = self._anchor_re.sub('', sans_markup)
+            if not anchor or anchor[0].isdigit() or anchor[0] in '.-':
+                # an ID must start with a Name-start character in XHTML
+                anchor = 'a' + anchor # keeping 'a' for backward compat
+        i = 1
+        anchor_base = anchor
+        while anchor in self._anchors:
+            anchor = anchor_base + str(i)
+            i += 1
+        self._anchors[anchor] = True
+        if shorten:
+            heading = wiki_to_oneliner(heading_text, self.env, self.db, True,
+                                       self._absurls)
+        return (depth, heading, anchor)
+
+    # Basic formatting
+    def _bolditalic_formatter(self, match, fullmatch):
+        return self.simple_tag_handler(match, "''//", "//''")
+
+    def _bold_formatter(self, match, fullmatch):
+        return self.simple_tag_handler(match, "''", "''")
+
+    def _italic_formatter(self, match, fullmatch):
+        return self.simple_tag_handler(match, '//', '//')
+
+    def _underline_formatter(self, match, fullmatch):
+        return self.simple_tag_handler(match, '__','__')
+
+    def _strike_formatter(self, match, fullmatch):
+        return self.simple_tag_handler(match, '--', '--')
+
+    def _subscript_formatter(self, match, fullmatch):
+        return self.simple_tag_handler(match, '~~', '~~')
+
+    def _superscript_formatter(self, match, fullmatch):
+        return self.simple_tag_handler(match, '^^', '^^')
+        
+    def _inlinecode_formatter(self, match, fullmatch):
+        return '{{{' + fullmatch.group('inline') + '}}}'
+
+    def _inlinecode2_formatter(self, match, fullmatch):
+        return '{{{' + fullmatch.group('inline2') + '}}}'
+        
+    # Paragraph formatting
+    def open_paragraph(self):
+        if not self.paragraph_open:
+            self.paragraph_open = 1
+     
+    def close_paragraph(self):
+        if self.paragraph_open:
+            while self._open_tags != []:
+                self.out.write(self._open_tags.pop()[1])
+            self.out.write(os.linesep)
+            self.paragraph_open = 0
+
+    def _list_formatter(self, match, fullmatch):
+        ldepth = len(fullmatch.group('ldepth'))
+        listid = match[ldepth]
+        ldepth = ldepth//2 + 1
+        self.in_list_item = True
+        class_ = start = None
+        if listid in '-*':
+            type_ = 'ul'
+            self.out.write('%s '%('*'*ldepth))
+        else:
+            self.out.write('%s '%('#'*ldepth))
+            type_ = 'ol'
+            idx = '01iI'.find(listid)
+            if idx >= 0:
+                class_ = ('arabiczero', None, 'lowerroman', 'upperroman')[idx]
+            elif listid.isdigit():
+                start = match[ldepth:match.find('.')]
+            elif listid.islower():
+                class_ = 'loweralpha'
+            elif listid.isupper():
+                class_ = 'upperalpha'
+        #self._set_list_depth(ldepth, type_, class_, start)
+        return ''
+        
+    def handle_code_block(self, line):
+        if line.strip() == Formatter.STARTBLOCK:
+            self.in_code_block += 1
+            if self.in_code_block == 1:
+                self.code_processor = None
+                self.code_text = ''
+            else:
+                self.code_text += line + os.linesep
+                if not self.code_processor:
+                    self.code_processor = TiddlyWikiProcessor(self.env, 'default')
+        elif line.strip() == Formatter.ENDBLOCK:
+            self.in_code_block -= 1
+            if self.in_code_block == 0 and self.code_processor:
+                self.close_table()
+                self.close_paragraph()
+                self.out.write(to_unicode(self.code_processor.process(
+                    self.req, self.code_text)))
+            else:
+                self.code_text += line + os.linesep
+        elif not self.code_processor:
+            match = Formatter._processor_re.search(line)
+            if match:
+                name = match.group(1)
+                self.code_processor = TiddlyWikiProcessor(self.env, name)
+            else:
+                self.code_text += line + os.linesep 
+                self.code_processor = TiddlyWikiProcessor(self.env, 'default')
+        else:
+            self.code_text += line + os.linesep
+
+class TiddlyWikiProcessor(WikiProcessor):
+    
+    def _default_processor(self, req, text):
+        return ''.join(['{{{\n', text, '}}}\n'])
