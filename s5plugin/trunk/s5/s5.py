ad1a2880af1799e13e43d54b7b16a5f013ade72b
athomas <athomas@7322e99d-02ea-0310-aa39-e9a107903beb>
2007-01-17 12:02:10 +0000

    S5Plugin:
    Initial import.
    
    git-svn-id: https://trac-hacks.org/svn@1868 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ s5plugin/trunk/s5/s5.py
@@ -0,0 +1,125 @@
+# s5 plugin
+
+import re
+from trac.core import *
+from trac.web.chrome import ITemplateProvider, add_stylesheet
+from trac.web.main import IRequestHandler
+from trac.wiki.api import IWikiMacroProvider
+from trac.wiki.formatter import wiki_to_html
+from trac.wiki.model import WikiPage
+from trac.util import escape, Markup
+from trac.config import Option
+
+class S5Renderer(Component):
+    implements(ITemplateProvider, IRequestHandler, IWikiMacroProvider)
+
+    heading_re = re.compile(r'^==\s*(?P<slide>.*?)\s*==$|^=\s*(?P<title>.*?)\s*=$')
+    fixup_re = re.compile(r'^=(\s*.*?\s*)=$', re.S|re.M)
+    fixup_images_re = re.compile(r'\[\[Image\(([^/].*?)\)\]\]')
+
+    # IRequestHandler methods
+    def match_request(self, req):
+        match = re.match('^/s5/(.*)', req.path_info)
+        if match:
+            if match.group(1):
+                req.args['page'] = match.group(1)
+            return 1
+
+    def process_request(self, req):
+        page = req.args.get('page', None)
+        location = req.args.get('location', None)
+        theme = req.args.get('theme', 'default')
+
+        if not page:
+            raise TracError('Invalid S5 template')
+
+        page = WikiPage(self.env, name=page)
+        if not page.exists:
+            raise TracError('Invalid S5 template "%s"' % page.name)
+
+        page_text = self.fixup_images_re.sub(r'[[Image(wiki:%s:\1)]]' % page.name, page.text)
+
+        in_section = -1
+        text = ''
+        title = ''
+        title_page = ''
+        handout = ''
+        slides = []
+
+        def htmlify(text):
+            return wiki_to_html(text, self.env, req)
+
+        for line in page_text.splitlines():
+            match = self.heading_re.match(line)
+            if match:
+                # Insert accumulated text into appropiate location
+                if in_section == 1:
+                    title_page = htmlify(text)
+                elif in_section == 2:
+                    text = self.fixup_re.sub(r'\1', text)
+                    slides.append({'body': htmlify(text), 'handout': htmlify(handout)})
+
+                if match.lastgroup == 'title':
+                    title = match.group(match.lastgroup)
+                    in_section = 1
+                else:
+                    in_section = 2
+                text = ''
+
+            text += line + '\n'
+
+        if in_section == 1:
+            title_page = text
+        elif in_section == 2:
+            text = self.fixup_re.sub(r'\1', text)
+            slides.append({'body': htmlify(text), 'handout': htmlify(handout)})
+
+        req.hdf['theme'] = theme
+        req.hdf['title'] = title
+        req.hdf['location'] = location
+        req.hdf['title_page'] = title_page
+        req.hdf['slides'] = slides
+
+        return 's5.cs', None
+
+    # ITemplateProvider methods
+    def get_templates_dirs(self):
+        """
+        Return the absolute path of the directory containing the provided
+        ClearSilver templates.
+        """
+        from pkg_resources import resource_filename
+        return [resource_filename(__name__, 'templates')]
+
+    def get_htdocs_dirs(self):
+        """
+        Return a list of directories with static resources (such as style
+        sheets, images, etc.)
+
+        Each item in the list must be a `(prefix, abspath)` tuple. The
+        `prefix` part defines the path in the URL that requests to these
+        resources are prefixed with.
+
+        The `abspath` is the absolute path to the directory containing the
+        resources on the local file system.
+        """
+        from pkg_resources import resource_filename
+        return [('s5', resource_filename(__name__, 'htdocs'))]
+
+    # IWikiMacroProvider methods
+    def get_macros(self):
+        yield 'S5'
+
+    def get_macro_description(self, name):
+        return "Allow the current Wiki page to be viewed as an S5 slidewhos."""
+
+    def render_macro(self, req, name, content):
+        match = re.match('^/wiki/(.*)', req.path_info)
+        if match:
+            return Markup("""
+                <a href="%s%s">
+                    <img style="float: right;" src="%s/icon.png" title="View as presentation"/>
+                </a>
+                """% (req.href('s5', match.group(1)), content and '?' + content or '',
+                      req.href('chrome', 's5')))
+
