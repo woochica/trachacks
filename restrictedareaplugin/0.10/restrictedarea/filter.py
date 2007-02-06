54bb09ace9bff03e628b8abb8b17d6204b2cbe0a
Chriki <Chriki@7322e99d-02ea-0310-aa39-e9a107903beb>
2007-02-06 08:14:35 +0000

    RestrictedAreaPlugin:
    initial checkin of the plugin
    
    git-svn-id: https://trac-hacks.org/svn@1923 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ restrictedareaplugin/0.10/restrictedarea/filter.py
@@ -0,0 +1,31 @@
+from trac.core import *
+from trac.config import ListOption
+from trac.perm import IPermissionRequestor, PermissionError
+from trac.web.api import IRequestFilter, HTTPForbidden
+from trac.util.text import to_unicode
+
+__all__ = ['RestrictedAreaFilter']
+
+class RestrictedAreaFilter(Component):
+    """Request filter allowing only privileged users to access certain pages."""
+    
+    __action_name = 'RESTRICTED_AREA_ACCESS'
+    
+    paths = ListOption('restrictedarea', 'paths', default='/wiki/restricted',
+                doc='Paths that allow only users with the `RESTRICTED_AREA_ACCESS` privileges.')
+    
+    implements(IRequestFilter, IPermissionRequestor)
+
+    # IPermissionRequestor methods
+    def get_permission_actions(self):
+        return [self.__action_name]
+
+    # IRequestFilter methods
+    def pre_process_request(self, req, handler):
+        for path in self.paths:
+            if req.path_info.startswith(path) and not req.perm.has_permission(self.__action_name):
+                raise HTTPForbidden(to_unicode(PermissionError(self.__action_name)))
+        return handler
+
+    def post_process_request(self, req, template, content_type):
+        return template, content_type
---
+++ restrictedareaplugin/0.10/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+
+from setuptools import setup
+
+setup(
+    name = 'TracRestrictedArea',
+    version = '1.0.0',
+    packages = ['restrictedarea'],
+
+    author = "Christian Spurk, DFKI",
+    author_email = "Christian.Spurk@dfki.de",
+    description = "Allows the setup of restricted areas with access only for privileged users.",
+    license = "BSD",
+    keywords = "trac plugin restricted area access",
+    url = "http://dfki.de/~cspurk",
+    classifiers = [
+        'Framework :: Trac',
+    ],
+
+    entry_points = {
+        'trac.plugins': [
+            'restrictedarea.filter = restrictedarea.filter',
+        ]
+    }
+)
