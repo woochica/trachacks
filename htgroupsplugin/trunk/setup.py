8a1006b61540c626fc0af6a8cbaecfb0e4ba04c2
froosh <froosh@7322e99d-02ea-0310-aa39-e9a107903beb>
2007-02-17 03:14:33 +0000

    HtgroupsPlugin:
    HtgroupsPlugin: added entry_points as required to load on 0.11 and later.  Fixes #1211
    
    git-svn-id: https://trac-hacks.org/svn@1991 7322e99d-02ea-0310-aa39-e9a107903beb

--- htgroupsplugin/trunk/setup.py
+++ htgroupsplugin/trunk/setup.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: iso-8859-1 -*-
 
-from setuptools import setup
+from setuptools import setup, find_packages
+
+PACKAGE = 'TracHtgroups'
+VERSION = '0.11'
 
 setup(
-    name = 'TracHtgroups',
-    version = '0.1',
-    packages = ['htgroups'],
+    name = PACKAGE,
+    version = VERSION,
+
+    packages = find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data = True,
 
     author = "Robin Frousheger",
     author_email = "Robin@home.froosh.net",
-    description = "TracHtGroups plugin to use Apache's group file.",
+    description = "TracHtgroups plugin to use Apache's group file.",
     long_description = "The TracHtgroups plugin allows Trac to use the" \
         " same group definition file as used by AuthGroupFile or" \
         " AuthDigestGroupFile directives.  Groups listed in that file" \
@@ -18,6 +23,12 @@ setup(
     license = "Beerware",
     keywords = "trac group permission plugin",
     url = "http://www.froosh.net/projects/htgroups/",
+
+    entry_points = {
+        'trac.plugins': [
+            '%s = %s' % (PACKAGE, 'htgroups')
+        ],
+    },
 
     zip_safe = True
 )
