0dc14527e52898d9da4960e6587742594fb2d958
merlimat <merlimat@7322e99d-02ea-0310-aa39-e9a107903beb>
2007-02-22 19:20:20 +0000

    EclipseTracPlugin:
    Return the correct encoding in the DocumentProvider. 
    Fixes #500
    
    git-svn-id: https://trac-hacks.org/svn@2020 7322e99d-02ea-0310-aa39-e9a107903beb

--- eclipsetracplugin/eclipse/trunk/mm.eclipse.trac/src/mm/eclipse/trac/editors/WikiDocumentProvider.java
+++ eclipsetracplugin/eclipse/trunk/mm.eclipse.trac/src/mm/eclipse/trac/editors/WikiDocumentProvider.java
@@ -11,18 +11,32 @@ import org.eclipse.ui.editors.text.StorageDocumentProvider;
 
 public class WikiDocumentProvider extends StorageDocumentProvider
 {
+    private static final String Encoding = "utf-8";
+    
     protected IDocument createDocument( Object element ) throws CoreException
     {
         IDocument document = super.createDocument( element );
         if ( document != null )
         {
             WikiPartitionScanner scanner = new WikiPartitionScanner();
-            IDocumentPartitioner part = new FastPartitioner( scanner, scanner
-                    .getTokens() );
+            IDocumentPartitioner part = new FastPartitioner( scanner, scanner.getTokens() );
             
             part.connect( document );
             document.setDocumentPartitioner( part );
         }
+        
         return document;
     }
+    
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.editors.text.StorageDocumentProvider#getDefaultEncoding()
+     */
+    @Override
+    public String getDefaultEncoding()
+    {
+        return Encoding;
+    }
+    
 }
