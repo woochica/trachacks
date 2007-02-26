fb02cb19cb4caf49a37e8585fea37b30e0ccd2aa
ErikRose <ErikRose@7322e99d-02ea-0310-aa39-e9a107903beb>
2007-02-26 21:34:22 +0000

    WikiWygPlugin:
    Added first draft of a setup.py file. Might need more `entry_points`.
    
    git-svn-id: https://trac-hacks.org/svn@2044 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ wikiwygplugin/0.11/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+
+from setuptools import setup
+
+setup(
+    name = 'TracWikiwyg',
+    version = '1.0',
+    packages = ['wikiwyg'],
+    package_data = {'wikiwyg': ['templates/*.html', 'htdocs/*.js', 'htdocs/*.css' ]},
+    
+    author = "soloturn, Erik Rose, and Frank Wierzbicki",
+    author_email = "somebody@example.com",
+    description = "Add a WYSIWYG wiki editor to Trac.",
+    long_description = "Gives Trac a WYSIWYG editor which outputs nice WikiFormatting rather than HTML, care of the Wikiwyg library.",
+    license = "LGPL",
+    keywords = "trac plugin wiki wysiwyg wikiwyg editor",
+    url = "http://trac-hacks.org/wiki/WikiWygPlugin",
+    classifiers = [
+        'Framework :: Trac',
+    ],
+    
+    entry_points = {
+        'trac.plugins': [
+            'wikiwyg.web_ui = wikiwyg.web_ui'
+        ]
+    }
+)
