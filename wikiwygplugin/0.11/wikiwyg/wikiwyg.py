addb033a4ad29dea6c217fcebb0205e862818d4e
ErikRose <ErikRose@7322e99d-02ea-0310-aa39-e9a107903beb>
2007-02-27 20:30:57 +0000

    WikiWygPlugin:
    setup.py:
     * Added *.htc to `package_data`, because we have one of those.
    
    wikiwyg.py:
     * Now loads the Wikiwyg JS libs on every page Trac serves.
     * Replaced the deprecated add_javascript() with add_script().
     * Quit implementing INavigationController. I think TraciWyg was doing that just to publish a test page at /wikiwyg.
     * Emptied get_templates_dirs(), because we don't have any templates anymore, because we're not doing that test page.
     * Other more minor changes
    
    ClientServer.js:
     * Added this as an example of how to do async wiki saving. It's verbatim from the Wikiwyg demo.
    
    git-svn-id: https://trac-hacks.org/svn@2051 7322e99d-02ea-0310-aa39-e9a107903beb

--- wikiwygplugin/0.11/wikiwyg/wikiwyg.py
+++ wikiwygplugin/0.11/wikiwyg/wikiwyg.py
@@ -1,56 +1,35 @@
 # vim: expandtab tabstop=4
 
-# wikiwyg plugin
-
 from trac.core import *
-from trac.web.chrome import INavigationContributor, ITemplateProvider, add_stylesheet, add_javascript
-from trac.web.main import IRequestHandler
-from trac.util import escape, Markup
+from trac.web.chrome import ITemplateProvider, add_stylesheet, add_script
+from trac.web.api import IRequestFilter
 
-class UserbaseModule(Component):
-    implements(INavigationContributor, IRequestHandler,  ITemplateProvider)
+from pkg_resources import resource_filename
 
-    # INavigationContributor methods
-    def get_active_navigation_item(self, req):
-        return 'wikiwyg'
-                
-    def get_navigation_items(self, req):
-        add_stylesheet(req, 'twyg/wikiwyg.css')
-        add_javascript(req, 'twyg/Wikiwyg.js')
-        add_javascript(req, 'twyg/Toolbar.js')
-        add_javascript(req, 'twyg/Wysiwyg.js')
-        add_javascript(req, 'twyg/Wikitext.js')
-        add_javascript(req, 'twyg/Preview.js')
-        add_javascript(req, 'twyg/Trac.js')
-        yield 'mainnav', 'prepare-wikiwyg', Markup('<a href="%s">prepare wikiwyg</a>', self.env.href.wikiwyg())
-
-    # IRequestHandler methods
-    def match_request(self, req):
-        return req.path_info == '/wikiwyg'
+class TracWikiwygModule(Component):
+    implements(ITemplateProvider, IRequestFilter)
     
-    def process_request(self, req):
-        return 'wikiwyg.cs', None
-
     # ITemplateProvider methods
+    
     def get_templates_dirs(self):
-        """
-        Return the absolute path of the directory containing the provided
-        ClearSilver templates.
-        """
-        from pkg_resources import resource_filename
-        return [resource_filename(__name__, 'templates')]
-
+        return []
+    
     def get_htdocs_dirs(self):
-        """
-        Return a list of directories with static resources (such as style
-        sheets, images, etc.)
-
-        Each item in the list must be a `(prefix, abspath)` tuple. The
-        `prefix` part defines the path in the URL that requests to these
-        resources are prefixed with.
+        """Provide the static JavaScript files, CSS, etc."""
+        yield ('', resource_filename(__name__, 'htdocs'))
+    
+    
+    # IRequestFilter methods
+    
+    def pre_process_request(self, req, handler):
+        """Load Wikiwyg libs on every page.
         
-        The `abspath` is the absolute path to the directory containing the
-        resources on the local file system.
+        (We might want to get more selective eventually.)
         """
-        from pkg_resources import resource_filename
-        return [('twyg', resource_filename(__name__, 'htdocs'))]
+        for curScript in ['Wikiwyg', 'Toolbar', 'Wysiwyg', 'Wikitext', 'Preview', 'ClientServer']:
+            add_script(req, 'wikiwyg/%s.js' % curScript)
+        return handler
+    
+    # for Genshi templates
+    def post_process_request(self, req, template, data, content_type):
+        return template, data, content_type
