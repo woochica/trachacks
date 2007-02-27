4d10a5e3dd90577c8475f8ffcfd5ca9732f59a54
tonin <tonin@7322e99d-02ea-0310-aa39-e9a107903beb>
2007-02-27 18:16:05 +0000

    PageToPdfPlugin:
    Fix #1076 better default_charset.
    
    git-svn-id: https://trac-hacks.org/svn@2050 7322e99d-02ea-0310-aa39-e9a107903beb

--- pagetopdfplugin/0.10/pagetopdf/pagetopdf.py
+++ pagetopdfplugin/0.10/pagetopdf/pagetopdf.py
@@ -16,17 +16,20 @@ class PageToPDFPlugin(Component):
 
     def convert_content(self, req, input_type, source, output_type):
         hfile, hfilename = mkstemp('tracpdf')
-        codepage = self.env.config.get('trac', 'default_charset', 0)
+	# htmldoc doesn't support utf-8, we need to use some other input encoding
+	codepage = self.env.config.get('trac', 'charset', 'iso-8859-1')
         page = wiki_to_html(source, self.env, req).encode(codepage)
         page = re.sub('<img src="(?!\w+://)', '<img src="%s://%s:%d' % (req.scheme, req.server_name, req.server_port), page)
-        os.write(hfile, '<html><body>' + page + '</body></html>')
+        self.env.log.debug('HTML output for PageToPDF charset: %s' % codepage)
+	meta = ('<meta http-equiv="Content-Type" content="text/html; charset=%s"/>' % codepage).encode(codepage)
+        os.write(hfile, '<html><head>' + meta + '</head><body>' + page + '</body></html>')
         os.close(hfile)
         pfile, pfilename = mkstemp('tracpdf')
         os.close(pfile)
         os.environ["HTMLDOC_NOCGI"] = 'yes'
         htmldoc_args = { 'webpage': None, 'format': 'pdf14', 'left': '1.5cm',
                          'right': '1.5cm', 'top': '1.5cm', 'bottom': '1.5cm',
-                         'charset': codepage.replace('iso-', '')}
+                         'charset': codepage }
         htmldoc_args.update(dict(self.env.config.options('pagetopdf')))
         args_string = ' '.join(['--%s %s' % (arg, value or '') for arg, value
                                 in htmldoc_args.iteritems()])
