e09a524ab55238a493b777961fca47794a6200b0
coderanger <coderanger@7322e99d-02ea-0310-aa39-e9a107903beb>
2007-02-28 00:21:29 +0000

    TagsPlugin:
    New UI based around IStreamFilter.
    
    git-svn-id: https://trac-hacks.org/svn@2055 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ tagsplugin/branches/filter/tractags/web_ui.py
@@ -0,0 +1,275 @@
+from trac.core import *
+from trac.web.main import IRequestHandler
+from trac.web.chrome import ITemplateProvider, INavigationContributor
+from trac.web.api import IStreamFilter, IRequestFilter
+from trac.util import Markup
+from StringIO import StringIO
+from trac.wiki.web_ui import WikiModule
+from trac.wiki.formatter import wiki_to_oneliner
+from genshi.builder import tag
+from genshi.core import START, END
+from tractags.expr import Expression
+
+from api import TagEngine
+
+import re
+
+
+
+try:
+    set = set
+except:
+    from sets import Set as set
+
+_tag_split = re.compile('[,\s]+')
+
+class TagsWikiModule(WikiModule):
+    """ Replacement for the default Wiki module. Tag editing is much more
+        intuitive now, as it no longer requires the TagIt macro and JavaScript
+        magic. """
+
+    def _do_save(self, req, db, page):
+        # This method is overridden so the user doesn't get "Page not modified"
+        # exceptions when updating tags but not wiki content.
+        from tractags.api import TagEngine
+        if 'tags' in req.args:
+            newtags = set([t.strip() for t in
+                          _tag_split.split(req.args.get('tags')) if t.strip()])
+            wikitags = TagEngine(self.env).tagspace.wiki
+            oldtags = wikitags.get_tags([page.name])
+
+            if oldtags != newtags:
+                wikitags.replace_tags(req, page.name, newtags)
+                # No changes, just redirect
+                if req.args.get('text') == page.text:
+                    req.redirect(self.env.href.wiki(page.name))
+                    return
+        return WikiModule._do_save(self, req, db, page)
+
+    def process_request(self, req):
+        from tractags.api import TagEngine
+        from trac.web.chrome import add_stylesheet
+
+        add_stylesheet(req, 'tags/css/tractags.css')
+
+        pagename = req.args.get('page', 'WikiStart')
+        action = req.args.get('action', 'view')
+
+        engine = TagEngine(self.env)
+        wikitags = engine.tagspace.wiki
+        tags = list(wikitags.get_tags([pagename]))
+        tags.sort()
+
+        if action == 'edit':
+            req.hdf['tags'] = req.args.get('tags', ', '.join(tags))
+        elif action == 'view':
+            hdf_tags = []
+            for tag in tags:
+                href, title = engine.get_tag_link(tag)
+                hdf_tags.append({'name': tag,
+                                 'href': href,
+                                 'title': title})
+            req.hdf['tags'] = hdf_tags
+        result = WikiModule.process_request(self, req)
+        if result is None:
+            return None
+        if result[0] == 'wiki.cs':
+            return 'tagswiki.cs', None
+        return result
+
+class TagsWikiFilter(Component):
+
+    implements(IStreamFilter, IRequestFilter)
+    
+    # IStreamFilter methods
+    def match_stream(self, req, stream, method):
+        return req.path_info.startswith('/wiki') and \
+               req.args.get('action') == 'edit' and \
+               method == 'xhtml'
+               
+    def process_stream(self, req, stream, method):
+        page_name = req.path_info[6:] or 'WikiStart'
+        stage = 1
+        elm = tag.div([tag.label('Tag under: (', 
+                                 tag.a('view all tags', href=req.href.tags()),
+                                  ')', 
+                                  for_='tags'), 
+                       tag.br(), 
+                       tag.input(title='Comma separated list of tags',
+                                 type='text',
+                                 id='tags',
+                                 size='30',
+                                 name='tags',
+                                 value=', '.join(TagEngine(self.env).tagspace.wiki.get_tags([page_name]))
+                                ),
+                      ], class_='field')
+                      
+        for kind, data, pos in stream:            
+            yield kind, data, pos
+            if stage == 1 and \
+               kind is START and \
+               data[0].localname == 'input' and \
+               data[1].get('id') == 'comment':
+                stage = 2
+            elif stage == 2 and \
+                 kind is END and \
+                 data.localname == 'div':
+                for e in elm.generate():
+                    yield e
+                stage = None
+            
+    # IRequestFilter methods
+    def pre_process_request(self, req, handler):
+        
+
+class TagsModule(Component):
+    """ Serve a /tags namespace. Top-level displays tag cloud, sub-levels
+        display output of ListTagged(tag).
+
+        The following configuration options are supported:
+
+        [tags]
+        # Use a tag list or cloud for the main index
+        index = cloud|list
+        # The keyword arguments to pass to the TagCloud or ListTags macros that
+        # is being used for the index.
+        index.args = ...
+        # Keyword arguments to pass to the listing for each tag under the
+        # /tags/ URL space.
+        listing.args = ...
+    """
+    implements(IRequestHandler, INavigationContributor, ITemplateProvider)
+
+    def _prepare_wiki(self, req):
+        from tractags.api import TagEngine
+        page = req.path_info[6:] or 'WikiStart'
+        engine = TagEngine(self.env)
+        wikitags = engine.tagspace.wiki
+        tags = list(wikitags.get_tags(page))
+        tags.sort()
+
+        action = req.args.get('action', 'view')
+        if action == 'edit':
+            req.hdf['tags'] = req.args.get('tags', ', '.join(tags))
+        elif action == 'view':
+            hdf_tags = []
+            for tag in tags:
+                href, title = engine.get_tag_link(tag)
+                hdf_tags.append({'name': tag,
+                                 'href': href,
+                                 'title': title})
+            req.hdf['tags'] = hdf_tags
+
+    # ITemplateProvider methods
+    def get_templates_dirs(self):
+        """
+        Return the absolute path of the directory containing the provided
+        ClearSilver templates.
+        """
+        from pkg_resources import resource_filename
+        return [resource_filename(__name__, 'templates')]
+
+    def get_htdocs_dirs(self):
+        """Return the absolute path of a directory containing additional
+        static resources (such as images, style sheets, etc).
+        """
+        from pkg_resources import resource_filename
+        return [('tags', resource_filename(__name__, 'htdocs'))]
+    
+    # INavigationContributor methods
+    def get_active_navigation_item(self, req):
+        return 'tags'
+
+    def get_navigation_items(self, req):
+        from trac.web.chrome import Chrome
+        yield ('mainnav', 'tags',
+               Markup('<a href="%s" accesskey="T">Tags</a>',
+                      self.env.href.tags()))
+
+    # IRequestHandler methods
+    def match_request(self, req):
+        return req.path_info.startswith('/tags')
+
+    def process_request(self, req):
+        from tractags.macros import TagMacros
+        from tractags.parseargs import parseargs
+        from trac.web.chrome import add_stylesheet
+
+        add_stylesheet(req, 'tags/css/tractags.css')
+        req.hdf['trac.href.tags'] = self.env.href.tags()
+
+        def update_from_req(args):
+            for k in req.args.keys():
+                args[str(k)] = str(req.args.get(k))
+
+        if not req.args.has_key('e') and re.match('^/tags/?$', req.path_info):
+            index = self.env.config.get('tags', 'index', 'cloud')
+            index_kwargs = {'smallest': 10, 'biggest': 30}
+            _, config_kwargs = parseargs(self.env.config.get('tags', 'index.args', ''))
+            index_kwargs.update(config_kwargs)
+            update_from_req(index_kwargs)
+
+            if index == 'cloud':
+                req.hdf['tag.body'] = Markup(
+                    TagMacros(self.env).render_tagcloud(req, **index_kwargs))
+            elif index == 'list':
+                req.hdf['tag.body'] = Markup(
+                    TagMacros(self.env).render_listtagged(req, **index_kwargs))
+            else:
+                raise TracError("Invalid index style '%s'" % index)
+        else:
+            _, args = parseargs(self.env.config.get('tags', 'listing.args', ''))
+            if req.args.has_key('e'):
+                expr = req.args.get('e')
+            else:
+                expr = req.path_info[6:]
+            req.hdf['tag.title'] = Markup('Objects matching the expression <i>%s</i>' % expr)
+            req.hdf['tag.expression'] = expr
+            try:
+                Expression(expr)
+            except Exception, e:
+                req.hdf['tag.expression.error'] = str(e).replace(' (line 1)', '')
+            args['expression'] = expr
+            tags = []
+            update_from_req(args)
+            req.hdf['tag.body'] = Markup(
+                TagMacros(self.env).render_listtagged(req, *tags, **args))
+        return 'tags.cs', None
+
+# XXX I think this is planned for some AJAX goodness, commenting out for now. (Alec) XXX
+#class TagsLi(Component):
+#    implements(IRequestHandler)
+#    
+#    # IRequestHandler methods
+#    def match_request(self, req):
+#        return req.path_info == '/tagli'
+#                
+#    def process_request(self, req):
+#        db = self.env.get_db_cnx()
+#        cursor = db.cursor()
+#        cs = db.cursor()
+#        tag = req.args.get('tag')
+#        req.send_response(200)
+#        req.send_header('Content-Type', 'text/plain')
+#        req.end_headers()
+#        buf = StringIO()
+#        if tag:
+#            buf.write('WHERE tag LIKE \'%s%s\'' % (tag,'%'))
+#            
+#        cursor.execute('SELECT DISTINCT tag FROM tags %s ORDER BY tag' % (buf.getvalue()))
+#
+#        msg = StringIO()
+#
+#        msg.write('<ul>')
+#        while 1:
+#            row = cursor.fetchone()
+#            if row == None:
+#                 break
+#
+#            t = row[0]
+#            msg.write('<li>')
+#            msg.write(t)
+#            msg.write('</li>')
+#
+#        msg.write('</ul>')
+#        req.write(msg.getvalue())
---
+++ tagsplugin/branches/filter/tractags/wiki.py
@@ -0,0 +1,89 @@
+from trac.core import *
+from tractags.api import DefaultTaggingSystem, ITaggingSystemProvider, TagEngine
+from trac.wiki.api import IWikiChangeListener, IWikiSyntaxProvider
+from tractags.expr import Expression
+from trac.util import Markup
+import re
+
+class WikiTaggingSystem(DefaultTaggingSystem):
+    """ Subclass of DefaultTaggingSystem that knows how to retrieve wiki page
+        titles. """
+    def __init__(self, env):
+        DefaultTaggingSystem.__init__(self, env, 'wiki')
+
+    def page_info(self, page):
+        from trac.wiki import model
+        """ Return tuple of (model.WikiPage, title) """
+        page = model.WikiPage(self.env, page)
+
+        title = ''
+
+        if page.exists:
+            text = page.text
+            ret = re.search('=\s+([^=]*)=',text)
+            title = ret and ret.group(1) or ''
+
+        return (page, title)
+
+    def name_details(self, name):
+        """ Return a tuple of (href, wikilink, title). eg. ("/ticket/1", "#1", "Broken links") """
+        page, title = self.page_info(name)
+        href = self.env.href.wiki(name)
+        defaults = DefaultTaggingSystem.name_details(self, name)
+        return defaults[0:2] + (title,)
+
+class WikiTags(Component):
+    """ Implement tags in the Wiki system, a tag:<tag> link resolver and
+        correctly deletes tags from Wiki pages that are removed. """
+
+    implements(ITaggingSystemProvider, IWikiChangeListener, IWikiSyntaxProvider)
+
+    # ITaggingSystemProvider methods
+    def get_tagspaces_provided(self):
+        yield 'wiki'
+
+    def get_tagging_system(self, tagspace):
+        return WikiTaggingSystem(self.env)
+
+    # IWikiChangeListener methods
+    def wiki_page_added(self, page):
+        TagEngine(self.env).flush_link_cache(page)
+
+    def wiki_page_changed(self, page, version, t, comment, author, ipnr):
+        TagEngine(self.env).flush_link_cache(page)
+
+    def wiki_page_deleted(self, page):
+        # No point having tags on a non-existent page.
+        self.env.log.debug("Removing all tags from 'wiki:%s'" % page.name)
+        engine = TagEngine(self.env)
+        engine.tagspace.wiki.remove_all_tags(None, page.name)
+        engine.flush_link_cache(page)
+
+    def wiki_page_version_deleted(self, page):
+        # Wiki tags are not versioned. If they were, we'd delete them here.
+        TagEngine(self.env).flush_link_cache(page)
+
+    # IWikiSyntaxProvider methods
+    def get_wiki_syntax(self):
+        yield (r'''\[tagged:(?P<tlpexpr>(?:'.*?'|".*?"|\S)+)\s+(?P<tlptitle>.*?]*)\]''',
+               lambda f, n, m: self._format_tagged(
+                                    m.group('tlpexpr'),
+                                    m.group('tlptitle')))
+        yield (r'''tagged:(?P<texpr>(?:'.*?'|".*?"|\S)+)''',
+               lambda f, n, m: self._format_tagged(
+                                    m.group('texpr'),
+                                    'tagged:' + m.group('texpr')))
+
+    def get_link_resolvers(self):
+        return []
+
+    def _format_tagged(self, target, label):
+        if '?' in target:
+            target, args = target.split('?')[0:2]
+            args = '?' + args
+        else:
+            args = ''
+        href, title = TagEngine(self.env).get_tag_link(target, is_expression=True)
+        return Markup('<a href="%s%s" title="%s">%s</a>' % (href, args,
+                                                            title, label))
+
