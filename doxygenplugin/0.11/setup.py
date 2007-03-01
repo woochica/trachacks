3e0796861088128c186caa6a80b9c12cc2d1f9f5
cboos <cboos@7322e99d-02ea-0310-aa39-e9a107903beb>
2007-03-01 07:37:56 +0000

    DoxygenPlugin:
    Quick adaptation for Trac 0.11.
    
    git-svn-id: https://trac-hacks.org/svn@2069 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ doxygenplugin/0.11/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='TracDoxygen',
+    description='Doxygen plugin for Trac',
+    keywords='trac doxygen',
+    version='0.11.1',
+    url='http://trac-hacks.org/wiki/DoxygenPlugin',
+    license = """Copyright (C) 2005 Jason Parks <jparks@jparks.net>
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+ 1. Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+ 2. Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in
+    the documentation and/or other materials provided with the
+    distribution.
+ 3. The name of the author may not be used to endorse or promote
+    products derived from this software without specific prior
+    written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
+OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.""",
+    author='Jason Parks, Radek Bartoň',
+    author_email='blackhex@post.cz',
+    maintainer = "Christian Boos",
+    maintainer_email = "cboos@neuf.fr",
+    long_description="""
+    """,
+    zip_safe=True,
+    packages=['doxygentrac'],
+    package_data={'doxygentrac': ['templates/*.cs', 'htdocs/css/*.css']},
+    entry_points={'trac.plugins': 'doxygentrac = doxygentrac.doxygentrac'})
