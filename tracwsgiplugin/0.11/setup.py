b8f9ea823608bb7c08714631d5a5ed3dbf4ae4bb
martinpaljak <martinpaljak@7322e99d-02ea-0310-aa39-e9a107903beb>
2007-04-03 19:49:05 +0000

    TracWsgiPlugin:
    Small addition to usage - paste entry point added.
    
    git-svn-id: https://trac-hacks.org/svn@2156 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracwsgiplugin/0.11/setup.py
+++ tracwsgiplugin/0.11/setup.py
@@ -8,11 +8,22 @@ setup(
     name=PACKAGE, version=VERSION,
     description='Trac WSGI plugin',
     author="Martin Paljak", author_email="martin@paljak.pri.ee",
-    license='GPL', url='http://ideelabor.ee/opensource/',
+    license='GPL', url='http://www.trac-hacks.org/wiki/TracWsgiPlugin',
     packages=find_packages(exclude=['ez_setup', '*.tests*']),
+    zip_safe=False,
+    install_requires = [
+            'setuptools>=0.6c5',
+            'Trac>=0.11dev',
+            'PasteDeploy',
+        ],
     entry_points = {
         'trac.plugins': [
             'wsgiplugin.wsgiplugin = wsgiplugin.wsgiplugin'
+        ],
+        'paste.app_factory': [
+            'main = wsgiplugin.wsgiplugin:wsgi_trac',
+            'permanent_redirect = wsgiplugin.wsgiplugin:permanent_redirect',
+            'temporary_redirect = wsgiplugin.wsgiplugin:temporary_redirect'
         ]
     }
 )
--- tracwsgiplugin/0.11/wsgiplugin/wsgiplugin.py
+++ tracwsgiplugin/0.11/wsgiplugin/wsgiplugin.py
@@ -1,11 +1,15 @@
 from paste.deploy import loadapp
+from paste.deploy.converters import asbool
 
 import re
+import urlparse
 
 from trac.config import *
 from trac.core import *
 from trac.web.api import IRequestHandler, RequestDone
 
+from trac.web.main import dispatch_request
+__all__ = ["WSGIPluginModule", "WSGITrac"]
 
 class WSGIPluginModule(Component):
     """Handles WSGI subapps for Trac."""
@@ -45,3 +49,69 @@ class WSGIPluginModule(Component):
         for chunk in appiter:
           req.write(chunk)
         raise RequestDone
+
+
+class WSGITrac:
+    """Callable class. Initi with path=/path/to/trac/env"""
+    def __init__(self, path, secure=False):
+      self.path = path
+      self.secure = secure
+      
+    def __call__(self, environ, start_response):
+      environ['trac.env_path'] = self.path
+      
+      https = environ.get("HTTPS", "off")
+      if self.secure and https != 'on':
+        return redirect_https(environ, start_response)
+
+      return dispatch_request(environ, start_response)
+
+
+def redirect_https(environ, start_response):
+	url = reconstruct_url(environ)
+	s=list(urlparse.urlsplit(url))
+	s[0]="https"
+	u2 = urlparse.urlunsplit(s)
+	start_response("302 Temporary Redirect", [('location', u2), ('content-type', 'text/plain')])
+	return [u2]
+
+def reconstruct_url(environ):
+	from urllib import quote
+	url = environ['wsgi.url_scheme']+'://'
+	
+	if environ.get('HTTP_HOST'):
+		url += environ['HTTP_HOST']
+	else:
+		url += environ['SERVER_NAME']
+
+		if environ['wsgi.url_scheme'] == 'https':
+			if environ['SERVER_PORT'] != '443':
+				url += ':' + environ['SERVER_PORT']
+		else:
+			if environ['SERVER_PORT'] != '80':
+				url += ':' + environ['SERVER_PORT']
+	url += quote(environ.get('SCRIPT_NAME',''))
+	url += quote(environ.get('PATH_INFO',''))
+	if environ.get('QUERY_STRING'):
+		url += '?' + environ['QUERY_STRING']
+	return url
+
+                        
+def wsgi_trac(global_conf, path = None, secure = False, **local_conf):
+    return WSGITrac(path, secure=asbool(secure))
+
+
+class Redirect():
+  def __init__(self, url, code=302):
+    self.url = url
+    self.status = "%d Redirect" %(code)
+  def __call__(self, environ, start_response):
+    start_response(self.status, [('Location', self.url)])
+    return []
+
+def permanent_redirect(global_conf, url):
+  return Redirect(url, 301)
+
+def temporary_redirect(global_conf, url):	
+  return Redirect(url, 302)
+                                                                                                                        
