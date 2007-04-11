5fa824de1f3746b566b7f05954eff0bc8a2c354d
coderanger <coderanger@7322e99d-02ea-0310-aa39-e9a107903beb>
2007-04-11 00:39:07 +0000

    PersonalReportsPlugin:
    Version bump
    
    git-svn-id: https://trac-hacks.org/svn@2170 7322e99d-02ea-0310-aa39-e9a107903beb

--- personalreportsplugin/0.10/setup.py
+++ personalreportsplugin/0.10/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name = 'TracPersonalReports',
-    version = '1.0',
+    version = '1.1',
     packages = ['personalreports'],
     package_data = { 'personalreports': ['templates/*.cs', 'htdocs/*.js', 'htdocs/*.css' ] },
 
