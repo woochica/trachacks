20313d21ddc17a6ec49f2aed87fbc875fa1e34ca
ant_39 <ant_39@7322e99d-02ea-0310-aa39-e9a107903beb>
2007-05-09 23:56:23 +0000

    CalendarPlugin:
     * Use entry point in `setup.py` instead of deprecated `trac_plugin.txt`.
    
    git-svn-id: https://trac-hacks.org/svn@2232 7322e99d-02ea-0310-aa39-e9a107903beb

--- calendarplugin/0.10/setup.py
+++ calendarplugin/0.10/setup.py
@@ -1,8 +1,29 @@
+# -*-coding:utf-8-*-
 from setuptools import setup
 
-PACKAGE = 'AztechCalendar'
-VERSION = '0.1'
+setup(
+    name = 'AztechCalendar',
+    version = '0.1',
+    packages = ['azcalendar'],
+    package_data = {
+        'azcalendar': [
+            'templates/*.cs',
+            'htdocs/images/*.jpg',
+            'htdocs/css/*.css',
+            'templates/*.cs'
+        ]
+    },
 
-setup(name=PACKAGE, version=VERSION, 
-	packages=['azcalendar'],
-	package_data={'azcalendar': ['templates/*.cs', 'htdocs/images/*.jpg', 'htdocs/css/*.css','templates/*.cs']})
+    description = 'Calendar for Trac',
+    author = 'Petr Machata, Jiří Moskovčák',
+    author_email = 'pmachata@gmail.com',
+    license = 'BSD',
+    keywords = 'trac calendar',
+    url = 'http://www.trac-hacks.org/wiki/CalendarPlugin',
+
+    entry_points = {
+        'trac.plugins': [
+            'azcalendar.azcalendar = azcalendar.azcalendar'
+        ]
+    }
+)
