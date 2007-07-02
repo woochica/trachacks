39ce192b73f9a01f53d89eccc44259a158d80f2d
coling <coling@7322e99d-02ea-0310-aa39-e9a107903beb>
2007-07-02 07:54:16 +0000

    ClientsPlugin:
    Initial version of ClientsPlugin
    
    git-svn-id: https://trac-hacks.org/svn@2363 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ clientsplugin/0.10/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+PACKAGE = 'clients'
+
+setup(name=PACKAGE,
+    description='Plugin to allow management of which ticket belong to which client',
+    keywords='trac plugin ticket client',
+    version='0.1',
+    url='',
+    license='http://www.opensource.org/licenses/mit-license.php',
+    author='Colin Guthrie',
+    author_email='trac@colin.guthr.ie',
+    long_description="""
+    I'll write this later!
+    """,
+    packages=[PACKAGE],
+    package_data={PACKAGE : ['templates/*.cs']},
+    entry_points = {
+        'trac.plugins': [
+            'clients.client = clients.client'
+        ]
+    },
+    install_requires=[ 'TracWebAdmin' ])   
+    #entry_points={'trac.plugins': '%s = %s' % (PACKAGE, PACKAGE)})
+
+
+#### AUTHORS ####
+## Primary Author:
+## Colin Guthrie
+## http://colin.guthr.ie/
+## trac@colin.guthr.ie
+## trac-hacks user: coling
