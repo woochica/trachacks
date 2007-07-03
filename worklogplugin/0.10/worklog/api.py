140fd578cbcdca079dc7c6b7b8ed17648024b4aa
coling <coling@7322e99d-02ea-0310-aa39-e9a107903beb>
2007-07-03 08:39:35 +0000

    WorkLogPlugin:
    Hopefully fixes #1710.
    
    git-svn-id: https://trac-hacks.org/svn@2367 7322e99d-02ea-0310-aa39-e9a107903beb

--- worklogplugin/0.10/worklog/api.py
+++ worklogplugin/0.10/worklog/api.py
@@ -1,6 +1,6 @@
 import re
 import dbhelper
-import time
+import time import time
 #from ticket_daemon import *
 from usermanual import *
 #from reports import all_reports
@@ -179,7 +179,7 @@ class WorkLogSetupParticipant(Component):
 
     def do_user_man_update(self):
 
-        when = int(time.time())
+        when = int(time())
         sql = """
         INSERT INTO wiki (name,version,time,author,ipnr,text,comment,readonly)
         VALUES ( %s, %s, %s, 'Timing and Estimation Plugin', '127.0.0.1', %s,'',0)
