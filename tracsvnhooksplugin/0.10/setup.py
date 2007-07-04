27506b0655804c5eb95fc34ae1048c4a82e7d151
Bombenbodo <Bombenbodo@7322e99d-02ea-0310-aa39-e9a107903beb>
2007-07-04 22:34:35 +0000

    TracSvnHooksPlugin:
    initial import
    
    git-svn-id: https://trac-hacks.org/svn@2386 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ tracsvnhooksplugin/0.10/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+PACKAGE = 'TracSVNHooks'
+VERSION = '0.1'
+
+setup(
+  name = PACKAGE,
+  version = VERSION,
+  author = "Robert Barsch",
+  author_email = "barsch@lmu.de",
+  description = "A interface to edit SVN repository hooks via admin panel",
+  license = "BSD",
+  keywords = "trac plugin SVN hooks",
+  url = "http://trac-hacks.org/wiki/TracSvnHooksPlugin",
+  classifiers = [ 'Framework :: Trac', ],
+  install_requires = ['TracWebAdmin'],
+  packages=['svnhooks'],
+  entry_points={'trac.plugins': '%s = svnhooks' % PACKAGE},
+  package_data={'svnhooks': ['htdocs/js/*.js','htdocs/css/*.css', 'htdocs/images/*.jpg', 'templates/*.cs']},
+)
+		  
