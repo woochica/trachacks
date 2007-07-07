9b13d064320955b2502183b7ba8e8eea0f0af244
Bombenbodo <Bombenbodo@7322e99d-02ea-0310-aa39-e9a107903beb>
2007-07-07 20:56:37 +0000

    TracSvnHooksPlugin:
    new layout, more hocks + tools, additional docs
    
    git-svn-id: https://trac-hacks.org/svn@2407 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracsvnhooksplugin/0.10/svnhooks/svnhooks.py
+++ tracsvnhooksplugin/0.10/svnhooks/svnhooks.py
@@ -1,6 +1,7 @@
 from trac.core import *
 from trac.web.chrome import ITemplateProvider, add_stylesheet
 from webadmin.web_ui import IAdminPageProvider
+from webadmin.plugin import TRAC_PATH
 from trac.config import Option
 import string
 import os
@@ -10,7 +11,11 @@ SECTIONS = {
   'pre-commit'         : 'There is a bug in some versions of SVN connected to the pre-commit hook! By filling the form you *have* to enable the hook.',
   'post-revprop-change': '',
   'pre-revprop-change' : '',
-  'start-commit'       : 'There is a bug in some versions of SVN connected to the start-commit hook! By filling the form you *have* to enable the hook.'
+  'start-commit'       : 'There is a bug in some versions of SVN connected to the start-commit hook! By filling the form you *have* to enable the hook.',
+  'pre-lock'	       : '',
+  'post-lock'          : '',
+  'pre-unlock'         : '',
+  'post-unlock'        : ''
 }
 
 class SVNHooksPlugin(Component):
@@ -19,16 +24,32 @@ class SVNHooksPlugin(Component):
     # IAdminPageProvider methods
     def get_admin_pages(self, req):
         if req.perm.has_permission('TRAC_ADMIN'):
-	    for section in SECTIONS.keys():
-		yield ('svn', 'SVN Hooks', section, section)
+    	    yield ('svn', 'Subversion', 'hooks', 'Hooks')
 
     def process_admin_request(self, req, cat, page, path_info):
         assert req.perm.has_permission('TRAC_ADMIN')
-        if page not in SECTIONS.keys():
-            raise TracError("Invalid section %s" % page)
 
+        sections = SECTIONS.keys()
+	sections.sort()
+
+        # catch selected section
+	if req.method == 'POST' and req.args.get('sections'):
+	  section = req.args.get('sections')
+        else:
+          section = sections[0]
+
+        # validate section
+        if section not in sections:
+            raise TracError("Invalid section %s" % section)
+
+        # get config vars
         repository_type = self.config.get('trac', 'repository_type').upper()
 	repository_dir = self.config.get('trac', 'repository_dir') + os.sep + 'hooks'
+        base_url = self.config.get('trac', 'base_url')
+	project_name = self.config.get('project', 'name')
+        smtp_default_domain = self.config.get('notification', 'smtp_default_domain')
+	smtp_from = self.config.get('notification', 'smtp_from')
+        smtp_replyto = self.config.get('notification', 'smtp_replyto')
 	
 	# repository type must be SVN
 	if repository_type!='SVN':
@@ -38,40 +59,53 @@ class SVNHooksPlugin(Component):
 	if not os.access(repository_dir,os.X_OK|os.W_OK|os.R_OK):
 	    raise TracError("Can't access repository hook directory %s" % repository_dir)
 
-        tmplfile = repository_dir + os.sep + page + '.tmpl'
-        hookfile = repository_dir + os.sep + page
+        tmplfile = repository_dir + os.sep + section + '.tmpl'
+        hookfile = repository_dir + os.sep + section
 
 	# evaluate forms
 	if req.method == 'POST':
           # Change status
-	  if req.args.get('status'):
-	    if req.args.get('status')==' Enable ':
+	  if req.args.get('tooglestatus'):
+	    if req.args.get('tooglestatus')==' Enable ':
               os.chmod(hookfile,0770)
 	    else:
 	      os.chmod(hookfile,0660)
-	  if req.args.get('apply'):
-	    if req.args.get('apply')==' Apply ':
- 	      current=req.args.get('current').strip()    
-              if current:
-	        try:
- 	          fp = open(hookfile,'w')
-  	          fp.write(current)
-	          fp.close()
-	        except:
-	          raise TracError("Can't write repository hook %s" % hookfile)
-	      else:
-	        try:
-		  os.remove(hookfile)
-		except:
-		  pass
-	  elif req.args.get('add'):
-	    if req.args.get('add')==' Add ' and req.args.get('emails'):
+	  if req.args.get('savehookfile'):
+ 	    current=req.args.get('current').strip()    
+            if current:
+	      try:
+ 	        fp = open(hookfile,'w')
+  	        fp.write(current)
+	        fp.close()
+	      except:
+	        raise TracError("Can't write repository hook %s" % hookfile)
+	    else:
+	      try:
+	        os.remove(hookfile)
+	      except:
+		pass
+	  elif req.args.get('addemail'):
+	    if req.args.get('emails'):
 	      emails = req.args.get('emails')
 	      subject = req.args.get('subject')
 	      mfilter = req.args.get('filter')
               try:
+	        fp = open(hookfile,'r')
+		current = fp.read()
+		fp.close()
 	        fp = open(hookfile,'a')
+		fp.writelines('\n')
+		if 'REPOS="$1"' not in current:
+		  fp.writelines('REPOS="$1"\n')
+		if 'REV="$2"' not in current:
+		  fp.writelines('REV="$2"\n')
   	        fp.writelines('/usr/lib/subversion/hook-scripts/commit-email.pl "$REPOS" "$REV" \\\n')
+                if smtp_from:
+		  fp.writelines('--from "'+smtp_from+'" \\\n')
+                if smtp_replyto:
+		  fp.writelines('-r "'+smtp_replyto+'" \\\n')
+                if smtp_default_domain:
+		  fp.writelines('-h "'+smtp_default_domain+'" \\\n')
                 if mfilter:
   		  fp.writelines('-m "'+mfilter+'" \\\n')
 		if subject:
@@ -80,6 +114,34 @@ class SVNHooksPlugin(Component):
 		  fp.writelines(email+' \\\n')
 		fp.writelines('\n')
 	        fp.close()
+	      except:
+	        raise TracError("Can't write repository hook %s" % hookfile)
+	  elif req.args.get('addtrac'):
+              try:
+	        fp = open(hookfile,'r')
+		current = fp.read()
+		fp.close()
+	        fp = open(hookfile,'a')
+		fp.writelines('\n')
+		if 'REPOS="$1"' not in current:
+		  fp.writelines('REPOS="$1"\n')
+		if 'REV="$2"' not in current:
+		  fp.writelines('REV="$2"\n')
+		if 'LOG=' not in current:
+		  fp.writelines('LOG=`/usr/bin/svnlook log -r $REV $REPOS`\n')
+		if 'AUTHOR=' not in current:
+		  fp.writelines('AUTHOR=`/usr/bin/svnlook author -r $REV $REPOS`\n')
+		if 'TRAC_ENV=' not in current:
+		  fp.writelines('TRAC_ENV="'+self.env.path+'"\n')
+		if 'TRAC_URL=' not in current:
+		  fp.writelines('TRAC_URL="'+base_url+'"\n')
+  	        fp.writelines('/usr/bin/python /usr/share/trac/contrib/trac-post-commit-hook \\\n')
+		fp.writelines('-p "$TRAC_ENV" \\\n')
+		fp.writelines('-r "$REV" \\\n')
+		fp.writelines('-u "$AUTHOR" \\\n')
+		fp.writelines('-m "$LOG" \\\n')
+		fp.writelines('-s "$TRAC_URL"\n')
+		fp.writelines('\n')
 	      except:
 	        raise TracError("Can't write repository hook %s" % hookfile)
 	      
@@ -104,10 +166,12 @@ class SVNHooksPlugin(Component):
         except:
           pass
 
-        req.hdf['svnhooks.section'] = page
+        req.hdf['svnhooks.section'] = section
+        req.hdf['svnhooks.sections'] = sections
         req.hdf['svnhooks.status'] = os.access(hookfile,os.X_OK)
         req.hdf['svnhooks.current'] = current
-        req.hdf['svnhooks.note'] = SECTIONS[page]
+        req.hdf['svnhooks.note'] = SECTIONS[section]
+	req.hdf['svnhooks.project_name'] = project_name
         req.hdf['svnhooks.description'] = description
 	
         return 'svnhooks.cs', None
--- tracsvnhooksplugin/0.10/svnhooks/templates/svnhooks.cs
+++ tracsvnhooksplugin/0.10/svnhooks/templates/svnhooks.cs
@@ -1,50 +1,49 @@
-<h2>Subversion <?cs var:svnhooks.section ?> hook</h2>
+<h2>Subversion: Hooks</h2>
 
-<form id="status" class="addnew" method="post">
+
+<form id="tooglestatus" class="addnew" method="post">
   <fieldset>
     <legend>Status</legend>
-    <p>Hook <?cs var:svnhooks.section ?> is currently <b><?cs if:svnhooks.status ?>ACTIVE<?cs else ?>INACTIVE<?cs /if ?></b>.</p>
+    <p class="help">Hook <?cs var:svnhooks.section ?> is currently <b style="color: black;"><?cs if:svnhooks.status ?>ACTIVE<?cs else ?>INACTIVE<?cs /if ?></b>.</p>
+    <?cs if:svnhooks.note ?><p class="help" style="color: red;"><?cs var:svnhooks.note ?></p><?cs /if ?>
     <div class="buttons">
-      <input type="submit" name="status" <?cs if:svnhooks.status ?>disabled="disabled"<?cs /if ?> value=" Enable " />
-      <input type="submit" name="status" <?cs if:svnhooks.status==0 ?>disabled="disabled"<?cs /if ?> value=" Disable " />
+      <input type="submit" name="tooglestatus" <?cs if:svnhooks.status ?>disabled="disabled"<?cs /if ?> value=" Enable " />
+      <input type="submit" name="tooglestatus" <?cs if:svnhooks.status==0 ?>disabled="disabled"<?cs /if ?> value=" Disable " />
+      <input type="hidden" name="sections" value="<?cs var:svnhooks.section ?>" />
     </div>
   </fieldset>
 </form>
 
-<form id="addemail" class="addnew" method="post">
-  <fieldset>
-    <legend>Append email notification</legend>
-    <div class="field">
-      <label>Subject prefix:</label><br />
-      <input type="text" name="subject" />
-      <br />
-      <label>Filter (perl regexp):</label><br />
-      <input size="40" type="text" name="filter" />
-      <br />
-      <label>E-mails: </label><br />
-      <textarea style="width: 100%" rows="5" type="text" name="emails"></textarea>
-      <br />
-    </div>
-    <p class="help">Helper tool for the SVN email notification script.</p>
-    <div class="buttons">
-      <input type="submit" name="add" value=" Add " />
-    </div>
-  </fieldset>
+<?cs if:svnhooks.section=='post-commit' ?><?cs include:"addtrac.cs" ?><?cs /if ?>
+
+<?cs if:svnhooks.section=='post-commit' ?><?cs include:"addemail.cs" ?><?cs /if ?>
+<?cs if:svnhooks.section=='post-revprop-change' ?><?cs include:"addemail.cs" ?><?cs /if ?>
+
+
+<form id="changesection" method="post">
+  <?cs call:hdf_select(svnhooks.sections, "sections", svnhooks.section, 0) ?> 
+  <input type="submit" name="changesection" value=" Select " />
 </form>
 
-<?cs if:svnhooks.note ?><p><?cs var:svnhooks.note ?></p><?cs /if ?>
-<form id="hook" method="post">
+<form id="savehookfile" method="post">
   <div class="field">
     <textarea rows="20" style="width:63%" name="current"><?cs var:svnhooks.current ?></textarea>
   </div>
   <div class="buttons">
-    <input type="submit" name="apply" value=" Apply " />
+    <input type="hidden" name="sections" value="<?cs var:svnhooks.section ?>" />
+    <input type="submit" name="savehookfile" value=" Apply changes " />
     <input type="reset" value=" Reset " />
   </div>
 </form>
 
-<?cs if:svnhooks.description ?>
 <br />
-<h2>Help for <?cs var:svnhooks.section ?> hook</h2>
+
+<h2>Documentation</h2>
+<?cs if:svnhooks.description ?>
+<h3><?cs var:svnhooks.section ?>.tmpl</h3>
 <pre><?cs var:svnhooks.description ?></pre>
 <?cs /if ?>
+<h3>Links</h3>
+<ul>
+  <li><a href="http://svnbook.red-bean.com/nightly/en/svn.ref.reposhooks.<?cs var:svnhooks.section ?>.html">http://svnbook.red-bean.com/nightly/en/svn.ref.reposhooks.<?cs var:svnhooks.section ?>.html</a></li>
+</ul>
