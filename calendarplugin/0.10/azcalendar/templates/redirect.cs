eb4aa083e2a4bbf49bf23adfc0c47e9607227e72
ant_39 <ant_39@7322e99d-02ea-0310-aa39-e9a107903beb>
2007-07-22 19:00:44 +0000

    CalendarPlugin:
     * Better redirect page. Thanks jc at vfnet.de.
     * Resolves: #1586
    
    git-svn-id: https://trac-hacks.org/svn@2509 7322e99d-02ea-0310-aa39-e9a107903beb

--- calendarplugin/0.10/azcalendar/templates/redirect.cs
+++ calendarplugin/0.10/azcalendar/templates/redirect.cs
@@ -1,3 +1,5 @@
- <h1>Working...</h1>
- <META HTTP-EQUIV="Refresh" CONTENT="0; URL=<?cs var:redir_url ?>">
- <img src="<?cs var:chrome.href ?>/hw/images/aztec.jpg" />
+<META HTTP-EQUIV="Refresh" CONTENT="0; URL=<?cs var:redir_url ?>">
+<?cs include "header.cs"?>
+<div id="ctxtnav" class="nav"></div>
+<div id="content" class="azcalendar">Data is processed...</div>
+<?cs include "footer.cs"?>
