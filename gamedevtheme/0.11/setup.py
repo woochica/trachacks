7bf24f28806a5d298292dfeb1ebe8e5237787659
coderanger <coderanger@7322e99d-02ea-0310-aa39-e9a107903beb>
2007-08-06 02:03:40 +0000

    GamedevTheme:
    Branch for 0.11.
    
    git-svn-id: https://trac-hacks.org/svn@2551 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ gamedevtheme/0.11/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+
+from setuptools import setup
+
+setup(
+    name = 'TracGamedevTheme',
+    version = '1.0',
+    packages = ['gamedevtheme'],
+    package_data = { 'gamedevtheme': ['templates/*.cs', 'htdocs/*.*' ] },
+
+    author = "Noah Kantrowitz",
+    author_email = "coderanger@yahoo.com",
+    description = "A theme for Trac from the RPI game development club.",
+    license = "BSD",
+    keywords = "trac plugin theme",
+    url = "http://trac-hacks.org/wiki/GamedevTheme",
+    classifiers = [
+        'Framework :: Trac',
+    ],
+    
+    install_requires = ['TracThemeEngine'],
+
+    entry_points = {
+        'trac.plugins': [
+            'gamedevtheme.theme = gamedevtheme.theme',
+        ]
+    }
+)
