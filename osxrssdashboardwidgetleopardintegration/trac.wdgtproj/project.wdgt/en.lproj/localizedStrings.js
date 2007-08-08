981b2df656732ee8a6ed0048da5142a2c66a0ea0
qualan <qualan@7322e99d-02ea-0310-aa39-e9a107903beb>
2007-08-08 19:27:38 +0000

    OsxRssDashboardWidgetLeopardIntegration:
    git-svn-id: https://trac-hacks.org/svn@2555 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ osxrssdashboardwidgetleopardintegration/trac.wdgtproj/project.wdgt/en.lproj/localizedStrings.js
@@ -0,0 +1,20 @@
+var localizedStrings = new Object;
+
+localizedStrings["My RSS Feed"] = 'Trac';
+localizedStrings["New URL: "] = "New URL: ";
+localizedStrings["Set"] = "Set";
+localizedStrings["Feed URLs: "] = "Feed URLs: ";
+localizedStrings["Article Length"] = "Article Length";
+localizedStrings["Reset"] = "Reset";
+localizedStrings["Done"] = "Done";
+localizedStrings["Invalid Feed"] = "Invalid Feed";
+localizedStrings["%s does not appear to be a valid RSS or Atom feed."] = "%s does not appear to be a valid RSS or Atom feed.";
+localizedStrings["No Items Found"] = "No Items Found";
+localizedStrings["The feed does not contain any entries."] = "The feed does not contain any entries.";
+localizedStrings["No items matched the search terms."] = "No items matched the search terms.";
+localizedStrings["Nothing To Display"] = "Nothing To Display";
+localizedStrings["The feed does not contain any entries within the specified criteria."] = "The feed does not contain any entries within the specified criteria.";
+localizedStrings["Loading"] = "Loading";
+localizedStrings["%s new"] = "%s new";
+localizedStrings["No Feed Specified"] = "No Feed Specified";
+localizedStrings["Please specify a valid feed URL."] = "Please specify a valid feed URL.";
