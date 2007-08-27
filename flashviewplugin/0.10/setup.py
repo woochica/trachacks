a89e8d4049d20fd4140a618facfc940578cef1b4
yuanying <yuanying@7322e99d-02ea-0310-aa39-e9a107903beb>
2007-08-27 05:43:58 +0000

    author and url are added.
    git-svn-id: https://trac-hacks.org/svn@2598 7322e99d-02ea-0310-aa39-e9a107903beb

--- flashviewplugin/0.10/setup.py
+++ flashviewplugin/0.10/setup.py
@@ -6,5 +6,10 @@ VERSION = '0.1.3'
 setup(name=PACKAGE,
       version=VERSION,
       packages=['flashview'],
+      author = 'Yuanying',
+      author_email = 'yuanying@fraction.jp',
+      url = 'http://trac-hacks.org/wiki/FlashViewPlugin',
+      description = 'Flash view plugin for Trac',
+      license='BSD',
       entry_points={'trac.plugins': '%s = flashview' % PACKAGE},
 )
