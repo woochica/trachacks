5a80701fa33c9bf4d9bdd853a3e97b3738ff37c2
coderanger <coderanger@7322e99d-02ea-0310-aa39-e9a107903beb>
2007-08-27 16:31:41 +0000

    Inital add.
    git-svn-id: https://trac-hacks.org/svn@2600 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ permredirectplugin/0.10/permredirect/filter.py
@@ -0,0 +1,49 @@
+# Created by Noah Kantrowitz on 2007-08-27.
+# Copyright (c) 2007 Noah Kantrowitz. All rights reserved.
+import sys
+import inspect
+
+from trac.core import *
+from trac.web.api import IRequestFilter, RequestDone
+from trac.perm import PermissionError
+
+class PermRedirectModule(Component):
+    """Redirect users to the login screen on PermissionError."""
+
+    implements(IRequestFilter)
+    
+    def __init__(self):
+        old_exc_info = sys.exc_info
+        def new_exc_info():
+            return list(old_exc_info())
+        sys.exc_info = new_exc_info
+    
+    # IRequestFilter methods
+    def pre_process_request(self, req, handler):
+        return handler
+            
+    def post_process_request(self, req, template, content_type):
+        if template is None:
+            # Some kind of exception in progress
+            exctype = sys.exc_info()[0]
+            if issubclass(exctype, PermissionError) and req.authname == 'anonymous':
+                # Do our redirect
+                try:
+                    req.redirect(req.href.login(referer=req.href(req.path_info)))
+                except RequestDone:
+                    pass # Mask the raise from here, we need to do it later
+                
+                for frame in inspect.stack()[1:]:
+                    l = frame[0].f_locals
+                    co = frame[0].f_code
+                    if 'err' in l and co.co_name == 'dispatch':
+                        # Hijack err
+                        err = l['err']
+                        err[0] = RequestDone
+                        err[1] = None
+                        err[2] = None
+                        break
+            
+        return template, content_type
+
+
---
+++ permredirectplugin/0.10/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+
+from setuptools import setup
+
+setup(
+    name = 'TracPermRedirect',
+    version = '1.0',
+    packages = ['permredirect'],
+    #package_data = { 'permredirect': ['templates/*.cs', 'htdocs/*.js', 'htdocs/*.css' ] },
+
+    author = 'Noah Kantrowitz',
+    author_email = 'coderanger@yahoo.com',
+    description = 'Redirect users to the login screen on PermissionError.',
+    license = 'BSD',
+    keywords = 'trac plugin',
+    url = 'http://trac-hacks.org/wiki/PermRedirectPlugin',
+    classifiers = [
+        'Framework :: Trac',
+    ],
+    
+    install_requires = [],
+
+    entry_points = {
+        'trac.plugins': [
+            'permredirect.filter = permredirect.filter',
+        ]
+    },
+)
