f8f54531b7978b75173739fef8081bfc71f41ffa
Alovak <Alovak@7322e99d-02ea-0310-aa39-e9a107903beb>
2007-10-26 10:49:46 +0000

    Import hack files
    git-svn-id: https://trac-hacks.org/svn@2717 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ breadcrumbtrailsscript/0.11/htdocs/js/path.js
@@ -0,0 +1,12 @@
+document.write("<div id=\"pathnav\" class=\"nav\"><ul>");
+var links = document.location.pathname.split("/");
+var url = "";
+for (i = 1;i < links.length;i++)
+{
+    if (links[i] == "") continue;
+    item = links[i];
+    url += item;
+    document.write("<li><a href=\"/"+url+"\">"+item+"</a></li>");
+    url += "/";
+}
+document.write("</ul></div>");
