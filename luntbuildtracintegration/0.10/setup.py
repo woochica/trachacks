3f8b332b211e6beeca2b0e2d72e94b96e2690525
diroussel <diroussel@7322e99d-02ea-0310-aa39-e9a107903beb>
2007-12-09 22:45:48 +0000

    LuntbuilTracIntegration: corrected package name in setup.py
    git-svn-id: https://trac-hacks.org/svn@2859 7322e99d-02ea-0310-aa39-e9a107903beb

--- luntbuildtracintegration/0.10/setup.py
+++ luntbuildtracintegration/0.10/setup.py
@@ -1,13 +1,13 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='LuntTrac', version='1.1',
+    name='LuntbuildTracIntegration', version='1.1',
     author="David Roussel",
     description="A trac plugin to add Luntbuild builds to the trac timeline",
     packages=find_packages(exclude=['*.tests*']),
-    package_data={'LuntTrac' : ['htdocs/*.css', 'htdocs/*.gif']},
+    package_data={'LuntbuildTracIntegration' : ['htdocs/*.css', 'htdocs/*.gif']},
     entry_points = """
 [trac.plugins]
-LuntTrac = LuntTrac.LuntTracPlugin
+LuntbuildTracIntegration = LuntbuildTracIntegration.LuntbuildTracIntegration.Plugin
     """
 )
