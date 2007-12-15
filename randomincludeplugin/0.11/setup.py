2132539b6668afb24a1a45c8a7d8bffda2d8cce5
Rottenchester <Rottenchester@7322e99d-02ea-0310-aa39-e9a107903beb>
2007-12-15 19:07:05 +0000

    Put license and url in setup.
    git-svn-id: https://trac-hacks.org/svn@2908 7322e99d-02ea-0310-aa39-e9a107903beb

--- randomincludeplugin/0.11/setup.py
+++ randomincludeplugin/0.11/setup.py
@@ -3,6 +3,10 @@ from setuptools import setup
 setup(
     name='TracRandomInclude',
     version='0.1',
+    author = 'RottenChester',
+    author_email = 'rottenchester@gmail.com',
+    url = 'http://trac-hacks.org/wiki/TracRandomIncludePlugin',
+    license = 'bsd',
     packages=['randominclude'],
     package_data={ 'randominclude': [  ]},
     description="Macros to include random items from specially formatted pages",
