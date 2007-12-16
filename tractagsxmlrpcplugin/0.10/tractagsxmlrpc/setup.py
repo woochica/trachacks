fdbcc6b9af0e619e5a2737482375e68aad7afaa7
jimbydamonk <jimbydamonk@7322e99d-02ea-0310-aa39-e9a107903beb>
2007-12-16 23:49:38 +0000

    I spelled trac wrong in setup.py.
    git-svn-id: https://trac-hacks.org/svn@2916 7322e99d-02ea-0310-aa39-e9a107903beb

--- tractagsxmlrpcplugin/0.10/tractagsxmlrpc/setup.py
+++ tractagsxmlrpcplugin/0.10/tractagsxmlrpc/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='TragTagsXMLRPC',
+    name='TracTagsXMLRPC',
     version='0.0.1',
     packages=['tractagsxmlrpc'],
     package_data={'tractags' : ['']},
