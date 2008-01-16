2a748909baec80912c35714e5d507bfcadf3f171
ixokai <ixokai@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-01-16 02:34:48 +0000

    Cleaned it up, in general; also added the ability to configure what resources you want to be bread-crumb-trackable.
    git-svn-id: https://trac-hacks.org/svn@3065 7322e99d-02ea-0310-aa39-e9a107903beb

--- breadcrumbsnavplugin/0.11/breadcrumbsnavplugin/breadcrumbs.py
+++ breadcrumbsnavplugin/0.11/breadcrumbsnavplugin/breadcrumbs.py
@@ -1,5 +1,5 @@
-from trac.core import Component, implements
-from trac.config import Option, IntOption
+from trac.core import Component, implements, TracError
+from trac.config import Option, IntOption, ListOption
 from trac.web import IRequestFilter
 from trac.wiki import parse_args
 from trac.web.chrome import ITemplateProvider, add_stylesheet
@@ -7,82 +7,117 @@ from pkg_resources import resource_filename
 from trac.web.api import ITemplateStreamFilter
 from genshi.builder import tag
 from genshi.filters.transform import Transformer
-import re
+import re, cPickle
+from trac.env import IEnvironmentSetupParticipant
 
 class BreadCrumbsSystem(Component):
-    implements(IRequestFilter, ITemplateProvider, ITemplateStreamFilter)
+    implements(IRequestFilter, ITemplateProvider, ITemplateStreamFilter, IEnvironmentSetupParticipant)
     
     ignore_pattern = Option('breadcrumbs', 'ignore pattern', None, 
         doc="""Resource names that match this pattern will not be added to 
         the breadcrumbs trail.""")
     
-    max_crumbs = IntOption('breadcrumbs', 'max crumbs', 6, 
+    max_crumbs = IntOption('breadcrumbs', 'max_crumbs', 6, 
         doc="""Indicates the maximum number of breadcrumbs to store per user.""")
     
+    supported_paths = ListOption('breadcrumbs', 'paths', '/wiki*,/ticket*,/milestone*',
+        doc='List of URL paths to allow breadcrumb tracking. Globs are supported.')
+    
     compiled_ignore_pattern = None
+    
+    ## IEnvironmentSetupParticipant
+    def environment_created(self):
+        self._upgrade_db(self.env.get_db_cnx())
+
+    def environment_needs_upgrade(self, db):
+        cursor = db.cursor()
+
+        try:
+            cursor.execute(
+                "SELECT count(*) FROM session_attribute WHERE name = %s", 
+                ("breadcrumbs list",)
+            )
+            result = cursor.fetchone()
+            if int(result[0]):
+                return True
+                
+            return False
+        except:
+            db.rollback()
+            return True
+
+    def upgrade_environment(self, db):
+        self._upgrade_db(db)
+
+    def _upgrade_db(self, db):
+        try:
+            from trac.db import DatabaseManager
+            db_backend, _ = DatabaseManager(self.env)._get_connector()            
+
+            cursor = db.cursor()
+            cursor.execute("DELETE FROM session_attribute WHERE name = %s", ("breadcrumbs list",))
+
+        except Exception, e:
+            db.rollback()
+            self.log.error(e, exc_info=True)
+            raise TracError(str(e))
+    
     
     ## IRequestFilter
     
     def pre_process_request(self, req, handler):
         return handler
         
-    def _get_crumbs(self, req):
-        sess = req.session
-
+    def _get_crumbs(self, sess):
         crumbs = []
-        if 'breadcrumbs list' in sess:
-            raw = sess['breadcrumbs list']
+        if 'breadcrumbs_list' in sess:
+            raw = sess['breadcrumbs_list']
             try:
-                crumbs = [x.replace('@COMMA@', ',') for x in raw.split(',')]
+                crumbs = cPickle.loads(raw.encode('ascii', 'ignore'))
             except:
-                pass
+                del sess['breadcrumbs_list']
         
         return crumbs
         
     def post_process_request(self, req, template, data, content_type):
         if self.compiled_ignore_pattern is None and self.ignore_pattern:
             self.compiled_ignore_pattern = re.compile(self.ignore_pattern)
-            
+        
+        path = req.path_info
         try:
-            path = req.path_info
             if path.count('/') >= 2:
-                _, realm, rest = path.split('/', 2)
+                _, realm, resource = path.split('/', 2)
+            
+                supported = False
+            
+                for pattern in self.supported_paths:
+                    if re.match(pattern, path):
+                        supported = True
+                        break
                 
-                if realm in ('wiki', 'ticket', 'milestone'):            
-                    if '#' in rest:
-                        name = rest[0:rest.index('#')]
-                    else:
-                        name = rest
+                if not supported or (self.compiled_ignore_pattern and
+                            self.compiled_ignore_pattern.match(resource)):
+                    return template, data, content_type
+                
+                if '&' in resource:
+                    resource = resource[0:resource.index('&')]
+                
+                sess = req.session
+                crumbs = self._get_crumbs(sess)
+                
+                current = '/'.join( (realm, resource) )
+                if current in crumbs:
+                    crumbs.remove(current)
+                    crumbs.insert(0, current)
+                else:
+                    crumbs.insert(0, current)
+                    crumbs = crumbs[0:self.max_crumbs]
                     
-                    if '&' in name:
-                        name = name[0:name.index('&')]
-                    
-                    id = name
-                    if realm == "ticket":
-                        name = "#" + name
-                     
-                    if self.compiled_ignore_pattern and self.compiled_ignore_pattern.match(id):
-                        return template, data, content_type
-                    
-                    sess = req.session
-                    
-                    crumbs = self._get_crumbs(req)
-                    
-                    current = "%s:%s" % (name, req.href("%s/%s" % (realm, id)))
-                    if current not in crumbs:
-                        crumbs.insert(0, current)
-                        crumbs = crumbs[0:self.max_crumbs]
-                    else:
-                        crumbs.remove(current)
-                        crumbs.insert(0, current)
-
-                    sess['breadcrumbs list'] = ','.join(
-                        x.replace(',', '@COMMA@') for x in crumbs
-                    )
+                sess['breadcrumbs_list'] = cPickle.dumps(crumbs)
         except:
             self.log.exception("Breadcrumb failed :(")
         
-
+        
         return template, data, content_type
     
     ## ITemplateProvider
@@ -97,7 +132,7 @@ class BreadCrumbsSystem(Component):
     ## ITemplateStreamFilter
     
     def filter_stream(self, req, method, filename, stream, data):
-        crumbs = self._get_crumbs(req)
+        crumbs = self._get_crumbs(req.session)
         if not crumbs:
             return stream
             
@@ -107,11 +142,20 @@ class BreadCrumbsSystem(Component):
         href = req.href(req.base_path)
         
         for crumb in crumbs:
-            title, link = crumb.split(':', 1)
+            realm, resource = crumb.split('/', 1)
+            name = resource.replace('_', ' ')
+
+            if realm == "ticket":
+                name = "#" + resource
+            elif realm != "wiki":
+                name = "%s:%s" % (realm, name)
+
+            link = req.href(realm, resource)
+            
             li.append(
                 tag.li(
-                    tag.a(title=title, href=link,
-                    )(title)
+                    tag.a(title=name, href=link,
+                    )(name)
                 )
             )
             
