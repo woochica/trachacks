c53d4d48f901ac8c0917cfcae4caf7b19c9529f5
coderanger <coderanger@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-01-17 22:52:30 +0000

    Initial add of 0.11 version.
    git-svn-id: https://trac-hacks.org/svn@3083 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ pydotorgtheme/0.11/pydotorgtheme/theme.py
@@ -0,0 +1,12 @@
+# Created by Noah Kantrowitz on 2007-07-16.
+# Copyright (c) 2007 Noah Kantrowitz. All rights reserved.
+
+from trac.core import *
+
+from themeengine.api import ThemeBase
+
+class PyDotOrgTheme(ThemeBase):
+    """A theme for Trac based on http://www.python.org."""
+
+    htdocs = css = True
+    
