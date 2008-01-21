5814378f339efa59eeb409efcea9e25caac09020
athomas <athomas@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-01-21 13:22:58 +0000

    Tagging 0.6.
    git-svn-id: https://trac-hacks.org/svn@3117 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ tagsplugin/tags/0.6/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='TracTags',
+    version='0.6',
+    packages=['tractags'],
+	package_data={'tractags' : ['templates/*.html', 'htdocs/js/*.js', 'htdocs/css/*.css']},
+    # With acknowledgement to Muness Albrae for the original idea :)
+    author='Alec Thomas',
+    license='BSD',
+    url='http://trac-hacks.org/wiki/TagsPlugin',
+    description='Tags plugin for Trac',
+    entry_points = {'trac.plugins': ['tractags = tractags']},
+    dependency_links=['http://svn.edgewall.org/repos/genshi/trunk#egg=Genshi-dev'],
+    install_requires=['Genshi >= 0.5.dev-r698,==dev'],
+    )
