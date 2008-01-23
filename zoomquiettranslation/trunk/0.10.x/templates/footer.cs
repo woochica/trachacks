56a95c9e28f673015da1b79dae7bee3306dfad6b
ZoomQuiet <ZoomQuiet@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-01-23 15:50:29 +0000

    append Trac0.10.4zh3-trunk
    git-svn-id: https://trac-hacks.org/svn@3134 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ zoomquiettranslation/trunk/0.10.x/templates/footer.cs
@@ -0,0 +1,35 @@
+<script type="text/javascript">searchHighlight()</script><?cs
+if:len(chrome.links.alternate) ?>
+<div id="altlinks"><h3>其它下载格式:</h3><ul><?cs
+ each:link = chrome.links.alternate ?><?cs
+  set:isfirst = name(link) == 0 ?><?cs
+  set:islast = name(link) == len(chrome.links.alternate) - 1?><li<?cs
+    if:isfirst || islast ?> class="<?cs
+     if:isfirst ?>first<?cs /if ?><?cs
+     if:isfirst && islast ?> <?cs /if ?><?cs
+     if:islast ?>last<?cs /if ?>"<?cs
+    /if ?>><a href="<?cs var:link.href ?>"<?cs if:link.class ?> class="<?cs
+    var:link.class ?>"<?cs /if ?>><?cs var:link.title ?></a></li><?cs
+ /each ?></ul></div><?cs
+/if ?>
+
+</div>
+
+<div id="footer">
+ <hr />
+ <a id="tracpowered" href="http://trac.edgewall.org/"><img src="<?cs
+   var:htdocs_location ?>trac_logo_mini.png" height="30" width="107"
+   alt="Trac Powered"/></a>
+ <p class="left">
+  动力源自 <a href="<?cs var:trac.href.about ?>"><strong>Trac <?cs
+  var:trac.version ?></strong></a><br />
+  <a href="http://www.edgewall.org/">Edgewall Software.创立</a>.
+ </p>
+ <p class="right">
+  <?cs var:project.footer ?>
+ </p>
+</div>
+
+<?cs include "site_footer.cs" ?>
+ </body>
+</html>
