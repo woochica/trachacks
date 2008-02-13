4aabb9e33b2e347c9241413e39b813c927b81603
athomas <athomas@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-02-13 12:53:56 +0000

    Respect `__docformat__` with fallback to basic newline formatting.
    git-svn-id: https://trac-hacks.org/svn@3227 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracdeveloperplugin/trunk/tracdeveloper/apidoc.py
+++ tracdeveloperplugin/trunk/tracdeveloper/apidoc.py
@@ -17,7 +17,6 @@ class APIDocumentation(Component):
     implements(IRequestHandler)
 
     # IRequestHandler methods
-
     def match_request(self, req):
         match = re.match(r'/developer/doc(?:/(.*))?$', req.path_info)
         if match:
@@ -32,22 +31,36 @@ class APIDocumentation(Component):
         except (ImportError, AttributeError), e:
             raise HTTPNotFound(e)
 
+        formatter = self._get_formatter(module)
+
         data = {
             'module': modname,
             'name': attrname or modname,
-            'doc': wiki_to_html(inspect.getdoc(obj), self.env, req),
-            'methods': self._get_methods(req, obj)
+            'doc': formatter(req, inspect.getdoc(obj)),
+            'methods': self._get_methods(req, formatter, obj)
         }
         output = Chrome(self.env).render_template(req, 'developer/apidoc.html',
                                                   data, fragment=True)
         req.send(output.render('xhtml'), 'text/html')
 
     # Internal methods
+    def _get_formatter(self, module):
+        format = getattr(module, '__docformat__', 'default').split()[0]
+        return getattr(self, '_format_' + format, self._format_default)
 
-    def _get_methods(self, req, cls, exclude_methods=None):
+    def _format_tracwiki(self, req, text):
+        return wiki_to_html(text, self.env, req)
+
+    def _format_restructuredtext(self, req, text):
+        return linebreaks(text)
+
+    def _format_default(self, req, text):
+        return linebreaks(text)
+
+    def _get_methods(self, req, formatter, cls, exclude_methods=None):
         methods = [getattr(cls, m) for m in dir(cls) if not m.startswith('_')
                    and m not in (exclude_methods or [])]
         return [{'name': m.__name__,
                  'args': inspect.formatargspec(*inspect.getargspec(m)),
-                 'doc': wiki_to_html(inspect.getdoc(m), self.env, req)}
+                 'doc': formatter(req, inspect.getdoc(m))}
                 for m in methods if inspect.ismethod(m)]
