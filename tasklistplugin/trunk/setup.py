5fcb7016da43c030514ee07d248e0743dd17698c
pacopablo <pacopablo@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-02-21 07:39:49 +0000

    * Adding proper license notices
    git-svn-id: https://trac-hacks.org/svn@3258 7322e99d-02ea-0310-aa39-e9a107903beb

--- tasklistplugin/trunk/setup.py
+++ tasklistplugin/trunk/setup.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2008 John Hampton <pacopablo@pacopablo.com>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+#
+# Author: John Hampton <pacopablo@pacopablo.com>
+
 
 from setuptools import setup
 
