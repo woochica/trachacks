dc20b81933fd252d7a7bdf83fba7635cc1b92969
dkhijniak <dkhijniak@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-04-24 17:15:36 +0000

    Initial release
    git-svn-id: https://trac-hacks.org/svn@3547 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ anchormacro/0.10/Anchor.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+This macro allows you to add anchor.
+
+Author: Dmitri Khijniak
+
+Usage:
+{{{
+  [[Anchor(anchor_name,text )]]
+}}}
+or
+{{{
+  [[Anchor(anchor_name)]]
+}}}
+Where: 
+anchor_name::
+  is a URL used to reference the anchor
+text::
+  visible text describing the anchor
+
+Example:
+{{{
+  [[Anchor(anchor)]]
+  [[Anchor(anchor, name)]]
+}}}
+"""
+
+def execute(hdf, args, env):
+    args = tuple(args.split(","))
+    print args
+    if len(args) == 2 :
+      return '<a name="%s" href="#%s">%s</a>' % (args[0],args[1],args[1])
+    else:
+      return '<a name="%s"></a>' % args
