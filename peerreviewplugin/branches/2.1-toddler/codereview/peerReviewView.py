f900e8f636b252c2c11190f88771a98b2e579506
proofek <proofek@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-04-24 22:36:15 +0000

    peerReviewView templated transformed to genshi
    git-svn-id: https://trac-hacks.org/svn@3548 7322e99d-02ea-0310-aa39-e9a107903beb

--- peerreviewplugin/branches/2.1-toddler/codereview/peerReviewView.py
+++ peerreviewplugin/branches/2.1-toddler/codereview/peerReviewView.py
@@ -21,6 +21,7 @@ from codereview.dbBackend import *
 from codereview.ReviewerStruct import *
 from trac.web.chrome import add_stylesheet
 import time
+import itertools
 
 class UserbaseModule(Component):
     implements(IRequestHandler, ITemplateProvider, INavigationContributor)
@@ -40,33 +41,27 @@ class UserbaseModule(Component):
         return req.path_info == '/peerReviewView'
                                         
     def process_request(self, req):
+
+        data = {}
         # check to see if the user is a manager of this page or not
         if req.perm.has_permission('CODE_REVIEW_MGR'):
-            req.hdf['manager'] = 1
+            data['manager'] = 1
         else:
             req.perm.assert_permission('CODE_REVIEW_DEV')
-            req.hdf['manager'] = 0
-
-        # set up dynamic links
-        req.hdf['trac.href.peerReviewMain'] = self.env.href.peerReviewMain()
-        req.hdf['trac.href.peerReviewNew'] = self.env.href.peerReviewNew()
-        req.hdf['trac.href.peerReviewSearch'] = self.env.href.peerReviewSearch()
-        req.hdf['trac.href.peerReviewPerform'] = self.env.href.peerReviewPerform()
-        req.hdf['trac.href.peerReviewView'] = self.env.href.peerReviewView()
-        req.hdf['trac.href.peerReviewOptions'] = self.env.href.peerReviewOptions()
+            data['manager'] = 0
 
         # reviewID argument checking
         reviewID = req.args.get('Review')
         if reviewID == None or not reviewID.isdigit():
-            req.hdf['error.type'] = "TracError"
-            req.hdf['error.title'] = "Review ID error"
-            req.hdf['error.message'] = "Invalid review ID supplied - unable to load page."
-            return 'error.cs', None
+            data['error.type'] = "TracError"
+            data['error.title'] = "Review ID error"
+            data['error.message'] = "Invalid review ID supplied - unable to load page."
+            return 'error.cs', data, None
 
-        req.hdf['main'] = "no"
-        req.hdf['create'] = "no"
-        req.hdf['search'] = "no"
-        req.hdf['options'] = "no"
+        data['main'] = "no"
+        data['create'] = "no"
+        data['search'] = "no"
+        data['option'] = "no"
 
         # set up to display the files that are in this review
         db = self.env.get_db_cnx()
@@ -84,46 +79,46 @@ class UserbaseModule(Component):
             newfile.append(len(dbBack.getCommentsByFileID(struct.IDFile)))
             returnfiles.append(newfile)
             newfile = []
-        req.hdf['files'] = returnfiles
-        req.hdf['filesLength'] = len(returnfiles)
-        req.hdf['reviewID'] = reviewID
+        data['files'] = returnfiles
+        data['filesLength'] = len(returnfiles)
+        data['reviewID'] = reviewID
 
-        req.hdf['users'] = dbBack.getPossibleUsers()
+        data['users'] = dbBack.getPossibleUsers()
         review = dbBack.getCodeReviewsByID(reviewID)
         # error if review id does not exist in the database
         if review == None:
-            req.hdf['error.type'] = "TracError"
-            req.hdf['error.title'] = "Review error"
-            req.hdf['error.message'] = "Review does not exist in database - unable to load page."
-            return 'error.cs', None
+            data['error.type'] = "TracError"
+            data['error.title'] = "Review error"
+            data['error.message'] = "Review does not exist in database - unable to load page."
+            return 'error.cs', data, None
 
         # set up the fields that will be displayed on the page
-        req.hdf['name'] = review.Name
-        req.hdf['notes'] = review.Notes
-        req.hdf['status'] = review.Status
-        req.hdf['author'] = review.Author
-        req.hdf['myname'] = util.get_reporter_id(req)
-        req.hdf['datecreate'] = util.format_date(review.DateCreate)
-        req.hdf['voteyes'] = dbBack.getVotesByID("1", reviewID)
-        req.hdf['voteno'] = dbBack.getVotesByID("0", reviewID)
-        req.hdf['notvoted'] = dbBack.getVotesByID("-1", reviewID)
-        req.hdf['total_votes_possible'] = float(req.hdf['voteyes']) + float(req.hdf['voteno']) + float(req.hdf['notvoted'])
-        req.hdf['threshold'] = float(dbBack.getThreshold())/100
+        data['name'] = review.Name
+        data['notes'] = review.Notes
+        data['status'] = review.Status
+        data['author'] = review.Author
+        data['myname'] = util.get_reporter_id(req)
+        data['datecreate'] = util.format_date(review.DateCreate)
+        data['voteyes'] = dbBack.getVotesByID("1", reviewID)
+        data['voteno'] = dbBack.getVotesByID("0", reviewID)
+        data['notvoted'] = dbBack.getVotesByID("-1", reviewID)
+        data['total_votes_possible'] = float(data['voteyes']) + float(data['voteno']) + float(data['notvoted'])
+        data['threshold'] = float(dbBack.getThreshold())/100
 
         # figure out whether I can vote on this review or not
-        entry = dbBack.getReviewerEntry(reviewID, req.hdf['myname'])
+        entry = dbBack.getReviewerEntry(reviewID, data['myname'])
         if entry != None:
-            req.hdf['canivote'] = 1
-            req.hdf['myvote'] = entry.Vote
+            data['canivote'] = 1
+            data['myvote'] = entry.Vote
         else:
-            req.hdf['canivote'] = 0
+            data['canivote'] = 0
 
         #display vote summary only if I have voted or am the author/manager, or if the review is "Ready for inclusion" or "Closed
-        req.hdf['viewvotesummary'] = 0
-        if req.hdf['author'] == req.hdf['myname'] or req.hdf['manager'] == '1' or (dbBack.getReviewerEntry(reviewID, req.hdf['myname']) != None and dbBack.getReviewerEntry(reviewID, req.hdf['myname']).Vote != '-1') or req.hdf['status']=="Closed" or req.hdf['status']=="Ready for inclusion":
-            req.hdf['viewvotesummary'] = 1
+        data['viewvotesummary'] = 0
+        if data['author'] == data['myname'] or data['manager'] == '1' or (dbBack.getReviewerEntry(reviewID, data['myname']) != None and dbBack.getReviewerEntry(reviewID, data['myname']).Vote != '-1') or data['status']=="Closed" or data['status']=="Ready for inclusion":
+            data['viewvotesummary'] = 1
         else:
-            req.hdf['viewvotesummary'] = 0
+            data['viewvotesummary'] = 0
 
         rvs = []  # reviewer/vote pairs
         reviewers = dbBack.getReviewers(reviewID)
@@ -132,7 +127,7 @@ class UserbaseModule(Component):
         # if we are the manager, list who has voted and what their vote was.
         # if we are the author, list who has voted and who has not.
         # if we are neither, list the users who are participating in this review. 
-        if req.hdf['manager'] == '1':
+        if data['manager'] == '1':
             for reviewer in reviewers:
                 newrvpair.append(reviewer.Reviewer)
                 if reviewer.Vote == '-1':
@@ -158,15 +153,15 @@ class UserbaseModule(Component):
                 rvs.append(newrvpair)
                 newrvpair = []
 
-        req.hdf['rvs'] = rvs
-        req.hdf['rvsLength'] = len(rvs)
+        data['rvs'] = rvs
+        data['rvsLength'] = len(rvs)
 
 
         # execute based on URL arguments
         if req.args.get('Vote') == 'yes':
-            self.vote("1", reviewID, req)
+            self.vote("1", reviewID, req, data['myname'])
         if req.args.get('Vote') == 'no':
-            self.vote("0", reviewID, req)
+            self.vote("0", reviewID, req, data['myname'])
 
         # process state (Open for review, ready for inclusion, etc.) change by manager
         mc = req.args.get('ManagerChoice')
@@ -174,14 +169,17 @@ class UserbaseModule(Component):
             self.manager_change_status(mc, reviewID, req)
 
         if req.args.get('Close') == '1':
-            self.close_review(reviewID, req)
+            self.close_review(reviewID, req, data['manager'])
 
         if req.args.get('Inclusion') == '1':
             self.submit_for_inclusion(reviewID, req)
 
-	add_stylesheet(req, 'common/css/code.css')
+        add_stylesheet(req, 'common/css/code.css')
         add_stylesheet(req, 'common/css/browser.css')
-        return 'peerReviewView.cs', None
+
+        data['cycle'] = itertools.cycle
+
+        return 'peerReviewView.html', data, None
                 
     # ITemplateProvider methods
     def get_templates_dirs(self):
@@ -199,10 +197,10 @@ class UserbaseModule(Component):
     # If user has not voted for this review and is a voting member, and attempts
     # to vote, change the vote type in the review entry struct in the database
     # and reload the page.
-    def vote(self, type, number, req):
+    def vote(self, type, number, req, myname):
         db = self.env.get_db_cnx()
         dbBack = dbBackend(db)
-        reviewEntry = dbBack.getReviewerEntry(number, req.hdf['myname'])
+        reviewEntry = dbBack.getReviewerEntry(number, myname)
         if reviewEntry != None:
             reviewEntry.Vote = type
             reviewEntry.save(db)
@@ -224,7 +222,7 @@ class UserbaseModule(Component):
             else:
                 review.Status = "Open for review"
             review.save(db)
-        req.redirect(req.hdf['trac.href.peerReviewView'] + "?Review=" + reviewID)
+        req.redirect(self.env.href.peerReviewView() + "?Review=" + reviewID)
             
     # If it is confirmed that the user is the author of this review and they
     # have attempted to submit for inclusion, change the status of this review
@@ -237,19 +235,19 @@ class UserbaseModule(Component):
             if review.Status == "Reviewed":
                 review.Status = "Ready for inclusion"
                 review.save(db)
-                req.redirect(req.hdf['trac.href.peerReviewView'] + "?Review=" + number)
+                req.redirect(self.env.href.peerReviewView() + "?Review=" + number)
 
     # If the user is confirmed to be the author or manager and tries to close
     # this review, close it by changing the status of the review to "Closed."
-    def close_review(self, number, req):
+    def close_review(self, number, req, manager):
         db = self.env.get_db_cnx()
         dbBack = dbBackend(db)
         review = dbBack.getCodeReviewsByID(number)
         # this option available if you are the author or manager of this code review
-        if review.Author == util.get_reporter_id(req) or req.hdf('Manager') == '1':
+        if review.Author == util.get_reporter_id(req) or manager == 1:
             review.Status = "Closed"
             review.save(db)
-            req.redirect(req.hdf['trac.href.peerReviewView'] + "?Review=" + number)
+            req.redirect(self.env.href.peerReviewView() + "?Review=" + number)
             
     # It has already been confirmed that this user is a manager, so this routine
     # just changes the status of the review to the type specified by the manager.
@@ -259,5 +257,5 @@ class UserbaseModule(Component):
         review = dbBack.getCodeReviewsByID(number)
         review.Status = type
         review.save(db)
-        req.redirect(req.hdf['trac.href.peerReviewView'] + "?Review=" + number)
+        req.redirect(self.env.href.peerReviewView() + "?Review=" + number)
         
