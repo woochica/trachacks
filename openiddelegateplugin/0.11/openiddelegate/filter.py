609c7a314a31524fb8f3141038d120d50a72901b
coderanger <coderanger@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-05-24 06:39:50 +0000

    Initial add.
    git-svn-id: https://trac-hacks.org/svn@3710 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ openiddelegateplugin/0.11/openiddelegate/filter.py
@@ -0,0 +1,52 @@
+# Created by  on 2008-05-23.
+# Copyright (c) 2008 Noah Kantrowitz. All rights reserved.
+import re
+
+from trac.core import *
+from trac.web.api import IRequestFilter
+from trac.web.chrome import add_link, add_warning
+from trac.config import Option
+
+class OpenIDDelegateFilter(Component):
+    """A filter to add OpenID delegate links."""
+
+    implements(IRequestFilter)
+    
+    server = Option('openid', 'server',
+                    doc='Server to use for OpenID delegation.')
+    
+    delegate = Option('openid', 'delegate',
+                      doc='Identity to use for OpenID delegation.')
+
+    known_servers = {
+        r'http://[^.]+.livejournal.com': 'http://www.livejournal.com/openid/server.bml',
+    }
+    
+    def __init__(self):
+        self.known_servers = dict([(re.compile(k, re.U), v) 
+                                  for k, v in self.known_servers.iteritems()])
+
+    # IRequestFilter methods
+    def pre_process_request(self, req, handler):
+        return handler
+
+    def post_process_request(self, req, template, data, mimetype):
+        if self.delegate:
+            add_link(req, 'openid.delegate', self.delegate)
+            server = self.server
+            if not server:
+                for id_regex, server_href in self.known_servers:
+                    if id_regex.match(self.delegate):
+                        server = server_href
+                        break
+                else:
+                    add_warning(req, 'OpenID for identity %s unknown.',
+                                     self.delegate)
+                    return template, data, mimetype
+            add_link(req, 'openid.server', server)
+            
+        elif 'TRAC_ADMIN' in req.perm:
+            # Warn admins if the plugin is enabled, but not configured
+            add_warning(req, 'No OpenID identity specified for delegation.')
+        return template, data, mimetype
+
---
+++ openiddelegateplugin/0.11/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+import os
+
+from setuptools import setup
+
+setup(
+    name = 'TracOpenIDDelegate',
+    version = '1.0',
+    packages = ['openiddelegate'],
+
+    author = 'Noah Kantrowitz',
+    author_email = 'noah@coderanger.net',
+    description = 'Add OpenID delegation links to a Trac site.',
+    long_description = open(os.path.join(os.path.dirname(__file__), 'README').read(),
+    license = 'BSD',
+    keywords = 'trac plugin',
+    url = 'http://trac-hacks.org/wiki/OpenIDDelegatePlugin',
+    classifiers = [
+        'Framework :: Trac',
+        'Development Status :: 1 - Planning',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    
+    install_requires = ['Trac'],
+
+    entry_points = {
+        'trac.plugins': [
+            'openiddelegate.filter = openiddelegate.filter',
+        ]
+    },
+)
