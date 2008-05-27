21b4dc0c3790720e7cbd57491c6d58b15bb9acde
pacopablo <pacopablo@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-05-27 06:33:19 +0000

    Added the option of specifying the number of columns to use for wrapping.  0.11 branch is tested, 0.10 is untested, though should work fine.  Closes #2584
    git-svn-id: https://trac-hacks.org/svn@3720 7322e99d-02ea-0310-aa39-e9a107903beb

--- emailprocessormacro/0.11/emailprocessor.py
+++ emailprocessormacro/0.11/emailprocessor.py
@@ -17,14 +17,25 @@ __all__ = ['EmailMacro']
 class EmailMacro(WikiMacroBase):
     """Email wrapping formatter
 
-    This macro takes an email message and will wrap lines to 72 characters.
-    It will also put the emails inside a preformatted block.
+    This macro takes an email message and will wrap lines to 
+    72 characters (default), or a length specified.  It will
+    also put the emails inside a preformatted block.
 
     Invocation:
     {{{
     #!email
     <email stuff here>
     }}}
+    To wrap to a specified length, the line imediately following
+    the invocation should contain `cols: ` followed by the number
+    of columns at wich we wrap.  For example:
+    {{{
+    #!email
+    cols: 40
+    <email stuff here>
+    }}}
+    It is important that the `cols:` starts at the beginning of
+    the line and that only a number follows it.
 
     """
 
@@ -34,6 +45,17 @@ class EmailMacro(WikiMacroBase):
         yield 'Email'
 
     def expand_macro(self, formatter, name, args):
-        text = args and wrap(args, cols=72) or ''
+        text = ''
+        if args:
+            lines = args.split('\n')
+            if lines[0].startswith('cols:'):
+                try:
+                    width = int(lines[0][5:].strip())
+                    lines.pop(0)
+                except ValueError:
+                    width = 72
+            else:
+                width = 72
+            text = wrap('\n'.join(lines), cols=width)
         return '<pre class="wiki">%s</pre>' % escape(text)
     
