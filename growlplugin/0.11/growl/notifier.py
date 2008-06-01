61b36b2efe2b83d6a92927339aaaaf0859c8b517
eblot <eblot@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-06-01 23:34:59 +0000

    Add optional support for Bitten build notifications and improve notification format
    git-svn-id: https://trac-hacks.org/svn@3761 7322e99d-02ea-0310-aa39-e9a107903beb

--- growlplugin/0.11/growl/notifier.py
+++ growlplugin/0.11/growl/notifier.py
@@ -45,14 +45,28 @@ class GrowlSender(object):
 class GrowlNotifierSystem(Component):
     """Trac notifier for Growl network clients."""
 
+    SOURCES = [u'wiki', u'ticket', u'attachment']
+    
+    # default notification
     implements(IWikiChangeListener, 
                ITicketChangeListener, 
                IAttachmentChangeListener)
 
+    # optional support for Bitten notifications
+    try:
+        from bitten.api import IBuildListener
+        from bitten.model import Build
+        implements(IBuildListener)
+        SOURCES.append(u'bitten')
+    except:
+        pass
+        
     # IAttachmentChangeListener Interface
 
     def attachment_added(self, attachment):
         """Called when an attachment is added."""
+        if 'attachment' not in self.sources:
+            return
         gnp = GrowlNotificationPacket(notification='attachment',
                                       title='Attachment added',
                                       description=attachment.title)
@@ -60,6 +74,8 @@ class GrowlNotifierSystem(Component):
 
     def attachment_deleted(self, attachment):
         """Called when an attachment is deleted."""
+        if 'attachment' not in self.sources:
+            return
         gnp = GrowlNotificationPacket(notification='ticket',
                                       title='Attachment deleted',
                                       description=attachment.title)
@@ -70,23 +86,29 @@ class GrowlNotifierSystem(Component):
 
     def ticket_created(self, ticket):
         """Called when a ticket is created."""
+        if 'ticket' not in self.sources:
+            return
         gnp = GrowlNotificationPacket(notification='ticket',
                                       title='Ticket created',
-                                      description='#%d' % ticket.id)
+                                      description=self._ticket_repr(ticket))
         self._notify(gnp)
 
     def ticket_changed(self, ticket, comment, author, old_values):
         """Called when a ticket is modified."""
+        if 'ticket' not in self.sources:
+            return
         gnp = GrowlNotificationPacket(notification='ticket',
                                       title='Ticket updated',
-                                      description='#%d' % ticket.id)
+                                      description=self._ticket_repr(ticket))
         self._notify(gnp)
 
     def ticket_deleted(self, ticket):
         """Called when a ticket is deleted."""
+        if 'ticket' not in self.sources:
+            return
         gnp = GrowlNotificationPacket(notification='ticket',
                                       title='Ticket deleted',
-                                      description='#%d' % ticket.id)
+                                      description=self._ticket_repr(ticket))
         self._notify(gnp)
 
 
@@ -94,45 +116,92 @@ class GrowlNotifierSystem(Component):
 
     def wiki_page_added(self, page):
         """Called whenever a new Wiki page is added."""
+        if 'wiki' not in self.sources:
+            return
         gnp = GrowlNotificationPacket(notification='wiki',
-                                      title='Wiki page created',
+                                      title='Page created',
                                       description=page.name)
         self._notify(gnp)
 
     def wiki_page_changed(self, page, version, t, comment, author, ipnr):
         """Called when a page has been modified."""
+        if 'wiki' not in self.sources:
+            return
         gnp = GrowlNotificationPacket(notification='wiki',
-                                      title='Wiki page updated',
-                                      description=page.name)
+                                      title='Page updated',
+                                      description=self._wiki_repr(page,
+                                                                  comment))
         self._notify(gnp)
 
     def wiki_page_deleted(self, page):
         """Called when a page has been deleted."""
+        if 'wiki' not in self.sources:
+            return
         gnp = GrowlNotificationPacket(notification='wiki',
-                                      title='Wiki page deleted',
-                                      description=page.name)
+                                      title='Page deleted',
+                                      description=self._wiki_repr(page))
         self._notify(gnp)
 
     def wiki_page_version_deleted(self, page):
         """Called when a version of a page has been deleted."""
+        if 'wiki' not in self.sources:
+            return
         gnp = GrowlNotificationPacket(notification='wiki',
-                                      title='Wiki page removed',
-                                      description= \
-                                        'Last version of %s removed' % \
-                                            page.name)
+                                      title='Page suppressed',
+                                      description=self._wiki_repr(page))
+        self._notify(gnp)
+
+
+    # IBuildListener Interface
+
+    def build_started(build):
+        """Called when a build slave has accepted a build initiation."""
+        if 'bitten' not in self.sources:
+            return
+        gnp = GrowlNotificationPacket(notification='bitten',
+                                      title='Build started',
+                                      description=self._bitten_repr(build),
+                                      priority=-2)
+        self._notify(gnp)
+    
+    def build_aborted(build):
+        """Called when a build slave cancels a build or disconnects."""
+        if 'bitten' not in self.sources:
+            return
+        gnp = GrowlNotificationPacket(notification='bitten',
+                                      title='Build aborted',
+                                      description=self._bitten_repr(build))
+        self._notify(gnp)
+    
+    def build_completed(build):
+        """Called when a build slave has completed a build, regardless of the
+        outcome."""
+        if 'bitten' not in self.sources:
+            return
+        failure = self.status == Build.FAILURE
+        status = 'Build %s' % failure and 'failed' or 'successful'
+        gnp = GrowlNotificationPacket(notification='bitten',
+                                      title=status,
+                                      description=self._bitten_repr(build),
+                                      sticky=failure,
+                                      priority=failure and 2 or 0)
         self._notify(gnp)
 
 
     # Implementation
+    
     def __init__(self):
-        sources = self.config.get('growl', 'sources', 
-                                  'wiki, ticket, attachment')
+        sources = self.config.get('growl', 'sources', ', '.join(self.SOURCES))
         sources = [s.strip().lower() for s in sources.split(',')]
         hosts = self.config.get('growl', 'hosts', '')
         self.hosts = [h.strip() for h in hosts]
+        # Asks Growl clients to register Trac application every time Trac
+        # is started. A bit suboptimal, but that's the only way to register
+        # without an explicit user registration
         grp = GrowlRegistrationPacket()
-        for n in ('wiki', 'ticket', 'attachment'):
+        for n in self.SOURCES:
             grp.addNotification(n, n in sources)
+            self.sources = sources
         gs = GrowlSender()
         gs.notify(grp)
         
@@ -145,3 +214,25 @@ class GrowlNotifierSystem(Component):
             gs.notify(gp)
         except IOError, e:
             self.log.error('Grow notification error: %s', e)
+
+    def _ticket_repr(self, ticket):
+        """String representation of a Trac ticket"""
+        rep = '%s #%d (%s)' % (ticket['summary'], ticket.id, ticket['status'])
+        return rep
+        
+    def _wiki_repr(self, page, comment=None):
+        """String representation of a Trac wiki page"""
+        rep = page.name
+        if comment:
+            rep += '\n%s' % comment
+        return rep
+        
+    def _bitten_repr(self, build):
+        """String representation of a Bitten build"""
+        rep = '%s (%d) r%d' % (build.config, build.id, build.rev)
+        if build.platform:
+            rep += ' %s' % build.platform 
+        if build.slave:
+            rep += ' %s' % build.slave
+        return rep
+        
