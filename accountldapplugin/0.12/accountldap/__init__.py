d46fd9a386849e84f971adc99c177bbfebc40740
clp <clp@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-06-09 08:59:56 +0000

    Version 0.2. Last changes:  * Eliminate dependency with tracldapplugin.  * The code is compatible with 0.11rc1 and 0.12dev version of TRAC.
    git-svn-id: https://trac-hacks.org/svn@3803 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ accountldapplugin/0.12/accountldap/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+ Copyright (C) 2008 General de Software de Canarias.
+ 
+ Author: Carlos López Pérez <carlos.lopezperez@gmail.com>
+'''
+from accountldap import *
