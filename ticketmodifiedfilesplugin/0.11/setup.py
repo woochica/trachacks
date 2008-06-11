748ecc8c320d7c818093906b6ab3bc1930a859fa
e2jk <e2jk@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-06-11 07:18:42 +0000

    Corrected faulty file status (rev is stored as text in the db, not int...)
    git-svn-id: https://trac-hacks.org/svn@3821 7322e99d-02ea-0310-aa39-e9a107903beb

--- ticketmodifiedfilesplugin/0.11/setup.py
+++ ticketmodifiedfilesplugin/0.11/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='TicketModifiedFiles',
     description='Trac plugin that lists the files that have been modified while resolving a ticket',
-    version='0.4',
+    version='0.5',
     license='BSD',
     author='Emilien Klein',
     url='http://trac-hacks.org/wiki/TicketModifiedFilesPlugin',
