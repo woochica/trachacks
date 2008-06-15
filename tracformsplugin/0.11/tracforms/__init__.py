82ac0fe27d8a85638ab84241cfede6659f603c68
rharkins <rharkins@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-06-15 00:38:19 +0000

    Stub checkin.
    git-svn-id: https://trac-hacks.org/svn@3839 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ tracformsplugin/0.11/tracforms/__init__.py
@@ -0,0 +1,3 @@
+
+from tracforms import *
+
---
+++ tracformsplugin/0.11/tracforms/tracforms.py
@@ -0,0 +1,12 @@
+
+import re
+
+from genshi.builder import tag
+from trac.core import *
+from trac.web import IRequestHandler
+from trac.web.chrome import INavigationContributor
+
+class TracFormPlugin(Component):
+    pass
+#    implements(INavigationContributor, IRequestHandler)
+
