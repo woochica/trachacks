b588c27517164a9527ddedd4aed948bf3d4372a4
chmullig <chmullig@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-06-24 16:40:10 +0000

    Script for yoheeb to insert tickets from a CSV. For http://groups.google.com/group/trac-users/browse_frm/thread/32d77507bdb89054
    git-svn-id: https://trac-hacks.org/svn@3889 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ tracmergescript/yoheeb/yoheeb.py
@@ -0,0 +1,17 @@
+from ptrac import Trac
+import csv
+import time
+
+t = Trac('../test/testenv1')
+
+CSV = 'ticketsToAdd.csv'
+
+tickets = csv.DictReader(open(CSV))
+
+for tick in tickets:
+	tick['id'] = int(tick['id'])
+	tick['time'] = int(time.time())
+	tick['changetime'] = int(time.time())
+	tick['ticket_change'] = []
+	tick['attachment'] = []
+	t.addTicket(tick, '')
