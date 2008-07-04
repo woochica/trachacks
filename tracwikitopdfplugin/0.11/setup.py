5ca591bd469de7891d2c403b3900ead28656e1dc
diorgenes <diorgenes@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-07-04 17:46:47 +0000

    Applied diff (ticket #3278) but I did some modification. Thank you!
    git-svn-id: https://trac-hacks.org/svn@3963 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ tracwikitopdfplugin/0.11/setup.py
@@ -0,0 +1,31 @@
+"""
+Copyright (C) 2008 Prognus Software Livre - www.prognus.com.br
+Author: Diorgenes Felipe Grzesiuk <diorgenes@prognus.com.br>
+"""
+
+from setuptools import setup
+
+setup(
+    name = 'TracWikiToPdfPlugin',
+    version = '2.0',
+    packages = ['wikitopdf'],
+    package_data={ 'wikitopdf' : [ 'templates/*.cs', 'htdocs/js/*.js' ] },     
+    author = "Diorgenes Felipe Grzesiuk",
+    author_email = "diorgenes@prognus.com.br",
+    description = "Generating PDF files from Wiki pages",
+    long_description = "WikiToPdfPlugin was developed based on the work of coderanger and athomas. WikiToPdf combines the functionality of CombineWikiPlugin and PagetoPdfPlugin on only one plugin and allows one to setup a template file used to generate the PDF file with a cover and a licence page, for example. ",
+    license = "GPL",
+    keywords = "trac plugin wiki pdf",
+    url = "http://trac-hacks.org/wiki/TracWikiToPdfPlugin",
+
+    entry_points = {
+        'trac.plugins': [
+            'wikitopdf.formats   = wikitopdf.formats',
+            'wikitopdf.web_ui    = wikitopdf.web_ui',
+            'wikitopdf.wikitopdf = wikitopdf.wikitopdf',
+        ],
+    },
+ 	
+    install_requires = [ 'Trac' ],
+    
+)
---
+++ tracwikitopdfplugin/0.11/wikitopdf/htdocs/js/admin_wikitopdf.js
@@ -0,0 +1,32 @@
+function move_item(from, to) {
+    var from_box = document.getElementById(from+'pages_select');
+    var to_box = document.getElementById(to+'pages_select');
+    for (var i = 0; i < from_box.options.length; i++) {
+       var opt = from_box.options[i];
+       if (opt.selected) {
+           to_box.options.add(opt);
+           i--;
+       }
+    }
+}
+
+function reorder_item(from, dir) {
+    var box = document.getElementById(from+'pages_select');
+    var i = box.selectedIndex;
+    var j = i + dir;
+    if(j<0 || j>=box.options.length) { return }
+    var temp = box.options[i];
+    var temp2 = box.options[j];
+    box.options[i] = new Option(temp2.value, temp2.value);
+    box.options[j] = new Option(temp.value, temp.value);
+    box.selectedIndex = j;
+}
+
+function compile_pages(form) {
+    var arr = new Array();
+    for(i=0;i<form.rightpages.options.length;i++) {
+        arr.push(form.rightpages.options[i].value);
+    }
+    form.rightpages_all.value = arr.join(',');
+    return 1;
+}
