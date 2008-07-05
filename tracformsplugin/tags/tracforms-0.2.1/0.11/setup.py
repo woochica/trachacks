34d4c7f4a5a871e24e8928762429f222b12e3586
rharkins <rharkins@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-07-05 18:46:45 +0000

    Fixed release version.
    git-svn-id: https://trac-hacks.org/svn@3971 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ tracformsplugin/tags/tracforms-0.2.1/0.11/setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import find_packages, setup
+
+setup(
+    name="TracForms",
+    description='Provides forms on any page with a wiki',
+    version="0.2",
+    author='Rich Harkins',
+    author_email='rich@worldsinfinite.com',
+    license='GPL',
+    packages=find_packages(exclude=['*.tests*']),
+    entry_points = '''
+        [trac.plugins]
+        tracforms = tracforms
+    ''')
+
