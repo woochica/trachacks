92836fe2e6c2ef7af5e303ff36a2c0de667bcaa0
rharkins <rharkins@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-07-05 18:45:36 +0000

    Added missing file.
    git-svn-id: https://trac-hacks.org/svn@3968 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ tracformsplugin/branches/tracforms-0.2/0.11/tracforms/errors.py
@@ -0,0 +1,24 @@
+
+class TracFormError(Exception):
+    def __str__(self):
+        return self.message % self.args
+
+class TracFormTooManyValuesError(TracFormError):
+    def __init__(self, name):
+        TracFormError.__init__(self, name)
+
+    message = ('ERROR: Too many values for TracForm variable %r' +
+        ' (maybe the same field is being used multiple times?)')
+
+class TracFormNoOperationError(TracFormError):
+    def __init__(self, name):
+        TracFormError.__init__(self, name)
+
+    message = 'ERROR: No TracForm operation named %r'
+
+class TracFormNoCommandError(TracFormError):
+    def __init__(self, name):
+        TracFormError.__init__(self, name)
+
+    message = 'ERROR: No TracForm command named %r'
+
