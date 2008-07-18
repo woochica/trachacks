7d644470efe23d50c5c84820b1d58ab22f8f199b
e2jk <e2jk@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-07-18 09:46:24 +0000

    Fixing #3254 (list of changesets)
    git-svn-id: https://trac-hacks.org/svn@4037 7322e99d-02ea-0310-aa39-e9a107903beb

--- ticketmodifiedfilesplugin/0.11/setup.py
+++ ticketmodifiedfilesplugin/0.11/setup.py
@@ -3,14 +3,14 @@ from setuptools import find_packages, setup
 setup(
     name='TicketModifiedFiles',
     description='Trac plugin that lists the files that have been modified while resolving a ticket.',
-    version='0.7',
+    version='0.7.1',
     license='BSD-ish (see the COPYING.txt file)',
     author='Emilien Klein',
     author_email='Emilien Klein <e2jk AT users DOT sourceforge DOT net>',
     url='http://trac-hacks.org/wiki/TicketModifiedFilesPlugin',
     packages=['ticketmodifiedfiles'],
     entry_points = {'trac.plugins': ['ticketmodifiedfiles = ticketmodifiedfiles']},
-    package_data={'ticketmodifiedfiles': ['templates/*.html', 'htdocs/css/*.css']},
+    package_data={'ticketmodifiedfiles': ['templates/*.html', 'htdocs/css/*.css', 'htdocs/js/*.js']},
     dependency_links=['http://svn.edgewall.org/repos/genshi/trunk#egg=Genshi-dev'],
     install_requires=['Genshi >= 0.5.dev-r698,==dev'],
 )
