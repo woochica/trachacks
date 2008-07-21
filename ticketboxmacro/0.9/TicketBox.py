72d7fbca0b556df347493f2b83b86c5981d3c8a9
gotoh <gotoh@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-07-21 18:57:05 +0000

    Backport of r4049.
    git-svn-id: https://trac-hacks.org/svn@4050 7322e99d-02ea-0310-aa39-e9a107903beb

--- ticketboxmacro/0.9/TicketBox.py
+++ ticketboxmacro/0.9/TicketBox.py
@@ -11,13 +11,17 @@ Example:
 [[TicketBox({1})]]                     ... expand report result as ticket list
 [[TicketBox([report:1])]]              ... alternate format of report
 [[TicketBox([report:9?name=val])]]     ... report with dynamic variable
-[[TicketBox({1),#50,{2},100)]]         ... convination of above
+[[TicketBox({1},#50,{2},100)]]         ... convination of above
 [[TicketBox(500pt,{1})]]               ... with box width as 50 point
 [[TicketBox(200px,{1})]]               ... with box width as 200 pixel
 [[TicketBox(25%,{1})]]                 ... with box width as 25%
 [[TicketBox('Different Title',#1,#2)]] ... Specify title
 [[TicketBox(\"Other Title\",#1,#2)]]     ... likewise
 [[TicketBox('%d tickets',#1,#2)]]      ... embed ticket count in title
+[[TicketBox({1}, inline)]]             ... display the box as block element.
+[[TicketBox({1}, summary)]]            ... display with summary per line
+[[TicketBox({1}, summary=Titre)]]      ... specify field name of summary
+[[TicketBox({1}, ticket=ID)]]          ... specify field name of ticket num.
 [[TicketBox({1}, nosort)]]             ... display numbers without sort
 }}}
 
@@ -53,9 +57,13 @@ args_pat = [r"#?(?P<tktnum>\d+)",
             r"{(?P<rptnum>\d+)}",
             r"\[report:(?P<rptnum2>\d+)(?P<dv>\?.*)?\]",
             r"(?P<width>\d+(pt|px|%))",
-            r"(?P<keyword>nosort|summary|inline)",
+            r"(?P<keyword>nosort|summary|inline|ticket)(?:=(?P<kwarg>.*))?",
             r"(?P<title1>'.*')",
             r'(?P<title2>".*")']
+
+# default name of fields
+default_summary_field = 'summary'
+default_ticket_field = 'ticket'
 
 def uniq(x):
     y=[]
@@ -72,13 +80,16 @@ def execute(hdf, txt, env):
     if not txt:
         txt = ''
     items = []
+    summary = None
+    ticket = default_ticket_field
     summaries = {}
-    show_summary = False
     inline = False
     nosort = False
     title = "Tickets"
     args_re = re.compile("^(?:" + string.join(args_pat, "|") + ")$")
-    for arg in [string.strip(s) for s in txt.split(',')]:
+    args = [string.strip(s) for s in txt.split(',')]
+    # process options first
+    for arg in args:
         match = args_re.match(arg)
         if not match:
             env.log.debug('TicketBox: unknown arg: %s' % arg)
@@ -89,16 +100,22 @@ def execute(hdf, txt, env):
             title = match.group('title2')[1:-1]
         elif match.group('width'):
             styles['width'] = match.group('width')
-        elif match.group('tktnum'):
-            items.append(int(match.group('tktnum')))
         elif match.group('keyword'):
             kw = match.group('keyword').lower()
+            kwarg = match.group('kwarg')
             if kw == 'summary':
-                show_summary = True
+                summary = kwarg or default_summary_field
             elif kw == 'inline':
                 inline = True
             elif kw == 'nosort':
                 nosort = True
+    # pick up ticket numbers and report numbers
+    for arg in args:
+        match = args_re.match(arg)
+        if not match:
+            continue
+        elif match.group('tktnum'):
+            items.append(int(match.group('tktnum')))
         elif match.group('rptnum') or match.group('rptnum2'):
             num = match.group('rptnum') or match.group('rptnum2')
             dv = {}
@@ -128,11 +145,21 @@ def execute(hdf, txt, env):
                 rows = curs.fetchall()
                 if rows:
                     descriptions = [desc[0] for desc in curs.description]
-                    idx = descriptions.index('ticket')
-                    summ = descriptions.index('summary')
+                    try:
+                        idx = descriptions.index(ticket)
+                    except:
+                        raise Exception('No such column for ticket: %r'
+                                        % ticket )
+                    if summary:
+                        try:
+                            sidx = descriptions.index(summary)
+                        except:
+                            raise Exception('No such column for summary: %r'
+                                            % summary)
                     for row in rows:
                         items.append(row[idx])
-                        summaries[row[idx]] = row[summ]
+                        if summary:
+                            summaries[row[idx]] = row[sidx]
             finally:
                 if not hasattr(env, 'get_cnx_pool'):
                     # without db connection pool, we should close db.
@@ -143,7 +170,7 @@ def execute(hdf, txt, env):
         items.sort()
     html = ''
 
-    if show_summary:
+    if summary:
         html = string.join([wiki_to_oneliner("%s (#%d)" % (summaries[n], n),
                                              env, env.get_db_cnx()) for n in items], "<br>")
     else:
