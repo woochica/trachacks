da6156b7050eb11eb9651c510e0f15cb42841a06
e2jk <e2jk@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-07-24 10:50:20 +0000

    Implementing #3441 (Hide the checkboxes by default)
    git-svn-id: https://trac-hacks.org/svn@4057 7322e99d-02ea-0310-aa39-e9a107903beb

--- ticketmodifiedfilesplugin/0.11/setup.py
+++ ticketmodifiedfilesplugin/0.11/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='TicketModifiedFiles',
     description='Trac plugin that lists the files that have been modified while resolving a ticket.',
-    version='0.8',
+    version='0.8.1',
     license='BSD-ish (see the COPYING.txt file)',
     author='Emilien Klein',
     author_email='Emilien Klein <e2jk AT users DOT sourceforge DOT net>',
--- ticketmodifiedfilesplugin/0.11/ticketmodifiedfiles/htdocs/js/ticketmodifiedfiles.js
+++ ticketmodifiedfilesplugin/0.11/ticketmodifiedfiles/htdocs/js/ticketmodifiedfiles.js
@@ -9,5 +9,24 @@
                 document.getElementById("changesetslink").innerHTML="Hide the list of changesets relative to this ticket.";
 		    }
 		}
+        if(element == "checkboxes"){
+            inputs = document.getElementsByTagName("input");
+            actualvisibility = "";
+            for(j=0; j<inputs.length; j++){
+                if(inputs[j].getAttribute('class') == "helpcheckbox"){
+                    if(inputs[j].style.display == "block"){
+                        inputs[j].style.display = "none";
+                    } else {
+                        inputs[j].style.display = "block";
+                    }
+                    actualvisibility = inputs[j].style.display;
+                }
+            }
+            if(actualvisibility == "block"){
+                document.getElementById("togglecheckboxlink").innerHTML = "Hide the checkboxes";
+            } else {
+                document.getElementById("togglecheckboxlink").innerHTML = "Display the checkboxes";
+            }
+        }
 	}
 //-->
