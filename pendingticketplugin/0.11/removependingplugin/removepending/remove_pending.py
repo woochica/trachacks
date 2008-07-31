3a543e76ce7edb5d7d72dff2ffde2773f2b1cf78
datallah <datallah@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-07-31 20:52:50 +0000

    Updates to support 0.11 (now featuring the ability to unset pending when attachments are uploaded)
    git-svn-id: https://trac-hacks.org/svn@4099 7322e99d-02ea-0310-aa39-e9a107903beb

--- pendingticketplugin/0.11/removependingplugin/removepending/remove_pending.py
+++ pendingticketplugin/0.11/removependingplugin/removepending/remove_pending.py
@@ -1,33 +1,80 @@
 # Plugin to remove Pending status when the reporter modifies a ticket
-# Copyright 2007 Daniel A. Atallah <datallah@pidgin.im>
+# Copyright 2007-2008 Daniel A. Atallah <datallah@pidgin.im>
 
+from datetime import datetime
 from trac.core import *
-from trac.ticket import ITicketChangeListener
+from trac.ticket import Ticket, ITicketChangeListener
+from trac.ticket.notification import TicketNotifyEmail
+from trac.ticket.web_ui import TicketModule
+from trac.attachment import IAttachmentChangeListener
+from trac.util.datefmt import to_timestamp, utc
 
 class RemovePendingPlugin(Component):
-	implements (ITicketChangeListener)
+	implements (ITicketChangeListener, IAttachmentChangeListener)
 
 	def ticket_created(self, ticket):
 		pass
 
 	def ticket_changed(self, ticket, comment, author, old_values):
-		if (author == ticket['reporter'] and ticket['pending'] == '1' and not old_values.has_key('pending')):
-			db, handle_ta = ticket._get_db_for_write(None)
-			cursor = db.cursor()
+		if (not old_values.has_key('pending')):
+                	if (author == ticket['reporter'] and ticket['pending'] == '1'):
+	                	self.env.log.info('Removing Pending status for ticket %s due to comment'
+						  % (ticket.id))
 
-			cursor.execute("UPDATE ticket_custom SET value = %s " \
-				" WHERE ticket = %s AND name = %s ",
-				('0', ticket.id, 'pending'))
+	                        db, handle_ta = ticket._get_db_for_write(None)
+        	                cursor = db.cursor()
 
-			#Add the ticket change so that it will appear
-			#correctly in the history and notifications
-			cursor.execute("INSERT INTO ticket_change "
-				"(ticket,time,author,field,oldvalue,newvalue) "
-				"VALUES (%s, %s, %s, %s, %s, %s)",
-			(ticket.id, ticket.time_changed, author, 'pending', '1', '0'))
+                	        cursor.execute("UPDATE ticket_custom SET value = %s " \
+                        	        " WHERE ticket = %s AND name = %s ",
+                                	('0', ticket.id, 'pending'))
 
-			db.commit();
+	                        #Add the ticket change so that it will appear
+        	                #correctly in the history and notifications
+                	        cursor.execute("INSERT INTO ticket_change "
+                        	        "(ticket,time,author,field,oldvalue,newvalue) "
+                                	"VALUES (%s, %s, %s, %s, %s, %s)",
+	                        (ticket.id, to_timestamp(time_changed), author, 'pending', '1', '0'))
+
+        	                db.commit();
 
 	def ticket_deleted(self, ticket):
+		pass
+
+	def attachment_added(self, attachment):
+		# Check whether we're dealing with a ticket resource
+		resource = attachment.resource
+		while resource:
+			if resource.realm == 'ticket':
+				break
+			resource = resource.parent
+
+		if (resource and resource.realm == 'ticket' and resource.id is not None):
+			db = attachment.env.get_db_cnx();
+			ticket = Ticket(attachment.env, resource.id, db)
+                	if (attachment.author == ticket['reporter'] and ticket['pending'] == '1'):
+	                	self.env.log.info('Removing Pending status for ticket %s due to attachment'
+						  % (ticket.id))
+
+				comment = 'Attachment (%s) added by ticket reporter.' % (attachment.filename)
+				ticket['pending'] = '0'
+
+				# determine sequence number...
+				cnum = 0
+				tm = TicketModule(self.env)
+				for change in tm.grouped_changelog_entries(ticket, db):
+					if change['permanent']:
+		                                cnum += 1
+
+				#We can't just use attachment.date as it screws up event sequencing
+				now = datetime.now(utc)
+
+				ticket.save_changes(attachment.author, comment, now, db, cnum + 1)
+		                db.commit()
+
+				#trigger notification since we've changed the ticket
+                		tn = TicketNotifyEmail(self.env)
+		                tn.notify(ticket, newticket=False, modtime=now)
+
+	def attachment_deleted(self, attachment):
 		pass
 
--- pendingticketplugin/0.11/removependingplugin/setup.py
+++ pendingticketplugin/0.11/removependingplugin/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name = 'RemovePendingStatusPlugin',
-    version = '0.0.2',
+    version = '0.0.3',
     packages = ['removepending'],
 
     author = "Daniel Atallah",
