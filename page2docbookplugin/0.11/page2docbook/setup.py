a517f9ca55a86b96a36871f31b88f0eba336501b
FilipeCorreia <FilipeCorreia@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-08-11 15:01:04 +0000

    Code cleanup, and trivial changes to setup.py.
    git-svn-id: https://trac-hacks.org/svn@4127 7322e99d-02ea-0310-aa39-e9a107903beb

--- page2docbookplugin/0.11/page2docbook/setup.py
+++ page2docbookplugin/0.11/page2docbook/setup.py
@@ -1,12 +1,25 @@
 from setuptools import setup
 
 PACKAGE = 'PageToDocbook'
-VERSION = '0.5'
+VERSION = '0.6'
 
 setup(name=PACKAGE,
       version=VERSION,
       packages=['pagetodocbook'],
       entry_points={'trac.plugins': '%s = pagetodocbook' % PACKAGE},
+
       package_data={'pagetodocbook': ['data/*.*']},
+
+      # dependencies
+      install_requires = ['setuptools',
+                          ],
+
+      #extras_require = {'tidy':['ctypes','utidylib']},
+
+      #dependency_links=['http://cctools.svn.sourceforge.net/svnroot/cctools/vendorlibs/utidylib/#egg=utidylib-0.2-cvs',],
+
+      # author metadata
+      author = 'Filipe F. Correia',
+      author_email = 'fcorreia@gmail.com',
 )
 
