fa7091758c475b9d8fd451e2a5bd69b2a39bcfcc
athomas <athomas@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-08-14 12:58:04 +0000

    Applied patch from takanorig@gmail.com, thanks. Fixes #2258.
    git-svn-id: https://trac-hacks.org/svn@4139 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ reposearchplugin/0.11/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+PACKAGE = 'tracreposearch'
+VERSION = '0.1'
+
+setup(name='tracreposearch',
+      version='0.2',
+      packages=['tracreposearch'],
+      author='Alec Thomas',
+      url="http://trac-hacks.org/wiki/TracRepoSearch",
+      license='BSD',
+      scripts=['update-index'],
+      entry_points = {'trac.plugins': ['tracreposearch = tracreposearch']})
