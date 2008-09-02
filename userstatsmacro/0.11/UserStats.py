5681e3acc985073ab8e6949a933c97999a11aa6d
pedro <pedro@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-09-02 00:35:46 +0000

    Initial version
    git-svn-id: https://trac-hacks.org/svn@4221 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ userstatsmacro/0.11/UserStats.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""Adds a wiki macro [[UserStats]] which produces a table with all the users and their last loginså
+
+pedro@readingtab.com
+"""
+
+import inspect
+import sys
+import re
+import time
+import datetime
+
+from trac.core import Component, implements
+from trac.wiki.api import IWikiMacroProvider
+from trac.wiki import format_to_html
+
+class UserStatsProcessor(Component):
+    implements(IWikiMacroProvider)
+
+    # IWikiMacroProvider interface
+
+    def get_macros(self):
+        yield 'UserStats'
+
+    def get_macro_description(self, name):
+        return inspect.getdoc(sys.modules.get(self.__module__))
+    
+    def get_user_last_login(self,user):
+        cursor = self.env.get_db_cnx().cursor()
+        cursor.execute("SELECT last_visit FROM session "
+                       "WHERE sid=%s AND authenticated=%s",
+                       (user, 1))
+        row = cursor.fetchone()
+        if not row:
+            return
+
+        when = time.localtime(int(row[0]))
+        last = datetime.datetime.fromtimestamp(int(row[0]))
+        now = datetime.datetime.fromtimestamp(time.time())
+        how_long = now - last
+        last_date = time.strftime("%Y/%m/%d %H:%M:%S", when)
+        if how_long.days>0:
+            return "|| %s || %s || %s day(s)||\n" % (user, last_date, how_long.days)
+        else:
+            return "|| %s || %s || <24h ||\n" % (user, last_date)
+    
+    def get_users_last_login(self):
+        db = self.env.get_db_cnx()
+        cursor = db.cursor()
+        result = set()
+        dt = "||'''User'''||'''Last Login'''||'''How long ago'''||\n"
+        users = set([u[0] for u in self.env.get_known_users()])
+        for user in sorted(users):
+            dt += self.get_user_last_login(user) 
+        return dt
+
+    
+    def expand_macro(self, formatter, name,other):
+        dt = self.get_users_last_login()
+        content = format_to_html(self.env, formatter.context, dt)
+        content = '<div class="component-list">%s</div>' % content
+        return content
