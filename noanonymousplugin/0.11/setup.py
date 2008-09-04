b23402d5a4e88fd0a64ad39b081e07b43ce07e38
pedro <pedro@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-09-04 23:36:53 +0000

    initial import
    git-svn-id: https://trac-hacks.org/svn@4239 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ noanonymousplugin/0.11/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+
+from setuptools import setup
+
+setup(
+    name = 'TracNoAnonymous',
+    version = '2.0',
+    packages = ['noanonymous'],
+
+    author = 'Pedro Paixao',
+    author_email = 'pedro@readingtab.com',
+    description = 'Redirect anonymous users to the login screen on PermissionError.',
+    license = 'BSD',
+    keywords = 'trac plugin',
+    url = 'http://trac-hacks.org/wiki/NoAnonymousPlugin',
+    classifiers = [
+        'Framework :: Trac',
+    ],
+    
+    install_requires = ['Trac'],
+
+    entry_points = {
+        'trac.plugins': [
+            'noanonymous.filter = noanonymous.filter',
+        ]
+    },
+)
+
