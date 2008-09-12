524a181ace4384cd778112cf4deb68816c02ea2d
CStrac <CStrac@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-09-12 10:10:57 +0000

    git-svn-id: https://trac-hacks.org/svn@4273 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ flexiblereporternotificationplugin/0.11/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+PACKAGE = 'flexiblereporternotification'
+
+setup(name=PACKAGE,
+      version='0.0.1',
+      packages=[PACKAGE],
+      url='http://trac-hacks.org/wiki/FlexibleReporterNotificationPlugin',
+      author='Satyam'
+      long_description="""
+      
+      """,
+      entry_points={'trac.plugins': '%s = %s' % (PACKAGE, PACKAGE)},
+)
+
