15b7a6bc117939e442179da442a3cf8ad4246071
k0s <k0s@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-09-19 20:45:42 +0000

    get list items from a string
    git-svn-id: https://trac-hacks.org/svn@4296 7322e99d-02ea-0310-aa39-e9a107903beb

--- traclegosscript/anyrelease/traclegos/wsgiapp.py
+++ traclegosscript/anyrelease/traclegos/wsgiapp.py
@@ -7,7 +7,7 @@ from traclegos.web import View
 def str2list(string):
     """returns a list from a comma-separated string"""
     # XXX this could go in a utils.py file
-    return [ i.strip() for i in string.split(',') ]
+    return [ i.strip() for i in string.split(',') if i.strip() ]
 
 def make_app(global_conf, **app_conf):
     """create the traclegos view and wrap it in middleware"""
@@ -16,5 +16,7 @@ def make_app(global_conf, **app_conf):
     args = dict([(key.split(key_str, 1)[-1], value)
                  for key, value in app_conf.items()
                  if key.startswith(key_str) ])
+    for item in list_items:
+        args[item] = str2list(args.get(item, ''))
     app = View(**args)
     return HTTPExceptionHandler(app)
