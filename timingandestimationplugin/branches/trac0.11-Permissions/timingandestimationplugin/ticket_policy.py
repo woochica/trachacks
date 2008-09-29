da73dd8d116fc9e019bacb8384686e9be860d3b3
bobbysmith007 <bobbysmith007@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-09-29 17:16:53 +0000

    re #3828 fixed a bug in ticket policy where it should have been checking for a permission or a group, but was only checking for a permission
    git-svn-id: https://trac-hacks.org/svn@4358 7322e99d-02ea-0310-aa39-e9a107903beb

--- timingandestimationplugin/branches/trac0.11-Permissions/timingandestimationplugin/ticket_policy.py
+++ timingandestimationplugin/branches/trac0.11-Permissions/timingandestimationplugin/ticket_policy.py
@@ -7,7 +7,8 @@ from trac.util.compat import set
 class InternalTicketsPolicy(Component):
     """Hide internal tickets."""
     implements(IPermissionPolicy)
-    
+    group_providers = ExtensionPoint(IPermissionGroupProvider)
+
     # IPermissionPolicy(Interface)
     def check_permission(self, action, username, resource, perm):
         self.log.debug("Internal: action:%s, user:%s, resource:%s, perm: %s" %
@@ -19,22 +20,43 @@ class InternalTicketsPolicy(Component):
                 break
             resource = resource.parent
         if resource and resource.realm == 'ticket' and resource.id is not None:
-            rtn = self.check_ticket_access(perm, resource)
+            rtn = self.check_ticket_access(perm, resource, username)
             self.log.debug("Internal: RESULTS for %s: %s" % (action,rtn))
             return rtn
         return None
-    
+
+    # Internal methods   
+    def _get_groups(self, user):
+        # Get initial subjects
+        groups = set([user])
+        for provider in self.group_providers:
+            for group in provider.get_permission_groups(user):
+                groups.add(group)
+        
+        perms = PermissionSystem(self.env).get_all_permissions()
+        repeat = True
+        while repeat:
+            repeat = False
+            for subject, action in perms:
+                if subject in groups and action.islower() and action not in groups:
+                    groups.add(action)
+                    repeat = True 
+        
+        return groups    
+
     # Public methods
-    def check_ticket_access(self, perm, res):
+    def check_ticket_access(self, perm, res, user):
         """Return if this req is permitted access to the given ticket ID."""
         try:
             tkt = Ticket(self.env, res.id)
         except TracError:
             return None # Ticket doesn't exist
-        private_tkt = tkt.get_value_or_default('internal') == '1'
+        private_tkt = tkt['internal'] == '1'
 
         if private_tkt:
             # cant just check or we get in an infinite call loop
             perm = PermissionCache(self.env, self.username, None, perm._cache)
-            return perm.has_permission(self.config.get('ticket', 'internalgroup', 'TIME_ADMIN' ).upper())
+            groups = self._get_groups(user)
+            perm_or_group = self.config.get('ticket', 'internalgroup', 'TIME_ADMIN' )
+            return perm_or_group in groups or perm.has_permission(perm_or_group)
         return None
