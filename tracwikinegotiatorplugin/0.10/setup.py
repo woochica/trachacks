1d2bf097a93779bdef90833a5631d124d3019320
gotoh <gotoh@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-10-02 02:11:31 +0000

    Alter the way to list up system pages (#3832).
    Now we know system page names by listing files in 'wiki-default'
    directory. This is a solution of for ticket #3832.
    
    Note that the pages provided by plugins (they are not placed in
    wiki-default directory) are not categorized as system page any more.
    
    There is no way to force them categorizing as system page yet,
    but it may be implemented in near future.
    
    
    git-svn-id: https://trac-hacks.org/svn@4367 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracwikinegotiatorplugin/0.10/setup.py
+++ tracwikinegotiatorplugin/0.10/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 PACKAGE = 'TracWikiNegotiator'
-VERSION = '1.0'
+VERSION = '1.1'
 
 setup(name=PACKAGE, version=VERSION,
       description='Content negotiation plugin for Trac wiki page.',
--- tracwikinegotiatorplugin/0.10/wikinegotiator/macros.py
+++ tracwikinegotiatorplugin/0.10/wikinegotiator/macros.py
@@ -1,12 +1,38 @@
 # -*- coding: utf-8 -*-
-import re
+import os, re
 from trac.core import *
 from trac.config import Option, ListOption, BoolOption
 from trac.util import sorted
 from trac.wiki import WikiSystem, html
 from trac.wiki.macros import WikiMacroBase
 
+
+
 ## macro
+
+def _list_wiki_default_pages():
+    """List wiki page files in 'wiki-default' directory.
+    This is aimed to know page names to be categorized as 'system'.
+    """
+    # get wiki-default directory
+    try:
+        # try for 0.10.x
+        from trac.config import default_dir
+        dir = default_dir('wiki')
+    except:
+        # try for 0.11.x (with setup tools)
+        import pkg_resources
+        dir = pkg_resources.resource_filename('trac.wiki', 'default-pages')
+    if not os.path.isdir(dir):
+        return []
+    pages = []
+    # list wiki default pages. No need to include lang variations here.
+    re_wiki_page = re.compile(r'[A-Z][a-zA-Z0-9]+')
+    for f in os.listdir(dir):
+        if re_wiki_page.match(f):
+            pages.append(f)
+    return pages
+            
 
 class MultiLangTitleIndex(WikiMacroBase):
     """Inserts an alphabetic list of all wiki pages cosidering suffixes.
@@ -33,72 +59,55 @@ class MultiLangTitleIndex(WikiMacroBase):
                                       doc="List of page names to be grouped"
                                       " in user page.")
 
+    _wiki_default_pages = _list_wiki_default_pages()
+    
     def render_macro(self, req, name, content):
-        # get system pages which is author=trac at version 1
         env = self.env
-        db = env.get_db_cnx()
-        curs = db.cursor()
-        # Pick pages whose author is 'trac' in some version.  This means
-        # that the system page modified by user is grouped as 'system
-        # page'.
-        curs.execute("select name,author from wiki")
-        system_pages = []                   # pages marked as system's
+        wiki = WikiSystem(env)
+        # Get system provided pages excluding some.
+        system_pages = list(self._wiki_default_pages) # pages provided by trac
+        for page in self._explicit_user_pages:
+            if page in system_pages:
+                system_pages.remove(page)
+        # Pick all wiki pages and make list of master pages and also
+        # make list of language variations for each master page.
+        # The name of master page should not have language suffix.
+        # And make list of user pages (non system page).
+        user_pages = []                       # pages marked as user's
         lang_map = {}                       # language list for each page
-        re_lang = re.compile('^(.*)\.([a-z]{2}(?:-[a-z]{2})?)$')
-        for page, author in curs:
+        re_lang = re.compile('^(.*)\.([a-z]{2}(?:-[a-z]{2})?)?$')
+        for page in wiki.get_pages():
             # note: language variant is not stored in system_pages.
             m = re_lang.match(page)
             if m:
                 p, l = m.groups()
-                langs = lang_map.get(p,[])
-                if l not in langs:
-                    lang_map[p] = langs + [l]
-            elif author == 'trac' and page not in system_pages:
-                system_pages.append(page)
-        if not hasattr(env, 'get_cnx_pool'):
-            # without db connection pool, we should close db.
-            curs.close()
-            db.close()
-        # Exclude some pages to be grouped in user page.
-        for page in self._explicit_user_pages:
-            if page in system_pages:
-                system_pages.remove(page)
-        # Pick user pages. Specially for language variation pages,
-        # translated page of system page is also grouped as system page as
-        # if author is not 'trac'. This is for shrinked view.
-        re_sys = re.compile('^(?:' + '|'.join(system_pages) +
-                            '(?:\.[a-z]{2}(?:-[a-z]{2})?)?)')
-        prefix = content or None
-        wiki = WikiSystem(env)
-        user_pages = []                       # pages marked as user's
-        pages = list(wiki.get_pages(prefix))
-        for page in lang_map.keys():
-            # register base page despite of eixstance
-            if page not in pages:
-                pages.append(page)
-        for page in sorted(pages):
-            if re_lang.match(page):
-                continue
-            if not re_sys.match(page):
-                user_pages.append(page)
-        ## should be langu sub item'ed
+            else:
+                p, l = page, None
+            langs = lang_map.get(p,[])
+            if l and l not in langs:
+                lang_map[p] = langs + [l]
+            if p not in system_pages and p not in user_pages:
+                user_pages.append(p)
+        # generate output
+        prefix = content
         tr = html.TR(valign='top')
         for title, pages in (('Project', user_pages),
                              ('System Provided', system_pages)):
             if 0 < len(pages):
                 td = html.TD
                 tr.append(td)
-                if not prefix:
-                    td.append(html.B(title + ' Pages'))
+                td.append(html.B(title + ' Pages'))
                 ul = html.UL()
                 td.append(ul)
                 for page in sorted(pages):
+                    if prefix and not page.startswith(prefix):
+                        continue
                     item = [html.A(wiki.format_page_name(page),
                                    href=env.href.wiki(page))]
                     if lang_map.has_key(page):
                         item.append(' (')
                         langs = lang_map[page]
-                        for lang in sorted(langs) + ['other']:
+                        for lang in sorted(langs):
                             p = '%s.%s' % (page, lang)
                             item.append(html.A(lang,
                                                href=env.href.wiki(page) + '?lang='+lang,
