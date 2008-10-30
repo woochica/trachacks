89a03aa653fd0cc5b018adf46f73146185444da3
optilude <optilude@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-10-30 13:34:47 +0000

    Initial checkin - mostly working
    git-svn-id: https://trac-hacks.org/svn@4668 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ wikitablemacro/0.11/trunk/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+
+from setuptools import setup
+
+setup(
+    name = 'wikitable',
+    author = 'Martin Aspeli',
+    author_email = 'optilude@gmail.com',
+    description = 'Trac plugin for drawing a table from a SQL query in a wiki page',
+    version = '0.1',
+    license='BSD',
+    packages=['wikitable'],
+    package_data={'helloworld': ['htdocs/css/*.css',]},
+    entry_points = {
+        'trac.plugins': [
+            'wikitable = wikitable'
+        ]
+    },
+)
