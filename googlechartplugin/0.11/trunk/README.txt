731bf23a34ee67f26c9be81a5b3a0aa564a40791
optilude <optilude@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-10-31 14:49:15 +0000

    Add support for labels
    git-svn-id: https://trac-hacks.org/svn@4684 7322e99d-02ea-0310-aa39-e9a107903beb

--- googlechartplugin/0.11/trunk/README.txt
+++ googlechartplugin/0.11/trunk/README.txt
@@ -21,6 +21,14 @@ and/or the Google Chart API (http://code.google.com/apis/chart/):
 
   [[GChart(type="bvs", chtt="My title" chco="4d89f9,c6d9fd", query="SELECT id, (time - 1225107400) FROM ticket")]]
 
+If you'd like columns from the result set to be used as axis labels (bar,
+line and scatter charts), you can specify the index (starting at 0) of the
+column, e.g.::
+
+  [[GChart(type="bvs", query="SELECT label, value FROM my_table", xlabels=0)]]
+
+The possible axes are xlabels, ylabels, tlabels (top) and rlabels (right).
+
 When constructing a query, it should return columns of numbers - other data
 will be ignored. By default, each column is treated as a chart data set, using
 text encoding. Multiple columns will result in multiple data sets.
--- googlechartplugin/0.11/trunk/gchartplugin/charting.py
+++ googlechartplugin/0.11/trunk/gchartplugin/charting.py
@@ -3,7 +3,8 @@ from trac.util.html import Markup
 
 from GChartWrapper import GChart as GoogleChart
 
-NON_API_KEYS = ['type', 'query', 'tuples']
+NON_API_KEYS = ['type', 'query', 'tuples',
+                'xlabels', 'ylabels', 'tlabels', 'rlabels']
 
 class GChart(WikiMacroBase):
     """Create a chart using the Google Chart API.
@@ -27,7 +28,6 @@ class GChart(WikiMacroBase):
     """
     
     def render_macro(self, req, name, content):
-        
         # XXX: This is a big security hole. Need to replace this with something that
         # only accepts simple key/value pairs.
         options = eval("dict(%s)" % content)
@@ -35,7 +35,11 @@ class GChart(WikiMacroBase):
         query = options.get('query', None)
         ctype = options.get('type', None)
         tuples = options.get('tuples', False)
-        
+	xlabels = options.get('xlabels', None)
+	ylabels = options.get('ylabels', None)
+	tlabels = options.get('tlabels', None)
+	rlables = options.get('rlabels', None)
+
         if query is None or ctype is None:
             raise ValueError("Chart 'type' and 'query' parameters are required")
         
@@ -43,19 +47,38 @@ class GChart(WikiMacroBase):
             if key in options:
                 del options[key]
         
-        db = self.env.get_db_cnx()
-        cursor = db.cursor()
-        cursor.execute(query)
-        
         dataset = []
         
+	label_lookup = {}
+	label_types = {}
+
+	if xlabels is not None:
+	    label_types['x'] = xlabels
+	    label_lookup[xlabels] = [] 
+	if ylabels is not None:
+	    label_types['y'] = ylabels
+	    label_lookup[ylabels] = [] 
+	if tlabels is not None:
+	    label_types['t'] = tlabels
+	    label_lookup[tlabels] = []
+	if rlables is not None:
+	    label_types['r'] = rlables
+	    label_lookup[rlabels] = []
+
+	db = self.env.get_db_cnx()
+        cursor = db.cursor()
+        cursor.execute(query)
+
         for row in cursor:
             row_data = []
-            for col in row:
-                try:
-                    row_data.append(float(col))
-                except ValueError:
-                    pass
+            for idx, col in enumerate(row):
+		if idx in label_lookup:
+		    label_lookup[idx].append(col)
+		else:
+		    try:
+			row_data.append(float(col))
+		    except (TypeError, ValueError):
+			pass
             if not row_data:
                 continue
             if tuples:
@@ -67,5 +90,14 @@ class GChart(WikiMacroBase):
                     else:
                         dataset.append([data])
         
+	if label_types:
+	    label_order = sorted(label_types.keys())
+	    options['chxt'] = ','.join(label_order)
+	    label_strings = []
+	    for idx, type in enumerate(label_order):
+		label_strings.append('%d:|%s' % 
+		    (idx, '|'.join(label_lookup[label_types[type]])))
+	    options['chxl'] = '|'.join(label_strings)
+
         chart = GoogleChart(ctype=ctype, dataset=dataset, **options)
         return Markup(chart.img())
