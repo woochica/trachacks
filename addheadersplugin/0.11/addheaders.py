f31688140235974192e38b5128bb96b7b35b331f
martin_s <martin_s@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-11-01 00:33:28 +0000

    addheaders.py:: Initial version.
    git-svn-id: https://trac-hacks.org/svn@4690 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ addheadersplugin/0.11/addheaders.py
@@ -0,0 +1,65 @@
+""" Copyright (c) 2008 Martin Scharrer <martin@scharrer-online.de>
+    $Id: googlemap.py 4664 2008-10-29 23:17:17Z martin_s $
+    $HeadURL: http://trac-hacks.org/svn/googlemapmacro/0.11/googlemap.py $
+
+    This is Free Software under the GPL v3!
+""" 
+from  trac.core        import  *
+from  trac.web.api     import  IRequestFilter
+from  trac.web.chrome  import  add_link,add_stylesheet,add_script
+
+class AddHeadersPlugin(Component):
+    implements(IRequestFilter)
+    """ Provides a plugin to insert header tags into trac pages.
+
+        This plugin is limited to the features of the methods
+        add_link, add_stylesheet and add_script from trac.web.chrome,
+        so not all valid (X)HTML attributes can be set.
+    """
+
+    def pre_process_request(self, req, handler):
+        return handler
+
+    def post_process_request(self, req, template, data, content_type):
+        config        = self.env.config
+        section       = 'addheaders'
+        self.env.log.info("Executing ...")
+
+        default_base        = config.get(section, 'default_base', 'site/')
+        default_script_base = config.get(section, 'default_script_base', default_base)
+        default_style_base  = config.get(section, 'default_style_base',  default_base)
+
+        links         = config.getlist(section, 'add_links')
+        stylesheets   = config.getlist(section, 'add_styles')
+        scripts       = config.getlist(section, 'add_scripts')
+
+
+        for link in links:
+            rel       = config.get(section, link       + '.rel'   )
+            href      = config.get(section, link       + '.href'  )
+            title     = config.get(section, link       + '.title' )
+            mimetype  = config.get(section, link       + '.type'  )
+            classname = config.get(section, link       + '.class' )
+
+            if rel and href:
+                add_link(req, rel, href, title or None, mimetype or None, classname or None)
+
+
+        for stylesheet in stylesheets:
+            filename  = config.get(section, stylesheet + '.filename', default_style_base + stylesheet + '.css' )
+            mimetype  = config.get(section, stylesheet + '.mimetype', 'text/css')
+
+            if filename:
+                add_stylesheet(req, filename, mimetype)
+
+
+        for script in scripts:
+            filename  = config.get(section, script     + '.filename', default_script_base + script + '.js' )
+            mimetype  = config.get(section, script     + '.mimetype', 'text/javascript')
+
+            if filename:
+                add_script(req, filename, mimetype)
+
+
+        return (template, data, content_type)
+
