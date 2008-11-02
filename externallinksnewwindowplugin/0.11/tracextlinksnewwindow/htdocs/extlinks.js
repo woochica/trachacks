e4325a225b6722699dabc842a954797daeca2556
martin_s <martin_s@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-11-02 17:50:28 +0000

    extlinks.js:: Added copyright and license header.
    git-svn-id: https://trac-hacks.org/svn@4717 7322e99d-02ea-0310-aa39-e9a107903beb

--- externallinksnewwindowplugin/0.11/tracextlinksnewwindow/htdocs/extlinks.js
+++ externallinksnewwindowplugin/0.11/tracextlinksnewwindow/htdocs/extlinks.js
@@ -1,3 +1,13 @@
+/*
+    Attaches an onClick event on all links in class ext-link which opens this
+    link in a new window.
+
+    Copyright (c) 2008 Martin Scharrer <martin@scharrer-online.de>
+    $Id$
+    $HeadURL$
+
+    This is Free Software under the GPL v3!
+*/
 
 $(document).ready( function() {
     $('a.ext-link').click ( function () {
