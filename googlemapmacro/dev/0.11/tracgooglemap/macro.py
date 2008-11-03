7066805a4a7f0a99de1744b8262af8d98cbb92a9
martin_s <martin_s@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-11-03 19:49:02 +0000

    0.11/tracgooglemap/macro.py:: Code cleanup.
    git-svn-id: https://trac-hacks.org/svn@4731 7322e99d-02ea-0310-aa39-e9a107903beb

--- googlemapmacro/dev/0.11/tracgooglemap/macro.py
+++ googlemapmacro/dev/0.11/tracgooglemap/macro.py
@@ -68,10 +68,9 @@ TracGoogleMap( function (mapdiv) {
 """
 
 class GoogleMapMacro(WikiMacroBase):
-    implements(IRequestFilter,ITemplateProvider,IRequestFilter,IEnvironmentSetupParticipant)
-
     """ Provides a macro to insert Google Maps(TM) in Wiki pages
     """
+    implements(IRequestFilter,ITemplateProvider,IRequestFilter,IEnvironmentSetupParticipant)
 
     def __init__(self):
         self.geocoding_server = unicode(self.env.config.get('googlemap', 'geocoding',
@@ -150,11 +149,9 @@ class GoogleMapMacro(WikiMacroBase):
         return arg
 
     def _format_address(self, address):
-        self.env.log.debug("address before = %s" % address)
         address = self._strip(address).replace(';',',')
         address = _reWHITESPACES.sub(' ', address)
         address = _reCOMMA.sub(', ', address)
-        self.env.log.debug("address after  = %s" % address)
         return address
 
     def _parse_args(self, args, strict=True, sep = ',', quote = '"', kw = True, min = -1, num = -1):
@@ -206,9 +203,6 @@ class GoogleMapMacro(WikiMacroBase):
             for i in range(len(largs), min):
                 largs.append(None)
 
-        self.env.log.debug("Parsed Arguments:")
-        self.env.log.debug(largs)
-        self.env.log.debug(kwargs)
         if kw:
             return largs, kwargs
         else:
@@ -222,11 +216,7 @@ class GoogleMapMacro(WikiMacroBase):
 
         db = self.env.get_db_cnx()
         cursor = db.cursor()
-        #try:
         cursor.execute("SELECT lon,lat,acc FROM googlemapmacro WHERE id='%s';" % hash)
-        #except:
-        #    pass
-        #else:
         for row in cursor:
             if len(row) == 3:
                 self.env.log.debug("Reusing coordinates from database")
@@ -244,14 +234,11 @@ class GoogleMapMacro(WikiMacroBase):
             raise TracError("Given address '%s' couldn't be resolved by Google Maps!" % address);
         acc, lon, lat = resp[1:4]
 
-        #try:
         cursor.execute(
             "INSERT INTO googlemapmacro (id, lon, lat, acc) VALUES ('%s', %s, %s, %s);" %
             (hash, lon, lat, acc))
         db.commit()
         self.env.log.debug("Saving coordinates to database")
-        #except:
-        #    pass
 
         return (lon, lat, acc)
 
@@ -449,12 +436,6 @@ class GoogleMapMacro(WikiMacroBase):
                         else:
                             markers.append( gmarkeraddr( location, letter, link, title) )
             markers_str = ''.join( markers )
-
-
-        # Produce unique id for div tag
-        #GoogleMapMacro.nid += 1
-        #id = "tracgooglemap-%i" % GoogleMapMacro.nid
-
 
         # put everything in a tidy div
         html = tag.div(
