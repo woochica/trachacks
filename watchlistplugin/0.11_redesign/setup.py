85021ab806114e06bd37de8a8a6c70f764170bd7
martin_s <martin_s@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-11-16 15:51:02 +0000

    0.11_redesign:: Created a develop dir for plugin internal redesign.
    git-svn-id: https://trac-hacks.org/svn@4828 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ watchlistplugin/0.11_redesign/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name = 'TracWatchlistPlugin',
+    version = '0.1',
+    packages = ['tracwatchlist'],
+    author = 'Martin Scharrer',
+    author_email = 'martin@scharrer-online.de',
+    package_data = {
+        'tracwatchlist' : [ 'templates/*.html', 'htdocs/css/*.css',],
+    },
+    description = "Example Trac Macro.",
+    url = 'http://www.trac-hacks.org/wiki/WatchlistPlugin',
+    license = 'BSD',
+    keywords = 'trac watchlist wiki plugin',
+    classifiers = ['Framework :: Trac'],
+    entry_points = {'trac.plugins': ['tracwatchlist.plugin = tracwatchlist.plugin']}
+)
