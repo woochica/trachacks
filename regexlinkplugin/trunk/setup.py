470733dcafadea8b6c5b8479d9f98740992a59ad
ComaVN <ComaVN@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-11-24 23:12:43 +0000

    increased trunk version to 0.3
    git-svn-id: https://trac-hacks.org/svn@4870 7322e99d-02ea-0310-aa39-e9a107903beb

--- regexlinkplugin/trunk/setup.py
+++ regexlinkplugin/trunk/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "RegexLink",
-    version = "0.2",
+    version = "0.3",
     packages = find_packages(exclude=['*.tests*']),
     entry_points = """
         [trac.plugins]
