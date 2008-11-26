84b65ab760f8ec6a4e326d2acddefc8914bf16a5
bobbysmith007 <bobbysmith007@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-11-26 19:59:05 +0000

    re #4150 bumped version to 0.0.8
    git-svn-id: https://trac-hacks.org/svn@4878 7322e99d-02ea-0310-aa39-e9a107903beb

--- estimatorplugin/0.11/setup.py
+++ estimatorplugin/0.11/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 PACKAGE = 'estimatorplugin'
 
 setup(name=PACKAGE,
-      version='0.0.7',
+      version='0.0.8',
       packages=[PACKAGE],
       url='http://www.trac-hacks.org/wiki/EstimatorPlugin',
       license='http://www.opensource.org/licenses/mit-license.php',
