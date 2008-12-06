163f86b95f3ff4fbc7f423a6bed623460c8546a3
ComaVN <ComaVN@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-12-06 19:29:30 +0000

    credits
    git-svn-id: https://trac-hacks.org/svn@4969 7322e99d-02ea-0310-aa39-e9a107903beb

--- regexlinkplugin/trunk/setup.py
+++ regexlinkplugin/trunk/setup.py
@@ -1,6 +1,10 @@
 from setuptools import setup, find_packages
 setup(
     name = "RegexLink",
+    author = "Roel Harbers",
+    license = "THE BEER-WARE LICENSE (Revision 42)",
+    url = "http://trac-hacks.org/wiki/RegexLinkPlugin",
+    description = "Trac plugin for creating external links based on regular expressions",
     version = "0.3",
     packages = find_packages(exclude=['*.tests*']),
     entry_points = """
