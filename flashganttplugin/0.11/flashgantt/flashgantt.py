9115a03256a3a9cbd71e30cc9b0ff9facb16d48d
adeponte <adeponte@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-12-08 08:33:37 +0000

    Added functionality to start handling dates. Note: These functions have not been tested but I wanted to get them out before I fell asleep tonight so that I could pick them up tomorrow and Murph would know about them if he decided to work on stuff.
    git-svn-id: https://trac-hacks.org/svn@4976 7322e99d-02ea-0310-aa39-e9a107903beb

--- flashganttplugin/0.11/flashgantt/flashgantt.py
+++ flashganttplugin/0.11/flashgantt/flashgantt.py
@@ -22,6 +22,8 @@ from trac.web import IRequestHandler
 from trac.web.chrome import INavigationContributor, ITemplateProvider, add_stylesheet
 from trac.ticket import Milestone
 
+import datetime
+
 class FlashGanttPlugin(Component):
     implements(INavigationContributor, IRequestHandler, ITemplateProvider)
 
@@ -43,6 +45,40 @@ class FlashGanttPlugin(Component):
             req.perm.require('MILESTONE_VIEW')
         
             showall = req.args.get('show') == 'all'
+
+            quarters = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]
+
+            # Get the current date and time
+            cur_dt = datetime.today()
+
+            # Figure out what month the cur_dt is in.
+            cur_month = cur_dt.month
+
+            # Figure out what Quarter the cur_dt is in.
+            cur_quarter = None
+            for quarter_index in range(0,4):
+                if cur_month in quarters[quarter_index]:
+                    cur_quarter = quarter_index
+                    break;
+
+            # Figure out the beginnig of tha Quarter it is in.
+            calendar
+
+            # Calculate the beginning date of the Quarter before that
+            # one.
+            prev_quarter = None
+            if (cur_quarter == 0):
+                prev_quarter = 4
+            else:
+                prev_quarter = cur_quarter - 1
+
+            # Calculate the ending date of the Quarter after that
+            # one.
+            next_quarter = None
+            if (cur_quarter == 4):
+                next_quarter = 1
+            else:
+                next_quarter = cur_quarter + 1
 
             db = self.env.get_db_cnx()
             #milestones = [m for m in Milestone.select(self.env, showall, db)
@@ -89,3 +125,65 @@ class FlashGanttPlugin(Component):
     def get_htdocs_dirs(self):
         from pkg_resources import resource_filename
         return [('fg', resource_filename(__name__, 'htdocs'))]
+
+    def _get_month_start_end_dates(self, year, month):
+        thirtyone_months = [1, 3, 5, 7, 8, 10, 12]
+        thirty_months = [4, 6, 9, 11]
+
+        start_date = None
+        end_date = None
+        if (month in thirty_months):
+            start_date = datetime.date(year, month, 1)
+            end_date = datetime.date(year, month, 31)
+        elif (month in thirtyone_months):
+            start_date = datetime.date(year, month, 1)
+            end_date = datetime.date(year, month, 31)
+        elif (month == 2):
+            if (year % 4) == 0: # leap year hence, has 29 days
+                start_date = datetime.date(year, month, 1)
+                end_date = datetime.date(year, month, 29)
+            else: # has 28 days
+                start_date = datetime.date(year, month, 1)
+                end_date = datetime.date(year, month, 28)
+
+        return (start_date, end_date)
+    
+    def _get_quarter(self, month):
+        quarters = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]
+
+        quarter_its_in = None
+        for x in range(0, 4):
+            if (quarter in quarters[x]):
+                quarter_its_in = x
+                break;
+
+        return quarter_its_in
+    
+    def _get_quarter_start_end_months(self, quarter):
+        quarters = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]
+        return (quarters[quarter][0], quarters[quarter][3])
+
+    def _get_quarter_start_end_dates(self, year, quarter):
+        (qstart_month, qend_month) = _get_quarter_start_end_months(quarter)
+        (qstart_date, _) = self._get_month_start_end_dates(year, qstart_month)
+        (_, qend_date) = self._get_month_start_end_dates(year, qend_month)
+
+        return (qstart_date, qend_date)
+
+    def _get_prev_quarters_start_end_dates(self, year, quarter):
+        if (quarter == 1):
+            # subtract one from the year and use the 4 quarter of that
+            # year
+            return _get_quarter_start_end_dates((year - 1), 4)
+        else:
+            # use the provided year and subtract one from the provided
+            # quarter
+            return _get_quarter_start_end_dates(year, (quarter - 1))
+
+    def _get_next_quarters_start_end_dates(self, year, quarter):
+        if (quarter == 4):
+            # add one to the year and use the 1 quarter of that year
+            return _get_quarter_start_end_dates((year + 1), 1)
+        else:
+            # use the provided year and add one to the provided quarter
+            return _get_quarter_start_end_dates(year, (quarter + 1))
