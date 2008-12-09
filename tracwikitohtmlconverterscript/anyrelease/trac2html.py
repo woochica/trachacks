88ef7725e1af533c35769039ffb1d69870ee35f4
coldpizza <coldpizza@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-12-09 10:25:59 +0000

    a variation which can read a file name from command line and outputs HTML to the console, attaches standard Trac CSS and adds HTML header/footer blocks
    git-svn-id: https://trac-hacks.org/svn@4988 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ tracwikitohtmlconverterscript/anyrelease/trac2html.py
@@ -0,0 +1,33 @@
+﻿#!/usr/bin/env python
+# found on trac google group:
+# http://groups.google.com/group/trac-dev/browse_thread/thread/2c97c6c514487778?q=
+
+import sys
+from trac.test import EnvironmentStub, Mock, MockPerm
+from trac.mimeview import Context
+from trac.wiki.formatter import HtmlFormatter
+from trac.web.href import Href
+
+env = EnvironmentStub()
+req = Mock(href=Href('/'), abs_href=Href('http://www.example.com/'),
+           authname='anonymous', perm=MockPerm(), args={})
+context = Context.from_request(req, 'wiki')
+
+if len(sys.argv) > 1:
+    try:
+        wiki = open(sys.argv[1]).read()
+    except IOError:
+        wiki = sys.argv[1] # parse it as tracwiki if not a file
+else:
+    wiki = '= The Trac Wiki to HTML converter ='
+
+print '''
+<html><head><link rel="stylesheet" href="trac.css" type="text/css" />
+</head><body>
+'''
+print HtmlFormatter(env, context, wiki).generate()
+
+print '''
+</body>
+</html>
+'''
