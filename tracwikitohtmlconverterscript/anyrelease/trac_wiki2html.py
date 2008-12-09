dc71e1e6feb8654c5782666004191ec542db3708
coldpizza <coldpizza@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-12-09 10:21:45 +0000

    tracwiki to html conversion script
    git-svn-id: https://trac-hacks.org/svn@4986 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ tracwikitohtmlconverterscript/anyrelease/trac_wiki2html.py
@@ -0,0 +1,18 @@
+﻿#!/usr/bin/env python
+# found on trac google group:
+# http://groups.google.com/group/trac-dev/browse_thread/thread/2c97c6c514487778?q=
+
+import sys
+from trac.test import EnvironmentStub, Mock, MockPerm
+from trac.mimeview import Context
+from trac.wiki.formatter import HtmlFormatter
+from trac.web.href import Href
+
+env = EnvironmentStub()
+req = Mock(href=Href('/'), abs_href=Href('http://www.example.com/'),
+           authname='anonymous', perm=MockPerm(), args={})
+context = Context.from_request(req, 'wiki')
+
+wiki = '= Trac Wiki to HTML Demo ='
+
+print HtmlFormatter(env, context, wiki).generate()
