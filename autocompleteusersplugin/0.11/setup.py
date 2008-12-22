afea3062e8237f5d1191e942d80f2ded50cf0580
k0s <k0s@7322e99d-02ea-0310-aa39-e9a107903beb>
2008-12-22 17:40:36 +0000

    more explicit specification of package_data;  bump version;  add GPL license; hopefully fixes #3758
    git-svn-id: https://trac-hacks.org/svn@5047 7322e99d-02ea-0310-aa39-e9a107903beb

--- autocompleteusersplugin/0.11/setup.py
+++ autocompleteusersplugin/0.11/setup.py
@@ -1,18 +1,18 @@
 from setuptools import find_packages, setup
 
-version='0.4'
+version='0.4.1'
 
 setup(name='AutocompleteUsers',
       version=version,
-      description="complm the known trac users, AJAX style",
+      description="complete the known trac users, AJAX style",
       author='Jeff Hammel',
       author_email='jhammel@openplans.org',
       url='http://trac-hacks.org/wiki/k0s',
       keywords='trac plugin',
-      license="",
+      license="GPL",
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests*']),      
       include_package_data=True,
-      package_data={'autocompleteusers': ['htdocs/*']},
+      package_data={'autocompleteusers': ['htdocs/css/*', 'htdocs/js/*']},
       zip_safe=False,
       entry_points = """
       [trac.plugins]
