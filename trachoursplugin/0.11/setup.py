01c62a9af68f4f69d744daeb5d7e8ef8d2f605c0
k0s <k0s@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-01-06 10:26:33 +0000

    appropriate capitalization for pypi
    git-svn-id: https://trac-hacks.org/svn@5092 7322e99d-02ea-0310-aa39-e9a107903beb

--- trachoursplugin/0.11/setup.py
+++ trachoursplugin/0.11/setup.py
@@ -13,7 +13,7 @@ setup(name='TracHoursPlugin',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests*']),
       include_package_data=True,
       zip_safe=False,
-      install_requires=['python-dateutil', 'feedparser'],
+      install_requires=['python-dateutil', 'FeedParser'],
       extras_require=dict(lxml=['lxml']),
       entry_points = """
       [trac.plugins]
