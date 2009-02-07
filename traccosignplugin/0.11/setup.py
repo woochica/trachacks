11ad7f9d1f8002fc1162691fbbb522cafe76befc
jiangxin <jiangxin@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-02-07 09:19:55 +0000

    TracCoSignPlugin 0.1.0 initialized: Integrate Trac with CoSign single sign-on system.
    git-svn-id: https://trac-hacks.org/svn@5214 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ traccosignplugin/0.11/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+
+from setuptools import setup
+
+setup(
+    name = 'TracCosign',
+    version = '0.1.0',
+    packages = ['traccosign'],
+
+    author = 'Jiang Xin',
+    author_email = 'worldhello.net@gmail.com',
+    description = 'A modified authentication plugin to use the Michigan CoSign SSO system.',
+    long_description = open(os.path.join(os.path.dirname(__file__), 'README')).read(),
+    license = 'BSD',
+    keywords = 'trac 0.12 plugin cosign authentication',
+    url = 'http://trac-hacks.org/wiki/TracCosignPlugin',
+
+    install_requires = ['Trac'],
+
+    zip_safe = False,
+
+    entry_points = {
+        'trac.plugins': [
+            'traccosign.traccosign = traccosign.traccosign'
+        ]
+    }
+)
