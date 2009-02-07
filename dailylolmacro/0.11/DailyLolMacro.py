1414bfa63ba16f60e74256ecb2a8fd8ec7383947
insaneintenti0n <insaneintenti0n@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-02-07 21:36:14 +0000

    Adding the first revision of the DailyLolMacro to the repository.
    git-svn-id: https://trac-hacks.org/svn@5222 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ dailylolmacro/0.11/DailyLolMacro.py
@@ -0,0 +1,20 @@
+class DailyLolMacro(WikiMacroBase):
+    """Display a picture of a cat with a funny caption."""
+
+    def expand_macro(self, formatter, name, args):
+        import urllib2
+
+        website = urllib2.urlopen("http://www.icanhascheezburger.com")
+        website_html = website.read()
+
+        beg_pos = -1
+        beg_pos = website_html.find('class="post"', beg_pos+1)
+
+        beg_pos = website_html.find('src=', beg_pos)
+        end_pos = website_html.find('"', beg_pos+5)
+        raw_url = website_html[beg_pos+5:end_pos]
+
+        attr = {}
+        result = tag.img(src=raw_url, **attr)
+
+        return result
