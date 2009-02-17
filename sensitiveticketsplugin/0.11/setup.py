275480afde1d3c823e71ca64d06aba1f3bc6c6f2
k0s <k0s@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-02-17 17:14:12 +0000

    updates to setup.py now that this is tested and functional
    git-svn-id: https://trac-hacks.org/svn@5283 7322e99d-02ea-0310-aa39-e9a107903beb

--- sensitiveticketsplugin/0.11/setup.py
+++ sensitiveticketsplugin/0.11/setup.py
@@ -1,18 +1,17 @@
 from setuptools import find_packages, setup
 
-version='0.0'
+version='0.1'
 
 setup(name='sensitivetickets',
       version=version,
-      description="A plugin that lets you mark tickets as 'sensitive' with a check box.  Those tickets can only be seen with permission.",
-      author='Legion',
+      description="A trac plugin that lets you mark tickets as 'sensitive' with a check box.  Those tickets can only be seen with permission.  This plugin is based on the example plugin http://trac.edgewall.org/browser/trunk/sample-plugins/permissions/vulnerability_tickets.py",
+      author='Jeff Hammel and Sebastian Benthall',
       author_email='jhammel@openplans.org',
-      url='topp.openplans.org',
-      keywords='trac plugin',
-      license="",
+      url='http://trac-hacks.org/wiki/SensitiveTicketsPlugin',
+      keywords='trac plugin security',
+      license="GPL",
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests*']),
       include_package_data=True,
-      package_data={ 'sensitivetickets': ['templates/*', 'htdocs/*'] },
       zip_safe=False,
       entry_points = """
       [trac.plugins]
