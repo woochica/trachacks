d9c6931fd642565f99ac316dec85d476ac5accae
dottedmag <dottedmag@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-02-25 15:31:01 +0000

    Reading list of languages from the wiki page, not from the constant.
    git-svn-id: https://trac-hacks.org/svn@5309 7322e99d-02ea-0310-aa39-e9a107903beb

--- translatedpagesmacro/0.11/translatedpages/templates/translatedpages.cs
+++
@@ -1,8 +0,0 @@
-<?cs include "header.cs" ?>
-<?cs include "macros.cs" ?>
-
-<div id="content" class="helloworld">
- <h1>Translated Pages...</h1>
-</div>
-
-<?cs include "footer.cs" ?>
--- translatedpagesmacro/0.11/translatedpages/translatedpages.py
+++ translatedpagesmacro/0.11/translatedpages/translatedpages.py
@@ -11,14 +11,17 @@ from trac.core import *
 from trac.web.main import IRequestHandler
 from trac.web.chrome import ITemplateProvider
 from trac.wiki.api import IWikiMacroProvider
-from StringIO import StringIO
+from cStringIO import StringIO
 from trac.wiki.formatter import Formatter
 from trac.wiki.model import WikiPage
+from trac.wiki.api import WikiSystem
 
 class TranslatedPagesMacro(Component):
-    """Plugin to show the translated pages list."""
+    """
+    Macro to show the translated pages list.
+    """
 
-    implements(IWikiMacroProvider, IRequestHandler, ITemplateProvider)
+    implements(IWikiMacroProvider)
 
     # IWikiMacroProvider methods
 
@@ -33,107 +36,110 @@ class TranslatedPagesMacro(Component):
     def parse_macro(self, parser, name, content):
         raise NotImplementedError
 
+    # --
 
-    # IRequestHandler methods
-
-    def match_request(self, req):
-        return req.path_info == u'/translations'
-
-    def process_request(self, req):
-        return u'translatedpages.cs', None
-
-
-    # ITemplateProvider methods
-
-    def get_templates_dirs(self):
-        """Return a list of directories containing the provided ClearSilver
-        templates.
-        """
-
-        from pkg_resources import resource_filename
-        return [resource_filename(__name__, u'templates')]
-
-    SUPPORT_LANGUAGES = {
-        u'en' : u'English',
-        u'ru' : u'Русский',
-        u'be' : u'Беларуская мова',
-        u'da' : u'dansk',
-        u'es' : u'español',
-        u'nl' : u'Nederlands',
-        u'sv' : u'Svenska',
-        u'zh' : u'中文',
-        }
+    TRAC_LANGUAGES_PAGE = u'TracLanguages'
+    BASE_LANG = u'en'
 
     def __init__(self):
-        self.supported_languages = TranslatedPagesMacro.SUPPORT_LANGUAGES.keys()
-        self.supported_languages.sort()
+        self.languages_page_version = 0
+        self._update_languages()
+
+    def _parse_languages_list(self, text):
+        """
+        Parses the list of languages in form
+         * <language code> <language name>
+         * <language code> <language name>
+        ...
+        """
+        langs = {}
+        for line in text.split(u'\r\n'):
+            if not line.startswith(u' * '):
+                self.env.log.warn(
+                    u"Wrong line syntax while parsing languages list: %s" % line)
+                continue
+            if ' ' not in line[3:]:
+                self.env.log.warn(
+                    u"Wrong line syntax: missing language name: %s" % line)
+                continue
+            (code, name) = line[3:].split(None, 1)
+            self.env.log.debug("Adding language %s -> %s" % (code, name))
+            langs[code] = name
+        return langs
+
+    def _update_languages(self):
+        languages_page = WikiPage(self.env, self.TRAC_LANGUAGES_PAGE)
+        if not languages_page.exists:
+            self.env.log.warn(u"Can't find page %s" % self.TRAC_LANGUAGES_PAGE)
+            self.languages = {}
+            self.languages_page_version = 0
+        else:
+            if languages_page.version > self.languages_page_version:
+                self.languages = self._parse_languages_list(languages_page.text)
+                self.languages_page_version = languages_page.version
 
     def _get_language_name(self, lang_code):
-        if TranslatedPagesMacro.SUPPORT_LANGUAGES.has_key(lang_code):
-            return TranslatedPagesMacro.SUPPORT_LANGUAGES[lang_code]
-        else:
-            return lang_code
+        self._update_languages()
+        return self.languages.get(lang_code, lang_code)
 
     def _seems_like_lang_code(self, lang_code):
-        if len(lang_code) != 2 : return False
-        if not lang_code.islower() : return False
-        return lang_code.isalpha()
+        return len(lang_code) in (2,3) \
+            and lang_code.islower() \
+            and lang_code.isalpha()
 
-    def _get_wiki_info(self, wiki_id):
-        wiki_lang_code = u'en'
-        wiki_base_name = wiki_id
-
-        wiki_lang_code_start = wiki_id.rfind(u'/')
-        if wiki_lang_code_start != -1:
-            wiki_lang_code = wiki_id[wiki_lang_code_start+1:]
-            wiki_base_name = wiki_id[:wiki_lang_code_start]
-
-        if wiki_lang_code not in TranslatedPagesMacro.SUPPORT_LANGUAGES.keys(): # Unknown wiki language.
-            if not self._seems_like_lang_code(wiki_lang_code): # seems don't like a language code.
-                wiki_lang_code = u'en'
-                wiki_base_name = wiki_id
-
-        return (wiki_base_name, wiki_lang_code)
-
-    def _is_translated_wiki_exists(self, wiki_base_name, lang_code):
-        if lang_code == u'en':
-            wiki_id = wiki_base_name
+    def _get_translated_page(self, name, lang_code):
+        if lang_code == self.BASE_LANG:
+            return name
         else:
-            wiki_id = u'%s/%s' % (wiki_base_name, lang_code)
+            return name + u'/' + lang_code
 
-        wiki_page = WikiPage(self.env, wiki_id)
-        return wiki_page.exists
-
-    def _get_lang_link(self, wiki_base_name, lang_code, formatter):
-        if lang_code != u'en':
-            wiki_id = u'%s/%s' % (wiki_base_name, lang_code)
+    def _get_page_info(self, page_name):
+        parts = page_name.rsplit(u'/', 1)
+        if len(parts) < 2:
+            return (page_name, self.BASE_LANG)
+        (prefix, suffix) = parts
+        if self._seems_like_lang_code(suffix):
+            return (prefix, suffix)
         else:
-            wiki_id = wiki_base_name
+            retrun (page_name, self.BASE_LANG)
 
-        text = u'  * [wiki:%s %s]' % (wiki_id, self._get_language_name(lang_code))
+    def _get_translations(self, base_page_name):
+        (base_page, page_lang) = self._get_page_info(base_page_name)
+        for subpage in sorted(WikiSystem(self.env).get_pages(base_page_name)):
+            (subpage_base_page, subpage_lang) = self._get_page_info(subpage)
+            if base_page == subpage_base_page:
+                yield subpage_lang
 
-        return text
+    def _get_lang_link(self, base_name, lang_code):
+        page_name = self._get_translated_page(base_name, lang_code)
+        return u"  * [wiki:%s %s]" % (page_name, self._get_language_name(lang_code))
+
+    def _get_current_lang_link(self, lang_code):
+        return u"  * '''%s'''" % self._get_language_name(lang_code)
 
     def expand_macro(self, formatter, name, args):
-        """Return a list of translated pages with the native language names.
-        The list of languages supported can be configured by the SUPPORT_LANGUAGES
-        class member. Refer to iso639.1 for more information.
+        """
+        Return a list of translated pages with the native language names.
+        The list of languages supported can be configured by adding new
+        entries to TracLanguages page. Refer to ISO 639-1 for more information.
         """
 
-        wiki_id = formatter.context.resource.id
-
-        (wiki_base_name, wiki_lang_code) = self._get_wiki_info(wiki_id)
+        page_name = formatter.context.resource.id
+        (base_page_name, lang_code) = self._get_page_info(page_name)
 
         lang_link_list = []
-
-        for lang_code in self.supported_languages:
-            if self._is_translated_wiki_exists(wiki_base_name, lang_code):
-                if lang_code != wiki_lang_code:
-                    lang_link_list.append(self._get_lang_link(wiki_base_name, lang_code, formatter))
-
-        text = u'\n'.join(lang_link_list)
+        for translation in self._get_translations(base_page_name):
+            if translation != lang_code:
+                lang_link_list.append(self._get_lang_link(base_page_name, translation))
+            else:
+                lang_link_list.append(self._get_current_lang_link(lang_code))
 
         out = StringIO()
-        Formatter(self.env, formatter.context).format(text, out)
+        Formatter(self.env, formatter.context).format(u'\n'.join(lang_link_list), out)
 
-        return u'<div class="wiki-toc trac-nav"><h4>Other Languages:</h4>' + out.getvalue() + u'</div>'
+        return u"""
+<div class="wiki-toc trac-nav" style="clear:both">
+<h4>Translations:</h4>
+%s
+</div>""" % out.getvalue()
+
