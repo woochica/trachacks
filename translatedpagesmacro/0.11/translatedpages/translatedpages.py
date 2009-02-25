1aee518fee2606535227cf0936e182cff2a4134b
dottedmag <dottedmag@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-02-25 15:30:35 +0000

    Whitespace cleanup.
    git-svn-id: https://trac-hacks.org/svn@5307 7322e99d-02ea-0310-aa39-e9a107903beb

--- translatedpagesmacro/0.11/translatedpages/translatedpages.py
+++ translatedpagesmacro/0.11/translatedpages/translatedpages.py
@@ -49,10 +49,10 @@ class TranslatedPagesMacro(Component):
         """Return a list of directories containing the provided ClearSilver
         templates.
         """
-        
+
         from pkg_resources import resource_filename
         return [resource_filename(__name__, 'templates')]
-        
+
     SUPPORT_LANGUAGES = {
         'en' : u'English',
         'ru' : u'Русский',
@@ -73,47 +73,47 @@ class TranslatedPagesMacro(Component):
             return TranslatedPagesMacro.SUPPORT_LANGUAGES[lang_code]
         else:
             return lang_code
-            
+
     def _seems_like_lang_code(self, lang_code):
         if len(lang_code) != 2 : return False
         if not lang_code.islower() : return False
         return lang_code.isalpha()
-        
+
     def _get_wiki_info(self, wiki_id):
         wiki_lang_code = 'en'
         wiki_base_name = wiki_id
-        
+
         wiki_lang_code_start = wiki_id.rfind('/')
         if wiki_lang_code_start != -1:
             wiki_lang_code = wiki_id[wiki_lang_code_start+1:]
             wiki_base_name = wiki_id[:wiki_lang_code_start]
-        
+
         if wiki_lang_code not in TranslatedPagesMacro.SUPPORT_LANGUAGES.keys(): # Unknown wiki language.
             if not self._seems_like_lang_code(wiki_lang_code): # seems don't like a language code.
                 wiki_lang_code = 'en'
                 wiki_base_name = wiki_id
 
         return (wiki_base_name, wiki_lang_code)
-    
+
     def _is_translated_wiki_exists(self, wiki_base_name, lang_code):
         if lang_code == 'en':
             wiki_id = wiki_base_name
         else:
             wiki_id = '%s/%s' % (wiki_base_name, lang_code)
-    
+
         wiki_page = WikiPage(self.env, wiki_id)
         return wiki_page.exists
-        
+
     def _get_lang_link(self, wiki_base_name, lang_code, formatter):
         if lang_code != 'en':
             wiki_id = u'%s/%s' % (wiki_base_name, lang_code)
         else:
             wiki_id = wiki_base_name
-        
+
         text = u'  * [wiki:%s %s]' % (wiki_id, self._get_language_name(lang_code))
-        
+
         return text
-    
+
     def expand_macro(self, formatter, name, args):
         """Return a list of translated pages with the native language names.
         The list of languages supported can be configured by the SUPPORT_LANGUAGES
@@ -121,7 +121,7 @@ class TranslatedPagesMacro(Component):
         """
 
         wiki_id = formatter.context.resource.id
-        
+
         (wiki_base_name, wiki_lang_code) = self._get_wiki_info(wiki_id)
 
         lang_link_list = []
@@ -132,7 +132,7 @@ class TranslatedPagesMacro(Component):
                     lang_link_list.append(self._get_lang_link(wiki_base_name, lang_code, formatter))
 
         text = '\n'.join(lang_link_list)
-                    
+
         out = StringIO()
         Formatter(self.env, formatter.context).format(text, out)
 
