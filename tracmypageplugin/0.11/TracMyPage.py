5530b06894b53895c98ba43b6e3f9e63cfcd0b45
dgc <dgc@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-03-06 16:25:55 +0000

    Initial commit
    git-svn-id: https://trac-hacks.org/svn@5340 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ tracmypageplugin/0.11/TracMyPage.py
@@ -0,0 +1,47 @@
+import trac
+from trac.core import *
+from trac.util.html import html
+from trac.web import IRequestHandler
+from trac.web.api import IRequestFilter
+from trac.web.chrome import INavigationContributor
+from trac.perm import IPermissionRequestor, IPermissionPolicy
+
+
+class MyPagePlugin(Component):
+	trac.core.implements(INavigationContributor, IRequestHandler)
+
+	# INavigationContributor methods
+	def get_active_navigation_item(self, req):
+		return 'mypage'
+
+	def get_navigation_items(self, req):
+		if req.perm.has_permission('MYPAGE_VIEW'):
+			yield ('mainnav', 'mypage', html.A('My Page', href=req.href.me()))
+
+
+	# IRequestHandler methods
+	def match_request(self, req):
+		url = self.mypage_url(req)
+		self.log.debug('match_request: url = %s' % url)
+		self.log.debug('match_request: req = %s' % req.path_info)
+		if req.path_info == '/me':
+			return True
+		return False
+
+
+	def process_request(self, req):
+		url = self.mypage_url(req)
+		self.log.debug('process_request: %s' % url)
+		if req.path_info == '/me':
+			req.send_response(307)
+			req.send_header('Location', url)
+			req.end_headers()
+			return
+
+
+	# public methods
+	def mypage_url(self, req):
+		url = self.config.get('mypage', 'url')
+		if '%' in url:
+			url = url % req.authname
+		return url
