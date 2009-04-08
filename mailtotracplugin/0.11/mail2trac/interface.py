3b8e7bd64d364ea639ad5660d71e0adbd2ce946d
k0s <k0s@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-04-08 20:39:03 +0000

    flesh out interface
    git-svn-id: https://trac-hacks.org/svn@5507 7322e99d-02ea-0310-aa39-e9a107903beb

--- mailtotracplugin/0.11/mail2trac/interface.py
+++ mailtotracplugin/0.11/mail2trac/interface.py
@@ -7,5 +7,21 @@ from trac.core import Interface
 
 class IEmailHandler(Interface):
     
+    def match(message):
+        """
+        whether this handler can be used on this message
+        """
+
     def invoke(message):
-        """what to do on receiving an email"""
+        """
+        what to do on receiving an email;
+        returns the message if it is availble to other 
+        IEmailHandler plugins or None
+        if the message is consumed
+        """
+
+    def order():
+        """
+        what order to process the IEmailHandler in.
+        higher order == higher precedence
+        """
