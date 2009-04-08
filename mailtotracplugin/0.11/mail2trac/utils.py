e315b68a2985f8def6f704030a2a0272d4c00c9b
k0s <k0s@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-04-08 16:56:42 +0000

    fix python 2.4 backwards compatability
    git-svn-id: https://trac-hacks.org/svn@5504 7322e99d-02ea-0310-aa39-e9a107903beb

--- mailtotracplugin/0.11/mail2trac/utils.py
+++ mailtotracplugin/0.11/mail2trac/utils.py
@@ -1,9 +1,10 @@
 import email
+import email.Utils
 
 def emailaddr2user(env, addr):
     """returns Trac user name from an email address"""
 
-    name, address = email.utils.parseaddr(addr)
+    name, address = email.Utils.parseaddr(addr)
     for user in env.get_known_users():
         if address == user[2]: # ?
             return user[0]
