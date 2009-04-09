b798c8dffeb082ab27a3e49605394e4b9c41e09b
k0s <k0s@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-04-09 18:51:53 +0000

    streamlining plugins a bit for oss project;  adding some, removing privatetickets
    git-svn-id: https://trac-hacks.org/svn@5510 7322e99d-02ea-0310-aa39-e9a107903beb

--- traclegosscript/anyrelease/example/oss/setup.py
+++ traclegosscript/anyrelease/example/oss/setup.py
@@ -16,41 +16,51 @@ setup(name='OSSTracProject',
                            'TracLegos',
 
                            # trac wiki macros
+                           'TracIncludeMacro',
                            'TracProgressMeterMacro',
+                           'TracRedirect',
+                           'TracTocMacro',
 
                            # trac plugins
+                           'graphviz',
                            'IniAdmin',
                            'TracAccountManager',
                            'TracCustomFieldAdmin',
                            'TracDateField',
-                           'TracIncludeMacro',
                            'TracMasterTickets',
                            'TracPermRedirect',
-                           'TracPrivateTickets',
                            'TracTags',
                            'TracTicketDelete',
 
                            # plugins with TOPP authors
                            'AutocompleteUsers',
+                           'icalexporter',
                            'SVN_URLs',
                            'Tracbacks',
                            'svnsyncplugin'],
       dependency_links = [
       "http://trac-hacks.org/svn/traclegosscript/anyrelease#egg=TracLegos",
-      "http://trac-hacks.org/svn/accountmanagerplugin/trunk#egg=TracAccountManager",
+
+      # wiki macros
+      "http://trac-hacks.org/svn/includemacro/0.11#egg=TracIncludeMacro",
       "http://trac-hacks.org/svn/progressmetermacro/0.11#egg=TracProgressMeterMacro",
+      "http://svn.ipd.uka.de/repos/javaparty/JP/trac/plugins/redirect-0.11#egg=TracRedirect",
+      "http://trac-hacks.org/svn/tocmacro/0.11#egg=TracTocMacro",
+
+      # plugins
+      "http://trac-hacks.org/svn/accountmanagerplugin/trunk#egg=TracAccountManager",
       "http://trac-hacks.org/svn/customfieldadminplugin/0.11#egg=TracCustomFieldAdmin",
       "http://trac-hacks.org/svn/datefieldplugin/0.11#egg=TracDateField",
-      "http://trac-hacks.org/svn/includemacro/0.11#egg=TracIncludeMacro",
+      "http://trac-hacks.org/svn/graphvizplugin/0.11/#egg=graphviz",
       "http://trac-hacks.org/svn/iniadminplugin/0.11#egg=IniAdmin",
       "http://trac-hacks.org/svn/masterticketsplugin/0.11#egg=TracMasterTickets",
       "http://trac-hacks.org/svn/permredirectplugin/0.11#egg=TracPermRedirect",
-      "http://trac-hacks.org/svn/privateticketsplugin/0.11#egg=TracPrivateTickets",
       "http://trac-hacks.org/svn/tagsplugin/tags/0.6#egg=TracTags",
       "http://trac-hacks.org/svn/ticketdeleteplugin/0.11#egg=TracTicketDelete",
 
       # plugins with TOPP authors
       "http://trac-hacks.org/svn/autocompleteusersplugin/0.11#egg=AutocompleteUsers",
+      "http://trac-hacks.org/svn/icalexporterplugin/0.11#egg=icalexporter",
       "http://trac-hacks.org/svn/svnurlsplugin/0.11#egg=SVN_URLs",
       "http://trac-hacks.org/svn/tracbacksplugin/0.11#egg=Tracbacks",
       "http://trac-hacks.org/svn/svnsyncplugin/0.11#egg=svnsyncplugin"
