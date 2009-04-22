1f666ce72f014f5b732800e744624a268692008f
k0s <k0s@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-04-22 14:18:12 +0000

    adding autocompletion of tags;  does not yet work
    git-svn-id: https://trac-hacks.org/svn@5559 7322e99d-02ea-0310-aa39-e9a107903beb

--- loomingcloudsplugin/0.11/loomingclouds/autocompletetags.py
+++ loomingcloudsplugin/0.11/loomingclouds/autocompletetags.py
@@ -3,13 +3,15 @@ TagsRequestHandler:
 a plugin for Trac for autocompletion of tags
 http://trac.edgewall.org
 """
-
+from pkg_resources import resource_filename
 from trac.core import *
-
-from tractags.api import TagSystem
-
 from trac.web.api import IRequestHandler
 from trac.web.api import IRequestFilter
+from tractags.api import TagSystem
+from trac.web.chrome import ITemplateProvider
+from trac.web.chrome import add_script
+from trac.web.chrome import add_stylesheet
+
 
 class TagsRequestHandler(Component):
 
@@ -82,3 +84,63 @@ class TagsRequestHandler(Component):
             return self
         return handler
 
+class AutocompleteTags(Component):
+    
+    implements(IRequestFilter, ITemplateProvider)
+
+    ### methods for IRequestFilter
+
+    """Extension point interface for components that want to filter HTTP
+    requests, before and/or after they are processed by the main handler."""
+
+    def post_process_request(self, req, template, data, content_type):
+        """Do any post-processing the request might need; typically adding
+        values to the template `data` dictionary, or changing template or
+        mime type.
+        
+        `data` may be update in place.
+
+        Always returns a tuple of (template, data, content_type), even if
+        unchanged.
+
+        Note that `template`, `data`, `content_type` will be `None` if:
+         - called when processing an error page
+         - the default request handler did not return any result
+
+        (Since 0.11)
+        """
+        if template == 'ticket.html':
+            add_stylesheet(req, 'tags/css/autocomplete.css')
+            add_script(req, 'tags/js/autocomplete.js')
+            add_script(req, 'tags/js/autocomplete_keywords.js')
+
+        return (template, data, content_type)
+
+    def pre_process_request(self, req, handler):
+        """Called after initial handler selection, and can be used to change
+        the selected handler or redirect request.
+        
+        Always returns the request handler, even if unchanged.
+        """
+        return handler
+
+    ### methods for ITemplateProvider
+
+    def get_htdocs_dirs(self):
+        """Return a list of directories with static resources (such as style
+        sheets, images, etc.)
+        
+        Each item in the list must be a `(prefix, abspath)` tuple. The
+        `prefix` part defines the path in the URL that requests to these
+        resources are prefixed with.
+        
+        The `abspath` is the absolute path to the directory containing the
+        resources on the local file system.
+        """
+        return [('tags', resource_filename(__name__, 'htdocs'))]
+
+    def get_templates_dirs(self):
+        """Return a list of directories containing the provided template
+        files.
+        """
+        return []                    
---
+++ loomingcloudsplugin/0.11/loomingclouds/htdocs/js/autocomplete_keywords.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+	$("#field-cc").autocomplete("tags?format=txt", {
+	    multiple: true,});
+});
