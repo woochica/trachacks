86927b77b2895d6ddf54073d59df4e24d6df07de
k0s <k0s@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-04-27 14:59:08 +0000

    * check self.inject_map before adding the map to the query view * removal of obselete class
    git-svn-id: https://trac-hacks.org/svn@5609 7322e99d-02ea-0310-aa39-e9a107903beb

--- geotracplugin/0.11/geotrac/web_ui.py
+++ geotracplugin/0.11/geotrac/web_ui.py
@@ -91,7 +91,8 @@ class IssueMap(Component):
                 stream |= Transformer('//head').append(tag.script('', src="http://www.openlayers.org/api/OpenLayers.js"))
                 stream |= Transformer('//head').append(self.mapscript(**_data))
                 stream |= Transformer('//body').attr('onload', 'init()')
-                stream |= Transformer("//div[@id='content']").after(self.content(None, None))
+                if self.inject_map:
+                    stream |= Transformer("//div[@id='content']").after(self.content(None, None))
 
                 data.update(_data)
 
@@ -138,73 +139,4 @@ class IssueMap(Component):
         template = self.loader.load('mapscript.html')
         return template.generate(**data)
 
-
-# XXX Needed?
-class GeoRequestFilter(Component):
-    """add data to the request"""
-
-
-    implements(IRequestFilter)
-
-    ### methods for IRequestFilter
-
-    """Extension point interface for components that want to filter HTTP
-    requests, before and/or after they are processed by the main handler."""
-
-    def post_process_request(self, req, template, data, content_type):
-        """Do any post-processing the request might need; typically adding
-        values to the template `data` dictionary, or changing template or
-        mime type.
-        
-        `data` may be update in place.
-
-        Always returns a tuple of (template, data, content_type), even if
-        unchanged.
-
-        Note that `template`, `data`, `content_type` will be `None` if:
-         - called when processing an error page
-         - the default request handler did not return any result
-
-        (Since 0.11)
-        """
-        geotrac = self.env.components.get(GeoTrac)
-        if geotrac is None:
-            return template, data, content_type
-
-        if template == 'ticket.html':
-            ticket = data['ticket']
-            locations = []
-            if ticket['location']:
-
-                # get the latitude and longitude from the request environ if set
-                lat = req.environ.get('geolat')
-                lon = req.environ.get('geolon')
-
-                if lat is None or lon is None: # lat or lon could be zero
-                    # XXX blindly assume UTF-8
-                    try:
-                        location = ticket['location'].encode('utf-8')
-                    except UnicodeEncodeError:
-                        raise
-
-                    # geolocate the issue
-                    try:
-                        address, (lat, lon) = geotrac.geolocate(location)
-                        locations = [ { 'geolat': lat,
-                                        'geolon': lon,
-                                        }
-                                      ]
-                    except GeolocationException:
-                        pass
-                    
-            data['locations'] = locations
-        return template, data, content_type
-
-    def pre_process_request(self, req, handler):
-        """Called after initial handler selection, and can be used to change
-        the selected handler or redirect request.
-        
-        Always returns the request handler, even if unchanged.
-        """
-        return handler
 
