e025c6c3064ecf8e9b860fa7e764b5b49e8a62ec
k0s <k0s@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-04-28 20:47:32 +0000

    adding stub for map dashboard (does not yet work)
    git-svn-id: https://trac-hacks.org/svn@5638 7322e99d-02ea-0310-aa39-e9a107903beb

--- geotracplugin/0.11/geotrac/web_ui.py
+++ geotracplugin/0.11/geotrac/web_ui.py
@@ -1,5 +1,7 @@
 import geopy
 
+from pkg_resources import resource_filename
+
 from genshi.builder import tag
 from genshi.filters import Transformer
 from genshi.template import TemplateLoader
@@ -9,8 +11,19 @@ from ticketsidebarprovider import ITicketSidebarProvider
 from trac.config import BoolOption, Option
 from trac.core import *
 from trac.ticket import Ticket
+from trac.ticket.query import Query
 from trac.web.api import IRequestFilter
+from trac.web.api import IRequestHandler
 from trac.web.api import ITemplateStreamFilter
+from trac.web.chrome import add_script
+from trac.web.chrome import INavigationContributor
+from trac.web.chrome import ITemplateProvider
+
+
+# template loader
+templates_dir = resource_filename(__name__, 'templates')
+loader = TemplateLoader(templates_dir,
+                        auto_reload=True)
 
 
 class IssueMap(Component):
@@ -130,13 +143,107 @@ class IssueMap(Component):
 
     def mapscript(self, **data):
         """JS map for issues"""
-        if not hasattr(self, 'loader'):
-            from pkg_resources import resource_filename
-            templates_dir = resource_filename(__name__, 'templates')
-            self.loader = TemplateLoader(templates_dir,
-                                         auto_reload=True)
-        
-        template = self.loader.load('mapscript.html')
+        template = loader.load('mapscript.html')
         return template.generate(**data)
 
 
+class MapDashboard(Component):
+
+    implements(ITemplateProvider, IRequestHandler, INavigationContributor)
+
+    wms_url = Option('geo', 'wms_url', 
+                     'http://maps.opengeo.org/geoserver/gwc/service/wms',
+                     "URL for the WMS")
+
+    ### methods for ITemplateProvider
+
+    """Extension point interface for components that provide their own
+    ClearSilver templates and accompanying static resources.
+    """
+
+    def get_htdocs_dirs(self):
+        """Return a list of directories with static resources (such as style
+        sheets, images, etc.)
+
+        Each item in the list must be a `(prefix, abspath)` tuple. The
+        `prefix` part defines the path in the URL that requests to these
+        resources are prefixed with.
+        
+        The `abspath` is the absolute path to the directory containing the
+        resources on the local file system.
+        """
+        return []
+
+    def get_templates_dirs(self):
+        """Return a list of directories containing the provided template
+        files.
+        """
+        return [templates_dir]
+
+    ### methods for IRequestHandler
+
+    """Extension point interface for request handlers."""
+
+    def match_request(self, req):
+        """Return whether the handler wants to process the given request."""
+        return req.path_info.strip('/') == 'map'
+
+    def process_request(self, req):
+        """Process the request. For ClearSilver, return a (template_name,
+        content_type) tuple, where `template` is the ClearSilver template to use
+        (either a `neo_cs.CS` object, or the file name of the template), and
+        `content_type` is the MIME type of the content. For Genshi, return a
+        (template_name, data, content_type) tuple, where `data` is a dictionary
+        of substitutions for the template.
+
+        For both templating systems, "text/html" is assumed if `content_type` is
+        `None`.
+
+        Note that if template processing should not occur, this method can
+        simply send the response itself and not return anything.
+        """
+
+        # get the GeoTrac component
+        assert self.env.is_component_enabled(GeoTrac)
+        geotrac = self.env.components[GeoTrac]
+
+        query = Query.from_string(self.env, 'location!=')
+        results = query.execute(req)
+
+        locations = []
+        for result in results:
+            try:
+                address, (lat, lon) = geotrac.geolocate(result.get('location'))
+                locations.append({'geolat': lat,
+                                  'geolon': lon})
+            except GeolocationException:
+                continue
+
+        add_script(req, 'common/js/query.js')
+        template = loader.load("mapscript.html")
+        mapscript = template.generate(wms_url=self.wms_url, locations=locations)
+
+        locations = []
+        return ('mapdashboard.html', dict(locations=locations, mapscript=mapscript), 
+                                          'text/html')
+
+    ### methods for INavigationContributor
+
+    """Extension point interface for components that contribute items to the
+    navigation.
+    """
+
+    def get_active_navigation_item(self, req):
+        """This method is only called for the `IRequestHandler` processing the
+        request.
+        
+        It should return the name of the navigation item that should be
+        highlighted as active/current.
+        """
+        return 'map'
+
+    def get_navigation_items(self, req):
+        """Should return an iterable object over the list of navigation items to
+        add, each being a tuple in the form (category, name, text).
+        """
+        yield ('mainnav', 'map', tag.a('Map', href=req.href.map(), accesskey='M'))
