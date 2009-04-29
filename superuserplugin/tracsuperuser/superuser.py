0af28248488134c7001a5e47587cfa01476e23c3
airadier <airadier@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-04-29 15:19:26 +0000

    Make 'superuser' an option, to be editable with IniAdmin
    git-svn-id: https://trac-hacks.org/svn@5647 7322e99d-02ea-0310-aa39-e9a107903beb

--- superuserplugin/tracsuperuser/superuser.py
+++ superuserplugin/tracsuperuser/superuser.py
@@ -11,7 +11,7 @@
 
 from trac.core import *
 from trac.perm import IPermissionStore
-from trac.config import ExtensionOption
+from trac.config import ExtensionOption, Option
 
 import logging
 
@@ -20,26 +20,24 @@ class SuperUserPlugin(Component):
     """ Adds a superuser with TRAC_ADMIN permissions """
     implements(IPermissionStore)
     
+    superuser = Option('superuser', 'superuser', 'admin', 'Superuser username')
+    
     store = ExtensionOption('superuser', 'wrapped_permission_store', IPermissionStore,
         'DefaultPermissionStore', """Name of the component implementing `IPermissionStore`, which is used
         for managing user and group permissions.""")
     
 
     def get_user_permissions(self, username):
-        superuser = self.env.config.get('superuser', 'superuser', 'admin')
-
-        if username == superuser:
+        if username == self.superuser:
             return ['TRAC_ADMIN'] + self.store.get_user_permissions(username)
         else:
             return self.store.get_user_permissions(username)
 
     def get_users_with_permissions(self, permissions):
-        superuser = self.env.config.get('superuser', 'superuser', 'admin')
-        return [superuser] +  self.store.get_users_with_permissions(permissions)
+        return [self.superuser] +  self.store.get_users_with_permissions(permissions)
             
     def get_all_permissions(self):
-        superuser = self.env.config.get('superuser', 'superuser', 'admin')        
-        return self.store.get_all_permissions() + [(superuser, 'TRAC_ADMIN')]
+        return self.store.get_all_permissions() + [(self.superuser, 'TRAC_ADMIN')]
         
     def grant_permission(self, username, action):
         return self.store.grant_permission(username, action)
