35ca65f1ec31773ab69b55cf70dfd9d96c299f18
airadier <airadier@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-04-30 12:15:57 +0000

    Longer plugin description
    git-svn-id: https://trac-hacks.org/svn@5651 7322e99d-02ea-0310-aa39-e9a107903beb

--- superuserplugin/setup.py
+++ superuserplugin/setup.py
@@ -4,9 +4,15 @@ PACKAGE = 'tracsuperuser'
 VERSION = '0.2'
 
 setup(name=PACKAGE,
-      version=VERSION,
-      packages=['tracsuperuser'],
-      author='Alvaro J. Iradier (AMB Sistemas)',
-      url="http://www.trac-hacks.org/wiki/SuperUserPlugin",
-      license='BSD',
-      entry_points = {'trac.plugins': ['tracsuperuser = tracsuperuser']})
+    version=VERSION,
+    packages=['tracsuperuser'],
+    author='Alvaro J. Iradier',
+    author_email = "alvaro.iradier@polartech.es",
+    description = "Create a super user that always has TRAC_ADMIN permissions",
+    long_description = "",
+    license = "GPL",
+    keywords = "trac plugin auth superuser admin",
+    
+    url="http://www.trac-hacks.org/wiki/SuperUserPlugin",
+    
+    entry_points = {'trac.plugins': ['tracsuperuser = tracsuperuser']})
