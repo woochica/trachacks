f32a90dceed35ba65e8f0e9a0f3b2694d942cd79
airadier <airadier@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-04-30 12:40:13 +0000

    Long description
    git-svn-id: https://trac-hacks.org/svn@5652 7322e99d-02ea-0310-aa39-e9a107903beb

--- searchallplugin/setup.py
+++ searchallplugin/setup.py
@@ -4,9 +4,12 @@ PACKAGE = 'tracsearchall'
 VERSION = '0.5'
 
 setup(name=PACKAGE,
-      version=VERSION,
-      packages=['tracsearchall'],
-      author='Alvaro J. Iradier',
-      url="http://www.trac-hacks.org/wiki/SearchAllPlugin",
-      license='BSD',
-      entry_points = {'trac.plugins': ['tracsearchall = tracsearchall']})
+    version=VERSION,
+    packages=['tracsearchall'],
+    author='Alvaro J. Iradier',
+    author_email = "alvaro.iradier@polartech.es",
+    description = "Search in all projects in the same parent folder",
+    long_description = "",
+    url="http://www.trac-hacks.org/wiki/SearchAllPlugin",
+    license='GPL',
+    entry_points = {'trac.plugins': ['tracsearchall = tracsearchall']})
