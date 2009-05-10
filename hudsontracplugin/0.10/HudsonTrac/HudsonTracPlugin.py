4ec42d6cec963eb2acdd8987bea5db3369e9bd6c
roadrunner <roadrunner@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-05-10 22:11:04 +0000

    Added support for configuring hudson authentication info. This closes #4113.
    git-svn-id: https://trac-hacks.org/svn@5698 7322e99d-02ea-0310-aa39-e9a107903beb

--- hudsontracplugin/0.10/HudsonTrac/HudsonTracPlugin.py
+++ hudsontracplugin/0.10/HudsonTrac/HudsonTracPlugin.py
@@ -6,7 +6,8 @@ A plugin to display hudson results in the timeline and provide a nav-link
 import time
 import calendar
 import feedparser
-import urllib
+import urlparse
+import urllib2
 from datetime import datetime
 from trac.core import *
 from trac.config import Option, BoolOption
@@ -25,12 +26,25 @@ class HudsonTracPlugin(Component):
     feed_url = Option('hudson', 'feed_url', 'http://localhost/hudson/rssAll',
                       'The url of the hudson rss feed containing the build ' +
                       'statuses. This must be an absolute url.')
+    username = Option('hudson', 'username', '',
+                      'The username to use to access hudson (feed and details)')
+    password = Option('hudson', 'password', '',
+                      'The password to use to access hudson (feed and details)')
     nav_url  = Option('hudson', 'main_page', '/hudson/',
                       'The url of the hudson main page to which the trac nav ' +
                       'entry should link; if empty, no entry is created in ' +
                       'the nav bar. This may be a relative url.')
     disp_tab = BoolOption('hudson', 'display_in_new_tab', 'false',
                           'Open hudson page in new tab/window')
+
+    def __init__(self):
+        pwdMgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
+        pwdMgr.add_password(None, urlparse.urlsplit(self.feed_url)[1], self.username, self.password)
+
+        self.bAuth = urllib2.HTTPBasicAuthHandler(pwdMgr)
+        self.dAuth = urllib2.HTTPDigestAuthHandler(pwdMgr)
+
+        self.url_opener = urllib2.build_opener(self.bAuth, self.dAuth)
 
     # INavigationContributor methods
 
@@ -66,7 +80,7 @@ class HudsonTracPlugin(Component):
         if 'build' in filters:
             add_stylesheet(req, 'HudsonTrac/hudsontrac.css')
 
-            feed = feedparser.parse(self.feed_url)
+            feed = feedparser.parse(self.feed_url, handlers=[self.bAuth, self.dAuth])
 
             for entry in feed.entries:
                 # Only look at top-level entries
@@ -92,7 +106,7 @@ class HudsonTracPlugin(Component):
                 title = entry.title
 
                 url = href + '/api/json'
-                line = urllib.urlopen(url).readline()
+                line = self.url_opener.open(url).readline()
                 json = eval(line.replace('false', 'False').replace('true','True').replace('null', 'None'))
 
                 if json['description'] == None:
