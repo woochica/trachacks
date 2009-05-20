8549ca2c6d0c8a8c5d850fd1a402ce5edec945cd
kate <kate@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-05-20 01:24:50 +0000

    Oops... I thought these only took effect within the path given to `Transformer()`; looks like these need to be more explicit.
    git-svn-id: https://trac-hacks.org/svn@5810 7322e99d-02ea-0310-aa39-e9a107903beb

--- precodebrowserplugin/0.11/precodebrowser/precodebrowser.py
+++ precodebrowserplugin/0.11/precodebrowser/precodebrowser.py
@@ -11,7 +11,7 @@ class PreCodeBrowserPlugin(Component):
 	def filter_stream(self, req, method, filename, stream, data):
 		filter = Transformer('//table[@class = "code"]')
 		return stream | filter.rename('pre') \
-			.select('thead').remove().end() \
-			.select('tbody/tr/th').remove().end() \
-			.select('tbody/tr/td').append('\n').unwrap()
+			.select('//pre[@class = "code"]/thead').remove().end() \
+			.select('//pre[@class = "code"]/tbody/tr/th').remove().end() \
+			.select('//pre[@class = "code"]/tbody/tr/td').append('\n').unwrap()
 
