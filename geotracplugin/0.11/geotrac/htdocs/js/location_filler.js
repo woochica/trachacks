92a9684a3ad6473b4f8e7d9bac3044bf97e75039
k0s <k0s@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-05-21 20:48:34 +0000

    updated code to handle warning messages
    git-svn-id: https://trac-hacks.org/svn@5830 7322e99d-02ea-0310-aa39-e9a107903beb

--- geotracplugin/0.11/geotrac/htdocs/js/location_filler.js
+++ geotracplugin/0.11/geotrac/htdocs/js/location_filler.js
@@ -1,4 +1,5 @@
 location_error = false;
+warnings = [];
 
 function location_filler() {
     var locationnode = $("#field-location");
@@ -8,6 +9,21 @@ function location_filler() {
             map_locations([href]);
             if ( location_error ) {
                 $("#warning").remove();
+                if ( warnings.length )
+                    {
+                        warning_string = '<div id="warning" class="system-message"><strong>Warning: </strong>';
+                        if ( warnings.length == 1 ) {
+                            warning_string += warnings[0] + '</div>';
+                        }
+                        else {
+                            warning_string += '<ul>';
+                            for ( warning in warnings ) {
+                                warning_string += '<li>' + warning + '</li>';
+                            }
+                            warning_string += '</ul></div>';
+                        }
+                        $("#content").before(warning_string);
+                    }
             }
             return false;
         });
@@ -16,29 +32,53 @@ function location_filler() {
 $(document).ready(location_filler);
 
 $(document).ready(function() {
+        
+        $("#field-location").change(function() {
+                
+                var url = geolocator_url + "?location=" + escape($(this).val());
+                
+                if(location_error) {
+                    $("#warning").remove();
+                }
 
-$("#field-location").change(function() {
+                $.getJSON(url, function(locations) {
+                        if ( locations.error )
+                            {
+                                location_error = true;
+                                if ( warnings.length ) {
+                                    var warning_string = '<div id="warning" class="system-message"><strong>Warning: </strong> <ul><li>' + locations.error + '</li>';
+                                    for ( warning in warnings)
+                                        {
+                                            warning_string += '<li>' + warnings[warning] + '</li>';
+                                        }
+                                    $("#content").before(warning_string + '</ul></div>');
+                                    //                            for ( warning in warnings ) {
+                                    //                                $("#warning ul").append(warnings[warning]);
+                                }
+                                else {
+                                    $("#content").before('<div id="warning" class="system-message"><strong>Warning: </strong> ' + locations.error + '</div>');
+                                }
+                                location_filler();
+                            }
+                        else
+                            {
+                                map_locations(locations.locations);
+                            }
+                    });
+            });
+    });
 
-var url = geolocator_url + "?location=" + escape($(this).val());
-
-if(location_error)
-{
-  $("#warning").remove();
-}
-
-$.getJSON(url, function(locations) {
-if ( locations.error )
-{
-  location_error = true;
-  $("#ctxtnav").after('<div id="warning" class="system-message"><strong>Warning: </strong> ' + locations.error + '</div>');
-  location_filler();
-}
-else
-{
-
- map_locations(locations.locations);
-}
-});
-});
-});
-
+$(document).ready(function() {
+        var warning_text = 'Multiple locations found';
+        elements = $("#warning:contains('Multiple locations found')");
+        if ( elements.length ) {
+            $("#warning ul li").each(function() {
+                    text = $(this).text();
+                    if ( text.indexOf(warning_text) == -1 ) {
+                        warnings.push(text);
+                    }
+                });
+            location_error = true;
+            $("#field-location").change();
+        }
+    })
