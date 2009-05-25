c3a0cd043a9b7ce8c9c8de444e4cc043ad82625d
k0s <k0s@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-05-25 13:45:31 +0000

    switch to geoticketplugin
    git-svn-id: https://trac-hacks.org/svn@5848 7322e99d-02ea-0310-aa39-e9a107903beb

--- geoticketplugin/0.11/geotrac/query.py
+++ geoticketplugin/0.11/geotrac/query.py
@@ -1,14 +1,26 @@
+from geotrac.ticket import GeoTrac
 from geotrac.utils import get_column
-
 from trac.core import *
+from trac.web.api import ITemplateStreamFilter
+trac.web.chrome import Chrome
+
+# template loader
+templates_dir = resource_filename(__name__, 'templates')
+loader = TemplateLoader(templates_dir,
+                        auto_reload=True)
+
 
 class GeospatialQuery(Component):
     """query based on geographic data"""
+
+    implements(ITemplateStreamFilter)
 
     def query_by_radius(self, lat, lon, radius):
         """
         return tickets within a given radius
         """
+        # see http://postgis.refractions.net/pipermail/postgis-users/2006-January/010534.html
+        # XXX units of radius?
         query_str = "ST_DISTANCE(st_pointfromtext('POINT(' || geo_lng || ' ' || geo_lat || ')'), ST_PointFromText('POINT(%s %s)')) < %s" % (lon, lat, radius)
 
         return get_column('ticket_location', 'ticket', where=query_str)
@@ -16,3 +28,35 @@ class GeospatialQuery(Component):
     def query_by_polyon(self, lat, lon, polygon_id):
         pass
         # XXX stub
+
+    def geotrac(self):
+        if not hasattr(self, '_geotrac'):
+            assert self.env.is_component_enabled(GeoTrac)
+            geotrac = self.env.components[GeoTrac]
+            assert geotrac.postgis_enabled()
+            self._geotrac = geotrac
+        return self._geotrac
+        
+    ### method for ITemplateStreamFilter:
+    ### Filter a Genshi event stream prior to rendering.
+
+    def filter_stream(self, req, method, filename, stream, data):
+        """Return a filtered Genshi event stream, or the original unfiltered
+        stream if no match.
+
+        `req` is the current request object, `method` is the Genshi render
+        method (xml, xhtml or text), `filename` is the filename of the template
+        to be rendered, `stream` is the event stream and `data` is the data for
+        the current template.
+
+        See the Genshi documentation for more information.
+        """
+        self.geotrac()
+
+        chrome = Chrome(self.env)
+#        chrome.load_template()
+
+
+        return stream
+
+    
