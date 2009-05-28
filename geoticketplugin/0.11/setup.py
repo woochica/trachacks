472e9422628838d9cf77fbf65f6c642c75be4bc8
k0s <k0s@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-05-28 20:10:18 +0000

    switch to a branch of geopy that has reverse geocoding.  i wish theyd put this in the god damned trunk
    git-svn-id: https://trac-hacks.org/svn@5886 7322e99d-02ea-0310-aa39-e9a107903beb

--- geoticketplugin/0.11/setup.py
+++ geoticketplugin/0.11/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-version='0.5.2'
+version='0.6'
 
 setup(name='GeoTicket',
       version=version,
@@ -16,7 +16,7 @@ setup(name='GeoTicket',
       zip_safe=False,
       install_requires=[
         # -*- Extra requirements: -*-
-        'geopy',
+        'geopy>=0.93dev',
         'CustomFieldProvider',
         'TicketSidebarProvider',
         'simplejson'
@@ -25,6 +25,7 @@ setup(name='GeoTicket',
         'mail2trac': [ 'mail2trac' ],
         },
       dependency_links=[
+        "http://geopy.googlecode.com/svn/branches/reverse-geocode#egg=geopy-0.93dev",
         "http://trac-hacks.org/svn/customfieldproviderplugin/0.11#egg=CustomFieldProvider",
         "http://trac-hacks.org/svn/ticketsidebarproviderplugin/0.11#egg=TicketSidebarProvider",
         "http://trac-hacks.org/svn/mailtotracplugin/0.11/#egg=mail2trac",
