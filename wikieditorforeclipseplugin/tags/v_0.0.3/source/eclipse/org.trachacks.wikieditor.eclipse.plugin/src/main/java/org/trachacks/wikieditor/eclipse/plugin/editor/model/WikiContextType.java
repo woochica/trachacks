83075f4c3c90df10744e92feec8e4055853e3b66
ivangsa <ivangsa@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-06-03 18:26:12 +0000

    quick update
    git-svn-id: https://trac-hacks.org/svn@5955 7322e99d-02ea-0310-aa39-e9a107903beb

--- wikieditorforeclipseplugin/tags/v_0.0.3/source/eclipse/org.trachacks.wikieditor.eclipse.plugin/src/main/java/org/trachacks/wikieditor/eclipse/plugin/editor/model/WikiContextType.java
+++
@@ -1,8 +0,0 @@
-package org.trachacks.wikieditor.eclipse.plugin.editor.model;
-
-import org.eclipse.jface.text.templates.TemplateContextType;
-
-public class WikiContextType extends TemplateContextType
-{   
-    
-}
