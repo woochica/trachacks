5b962b1745c865126389e3c778f3463d7d728ff3
k0s <k0s@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-06-03 16:37:25 +0000

    setup for adding PostGIS to the DB....dont actually add yet
    git-svn-id: https://trac-hacks.org/svn@5948 7322e99d-02ea-0310-aa39-e9a107903beb

--- traclegosscript/anyrelease/example/geo/geotrac/project.py
+++ traclegosscript/anyrelease/example/geo/geotrac/project.py
@@ -1,3 +1,5 @@
+import subprocess
+
 from traclegos.project import TracProject
 from paste.script import templates
 
@@ -13,3 +15,44 @@ class GeoTracProject(TracProject):
                  default='localhost'),
              ]
 
+    def post(self, command, output_dir, vars):
+        """
+        install PostGIS on the PostgreSQL db;
+        see http://postgis.refractions.net/documentation/manual-1.3/ch02.html
+        """
+
+        TracProject.post(self, command, output_dir, vars)
+
+
+        # needed .sql files
+        postgis_files = { 'lwpostgis.sql': None, 
+                          'spatial_ref_sys.sql': None )
+        
+        # ensure `locate` is available
+        if subprocess.call(['locate', '--help'], stdout=subproces.PIPE):
+            print "locate not available and sadly that seems to be the only way to locate the PostGIS .sql files: %s" % ', '.join(postgis_files.keys())
+            return
+
+        # locate the needed PostGIS .sql files
+        # if they exist and can't be found with locate, 
+        # the locate db may need to be updated:
+        # `updatedb`
+        for f in postgis_files:
+            process = subprocess.Popen(['locate', f], stdout=subprocess.PIPE)
+            files, stderr = process.communicate()
+            files = files.strip().split('\n')
+            if not len(files):
+                print '"%s" could not be located (needed for PostGIS)' % f
+                return
+            if len(files) != 1:
+                print 'Multiple file found for "%s":\n %s' % (f, '\n'.join(files))
+                return
+            postgis_files[f] = files[0]
+
+        # ensure createlang is available
+        if subprocess.call(['createlang', '--help'], stdout=subproces.PIPE):
+            print "Error: createlang executable could not be found"
+            return
+        
+        # setup the DB with PostGIS
+        
