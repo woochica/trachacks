f185db2340329fb9d87e80cb041a6e976130fe72
aviram <aviram@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-06-05 12:22:54 +0000

    Added the changeset's title the branches that were changed
    git-svn-id: https://trac-hacks.org/svn@5967 7322e99d-02ea-0310-aa39-e9a107903beb

--- branchtimelineplugin/0.11/BranchTimeline.py
+++ branchtimelineplugin/0.11/BranchTimeline.py
@@ -29,6 +29,8 @@ from trac.core import *
 from trac.versioncontrol.web_ui import ChangesetModule
 from trac.versioncontrol.web_ui.util import get_existing_node
 
+from genshi.builder import tag
+
 BRANCHES_PATH = 'branches'
 TRUNK_PATH = 'trunk'
 FILTER_NAME_PREFIX = 'branch_filter_'
@@ -41,7 +43,10 @@ if python_version < (2, 5):
             if member:
                 return True
         return False
-    
+
+def _unique_list(iterable):
+    return list(set(iterable))
+
 def _get_filter_name(branch_name):
     return "%s%s" % (FILTER_NAME_PREFIX, branch_name)
 
@@ -61,15 +66,63 @@ def _changeset_belongs_to_branches(changeset_object, branches):
         if any([True
                 for branch in branches
                 # Again, use of indices...
-                if (change[0] and branch in change[0]) or (change[3] and branch in change[3])
+                if (change[0] and change[0].startswith(branch)) or (change[3] and change[3].startswith(branch))
                ]):
             return True
     return False
 
+def _get_branch_name(branch_path, branches_path, trunk_path):
+    if branch_path.startswith(trunk_path):
+        return trunk_path
+
+    # Get the branches dir name in case there's a sub-file in there
+    return branch_path[len(branches_path):].strip(os.sep)
+
+def _is_branch_path(change_path, branches_path):
+    if change_path:
+        return change_path.startswith(branches_path)
+    else:
+        return False
+
+def _is_trunk_path(change_path, trunk_path):
+    if change_path:
+        return change_path.startswith(trunk_path)
+    else:
+        return False
+
+def _conditional_add_branch_name(change_path, branches, branches_path, trunk_path):
+    if _is_branch_path(change_path, branches_path):
+        # Get only the path of the branch
+        branch_name = _get_branch_name(change_path, branches_path, trunk_path)
+        # branch_name now may include filenames in the branch, line 'branch2/file1'
+        subfile_index = branch_name.find(os.sep)
+        if subfile_index == -1:
+            subfile_index = None
+        branch_name = branch_name[:subfile_index]
+        
+        branches.append(os.path.join(branches_path, branch_name))
+        
+    if _is_trunk_path(change_path, trunk_path):
+        branches.append(trunk_path)
+            
+def _get_changeset_branches(changeset_object, branches_path, trunk_path):
+    branches = []
+    for change in changeset_object.get_changes():
+        _conditional_add_branch_name(change[0], branches, branches_path, trunk_path)
+        _conditional_add_branch_name(change[3], branches, branches_path, trunk_path)
+    return branches
+
+def _get_changeset_event_branches(changeset_event, branches_path, trunk_path):
+    branches = []
+    for changeset_object in changeset_event[3][0]:
+        branches += _get_changeset_branches(changeset_object, branches_path, trunk_path)
+    return _unique_list(branches)
+        
 class BranchesTimelineModule(ChangesetModule):
     def __init__(self):
         self._branches_path = self._get_branches_path()
         self._trunk_path = self._get_trunk_path()
+        self._filtered_branches = []
 
     def _get_configuration_path(self, variable_name, default_value):
         path = self.env.config.get('timeline-branches', variable_name)
@@ -95,9 +148,8 @@ class BranchesTimelineModule(ChangesetModule):
         repos = self.env.get_repository(req.authname)
         node = get_existing_node(req, repos, self._branches_path, repos.youngest_rev)
         undisplayed_branches = self._get_undisplayed_branches()
-        branches_directory_length = len(self._branches_path) # Optimization (although I never checked if that's slow)
         for branch_node in node.get_entries():
-            branch_name = branch_node.path[branches_directory_length:].strip(os.sep)
+            branch_name = _get_branch_name(branch_node.path, self._branches_path, self._trunk_path)
             # Branches are directories under the branches directory
             if node.isdir and (branch_name not in undisplayed_branches):
                 yield branch_name
@@ -149,6 +201,9 @@ class BranchesTimelineModule(ChangesetModule):
         # filtered_branches are the branches that we DO want to see...
         filtered_branches = _get_filtered_branches(filters, self._branches_path)
 
+        # Save the filtered branches for the method render_timeline_event
+        self._filtered_branches = filtered_branches
+        
         # Trunk filter
         if 'trunk' in filters:
             filtered_branches.append(self._trunk_path)
@@ -161,3 +216,25 @@ class BranchesTimelineModule(ChangesetModule):
             for changeset_object in changeset[3][0]:
                 if _changeset_belongs_to_branches(changeset_object, filtered_branches):
                     yield changeset
+
+    def _branches_to_display(self, branches):
+        if self._filtered_branches:
+            return [branch for branch in branches
+                    if branch in self._filtered_branches]
+        return branches
+    
+    def render_timeline_event(self, context, field, event):
+        return_value = super(BranchesTimelineModule, self).render_timeline_event(context, field, event)
+        
+        if field != 'title':
+            return return_value
+
+        # Add the branches names to the title
+        title = return_value
+        branches = _get_changeset_event_branches(event, self._branches_path, self._trunk_path)
+        branches = self._branches_to_display(branches)
+        
+        branch_names = tag(' in %s' % (', '.join([_get_branch_name(branch, self._branches_path, self._trunk_path)
+                                                for branch in branches]))
+                          )
+        return title + branch_names
