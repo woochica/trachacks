59bc805bf1acf6cfa682088aee0d62f863a240f2
giel <giel@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-07-01 09:51:58 +0000

    Use exception handling instead of an 'if' check to check for perm_user (e-mail address from SSL certificate):  * This fixes a bug where non-SSL-cert authentication (but not anonymous) caused an exception (AttributeError) while trying to access req.perm_user while it didn't exist
    git-svn-id: https://trac-hacks.org/svn@6136 7322e99d-02ea-0310-aa39-e9a107903beb

--- sslauthenticationplugin/0.10/sslauthentication/__init__.py
+++ sslauthenticationplugin/0.10/sslauthentication/__init__.py
@@ -6,9 +6,6 @@ class SSLAuthenticationPlugin(Component):
 
     # IAuthenticator methods
     def authenticate(self, req):
-        authname = None
-        req.perm_user = None
-
         # If SSL_CLIENT_S_DN_Email is set (an e-mail address part of the SSL
         # client certificate), then use this to check the permission store with
         if 'SSL_CLIENT_S_DN_Email' in req.environ:
@@ -17,9 +14,10 @@ class SSLAuthenticationPlugin(Component):
         # Check for the user's name (and possibly login-/permission-name as
         # well). First check for a name in an SSL client certicicate
         if 'SSL_CLIENT_S_DN_CN' in req.environ:
-            authname = req.environ['SSL_CLIENT_S_DN_CN']
+            return req.environ['SSL_CLIENT_S_DN_CN']
         # Use the e-mail address from the SSL client certificiate we found above
-        elif req.perm_user:
-            authname = req.perm_user
-
-        return authname
+        else:
+            try:
+                return req.perm_user
+            except AttributeError:
+                pass
--- sslauthenticationplugin/0.11/sslauthentication/__init__.py
+++ sslauthenticationplugin/0.11/sslauthentication/__init__.py
@@ -8,9 +8,6 @@ class SSLAuthenticationPlugin(Component):
 
     # IAuthenticator methods
     def authenticate(self, req):
-        authname = None
-        req.perm_user = None
-
         # If SSL_CLIENT_S_DN_Email is set (an e-mail address part of the SSL
         # client certificate), then use this to check the permission store with
         if 'SSL_CLIENT_S_DN_Email' in req.environ:
@@ -19,18 +16,19 @@ class SSLAuthenticationPlugin(Component):
         # Check for the user's name (and possibly login-/permission-name as
         # well). First check for a name in an SSL client certicicate
         if 'SSL_CLIENT_S_DN_CN' in req.environ:
-            authname = req.environ['SSL_CLIENT_S_DN_CN']
+            return req.environ['SSL_CLIENT_S_DN_CN']
         # Use the e-mail address from the SSL client certificiate we found above
-        elif req.perm_user:
-            authname = req.perm_user
-
-        return authname
+        else:
+            try:
+                return req.perm_user
+            except AttributeError:
+                pass
 
 def _new_get_perm(self, req):
-    self.authenticate(req)
-    if req.perm_user:
+    authname = self.authenticate(req)
+    try:
         return PermissionCache(self.env, req.perm_user)
-    else:
-        return PermissionCache(self.env, self.authenticate(req))
+    except AttributeError:
+        return PermissionCache(self.env, authname)
 
 RequestDispatcher._get_perm = _new_get_perm
