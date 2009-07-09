d4a5f6b3ce4f8be3507a7b3a30bb62d27f4295ab
pdoup <pdoup@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-07-09 14:27:52 +0000

    Initial checkin of the plugin.
    git-svn-id: https://trac-hacks.org/svn@6163 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ timelinecomponentfilterplugin/0.11/TimelineComponentFilter.py
@@ -0,0 +1,66 @@
+from trac.core import *
+from trac.web.api import IRequestFilter
+from trac.web.api import ITemplateStreamFilter
+from trac.ticket.model import Ticket
+
+from genshi.builder import tag
+from genshi.filters import Transformer
+
+author = "pdoup"
+
+class TimelineComponentFilterPlugin(Component):
+    """Filters timeline events by component.
+    """
+       
+    implements(IRequestFilter, ITemplateStreamFilter)
+
+    # IRequestFilter methods
+
+    def pre_process_request(self, req, handler):
+        return handler
+
+    def post_process_request(self, req, template, data, content_type):
+        if template == 'timeline.html':
+            components = req.args.get('filter-components', '')
+            if components:
+                tickettypes = ["newticket", "editedticket", "closedticket", "attachment", "reopenedticket"]
+                filtered_events = []
+                for event in data['events']:
+                    self.env.log.debug(event['kind'])
+                    if event['kind'] in tickettypes:
+                        resource = event['kind'] == "attachment" and event['data'][0].parent or event['data'][0]
+                        if resource.realm == "ticket":
+                            ticket = Ticket( self.env, resource.id )
+                            if ticket.values['component'] in components:
+                                filtered_events.append(event)
+#                    elif event['kind'] == "attachment":
+#                        self.env.log.debug("realm : %s" % event['data'][0].realm)
+#                        self.env.log.debug("id    : %s" % event['data'][0].id)
+#                        self.env.log.debug("parent: %s" % event['data'][0].parent.id)
+#                        filtered_events.append(event)
+                    else:
+                        filtered_events.append(event)
+                data['events'] = filtered_events
+        return (template, data, content_type)
+
+    # ITemplateStreamFilter methods
+
+    def filter_stream(self, req, method, filename, stream, data):
+        if filename == 'timeline.html':
+            # Insert the new field for entering user names
+            filter = Transformer('//form[@id="prefs"]/fieldset')
+            return stream | filter.before(tag.br()) | filter.before(tag.label("Filter Components (none for all): ")) | filter.before(tag.br()) | filter.before(self._components_field_input(req))
+        return stream
+
+    def _components_field_input(self, req):
+        cursor = self.env.get_db_cnx().cursor()
+        cursor.execute("SELECT name FROM component ORDER BY name")
+        select = tag.select(name="filter-components", id="filter-components", multiple="multiple", size="10")
+        selectedcomps = []
+        selectedcomps = req.args.get('filter-components', '')
+        for component in cursor:
+            if component[0] in selectedcomps:
+                select.append(tag.option(component[0], value=component[0], selected="selected"))
+            else:
+                select.append(tag.option(component[0], value=component[0]))
+        return select
