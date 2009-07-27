dd710371bd85129d357b5a446387245f881247e1
xpech <xpech@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-07-27 15:59:27 +0000

    update plugin info
    git-svn-id: https://trac-hacks.org/svn@6294 7322e99d-02ea-0310-aa39-e9a107903beb

--- icalviewplugin/0.11/setup.py
+++ icalviewplugin/0.11/setup.py
@@ -1,13 +1,13 @@
 from setuptools import find_packages, setup
 
-version='0.3'
+version='0.4'
 
 setup(name='icalview',
       version=version,
-      description="trac plugin Query as iCal format",
+      description="Provide icalendar export for Queries",
       author='Xavier Péchoultres',
       author_email='x.pechoulres@clariprint.com',
-      url='http://www.clariprint.com',
+      url='http://trac-hacks.org/wiki/IcalViewPlugin',
       keywords='trac plugin icalendar',
       license="GPL",
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests*']),
