2d41cee570964675b3d67cb7db71c8fa37521013
nicholasbs <nicholasbs@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-08-21 14:46:15 +0000

    Initial import of PDFRedirector
    git-svn-id: https://trac-hacks.org/svn@6406 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ pdfredirectorplugin/0.11/pdfredirector/pdfredirector.py
@@ -0,0 +1,57 @@
+"""
+PDFRedirector:
+a plugin for Trac
+http://trac.edgewall.org
+"""
+
+from trac.core import *
+
+from trac.web.api import IRequestFilter
+
+class PDFRedirector(Component):
+
+    implements(IRequestFilter)
+
+    ### methods for IRequestFilter
+
+    """Extension point interface for components that want to filter HTTP
+    requests, before and/or after they are processed by the main handler."""
+
+    def post_process_request(self, req, template, data, content_type):
+        """Do any post-processing the request might need; typically adding
+        values to the template `data` dictionary, or changing template or
+        mime type.
+        
+        `data` may be update in place.
+
+        Always returns a tuple of (template, data, content_type), even if
+        unchanged.
+
+        Note that `template`, `data`, `content_type` will be `None` if:
+         - called when processing an error page
+         - the default request handler did not return any result
+
+        (Since 0.11)
+        """
+
+        return template, data, content_type
+
+    def pre_process_request(self, req, handler):
+        """Called after initial handler selection, and can be used to change
+        the selected handler or redirect request.
+        
+        Always returns the request handler, even if unchanged.
+        """
+        path = req.path_info.strip('/').split('/')
+        if len(path) < 2 or path[0] != 'attachment':
+            return handler
+
+        if not path[-1].lower().endswith('.pdf'):
+            return handler
+
+        req.redirect('/'.join(['raw-attachment'] + path[1:])) 
+
+
+
+
+
