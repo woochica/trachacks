77cdad6d8041832a40d38fb0ae8463662cd91373
sarahstrong <sarahstrong@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-08-24 00:12:30 +0000

    first commit of content
    git-svn-id: https://trac-hacks.org/svn@6412 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ tracsnapplugin/0.11/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+PACKAGE = 'TracSocialnetwork'
+VERSION = '0.1'
+
+setup(name=PACKAGE,
+	version=VERSION,
+	packages=['socialnetwork'],
+	package_data={PACKAGE : ['htdocs/*', 'templates/*']},
+	entry_points={'trac.plugins': '%s = socialnetwork' % PACKAGE},
+)
+
