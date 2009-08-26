bdeb6f4f1e527e131e0ecc02fdd31c4c0e4860f8
k0s <k0s@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-08-26 17:09:34 +0000

    add image captchas and make them the default
    git-svn-id: https://trac-hacks.org/svn@6432 7322e99d-02ea-0310-aa39-e9a107903beb

--- captchaauthplugin/0.11/captchaauth/__init__.py
+++ captchaauthplugin/0.11/captchaauth/__init__.py
@@ -16,18 +16,24 @@ from skimpyGimpy import skimpyAPI
 
 from trac.core import *
 from trac.web import IRequestFilter
+from trac.web.api import IRequestHandler
 from trac.web import ITemplateStreamFilter
 from trac.web.chrome import add_warning 
+from trac.config import Option
 from trac.config import PathOption
 
 class CaptchaauthPlugin(Component):
-    implements(IRequestFilter, ITemplateStreamFilter)
 
+    ### class data
+    implements(IRequestFilter, ITemplateStreamFilter, IRequestHandler)
     dict_file = PathOption('captchaauth', 'dictionary_file',
                            default="http://java.sun.com/docs/books/tutorial/collections/interfaces/examples/dictionary.txt")
+    captcha_type = Option('captchaauth', 'type',
+                          default="png")
 
 
-    # IRequestFilter methods
+    ### IRequestFilter methods
+
     def pre_process_request(self, req, handler):
         """Called after initial handler selection, and can be used to change
         the selected handler or redirect request.
@@ -85,7 +91,35 @@ class CaptchaauthPlugin(Component):
         """
         return (template, data, content_type)
 
-    # ITemplateStreamFilter methods
+    
+    ### methods for IRequestHandler
+
+    def match_request(self, req):
+        """Return whether the handler wants to process the given request."""
+        if self.captcha_type == 'png' and req.path_info.strip('/') == 'captcha.png' and 'captcha' in req.session:
+            return True
+
+
+    def process_request(self, req):
+        """Process the request. For ClearSilver, return a (template_name,
+        content_type) tuple, where `template` is the ClearSilver template to use
+        (either a `neo_cs.CS` object, or the file name of the template), and
+        `content_type` is the MIME type of the content. For Genshi, return a
+        (template_name, data, content_type) tuple, where `data` is a dictionary
+        of substitutions for the template.
+
+        For both templating systems, "text/html" is assumed if `content_type` is
+        `None`.
+
+        Note that if template processing should not occur, this method can
+        simply send the response itself and not return anything.
+        """
+        img = skimpyAPI.Png(req.session['captcha']).data()
+        req.send(img, 'image/png')
+
+
+    ### ITemplateStreamFilter method
+
     def filter_stream(self, req, method, filename, stream, data):
         """Return a filtered Genshi event stream, or the original unfiltered
         stream if no match.
@@ -102,21 +136,29 @@ class CaptchaauthPlugin(Component):
         msg = "Please enter the text below to prove you're not a machine."
 
         if filename == "register.html":
-            key = random.Random().randint(0, sys.maxint)
             word = self.random_word()
             req.session['captcha'] = word
             req.session.save()
-            content = "<p>%s</p>%s" % (msg, self.skimpy_captcha(word))
+            if self.captcha_type == 'png':
+                captcha = '<img src="%s"/>' % req.href('captcha.png')
+            else:
+                captcha = self.skimpy_pre_captcha(word)
+            content = "<p>%s</p><p>%s</p>" % (msg, captcha)
             content += "<label>Confirm: <input type='text' name='captcha' class='textwidget' size='20'></label>"
             stream |= Transformer('//form[@id="%s"]/fieldset[1]' % form_id).append(tag.div(Markup(content)))
 
         return stream
 
-    # helpers
-    def skimpy_captcha(self, passphrase):
+
+    ### internal methods
+
+    def skimpy_pre_captcha(self, passphrase):
         """ Return HTML content of captcha as string """
         return skimpyAPI.Pre(passphrase).data()
                                        
+    def skimpy_png_captcha(self, passphrase):
+        pass
+
     def random_word(self):
         """ Return a random word for use with the captcha """
         min_len =  5
