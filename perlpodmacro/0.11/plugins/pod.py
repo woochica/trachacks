2703d92753d854c111b448bbdfa501653a095f23
sfandino <sfandino@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-08-26 22:17:02 +0000

    first version
    git-svn-id: https://trac-hacks.org/svn@6435 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ perlpodmacro/0.11/plugins/pod.py
@@ -0,0 +1,18 @@
+from trac.wiki.macros import WikiMacroBase
+from trac.util.html import Markup
+from trac.wiki import Formatter
+
+from StringIO import StringIO
+from subprocess import Popen, PIPE
+
+class PodMacro(WikiMacroBase):
+    """Converts from Perl Plain Old Documentation format (POD) to HTML"""
+
+    def expand_macro(self, formatter, name, args):
+        pod2wiki = Popen(['/usr/bin/pod2html'],
+                         stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        (stdout, stderr) = pod2wiki.communicate(args.encode('latin1'))
+        html = stdout.decode('latin1')
+        
+        return html[html.find('>', html.find('<body'))+1:html.find('</body>')].strip()
+
