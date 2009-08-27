f8fe4bf37229e2a9ee2ae5f39ae3255721e9f58f
sfandino <sfandino@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-08-27 11:22:54 +0000

    pod2html writes temporary files to the current directory so we chdir into /tmp before calling it
    git-svn-id: https://trac-hacks.org/svn@6436 7322e99d-02ea-0310-aa39-e9a107903beb

--- perlpodmacro/0.11/plugins/pod.py
+++ perlpodmacro/0.11/plugins/pod.py
@@ -9,7 +9,7 @@ class PodMacro(WikiMacroBase):
     """Converts from Perl Plain Old Documentation format (POD) to HTML"""
 
     def expand_macro(self, formatter, name, args):
-        pod2wiki = Popen(['/usr/bin/pod2html'],
+        pod2wiki = Popen(['/usr/bin/pod2html'], cwd='/tmp',
                          stdin=PIPE, stdout=PIPE, stderr=PIPE)
         (stdout, stderr) = pod2wiki.communicate(args.encode('latin1'))
         html = stdout.decode('latin1')
