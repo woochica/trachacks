33cfa14357308e115b8a46ce62faec41721b2fe5
dottedmag <dottedmag@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-08-30 11:14:04 +0000

    Add metadata to plugin (Closes: #5746)
    git-svn-id: https://trac-hacks.org/svn@6457 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracciaplugin/trunk/setup.py
+++ tracciaplugin/trunk/setup.py
@@ -6,6 +6,11 @@ from setuptools import setup
 setup(
     name = 'TracCia',
     version = '0.3.9',
+    maintainer = 'Mikhail Gusarov',
+    maintainer_email = 'dottedmag@dottedmag.net',
+    url = 'http://trac-hacks.org/wiki/TracCiaPlugin',
+    license = 'GPL2+',
+    description = 'Trac and CIA.vc integration plugin',
     packages = ['traccia'],
     entry_points = {
         'trac.plugins': [
