939a4cafa0ccde0f744f7d22321beb85a618381f
thomasvs <thomasvs@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-09-09 17:01:03 +0000

    update metadata
    git-svn-id: https://trac-hacks.org/svn@6524 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracdupplugin/0.10/setup.py
+++ tracdupplugin/0.10/setup.py
@@ -6,6 +6,17 @@ from setuptools import find_packages, setup
 setup(
   name='TracDupPlugin',
   version=0.1.1,
+  description='duplication of tickets to other tickets',
+  long_description=""" """,
+  classifiers=[
+    'Framework :: Trac',
+    'License :: OSI Approved :: BSD License', 
+  ],
+  keywords='trac.ticket',
+  author='Thomas Vander Stichele',
+  author_email='thomas (at) apestaart (dot) org',
+  url='http://trac-hacks.org/wiki/TracDupPlugin',
+  license='BSD',
   packages=find_packages(exclude=['*.tests*']),
   entry_points = """
     [trac.plugins]
