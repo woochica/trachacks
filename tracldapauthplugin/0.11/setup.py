72417c4e6c1b6e7e1317c2d992215a49a0454b8e
k0s <k0s@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-09-28 15:10:12 +0000

    adding python-ldap dependency the stupid way
    git-svn-id: https://trac-hacks.org/svn@6605 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracldapauthplugin/0.11/setup.py
+++ tracldapauthplugin/0.11/setup.py
@@ -1,6 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: iso-8859-1 -*-
 
+import sys
+
+try:
+    import ldap
+except ImportError:
+    print """The python-ldap package isn't installed on your system 
+(`import ldap` failed).  I would just put this in `install_requires`, but
+I *do* have python-ldap on my system and I get this:
+
+{{{
+Processing dependencies for TracLDAPAuth==1.0
+Searching for python-ldap==2.3.1
+Reading http://pypi.python.org/simple/python-ldap/
+Reading http://python-ldap.sourceforge.net/
+Reading http://sourceforge.net/project/showfiles.php?group_id=2072&package_id=2011
+No local packages or download links found for python-ldap==2.3.1
+error: Could not find suitable distribution for Requirement.parse('python-ldap==2.3.1')
+}}}
+
+Well, that's awful.  If you know how to fix this, please file a ticket at 
+http://trac-hacks.org/newticket?component=TracLdapAuthPlugin&owner=k0s
+"""
+    sys.exit(1)
+
 from setuptools import setup
 
 setup(
@@ -18,7 +42,7 @@ setup(
         'Framework :: Trac',
     ],
     
-    install_requires = ['TracAccountManager'],
+    install_requires = ['TracAccountManager' ],
 
     entry_points = {
         'trac.plugins': [
