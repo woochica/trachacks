6a15f8fb65309374a28483f0840de6303720eb4c
k0s <k0s@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-09-28 15:11:05 +0000

    add TLS authentication
    git-svn-id: https://trac-hacks.org/svn@6606 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracldapauthplugin/0.11/ldapauth/store.py
+++ tracldapauthplugin/0.11/ldapauth/store.py
@@ -1,6 +1,7 @@
 import ldap
 
 from trac.core import *
+from trac.config import BoolOption
 from trac.config import Option
 
 from acct_mgr.api import IPasswordStore
@@ -8,9 +9,9 @@ from acct_mgr.api import IPasswordStore
 class LDAPStore(Component):
     """An AccountManager backend to use LDAP."""
     
-    server_host = Option('ldap', 'server', doc='Server to use for LDAP authentication')
-    
+    server_host = Option('ldap', 'server', doc='Server to use for LDAP authentication')    
     bind_dn = Option('ldap', 'bind_dn', doc='Template to make the bind DN')
+    use_tls = Option('ldap', 'use_tls', default='false', doc='enable TLS support') 
     
     implements(IPasswordStore)
     
@@ -18,6 +19,8 @@ class LDAPStore(Component):
         self.log.debug('LDAPAuth: Checking password for user %s', user)
         try:
             l = ldap.open(self.server_host)
+            if self.use_tls:
+                l.start_tls_s()
             bind_name = (self.bind_dn%user).encode('utf8')
             self.log.debug('LDAPAuth: Using bind DN %s', bind_name)
             l.simple_bind_s(bind_name, password.encode('utf8'))
