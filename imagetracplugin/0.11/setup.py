94d0ca83ea04fa044e32c70d76a7a70614c97d9a
k0s <k0s@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-10-13 19:10:14 +0000

    rename setup.py to default_image.py since this is much more descriptive
    git-svn-id: https://trac-hacks.org/svn@6700 7322e99d-02ea-0310-aa39-e9a107903beb

--- imagetracplugin/0.11/setup.py
+++ imagetracplugin/0.11/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-version='0.3.2'
+version='0.3.3'
 
 setup(name='ImageTrac',
       version=version,
@@ -31,6 +31,6 @@ setup(name='ImageTrac',
       [trac.plugins]
       imagetrac = imagetrac.image
       imagesidebar = imagetrac.web_ui
-      defaultimage = imagetrac.setup
+      defaultimage = imagetrac.default_image
       """,
       )
