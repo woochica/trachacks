2038068eb00642144f2192dc923e6900bb99dbf8
gregmac <gregmac@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-10-13 18:59:09 +0000

    Add error messages; status message/icon indicating Loading; Animations to panel
    git-svn-id: https://trac-hacks.org/svn@6697 7322e99d-02ea-0310-aa39-e9a107903beb

--- duplicateticketsearchplugin/0.11/tracDupeSearch.js
+++ duplicateticketsearchplugin/0.11/tracDupeSearch.js
@@ -3,6 +3,16 @@ $(document).ready(function() {
 		var text = $('div#content.ticket input#field-summary').val();
 		if (text.length > 0) {
 			var xmlHref = location.href + '/../xmlrpc';
+			
+			var html = '<h5 class="loading">Loading related tickets..</h5>';
+			var dupeticketlistDiv = $('div#content.ticket input#field-summary + div#dupeticketlist');
+			if (dupeticketlistDiv.length == 0) {
+				$('div#content.ticket input#field-summary').after('<div id="dupeticketlist" style="display:none;"></div>');
+				dupeticketlistDiv = $('div#content.ticket input#field-summary + div#dupeticketlist');
+			}
+			$('ul',dupeticketlistDiv).slideUp('fast');
+			dupeticketlistDiv.html(html).slideDown();
+			
 			$.ajax({
 				url:xmlHref, 
 				data:
@@ -21,8 +31,14 @@ $(document).ready(function() {
 					var maxTickets = 15;
 					
 					var html = '';
-					if (tickets.length > 0) {
-						html = '<h5>Possible related tickets:</h5><ul>'
+					if (tickets === null) {
+						// error
+						dupeticketlistDiv.html('<h5 class="error">Error loading tickets.</h5>');
+					} else if (tickets.length <= 0) {
+						// no dupe tickets
+						dupeticketlistDiv.hide();
+					} else {
+						html = '<h5>Possible related tickets:</h5><ul style="display:none;">'
 						tickets = tickets.reverse();
 						
 						for (var i = 0; i < tickets.length && i < maxTickets; i++) {
@@ -36,14 +52,15 @@ $(document).ready(function() {
 							var text = $('div#content.ticket input#field-summary').val();
 							html += '<a href="' + searchBaseHref + escape(text) + '">More..</a>';
 						}
+						
+						dupeticketlistDiv.html(html);
+						$('> ul', dupeticketlistDiv).slideDown();
+						
 					}
 					
-					var existingDiv = $('div#content.ticket input#field-summary + div#dupeticketlist');
-					if (existingDiv.length > 0) {
-						existingDiv.html(html);
-					} else {
-						$('div#content.ticket input#field-summary').after('<div id="dupeticketlist">' + html + '</div>');
-					}
+				},
+				error: function(xhr, textStatus, exception) {
+					dupeticketlistDiv.html('<h5 class="error">Error loading tickets: ' + textStatus + '</h5>');
 				}
 			});
 		}
@@ -56,35 +73,41 @@ $(document).ready(function() {
 		var summaryRegex = /#(\d+)<\/span>:\s*([a-z0-9\-\_\ ]+):\s*(.*)\s*\(([a-z0-9\-\_\ ]+)(?:\: ([a-z0-9\-\_\ ]+))?\)$/i;
 		
 		//console.log('.each ', $('params > param > value > array > data > value', xmlData));
-		$('params > param > value > array > data > value', xmlData).each(function(i) {
-			var xmlEntry = $(this);
-			
-			var url = xmlEntry.find('value:eq(0) string')[0].textContent;
-			var summaryRaw = xmlEntry.find('value:eq(1) string')[0].textContent;
-			var date = xmlEntry.find('value:eq(2) dateTime\\.iso8601')[0].textContent;
-			var owner = xmlEntry.find('value:eq(3) string')[0].textContent;
-			var descr = xmlEntry.find('value:eq(4) string')[0].textContent;
-			
-			// filter for attachments
-			if (url.indexOf('attachment/ticket') == -1) {
+		var dataValues = $('params > param > value > array > data > value', xmlData);
+		if (dataValues.length > 0) {
+			dataValues.each(function(i) {
+				var xmlEntry = $(this);
 				
-				var summaryMatches = summaryRegex.exec(summaryRaw);
+				var url = xmlEntry.find('value:eq(0) string')[0].textContent;
+				var summaryRaw = xmlEntry.find('value:eq(1) string')[0].textContent;
+				var date = xmlEntry.find('value:eq(2) dateTime\\.iso8601')[0].textContent;
+				var owner = xmlEntry.find('value:eq(3) string')[0].textContent;
+				var descr = xmlEntry.find('value:eq(4) string')[0].textContent;
 				
-				returnData[returnIdx++] = {
-					url: url,
-					summaryRaw: summaryRaw,
-					date: date,
-					owner: owner,
-					description: descr,
-					ticket: summaryMatches[1],
-					type: summaryMatches[2],
-					summary: summaryMatches[3],
-					status: summaryMatches[4],
-					resolution: summaryMatches[5]
-				};
-			}
-			
-		});
+				// filter for attachments
+				if (url.indexOf('attachment/ticket') == -1) {
+					
+					var summaryMatches = summaryRegex.exec(summaryRaw);
+					
+					returnData[returnIdx++] = {
+						url: url,
+						summaryRaw: summaryRaw,
+						date: date,
+						owner: owner,
+						description: descr,
+						ticket: summaryMatches[1],
+						type: summaryMatches[2],
+						summary: summaryMatches[3],
+						status: summaryMatches[4],
+						resolution: summaryMatches[5]
+					};
+				}
+				
+			});
+		} else {
+			// didn't get a valid response at all
+			return null;
+		}
 		//console.log(returnData);
 		
 		return returnData;
