369853c282083efffc963d08e6750147c3c97834
martin_s <martin_s@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-10-23 21:30:42 +0000

    notify.py:: Added class for email notifications.
    git-svn-id: https://trac-hacks.org/svn@6828 7322e99d-02ea-0310-aa39-e9a107903beb

--- watchlistplugin/0.11/tracwatchlist/notify.py
+++ watchlistplugin/0.11/tracwatchlist/notify.py
@@ -18,16 +18,25 @@
 """
 from trac.core import *
 
-from  trac.web.href    import  Href
-from  trac.util.text   import  to_unicode
-from  trac.wiki.api    import  IWikiChangeListener
-from  trac.ticket.api  import  ITicketChangeListener
+from  trac.web.href      import  Href
+from  trac.util.text     import  to_unicode
+from  trac.wiki.api      import  IWikiChangeListener
+from  trac.ticket.api    import  ITicketChangeListener
+from  trac.notification  import  NotifyEmail
 
 import operator
 
+class WatchlistNotifier(NotifyEmail):
+    template_name = "ticket_notify_email.txt"
 
-class WatchlistNotifier(Component):
+    def get_recipients(self, resid):
+      debug = self.env.log.debug
+      debug("WLN: resid = " + str(resid))
+      return (list(resid), list())
+
+class WatchlistListener(Component):
     implements( IWikiChangeListener, ITicketChangeListener )
+
 
     def wiki_users(self,page_name):
       db = self.env.get_db_cnx()
@@ -53,6 +62,7 @@ class WatchlistNotifier(Component):
       users = self.wiki_users(page.name)
       debug("WLN: page %s changed: Notifing users: %s" % (page.name,
         ', '.join(users)) )
+      WatchlistNotifier(self.env).notify(users, "Watched wiki page changed")
 
     def wiki_page_deleted(self,page):
       pass
