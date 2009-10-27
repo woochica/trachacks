9d4393d12635a99aa2cf10c21c50513f18a2441c
martin_s <martin_s@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-10-27 17:10:35 +0000

    setup.py:: Minor name fix.
    git-svn-id: https://trac-hacks.org/svn@6891 7322e99d-02ea-0310-aa39-e9a107903beb

--- serversideredirectplugin/0.11/setup.py
+++ serversideredirectplugin/0.11/setup.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
 from setuptools import setup
-from tracserversideredirect.plugin import __revision__ as macrorev
+from tracserversideredirect.plugin import __revision__ as coderev
 
 __url__      = ur"$URL$"[6:-2]
 __author__   = ur"$Author$"[9:-2]
 __revision__ = r"$Rev$"[6:-2]
 __date__     = r"$Date$"[7:-2]
 
-rev = max(macrorev, __revision__)
+rev = max(coderev, __revision__)
 
 setup(
     name         = 'TracServerSideRedirectPlugin',
