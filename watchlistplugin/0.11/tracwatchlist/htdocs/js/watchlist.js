9d8da777c64e34ab0f418ded2c4228d65817afdf
martin_s <martin_s@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-10-29 22:04:16 +0000

    Added show/hide functionality for tables.
    git-svn-id: https://trac-hacks.org/svn@6929 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ watchlistplugin/0.11/tracwatchlist/htdocs/js/watchlist.js
@@ -0,0 +1,57 @@
+/* Javsscript code for Trac Watchlist Plugin */
+
+
+/* From http://chris-barr.com/entry/disable_text_selection_with_jquery/  */
+$(function(){
+  $.extend($.fn.disableTextSelect = function() {
+    return this.each(function(){
+      if($.browser.mozilla){//Firefox
+        $(this).css('MozUserSelect','none');
+      }else if($.browser.msie){//IE
+        $(this).bind('selectstart',function(){return false;});
+      }else{//Opera, etc.
+        $(this).mousedown(function(){return false;});
+      }
+    });
+  });
+  $('.noSelect').disableTextSelect();//No text selection on elements with a class of 'noSelect'
+});
+/*****************************************8*/
+
+
+function wlshowhide(ref, tag) {
+    $(tag).toggle("normal");
+
+    if ($(ref).text() == "(Show)") {
+      $(ref).text("(Hide)");
+    }
+    else {
+      $(ref).text("(Show)");
+    }
+    return true;
+}
+
+jQuery(document).ready(function() {
+
+  $("div#wlsettings").hide();
+  $("a").click(function () {
+    $(this).hide();
+    return true;
+  });
+
+  $("#wikis   span.showhide").text("(Hide)").disableTextSelect();
+  $("#tickets span.showhide").text("(Hide)").disableTextSelect();
+
+  $("#wikis span.showhide").click(function() {
+     wlshowhide(this, "#wikilist-parent");
+     return true;
+  });
+
+  $("#tickets span.showhide").click(function() {
+     wlshowhide(this, "#ticketlist-parent");
+     return true;
+  });
+
+
+});
+
