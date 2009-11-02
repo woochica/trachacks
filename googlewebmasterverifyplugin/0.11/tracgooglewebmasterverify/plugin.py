715b3f3531e05286aa867e482e26d20230ce7a2b
martin_s <martin_s@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-11-02 18:37:15 +0000

    Changed to `trac.config.ListOption` and added code to add meta tags (disabled for now, depends on t:#8792).
    git-svn-id: https://trac-hacks.org/svn@7001 7322e99d-02ea-0310-aa39-e9a107903beb

--- googlewebmasterverifyplugin/0.11/tracgooglewebmasterverify/plugin.py
+++ googlewebmasterverifyplugin/0.11/tracgooglewebmasterverify/plugin.py
@@ -12,24 +12,28 @@ __date__     = r"$Date$"[7:-2]
 from trac.core import *
 
 from  trac.web.api     import  IRequestFilter, IRequestHandler, RequestDone
+from  trac.web.chrome  import  add_link,add_stylesheet,add_script #,add_meta
+from  trac.config      import  ListOption
+
 
 class GoogleWebmasterVerifyPlugin(Component):
     """Simply plugin to return verify webpages for Google Webmaster Service."""
-    implements( IRequestHandler )
+    implements( IRequestHandler ) #, IRequestFilter )
+    vlist = ListOption('googlewebmasterverify', 'verify', doc="Verification code(s)")
+    #mvlist = ListOption('googlewebmasterverify', 'meta_verify', doc="Verification code(s) for meta tag verifcation.")
 
     ### methods for IRequestHandler
     def match_request(self, req):
         """Check if requested URL is '/googleXX..XX.html' where XX..XX is in
         config list."""
         try:
-          vlist = self.config.getlist('googlewebmasterverify', 'verify')
           path  = req.path_info
           if path.startswith("/google") and path.endswith(".html"):
             path = path[7:-5]
           else:
             return False
 
-          if path in vlist:
+          if path in self.vlist:
             return True
           else:
             return False
@@ -39,6 +43,20 @@ class GoogleWebmasterVerifyPlugin(Component):
     def process_request(self, req):
         """Send an empty HTTP 200 OK response."""
         req.send_response(200)
+        req.send_header('Content-Type', 'text/plain')
         req.end_headers()
+        req.write("google-site-verification: " + req.path_info[1:])
         raise RequestDone
+
+    def pre_process_request(self, req, handler):
+        return handler
+
+    def post_process_request(self, req, template, data, content_type):
+        self.env.log.debug("add_meta")
+        if self.mvlist:
+          for code in self.mvlist:
+            self.env.log.debug("add_meta: " + code)
+            add_meta(req,code,name="google-site-verification")
+
+        return (template, data, content_type)
 
