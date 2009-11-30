65d191703bf04c4598443eac3012b18c605161c3
obs <obs@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-11-30 22:37:13 +0000

    Added ticket validation for disabled and hidden fields, if they are modified by the user (i.e. faking the http post or editing the form with tools such as firebug) an access denied error will be thrown fixes #3510
    git-svn-id: https://trac-hacks.org/svn@7207 7322e99d-02ea-0310-aa39-e9a107903beb

--- blackmagictickettweaksplugin/0.11/blackmagic/blackmagic.py
+++ blackmagictickettweaksplugin/0.11/blackmagic/blackmagic.py
@@ -1,6 +1,8 @@
 from trac.core import Component, implements, TracError
 from trac.config import Option, IntOption, ListOption, BoolOption
 from trac.web.chrome import ITemplateProvider, add_stylesheet, add_script
+from trac.ticket.api import ITicketManipulator
+from trac.ticket import model
 from pkg_resources import resource_filename
 from trac.web.api import ITemplateStreamFilter
 from genshi.builder import tag
@@ -19,12 +21,58 @@ def istrue(v, otherwise=None):
             return otherwise
 
 class TicketTweaks(Component):
-    implements(ITemplateStreamFilter, ITemplateProvider, IPermissionRequestor)
+    implements(ITemplateStreamFilter, ITemplateProvider, IPermissionRequestor, ITicketManipulator)
 
     permissions = ListOption('blackmagic', 'permissions', [])
     gray_disabled = Option('blackmagic', 'gray_disabled', '',
         doc="""If not set, disabled items will have their label striked through.
         Otherwise, this color will be used to gray them out. Suggested #cccccc.""")
+
+    # ITicketActionController methods
+
+    def validate_ticket(self, req, ticket):
+        """Validate a ticket after it's been populated from user input.
+
+        Must return a list of `(field, message)` tuples, one for each problem
+        detected. `field` can be `None` to indicate an overall problem with the
+        ticket. Therefore, a return value of `[]` means everything is OK."""
+
+        res = []
+        self.env.log.debug('Validating ticket: %s' % ticket.id)
+
+        enchants = self.config.get('blackmagic', 'tweaks', '')
+        for field in (x.strip() for x in enchants.split(',')):
+            disabled = False
+            hidden = False
+            perm = self.config.get('blackmagic', '%s.permission' % field, '').upper()
+            if perm and perm not in req.perm:
+                denial = self.config.get('blackmagic', '%s.ondenial' % field, None)
+                if denial:
+                    if denial == "disable":
+                        disabled = True
+                    elif denial == "hide":
+                        hidden = True
+                    else:
+                        disabled = True
+                else:
+                    disabled = True
+            #field is disabled or hidden, cannot be modified by user
+            if disabled or istrue(self.config.get('blackmagic', '%s.disable' % field, False)) or hidden or istrue(self.config.get('blackmagic', '%s.hide' % field, None)):
+                self.env.log.debug('%s disabled or hidden ' % field)
+                #get default ticket state or orginal ticket if being modified
+                ot = model.Ticket(self.env, ticket.id)
+                original = ot.values.get('%s' % field, None)
+                new = ticket.values.get('%s' % field, None)
+                self.env.log.debug('OT: %s' % original)
+                self.env.log.debug('NEW: %s' % new)
+                #field has been modified throw error
+                if new != original:
+                    res.append(('%s' % field, 'Access denied to modifying %s' % field))
+                    self.env.log.debug('Denied access to: %s' % field)
+        return res
+
+
+
     ## IPermissionRequestor methods
 
     def get_permission_actions(self):
