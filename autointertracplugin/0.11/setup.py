5462048eb2f55c3726398686683a7d9029221bda
bobbysmith007 <bobbysmith007@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-12-15 15:24:28 +0000

    adding description, re tracMgmt:#54
    git-svn-id: https://trac-hacks.org/svn@7261 7322e99d-02ea-0310-aa39-e9a107903beb

--- autointertracplugin/0.11/setup.py
+++ autointertracplugin/0.11/setup.py
@@ -10,7 +10,20 @@ setup(name=PACKAGE,
       author='Russ Tyndall at Acceleration.net',
       author_email='russ@acceleration.net',
       long_description="""
-        Automatically make InterTrac links to directories full of trac instances
+        Automatically make InterTrac link configuration to directories full of trac instances.
+
+        One component: AutoInterTracPluginSetupParticipant
+            This component monkey patches env.setup_config to add configuration
+            keys to in memory config that expresses all of the intertrac linkings
+            for a directory of trac instances. Also overrides config.save so that 
+            these autofilled values are not written to disk.  This prevents these 
+            keys from ever being out of date with what is on disk.
+      
+        Expects the [intertrac] keys base_dir and base_url which should specify 
+            a folder of trac instances and their standard url (generally 
+            something like http://trac.mydomain.com/ which will result in links like:
+            http://trac.mydomain.com/MyProjectName/ticket/33
+
       """,
       entry_points={'trac.plugins': '%s = %s' % (PACKAGE, PACKAGE)},
 )
