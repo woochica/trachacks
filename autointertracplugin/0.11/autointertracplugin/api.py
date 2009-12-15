858ede0ac3b9d4ed81ace320c373cae107e47254
bobbysmith007 <bobbysmith007@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-12-15 15:24:14 +0000

    Added some more class documentation to the lone component re tracMgmt:#54 (.25)
    git-svn-id: https://trac-hacks.org/svn@7258 7322e99d-02ea-0310-aa39-e9a107903beb

--- autointertracplugin/0.11/autointertracplugin/api.py
+++ autointertracplugin/0.11/autointertracplugin/api.py
@@ -3,7 +3,19 @@ import trac, trac.core,  trac.env, trac.config, os, os.path, urlparse
 
 
 class AutoInterTracPluginSetupParticipant(trac.core.Component):
-    """ This component monkey patches env.setup_config to add some after the fact configuration """
+    """ 
+    This component monkey patches env.setup_config to add configuration
+      keys to in memory config that expresses all of the intertrac linkings
+      for a directory of trac instances. Also overrides config.save so that 
+      these autofilled values are not written to disk.  This prevents these 
+      keys from ever being out of date with what is on disk.
+      
+    Expects the [intertrac] keys base_dir and base_url which should specify 
+      a folder of trac instances and their standard url (generally 
+      something like http://trac.mydomain.com/ which will result in links like:
+      http://trac.mydomain.com/MyProjectName/ticket/33
+    
+    """
     trac.core.implements(trac.env.IEnvironmentSetupParticipant)
     # add auto config vars when we read in the config
     def run_setup (self):
