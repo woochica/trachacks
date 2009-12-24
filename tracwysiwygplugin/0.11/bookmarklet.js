ffc3877d66eb220c5f69cde876c94cd5a9ac0898
jun66j5 <jun66j5@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-12-24 17:04:00 +0000

    Fixed broken bookmarklet for tracwysiwyg
    git-svn-id: https://trac-hacks.org/svn@7300 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracwysiwygplugin/0.11/bookmarklet.js
+++ tracwysiwygplugin/0.11/bookmarklet.js
@@ -18,27 +18,41 @@
     head.appendChild(link);
 
     function getTracPaths() {
-        var paths = {};
+        var paths = { stylesheets: [] };
         var links = head.getElementsByTagName('link');
         var length = links.length;
         for (var i = 0; i < length; i++) {
             var link = links[i];
             var rel = (link.getAttribute('rel') || '').toLowerCase();
+            var type = (link.getAttribute('type') || '').toLowerCase();
             var href = link.getAttribute('href') || '';
             switch (rel) {
+            case 'help':
+                if (!paths.base && /\/wiki\/TracGuide$/.test(href)) {
+                    paths.base = href.slice(0, -14);
+                }
+                break;
             case 'search':
-                if (/\/search$/.test(href)) {
+                if (!paths.search && !type && /\/search$/.test(href)) {
+                    paths.search = href;
                     paths.base = href.slice(0, -6);
                 }
                 break;
             case 'stylesheet':
                 if (/\/css\/trac\.css$/.test(href)) {
-                    paths.htdocs = href.slice(0, -12);
+                    paths.stylesheets.push(href);
                 }
                 break;
             }
         }
-        return paths.base && paths.htdocs ? paths : null;
+        if (paths.base && paths.stylesheets.length > 0) {
+            if (!paths.search) {
+                paths.search = paths.base + 'search';
+            }
+            paths.stylesheets.push(base + 'editor.css');
+            return paths;
+        }
+        return null;
     }
 
     function lazy() {
