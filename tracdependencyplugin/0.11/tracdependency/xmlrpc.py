6572165f16ebfd516b88c3229b8ca1026417b773
okazaki <okazaki@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-12-28 12:29:36 +0000

    refs #6381
    git-svn-id: https://trac-hacks.org/svn@7305 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracdependencyplugin/0.11/tracdependency/xmlrpc.py
+++ tracdependencyplugin/0.11/tracdependency/xmlrpc.py
@@ -28,89 +28,89 @@ class WorkHoursRPC(Component):
 
     def executeQuery(self, req, query, sort):
         """Returns results of query"""
-        #クエリーを実行します．
-        if query == "":
-            query = "t.id>0"
-        if sort == "":
-            sort = "t.id"
-        sql = "SELECT "
-        sql = sql + "t.id, "
-        sql = sql + "t.type, "
-        sql = sql + "t.component, "
-        sql = sql + "t.severity, "
-        sql = sql + "t.priority, "
-        sql = sql + "t.owner, "
-        sql = sql + "t.reporter, "
-        sql = sql + "t.cc, "
-        sql = sql + "t.version, "
-        sql = sql + "t.milestone, "
-        sql = sql + "t.status, "
-        sql = sql + "t.resolution, "
-        sql = sql + "t.summary, "
-        sql = sql + "t.description, "
-        sql = sql + "due_assign.value as 'due_assign', "
-        sql = sql + "due_close.value as 'due_close', "
-        sql = sql + "complete.value as 'complete', "
-        sql = sql + "summary_ticket.value as 'summary_ticket', "
-        sql = sql + "dependencies.value as 'dependencies', "
-        sql = sql + "hours.value as 'hours', "
-        sql = sql + "estimatedhours.value as 'estimatedhours', "
-        sql = sql + "billable.value as 'billable', "
-        sql = sql + "baseline_start.value as 'baseline_start', "
-        sql = sql + "baseline_finish.value as 'baseline_finish', "
-        sql = sql + "baseline_cost.value as 'baseline_cost', "
-        sql = sql + "blocking.value as 'blocking' "
-        sql = sql + "FROM ticket t "
-        sql = sql + "LEFT JOIN ticket_custom due_assign ON due_assign.ticket = t.id AND due_assign.name='due_assign' "
-        sql = sql + "LEFT JOIN ticket_custom due_close ON due_close.ticket = t.id AND due_close.name='due_close' "
-        sql = sql + "LEFT JOIN ticket_custom complete ON complete.ticket = t.id AND complete.name='complete' "
-        sql = sql + "LEFT JOIN ticket_custom summary_ticket ON summary_ticket.ticket = t.id AND (summary_ticket.name='summary_ticket' OR summary_ticket.name='parent') "
-        sql = sql + "LEFT JOIN ticket_custom dependencies ON dependencies.ticket = t.id AND dependencies.name='dependencies' "
-        sql = sql + "LEFT JOIN ticket_custom hours ON hours.ticket = t.id AND hours.name='hours' "
-        sql = sql + "LEFT JOIN ticket_custom estimatedhours ON estimatedhours.ticket = t.id AND estimatedhours.name='estimatedhours' "
-        sql = sql + "LEFT JOIN ticket_custom billable ON billable.ticket = t.id AND billable.name='billable' "
-        sql = sql + "LEFT JOIN ticket_custom baseline_start ON baseline_start.ticket = t.id AND (baseline_start.name='baseline_start' OR baseline_start.name='plan_start') "
-        sql = sql + "LEFT JOIN ticket_custom baseline_finish ON baseline_finish.ticket = t.id AND (baseline_finish.name='baseline_finish' OR baseline_start.name='plan_end') "
-        sql = sql + "LEFT JOIN ticket_custom baseline_cost ON baseline_cost.ticket = t.id AND baseline_cost.name='baseline_cost' "
-        sql = sql + "LEFT JOIN ticket_custom blocking ON blocking.ticket = t.id AND blocking.name='blocking' "
-        sql = sql + "WHERE " + query
-        sql = sql + " ORDER BY " + sort
-        db = self.env.get_db_cnx()
-        cursor = db.cursor()
-        cursor.execute(sql)
+        ##クエリーを実行します．
+        #if query == "":
+        #    query = "t.id>0"
+        #if sort == "":
+        #    sort = "t.id"
+        #sql = "SELECT "
+        #sql = sql + "t.id, "
+        #sql = sql + "t.type, "
+        #sql = sql + "t.component, "
+        #sql = sql + "t.severity, "
+        #sql = sql + "t.priority, "
+        #sql = sql + "t.owner, "
+        #sql = sql + "t.reporter, "
+        #sql = sql + "t.cc, "
+        #sql = sql + "t.version, "
+        #sql = sql + "t.milestone, "
+        #sql = sql + "t.status, "
+        #sql = sql + "t.resolution, "
+        #sql = sql + "t.summary, "
+        #sql = sql + "t.description, "
+        #sql = sql + "due_assign.value as 'due_assign', "
+        #sql = sql + "due_close.value as 'due_close', "
+        #sql = sql + "complete.value as 'complete', "
+        #sql = sql + "summary_ticket.value as 'summary_ticket', "
+        #sql = sql + "dependencies.value as 'dependencies', "
+        #sql = sql + "hours.value as 'hours', "
+        #sql = sql + "estimatedhours.value as 'estimatedhours', "
+        #sql = sql + "billable.value as 'billable', "
+        #sql = sql + "baseline_start.value as 'baseline_start', "
+        #sql = sql + "baseline_finish.value as 'baseline_finish', "
+        #sql = sql + "baseline_cost.value as 'baseline_cost', "
+        #sql = sql + "blocking.value as 'blocking' "
+        #sql = sql + "FROM ticket t "
+        #sql = sql + "LEFT JOIN ticket_custom due_assign ON due_assign.ticket = t.id AND due_assign.name='due_assign' "
+        #sql = sql + "LEFT JOIN ticket_custom due_close ON due_close.ticket = t.id AND due_close.name='due_close' "
+        #sql = sql + "LEFT JOIN ticket_custom complete ON complete.ticket = t.id AND complete.name='complete' "
+        #sql = sql + "LEFT JOIN ticket_custom summary_ticket ON summary_ticket.ticket = t.id AND (summary_ticket.name='summary_ticket' OR summary_ticket.name='parent') "
+        #sql = sql + "LEFT JOIN ticket_custom dependencies ON dependencies.ticket = t.id AND dependencies.name='dependencies' "
+        #sql = sql + "LEFT JOIN ticket_custom hours ON hours.ticket = t.id AND hours.name='hours' "
+        #sql = sql + "LEFT JOIN ticket_custom estimatedhours ON estimatedhours.ticket = t.id AND estimatedhours.name='estimatedhours' "
+        #sql = sql + "LEFT JOIN ticket_custom billable ON billable.ticket = t.id AND billable.name='billable' "
+        #sql = sql + "LEFT JOIN ticket_custom baseline_start ON baseline_start.ticket = t.id AND (baseline_start.name='baseline_start' OR baseline_start.name='plan_start') "
+        #sql = sql + "LEFT JOIN ticket_custom baseline_finish ON baseline_finish.ticket = t.id AND (baseline_finish.name='baseline_finish' OR baseline_start.name='plan_end') "
+        #sql = sql + "LEFT JOIN ticket_custom baseline_cost ON baseline_cost.ticket = t.id AND baseline_cost.name='baseline_cost' "
+        #sql = sql + "LEFT JOIN ticket_custom blocking ON blocking.ticket = t.id AND blocking.name='blocking' "
+        #sql = sql + "WHERE " + query
+        #sql = sql + " ORDER BY " + sort
+        #db = self.env.get_db_cnx()
+        #cursor = db.cursor()
+        #cursor.execute(sql)
         result = []
-        for row in cursor:
-            d={}
-            d['id']=row[0]
-            d['type']=row[1]
-            d['component']=row[2]
-            d['severity']=row[3]
-            d['priority']=row[4]
-            d['owner']=row[5]
-            d['report']=row[6]
-            d['cc']=row[7]
-            d['version']=row[8]
-            d['milestone']=row[9]
-            d['status']=row[10]
-            d['resolution']=row[11]
-            d['summary']=row[12]
-            d['description']=row[13]
-            d['due_assign']=row[14]
-            d['due_close']=row[15]
-            d['complete']=row[16]
-            d['summary_ticket']=row[17]
-            d['dependencies']=row[18]
-            d['hours']=row[19]
-            d['estimatedhours']=row[20]
-            d['billable']=row[21]
-            d['baseline_start']=row[22]
-            d['baseline_finish']=row[23]
-            d['baseline_cost']=row[24]
-            d['blocking']=row[25]
-            for k in d.keys():
-                if d[k] == None:
-                    d[k] = ""
-            result.append(d)
+        #for row in cursor:
+        #    d={}
+        #    d['id']=row[0]
+        #    d['type']=row[1]
+        #    d['component']=row[2]
+        #    d['severity']=row[3]
+        #    d['priority']=row[4]
+        #    d['owner']=row[5]
+        #    d['report']=row[6]
+        #    d['cc']=row[7]
+        #    d['version']=row[8]
+        #    d['milestone']=row[9]
+        #    d['status']=row[10]
+        #    d['resolution']=row[11]
+        #    d['summary']=row[12]
+        #    d['description']=row[13]
+        #    d['due_assign']=row[14]
+        #    d['due_close']=row[15]
+        #    d['complete']=row[16]
+        #    d['summary_ticket']=row[17]
+        #    d['dependencies']=row[18]
+        #    d['hours']=row[19]
+        #    d['estimatedhours']=row[20]
+        #    d['billable']=row[21]
+        #    d['baseline_start']=row[22]
+        #    d['baseline_finish']=row[23]
+        #    d['baseline_cost']=row[24]
+        #    d['blocking']=row[25]
+        #    for k in d.keys():
+        #        if d[k] == None:
+        #            d[k] = ""
+        #    result.append(d)
         return result
 
     def getWorkHourChangeTimes(self, req, id):
