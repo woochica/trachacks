01641964cec89c21aa6b8240d1d6c832dbb33e1f
abompard <abompard@7322e99d-02ea-0310-aa39-e9a107903beb>
2009-12-31 16:59:12 +0000

    Update to the latest xhtml2odt, with pre-build list styles
    git-svn-id: https://trac-hacks.org/svn@7318 7322e99d-02ea-0310-aa39-e9a107903beb

--- odtexportplugin/0.11/odtexport/odtstyle.py
+++ odtexportplugin/0.11/odtexport/odtstyle.py
@@ -66,22 +66,4 @@ def add_styles(template_dirs, content_xml, import_style_callback, import_font_ca
             font_name = style_lib[stylename]["need_font"]
             font_xml = style_lib[font_name]["xml"]
             import_font_callback(font_xml)
-    # now the more complex list and numbering items
-    template_files = _build_templates_list(template_dirs)
-    list_styles = '<text:list-style style:name="List_20_1" style:display-name="List 1">'
-    list_item_tpl = open(template_files["list_level.txt"])
-    list_item = list_item_tpl.read()
-    list_item_tpl.close()
-    for i in range(10):
-        list_styles += list_item % {"level": i+1, "space": 0.4 * i}
-    list_styles += '</text:list-style>'
-    import_style_callback(list_styles, True)
-    list_styles = '<text:list-style style:name="Numbering_20_1" style:display-name="Numbering 1">'
-    list_item_tpl = open(template_files["numbering_level.txt"])
-    list_item = list_item_tpl.read()
-    list_item_tpl.close()
-    for i in range(10):
-        list_styles += list_item % {"level": i+1, "space": 0.5 * i}
-    list_styles += '</text:list-style>'
-    import_style_callback(list_styles, True)
 
