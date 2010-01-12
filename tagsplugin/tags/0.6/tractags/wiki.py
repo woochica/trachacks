cd025c196269de94565cee7a4d1ab7b81c8e4c2f
otaku42 <otaku42@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-01-12 09:47:05 +0000

    Ensure that tractags.css gets loaded even on wiki pages that are not tagged. This is required since such pages may call one of the tags macros.
    Thanks to mixedpuppy for the patch. I've applied a slightly adjusted version
    that keeps the call to add_stylesheet inside the _wiki_view method, rather
    than moving it to the filter_stream method.
    
    Fixes #3983.
    
    
    git-svn-id: https://trac-hacks.org/svn@7375 7322e99d-02ea-0310-aa39-e9a107903beb

--- tagsplugin/tags/0.6/tractags/wiki.py
+++ tagsplugin/tags/0.6/tractags/wiki.py
@@ -84,11 +84,11 @@ class WikiTagInterface(Component):
         return tags
 
     def _wiki_view(self, req, stream):
+        add_stylesheet(req, 'tags/css/tractags.css')
         tags = self._page_tags(req)
         if not tags:
             return stream
         tag_system = TagSystem(self.env)
-        add_stylesheet(req, 'tags/css/tractags.css')
         li = []
         for tag_ in tags:
             resource = Resource('tag', tag_)
