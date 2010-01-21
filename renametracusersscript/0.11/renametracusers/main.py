b9d981bba2f5b6ddf76dc740100dc975cbd7b572
k0s <k0s@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-01-21 19:28:37 +0000

    initial import of RenameTracUsersScript
    git-svn-id: https://trac-hacks.org/svn@7427 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ renametracusersscript/0.11/renametracusers/main.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+
+"""
+RenameTracUsers:
+a plugin for Trac to rename users
+http://trac.edgewall.org
+"""
+
+# TODO:
+# // TODO : Changing the cc fields -- ticket.cc || WHERE ticket_change.field = cc
+#   // ticket_change require special attention
+#   $query = 'UPDATE ticket_change SET oldvalue = "' . $newlogin . '" WHERE field = "owner" AND oldvalue = "' . $oldlogin . '";';
+#   $query .= 'UPDATE ticket_change SET newvalue = "' . $newlogin . '" WHERE field = "owner" AND newvalue = "' . $oldlogin . '";';
+#   // cc fields are lists, containing other logins/e-mails as well
+#   $query .= 'UPDATE ticket SET cc = REPLACE("cc", "' . $oldlogin .  '", "' . $newlogin . '") WHERE cc LIKE "%' . $oldlogin . '%";';
+
+import os
+import sys
+from optparse import OptionParser
+from trac.core import *
+from trac.env import open_environment
+
+class RenameTracUsers(object):
+
+    tables = { 'attachment': 'author',
+               'component': 'owner',
+               'permission': 'username',
+               'revision': 'author',
+               'session': 'sid',
+               'session_attribute': 'sid',
+               'ticket': 'owner',
+               'ticket_change': 'author',
+               'wiki': 'author' }
+
+    def __init__(self, env):
+        self.env = env
+
+    def rename(self, old_login, new_login):
+        """
+        rename one user
+        """
+        for table, field in self.tables.items():
+            db = self.env.get_db_cnx()
+            cur = db.cursor()
+            cur.execute("UPDATE %s SET %s=%s WHERE %s=%s", (table, field, new_login, field, old_login))
+#            cur.execute("UPDATE %s SET %s='%s' WHERE %s='%s'" % (table, field, new_login, field, old_login))
+            db.commit()
+            db.close()
+
+    def rename_users(self, users):
+        """
+        rename several users;
+        * users: a dictionary of old_login, new_login
+        """
+        for old_login, new_login in users.items():
+            self.rename(old_login, new_login)
+
+def main(args=sys.argv[1:]):
+    parser = OptionParser('%prog [options] project <project2> <project3> ...')
+    parser.add_option('-d', '--dict', dest='dict', default=None,
+                      help="python file mapping of old user, new user")
+    options, args = parser.parse_args(args)
+
+    # if no projects, print usage
+    if not args:
+        parser.print_help()
+        sys.exit(0)
+
+    # get the environments
+    envs = []
+    for arg in args:
+        env = open_environment(arg)
+        envs.append(env)
+
+    # get the users
+    assert options.dict
+    assert os.path.exists(options.dict)
+    users = eval(file(options.dict).read())
+    assert isinstance(users, dict)
+    if not users:
+        sys.exit(0) # nothing to do
+
+    # change the permissions
+    for env in envs:
+        renamer = RenameTracUsers(env)
+        renamer.rename_users(users)
+
+if __name__ == '__main__':
+    main()
+    
+    
+
+
---
+++ renametracusersscript/0.11/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+version='0.0'
+
+setup(name='RenameTracUsers',
+      version=version,
+      description="rename the users for Trac projects",
+      author='Jeff Hammel',
+      author_email='jhammel@openplans.org',
+      url='http://trac-hacks.org/wiki/k0s',
+      keywords='trac plugin',
+      license="",
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests*']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['Trac'],
+      entry_points = """
+      [console_scripts]
+      rename-trac-users = renametracusers.main:main
+      """,
+      )
+
