5a5dbd69d054d3990475b9e6c7538df7eb5cfeb3
insaneintenti0n <insaneintenti0n@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-02-04 19:42:10 +0000

    Updating the DailyLolMacro to work with the current layout on icanhascheezburger.com
    git-svn-id: https://trac-hacks.org/svn@7503 7322e99d-02ea-0310-aa39-e9a107903beb

--- dailylolmacro/0.11/DailyLolMacro.py
+++ dailylolmacro/0.11/DailyLolMacro.py
@@ -1,5 +1,10 @@
 class DailyLolMacro(WikiMacroBase):
-    """Display a picture of a cat with a funny caption."""
+    """Display a picture of a cat with a funny caption.
+    Usage:
+    {{{
+    [[DailyLol]]
+    }}}
+    """
 
     def expand_macro(self, formatter, name, args):
         import urllib2
@@ -8,7 +13,7 @@ class DailyLolMacro(WikiMacroBase):
         website_html = website.read()
 
         beg_pos = -1
-        beg_pos = website_html.find('<img class', beg_pos+1)
+        beg_pos = website_html.find('<img title', beg_pos+1)
         beg_pos = website_html.find('src=', beg_pos)
         end_pos = website_html.find('"', beg_pos+5)
         raw_url = website_html[beg_pos+5:end_pos]
