b619b839bd0d6ddb71e2642b49bbb5a0947dc53c
airadier <airadier@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-02-08 13:01:51 +0000

    Support for multiple superusers (comma separated). Fixes #6546.
    git-svn-id: https://trac-hacks.org/svn@7514 7322e99d-02ea-0310-aa39-e9a107903beb

--- superuserplugin/setup.py
+++ superuserplugin/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 PACKAGE = 'tracsuperuser'
-VERSION = '0.3'
+VERSION = '0.4'
 
 setup(name=PACKAGE,
     version=VERSION,
--- superuserplugin/tracsuperuser/superuser.py
+++ superuserplugin/tracsuperuser/superuser.py
@@ -11,7 +11,7 @@
 
 from trac.core import *
 from trac.perm import IPermissionStore
-from trac.config import ExtensionOption, Option
+from trac.config import ExtensionOption, ListOption
 
 import logging
 
@@ -20,7 +20,7 @@ class SuperUserPlugin(Component):
     """ Adds a superuser with TRAC_ADMIN permissions """
     implements(IPermissionStore)
     
-    superuser = Option('superuser', 'superuser', 'admin', 'Superuser username')
+    superusers = ListOption('superuser', 'superuser', 'admin', doc='Superuser username(s) (comma separated)')
     
     store = ExtensionOption('superuser', 'wrapped_permission_store', IPermissionStore,
         'DefaultPermissionStore', """Name of the component implementing `IPermissionStore`, which is used
@@ -28,23 +28,23 @@ class SuperUserPlugin(Component):
     
 
     def get_user_permissions(self, username):
-        if username == self.superuser:
+        if username in self.superusers:
             return list(set(['TRAC_ADMIN']) | set(self.store.get_user_permissions(username)))
         else:
             return self.store.get_user_permissions(username)
 
     def get_users_with_permissions(self, permissions):
-        return list(set([self.superuser]) | set(self.store.get_users_with_permissions(permissions)))
+        return list(set(self.superusers) | set(self.store.get_users_with_permissions(permissions)))
             
     def get_all_permissions(self):
-        return self.store.get_all_permissions() + [(self.superuser, 'TRAC_ADMIN')]
+        return self.store.get_all_permissions() + [(user, 'TRAC_ADMIN') for user in self.superusers]
         
     def grant_permission(self, username, action):
-        if username == self.superuser:
+        if username in self.superusers:
             assert False, "Superuser %s can't be given any permissions" % username
         return self.store.grant_permission(username, action)
         
     def revoke_permission(self, username, action):
-        if username == self.superuser:
+        if username in self.superusers:
             assert False, "Superuser %s can't be revoked any permissions" % username
         return self.store.revoke_permission(username, action)
