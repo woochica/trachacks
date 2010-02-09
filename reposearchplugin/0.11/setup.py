6c4f35d37b414af1f6fce82beb30437baf45edb5
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-02-09 07:20:54 +0000

    Remove obsolete 0.9 version. Update maintainer info in `setup.py`. Fixes #6596.
    git-svn-id: https://trac-hacks.org/svn@7525 7322e99d-02ea-0310-aa39-e9a107903beb

--- reposearchplugin/0.11/setup.py
+++ reposearchplugin/0.11/setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup
 
-PACKAGE = 'tracreposearch'
-VERSION = '0.1'
-
 setup(name='tracreposearch',
       version='0.2',
       packages=['tracreposearch'],
       author='Alec Thomas',
+      maintainer='Ryan J Ollos',
+      maintainer_email='ryano@physiosonics.com',
       url="http://trac-hacks.org/wiki/TracRepoSearch",
       license='BSD',
       scripts=['update-index'],
--- reposearchplugin/0.9/setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup
-
-PACKAGE = 'tracreposearch'
-VERSION = '0.1'
-
-setup(name=PACKAGE, version=VERSION, packages=['tracreposearch'])
