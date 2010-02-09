271bf9dc2a1b1b5386283e1bbb9904afc915821b
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-02-09 07:39:04 +0000

    Add repository URL to `setup.py`. Fixes #6462
    git-svn-id: https://trac-hacks.org/svn@7526 7322e99d-02ea-0310-aa39-e9a107903beb

--- wikitablemacro/0.11/setup.py
+++ wikitablemacro/0.11/setup.py
@@ -4,12 +4,13 @@
 from setuptools import setup
 
 setup(
-    name = 'wikitable',
+    name = 'WikiTableMacro',
     author = 'Martin Aspeli',
     author_email = 'optilude@gmail.com',
     maintainer = 'Ryan J Ollos',
     maintainer_email = 'ryano@physiosonics.com',
     description = 'Trac plugin for drawing a table from a SQL query in a wiki page',
+    url = 'http://trac-hacks.org/wiki/ProgressMeterMacro', 
     version = '0.1',
     license='BSD',
     packages=['wikitable'],
