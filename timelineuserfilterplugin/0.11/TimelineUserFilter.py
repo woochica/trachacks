a888d826460c247345b594d1a4da300ddf8debcf
daveappendix <daveappendix@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-02-15 09:17:12 +0000

    Patch from asic_druide to fix #5400, #5421 and #5858. Thanks!
    git-svn-id: https://trac-hacks.org/svn@7595 7322e99d-02ea-0310-aa39-e9a107903beb

--- timelineuserfilterplugin/0.11/TimelineUserFilter.py
+++ timelineuserfilterplugin/0.11/TimelineUserFilter.py
@@ -10,7 +10,6 @@ author = "daveappendix"
 class TimelineUserFilterPlugin(Component):
     """Filters timeline events by user.
     """
-       
     implements(IRequestFilter, ITemplateStreamFilter)
 
     # IRequestFilter methods
@@ -20,14 +19,46 @@ class TimelineUserFilterPlugin(Component):
 
     def post_process_request(self, req, template, data, content_type):
         if template == 'timeline.html':
-            users = req.args.get('users', '')
-            if users:
-                users = [user.strip() for user in users.split(',')]
+            if (req.args.has_key('inc_users')):
+              inc_users = req.args['inc_users']
+              
+              # make value persistent...
+              req.session['timeline.filter.inc_users'] = inc_users
+            elif (req.session.has_key('timeline.filter.inc_users')):
+              # use persistent value...
+              inc_users = req.session['timeline.filter.inc_users']
+            else:
+              inc_users = ''
+
+            if (req.args.has_key('exc_users')):
+              exc_users = req.args['exc_users']
+              
+              # make value persistent...
+              req.session['timeline.filter.exc_users'] = exc_users
+            elif (req.session.has_key('timeline.filter.exc_users')):
+              # use persistent value...
+              exc_users = req.session['timeline.filter.exc_users']
+            else:
+              exc_users = ''
+
+            if (inc_users!=''):
+                # use defined filter... 
+                inc_users = [user.strip() for user in inc_users.split(',')]
                 filtered_events = []
                 for event in data['events']:
-                    if event['author'] in users:
+                    if event['author'] in inc_users:
                         filtered_events.append(event)
                 data['events'] = filtered_events
+                
+            if (exc_users!=''):
+                # use defined filter... 
+                exc_users = [user.strip() for user in exc_users.split(',')]
+                filtered_events = []
+                for event in data['events']:
+                    if event['author'] not in exc_users:
+                        filtered_events.append(event)
+                data['events'] = filtered_events
+                
         return (template, data, content_type)
 
     # ITemplateStreamFilter methods
@@ -40,8 +71,22 @@ class TimelineUserFilterPlugin(Component):
         return stream
 
     def _user_field_input(self, req):
-        return tag.label("Filter users: ",
-                         tag.input(type="text",
-                                   name="users",
-                                   value=req.args.get('users', ''),
-                                   style_="width:60%"))
+        return tag.div(tag.label("Include users: ",
+                                 tag.input(type="text",
+                                           name="inc_users",
+                                           value=req.session.get('timeline.filter.inc_users',''),
+                                           style_="width:60%"
+                                          )
+                                )
+                       +
+                       tag.br()
+                       +
+                       tag.label("Exclude users: ",
+                                 tag.input(type="text",
+                                           name="exc_users",
+                                           value=req.session.get('timeline.filter.exc_users',''),
+                                           style_="width:60%"
+                                          )
+                                )
+                      )                    
+                                   
