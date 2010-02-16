4b9599a77aa5d2c17aa2c7f6ca3104c5d4d99e26
Havok <Havok@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-02-16 01:34:08 +0000

    First public commit. Enjoy it :D
    git-svn-id: https://trac-hacks.org/svn@7614 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ tsevetheme/0.11/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+
+from setuptools import setup
+
+setup(
+	name = 'TseveTheme',
+	version = '1.0',
+	packages = [ 'tsevetheme' ],
+	package_data = { 'tsevetheme': [ 'htdocs/*.css', 'htdocs/*.png', 'templates/*.html' ] },
+	author = 'Carlos Jenkins',
+	author_email = 'carlos.jenkins.perez@gmail.com',
+	description = 'Tseve Minimalist Theme for Trac',
+	license = 'CC-BY-SA',
+	keywords = 'trac plugin theme tseve',
+	url = 'http://www.cjenkins.net/',
+	classifiers = [
+		'Framework :: Trac',
+	],
+	install_requires = ['Trac', 'TracThemeEngine>=2.0'],
+	entry_points = {
+		'trac.plugins': [
+			'tsevetheme.theme = tsevetheme.theme',
+		]
+	},
+)
+
---
+++ tsevetheme/0.11/tsevetheme/theme.py
@@ -0,0 +1,10 @@
+# Created by Carlos Jenkins (Havok), 2009
+# http://www.cjenkins.net/
+# Copyleft (C) 2009 Carlos Jenkins. Some rights reserved.
+
+from trac.core import *
+from themeengine.api import ThemeBase
+
+class TseveTheme(ThemeBase):
+	"""Tseve Minimalist Theme for Trac"""
+	template = htdocs = css = True
