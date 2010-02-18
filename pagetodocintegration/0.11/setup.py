7ba87ec9c531c3052c0a117d8aef2dd746a37a50
markm <markm@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-02-18 09:35:46 +0000

    Added maintainer information to setup.py
    git-svn-id: https://trac-hacks.org/svn@7640 7322e99d-02ea-0310-aa39-e9a107903beb

--- pagetodocintegration/0.11/setup.py
+++ pagetodocintegration/0.11/setup.py
@@ -6,6 +6,8 @@ setup(name='PageToDoc',
       packages=['pagetodoc'],
       author='Lucas Eisenzimmer',
       author_email='lucas.eisenzimmer@t-systems-mms.com',
+      maintainer='Mark Mc Mahon',
+      maintainer_email='mark.m.mcmahon@gmail.com',
       description='A plugin for exporting Wiki pages as filtered HTML for import into MS Word',
       entry_points={'trac.plugins': ['pagetodoc.pagetodoc=pagetodoc.pagetodoc']})
 
