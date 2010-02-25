1e4f9d0179a6a485d0fe4d90c399704e99c68e98
coderanger <coderanger@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-02-25 21:29:01 +0000

    Add a readme file.
    git-svn-id: https://trac-hacks.org/svn@7724 7322e99d-02ea-0310-aa39-e9a107903beb

--- themeengineplugin/0.11/setup.py
+++ themeengineplugin/0.11/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name = 'TracThemeEngine',
-    version = '2.0',
+    version = '2.0.1',
     packages = ['themeengine'],
     package_data = { 'themeengine': ['templates/*.html', 'htdocs/*.js', 'htdocs/*.css', 'htdocs/img/*.gif',
                                      'htdocs/farbtastic/*.png', 'htdocs/farbtastic/*.js', 'htdocs/farbtastic/*.css' ] },
@@ -13,14 +13,28 @@ setup(
     author = 'Noah Kantrowitz',
     author_email = 'noah@coderanger.net',
     description = 'Provide a modular interface to styling Trac.',
+    long_description = open(os.path.join(os.path.dirname(__file__), 'README')).read(),
     license = 'BSD',
     keywords = 'trac plugin theme style',
     url = 'http://trac-hacks.org/wiki/ThemeEnginePlugin',
+    download_url = 'http://trac-hacks.org/svn/themeengineplugin/0.11#egg=TracThemeEngine-dev',
     classifiers = [
         'Framework :: Trac',
+        'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        # 'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
     ],
     
-    install_requires = ['Trac>=0.11dev-r6696'],
+    install_requires = ['Trac'],
 
     entry_points = {
         'trac.plugins': [
