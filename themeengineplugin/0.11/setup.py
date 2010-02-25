d3fcf89ea7e892e407f7d3b3b5e7e9a2abe961db
coderanger <coderanger@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-02-25 21:33:53 +0000

    Fix the dev status classifier.
    git-svn-id: https://trac-hacks.org/svn@7726 7322e99d-02ea-0310-aa39-e9a107903beb

--- themeengineplugin/0.11/setup.py
+++ themeengineplugin/0.11/setup.py
@@ -21,11 +21,11 @@ setup(
     download_url = 'http://trac-hacks.org/svn/themeengineplugin/0.11#egg=TracThemeEngine-dev',
     classifiers = [
         'Framework :: Trac',
-        'Development Status :: 1 - Planning',
+        #'Development Status :: 1 - Planning',
         # 'Development Status :: 2 - Pre-Alpha',
         # 'Development Status :: 3 - Alpha',
         # 'Development Status :: 4 - Beta',
-        # 'Development Status :: 5 - Production/Stable',
+        'Development Status :: 5 - Production/Stable',
         # 'Development Status :: 6 - Mature',
         # 'Development Status :: 7 - Inactive',
         'Environment :: Web Environment',
