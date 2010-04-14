847c8fcd43a9ea744192a89039f815d1721d2d14
dan <dan@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-04-14 14:35:52 +0000

    Fix for templates not being properly included with egg builds.
    git-svn-id: https://trac-hacks.org/svn@7846 7322e99d-02ea-0310-aa39-e9a107903beb

--- awesomeattachmentsplugin/0.11/setup.py
+++ awesomeattachmentsplugin/0.11/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='awesomeAttachmentsPlugin', version='0.1',
     packages=['ticket'],
-    package_data={ 'ticket': [ 'htdocs/images/*' ]},
+    package_data={ 'ticket': [ 'htdocs/images/*', 'templates/*' ]},
     entry_points = """
         [trac.plugins]
         ticketupload = ticket.ticketupload
