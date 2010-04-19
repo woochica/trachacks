695fe1793555842e3d91a910305550a3b12a687b
thomasvs <thomasvs@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-04-19 10:55:38 +0000

    fix version string
    git-svn-id: https://trac-hacks.org/svn@7865 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracdupplugin/0.11/setup.py
+++ tracdupplugin/0.11/setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
   name='TracDupPlugin',
-  version=0.1.1,
+  version="0.1.1",
   description='duplication of tickets to other tickets',
   long_description=""" """,
   classifiers=[
