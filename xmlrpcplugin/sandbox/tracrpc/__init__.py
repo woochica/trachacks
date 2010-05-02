fd17b4032f4252fd3f929ce036cf30ddc090eef2
osimons <osimons@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-05-02 14:41:02 +0000

    XmlRpcPlugin: Removing an old 'sandbox' branch that is no longer used (pre-0.11 work).
    git-svn-id: https://trac-hacks.org/svn@7915 7322e99d-02ea-0310-aa39-e9a107903beb

--- xmlrpcplugin/sandbox/tracrpc/__init__.py
+++
@@ -1,7 +0,0 @@
-from tracrpc.api import *
-from tracrpc.web_ui import *
-from tracrpc.ticket import *
-from tracrpc.wiki import *
-from tracrpc.search import *
-from tracrpc.xml_rpc import *
-from tracrpc.json_rpc import *
--- xmlrpcplugin/sandbox/tracrpc/util.py
+++
@@ -1,5 +0,0 @@
-import time
-
-def to_timestamp(datetime):
-    """ Convert xmlrpclib.DateTime string representation to UNIX timestamp. """
-    return time.mktime(time.strptime('%s UTC' % datetime.value, '%Y%m%dT%H:%M:%S %Z')) - time.timezone
