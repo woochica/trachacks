0a3d3dd565e5fbb00c19508cc8966dca08d78cf5
hasienda <hasienda@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-06-09 11:12:19 +0000

    WikiTicketCalendarMacro: Final maintenance release for 0.10 branch, closes #2617.
    Thanks for anonymous contribution of bug fix.
    See README for end-of-life statement for 0.10 branch.
    Take your chance to upgrade your Trac to version 0.12, please.
    
    git-svn-id: https://trac-hacks.org/svn@8106 7322e99d-02ea-0310-aa39-e9a107903beb

--- wikiticketcalendarmacro/branches/0.10/WikiTicketCalendar.py
+++ wikiticketcalendarmacro/branches/0.10/WikiTicketCalendar.py
@@ -1,19 +1,20 @@
 # Copyright (C) 2005 Matthew Good <trac@matt-good.net>
-# Copyright (C) 2005 Jan Finell <finell@cenix-bioscience.com> 
-# Copyright (C) 2007 Mike Comb <mcomb@mac.com> 
+# Copyright (C) 2005 Jan Finell <finell@cenix-bioscience.com>
+# Copyright (C) 2007 Mike Comb <mcomb@mac.com>
 #
 # "THE BEER-WARE LICENSE" (Revision 42):
 # <trac@matt-good.net> wrote this file.  As long as you retain this notice you
-# can do whatever you want with this stuff.  If we meet some day, and you think
-# this stuff is worth it, you can buy me a beer in return.  Matthew Good
+# can do whatever you want with this stuff.  If we meet some day, and you
+# think this stuff is worth it, you can buy me a beer in return.  Matthew Good
 # (Beer-ware license written by Poul-Henning Kamp
 #  http://people.freebsd.org/~phk/)
 #
 # Author: Matthew Good <trac@matt-good.net>
 # Month/Year navigation by: Jan Finell <finell@cenix-bioscience.com>
-
+#
 # Major revisions by Mike Comb <mcomb@mac.com> to display tickets, improve
 # formatting and turn into a full page calendar.
+# Display milestone name with accents, fix provided as anonymous contribution.
 
 import time
 import calendar
@@ -27,12 +28,13 @@ from trac.util import *
 #   WikiTicketCalendar([year,month,[showbuttons,[wiki_page_format]]])
 #
 #   displays a calendar, the days link to:
-#     - milestones (day in bold) if there is one on that day
-#     - a wiki page that has wiki_page_format (if exist)
-#     - create that wiki page if it does not exist
+#   - milestones (day in bold) if there is one on that day
+#   - a wiki page that has wiki_page_format (if exist)
+#   - create that wiki page if it does not exist
 #
-# arguments: 
-#   year, month = display calendar for month in year ('*' for current year/month)
+# arguments:
+#   year, month = display calendar for month in year
+#                 ('*' for current year/month)
 #   showbuttons = true/false, show prev/next buttons
 #   wiki_page_format = strftime format for wiki pages to display as link
 #                      (if there is not a milestone placed on that day)
@@ -53,9 +55,9 @@ def execute(hdf, txt, env):
         args = []
     else:
         args = txt.split(',', 3)
-    
+
     # find out whether use http param, current or macro param year/month
-    
+
     if http_param_year == "":
         # not clicked on a prev or next button
         if len(args) >= 1 and args[0] <> "*":
@@ -67,7 +69,7 @@ def execute(hdf, txt, env):
     else:
         # year in http params (clicked by user) overrides everything
         year = int(http_param_year)
-    
+
     if http_param_month == "":
         # not clicked on a prev or next button
         if len(args) >= 2 and args[1] <> "*":
@@ -79,15 +81,16 @@ def execute(hdf, txt, env):
     else:
         # month in http params (clicked by user) overrides everything
         month = int(http_param_month)
-    
+
     showbuttons = 1
     if len(args) >= 3:
-        showbuttons = bool(args[2]=="True" or args[2]=="true" or args[2]=="no" or args[2]=="0")
-        
+        showbuttons = bool(args[2]=="True" or args[2]=="true" or
+                           args[2]=="no" or args[2]=="0")
+
     wiki_page_format = "%Y-%m-%d"
     if len(args) >= 4:
         wiki_page_format = args[3]
-    
+
     curr_day = None
     if year == today.tm_year and month == today.tm_mon:
         curr_day = today.tm_mday
@@ -123,17 +126,17 @@ def execute(hdf, txt, env):
         # prev month link
         prevMonthURL = thispageURL+'?month=%d&year=%d' % (prevMonth, prevYear)
         buff.write('<a href="%s"><b>&lt;</b> </a>' % prevMonthURL)
-        
+
     # the caption
     buff.write(time.strftime('<strong>%B %Y</strong>', tuple(date)))
-    
+
     if showbuttons:
         # next month link
         nextMonthURL = thispageURL+'?month=%d&year=%d' % (nextMonth, nextYear)
         buff.write('<a href="%s"> <b>&gt;</b></a>' % nextMonthURL)
-        
+
     buff.write('</caption>\n<thead><tr align="center">')
-    
+
     for day in calendar.weekheader(2).split():
         buff.write('<th scope="col"><b>%s</b></th>' % day)
     buff.write('</tr></thead>\n<tbody>\n')
@@ -148,71 +151,104 @@ def execute(hdf, txt, env):
                 db = env.get_db_cnx()
                 cursor = db.cursor()
                 # ugly way but i don't know how to set up a struct_time
-                # (date as single numbers -> string -> time string parser -> struct_time -> mktime -> seconds
+                # (date as single numbers -> string -> time string parser
+                # time string parser -> struct_time -> mktime -> seconds)
                 duedateString = str(day) + "." + str(month) + "." + str(year)
                 duedateStruct = time.strptime(duedateString, "%d.%m.%Y")
                 duedatestamp = time.mktime(duedateStruct)
-					
+
                 dayString = str(day)
                 monthString = str(month)
                 yearString = str(year)
-                
-                buff.write('<td%(style)s width="110" valign="top"><b>%(day)s</b>' % {
-						'day': day,
-						'style': day == curr_day and ' style="background: #fbfbfb; border-color: #444444; color: #444; border-style:solid; border-width:1px;"' or ' style="background: #e5e5e5; border-color: #444444; color: #333; border-style:solid; border-width:1px;"',
-						})
-                
-                if day < 10:
-                	dayString = "0" + str(day)
-                if month < 10:
-                	monthString = "0" + str(month)
-                yearString = yearString[2:4]
-                
-                duedate = monthString + "/" + dayString + "/" + yearString
-                
-                cursor.execute("SELECT name FROM milestone WHERE due=%s", (duedatestamp,))
-                while (1):
-                	row = cursor.fetchone()
-                	if row == None:
-                		buff.write('<br>')
-                		break
-                	else:
-						name = row[0]
-						url = env.href.milestone(name)
-						buff.write('<table border="0" celpadding="0" cellspacing="0" width="110"><tr><td style="font-size: 9px; background: #f7f7f0; border: 1px solid #d7d7d7; border-bottom-color: #999;" align="left"><a href="%(url)s">* %(name)s</a></td></tr></table>' % {
-							'name': name,
-							'url': url,
-							})
-                cursor.execute("SELECT t.id,t.summary,t.keywords||' - '||t.owner,t.status,t.description FROM ticket t, ticket_custom tc where tc.ticket=t.id and tc.name='due_close' and tc.value=%s", (duedate,))
 
-                #if row:
+                buff.write("""<td%(style)s width="110"
+                    valign="top"><b>%(day)s</b>
+                    """ % {
+                    'day': day,
+                    'style': day == curr_day and
+                        """ style="background: #fbfbfb; border-color: #444444;
+                        color: #444; border-style:solid; border-width:1px;"
+                        """ or
+                        """ style="background: #e5e5e5; border-color: #444444;
+                        color: #333; border-style:solid; border-width:1px;"
+                        """,
+                    })
+
+                if day < 10:
+                    dayString = "0" + str(day)
+                if month < 10:
+                    monthString = "0" + str(month)
+                yearString = yearString[2:4]
+
+                duedate = monthString + "/" + dayString + "/" + yearString
+
+                cursor.execute("""
+                    SELECT name
+                      FROM milestone
+                     WHERE due=%s
+                """, (duedatestamp,))
                 while (1):
-                	row = cursor.fetchone()
-                	if row == None:
-                		buff.write('</td>')
-                		break
-                	else:
-						id = row[0]
-						url = env.href.ticket(id)
-						
-						ticket = row[1]
-						owner = row[2]
-						status = row[3]
-						description = "" #row[4]
-						buff.write('<div style="%(style)s" align="left">%(ticket)s (<a href="%(url)s" title=\'%(description)s\' target="_blank">%(owner)s</a>)</div><p>' % {
-								'url': url,
-								'day': day,
-								'ticket': ticket[0:100],
-								'owner': owner,
-								'style': status == 'closed' and 'font-size: 9px; color: #777777; text-decoration: line-through;' or 'font-size: 9px; color: #000000',
-								'description': replace(description, "\'", "&#39;"),
-								})
+                    row = cursor.fetchone()
+                    if row == None:
+                        buff.write('<br>')
+                        break
+                    else:
+                        name = row[0]
+                        url = env.href.milestone(name)
+                        name = name.encode('UTF-8')
+                        buff.write("""
+                            <table border="0" celpadding="0" cellspacing="0"
+                            width="110"><tr><td style="font-size: 9px;
+                            background: #f7f7f0; border: 1px solid #d7d7d7;
+                            border-bottom-color: #999;" align="left"><a
+                            href="%(url)s">* %(name)s</a></td></tr></table>
+                            """ % {
+                            'name': name,
+                            'url': url,
+                            })
+
+                cursor.execute("""
+                    SELECT t.id,t.summary,t.keywords||' - '||t.owner,
+                           t.status,t.description
+                      FROM ticket t, ticket_custom tc
+                     WHERE tc.ticket=t.id and tc.name='due_close' and
+                           tc.value=%s
+                """, (duedate,))
+
+                while (1):
+                    row = cursor.fetchone()
+                    if row == None:
+                        buff.write('</td>')
+                        break
+                    else:
+                        id = row[0]
+                        url = env.href.ticket(id)
+                        ticket = row[1]
+                        owner = row[2]
+                        status = row[3]
+                        description = "" #row[4]
+                        buff.write("""
+                            <div style="%(style)s" align="left">%(ticket)s (<a
+                            href="%(url)s" title=\'%(description)s\'
+                            target="_blank">%(owner)s</a>)</div><p>
+                            """ % {
+                            'url': url,
+                            'day': day,
+                            'ticket': ticket[0:100],
+                            'owner': owner,
+                            'style': status == 'closed' and
+                                """font-size: 9px; color: #777777;
+                                text-decoration: line-through;
+                                """ or
+                                """font-size: 9px; color: #000000;
+                                """,
+                            'description': replace(description, "\'", "&#39;"),
+                            })
 
         buff.write('</tr>\n')
 
     buff.write('</tbody>\n</table>')
-            
+
     table = buff.getvalue()
     buff.close()
     return table
-
