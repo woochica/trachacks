de8290d226d41d169f70251ae73e3116f91f89f8
bobbysmith007 <bobbysmith007@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-06-09 15:23:21 +0000

    Applied patch to add stylesheet in a better way - version 0.9.4 and 0.9.4b fix #7224
    git-svn-id: https://trac-hacks.org/svn@8107 7322e99d-02ea-0310-aa39-e9a107903beb

--- timingandestimationplugin/branches/trac0.11-Permissions/timingandestimationplugin/query_webui.py
+++ timingandestimationplugin/branches/trac0.11-Permissions/timingandestimationplugin/query_webui.py
@@ -1,28 +1,15 @@
-import re
-from trac.log import logger_factory
 from trac.core import *
-from trac.web import IRequestHandler
-from trac.util import Markup
-from trac.web.chrome import add_stylesheet, add_script, \
-     INavigationContributor, ITemplateProvider
-from trac.web.href import Href
+from trac.web.api import IRequestFilter
+from trac.web.chrome import add_script
 
 class QueryWebUiAddon(Component):
-    implements(INavigationContributor)
-    
-    def __init__(self):
-        pass
-    
-     # INavigationContributor methods
-    def get_active_navigation_item(self, req):
-    
-        if re.search('query', req.path_info):
-            return "query-addon"
-        else:
-            return ""
+    implements(IRequestFilter)
+                
+    def pre_process_request(self, req, handler):
+        return handler
 
-    def get_navigation_items(self, req):
-        src = req.href.chrome("Billing/query.js")
-        if re.search('query', req.path_info):
-            yield 'mainnav', "query-addon", \
-                  Markup("""<script language="javascript" type="text/javascript" src="%s"></script>"""%src)
+    def post_process_request(self, req, template, data, content_type):
+        if template == 'query.html':
+            add_script(req, req.href.chrome("Billing/query.js"))        
+            
+        return (template, data, content_type)   
