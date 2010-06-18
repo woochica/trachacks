f370b1196af85e5dc4a56282213d962d0128ee34
bobbysmith007 <bobbysmith007@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-06-18 18:43:55 +0000

    branch for trac 12
    git-svn-id: https://trac-hacks.org/svn@8126 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ overrideeditplugin/0.12/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+PACKAGE = 'overrideeditplugin'
+
+setup(name="OverrideEditPlugin",
+      version='0.0.2',
+      packages=[PACKAGE],
+      url='http://www.trac-hacks.org/wiki/OverrideEditPlugin',
+      license='http://www.opensource.org/licenses/mit-license.php',
+      author='Russ Tyndall at Acceleration.net',
+      author_email='russ@acceleration.net',
+      long_description="""
+      Allows you to bypass the warning about the ticket changing since last you loaded it
+      """,
+      entry_points={'trac.plugins': '%s = %s' % (PACKAGE, PACKAGE)},
+)
+
