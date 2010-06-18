cedc0d2194c1e04414949953cc16df742487c455
bobbysmith007 <bobbysmith007@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-06-18 17:48:30 +0000

    branching anew for trac12
    git-svn-id: https://trac-hacks.org/svn@8122 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ timingandestimationplugin/branches/trac0.12/timingandestimationplugin/htdocs/StopwatchControls.js
@@ -0,0 +1,71 @@
+/**
+ * Copyright (C) 2010, Tay Ray Chuan
+ */
+jQuery(function($) {
+	StopwatchControls = function() {
+		var m_state;
+
+		var btn_flow = $('<div class="stopwatch-button"></div>');
+		var btn_reset = $('<div class="stopwatch-button">Reset</div>');
+		var controls = $('<div></div>')
+			.append(btn_flow)
+			.append(btn_reset);
+
+		/* first handlers to run; set states */
+		controls.bind('pause', function() {
+			m_state.running = false;
+			m_state.use_value = true;
+			m_state.reset = false;
+		});
+
+		controls.bind('continue', function() {
+			m_state.running = true;
+			m_state.use_value = false;
+			m_state.reset = false;
+		});
+
+		controls.bind('reset', function() {
+			m_state.running = false;
+			m_state.use_value = false;
+			m_state.reset = true;
+		});
+
+		/* make buttons trigger events */
+		btn_flow.click(function() {
+			if (m_state.running)
+				controls.trigger('pause');
+			else
+				controls.trigger('continue');
+		});
+
+		btn_reset.click(function() {
+			if (m_state.running) return;
+
+			controls.trigger('reset');
+		});
+
+		/* make button states react to events */
+		controls.bind('pause', function() {
+			btn_flow.text('Continue');
+			btn_reset.show();
+		});
+
+		controls.bind('continue', function() {
+			btn_flow.text('Pause');
+			btn_reset.hide();
+		});
+
+		controls.bind('reset', function() {
+			btn_flow.text('Start');
+			btn_reset.hide();
+		});
+
+		return {
+			controls: controls,
+
+			init: function(state) {
+				m_state = state;
+			}
+		};
+	}();
+});
---
+++ timingandestimationplugin/branches/trac0.12/timingandestimationplugin/htdocs/StopwatchDisplay.js
@@ -0,0 +1,76 @@
+/**
+ * Copyright (C) 2010, Tay Ray Chuan
+ */
+
+/*
+ * add buttons to control the stopwatch
+ *
+ * Button 'flow' has states: start -> stop <-> continue
+ * Button 'reset' has state: reset
+ */
+jQuery(function($) {
+	StopwatchDisplay = function() {
+		/* the stopwatch (looks like 00:00:00) */
+		var field_hour = $('<span>00</span>');
+		var field_min = $('<span>00</span>');
+		var field_sec = $('<span>00</span>');
+
+		var display = $('<div></div>')
+			.append(field_hour)
+			.append(':')
+			.append(field_min)
+			.append(':')
+			.append(field_sec);
+
+		field_hour = field_hour[0].firstChild;
+		field_min = field_min[0].firstChild;
+		field_sec = field_sec[0].firstChild;
+
+		var interval_id;
+		var start_time = null;
+	        var end_time;
+	        var total_time=0 ;
+	        var now = function(){ return Math.floor((new Date()).getTime() / 1000);};
+		var interval_func = function() {
+                        var interval = (now() - start_time)+total_time;
+                        var h = 0, m = 0, s = 0;
+                        s = interval % 60;
+                        m = Math.floor(interval/60) % 60;
+                        h = Math.floor(interval/3600);
+                        field_sec.nodeValue = s < 10 ? '0'+s : s;
+                        field_min.nodeValue = m < 10 ? '0'+m : m;
+                        field_hour.nodeValue = h < 10 ? '0'+h : h;
+		};
+
+		return {
+			display: display,
+
+			pause_stopwatch: function() {
+				clearInterval(interval_id);
+				interval_id = null;
+			        total_time += now() - start_time;
+			        start_time = null;
+			},
+			continue_stopwatch: function() {
+				/*
+				 * We really want to do an add (of the seconds on the display)
+				 * - which is what we get when start_time is subtracted later.
+				 */
+			        start_time = now();
+				interval_id = setInterval(interval_func, 100);
+			},
+			reset_stopwatch: function() {
+				field_hour.nodeValue = '00';
+				field_min.nodeValue = '00';
+				field_sec.nodeValue = '00';
+                                start_time=null;
+                                total_time=0;
+			},
+			get_hours: function() {
+                                var total = total_time;
+			        if (start_time) total += (now() - start_time);
+                                return Math.round( ( total / 3600 ) * 100) / 100;
+			}
+		};
+	}();
+});
