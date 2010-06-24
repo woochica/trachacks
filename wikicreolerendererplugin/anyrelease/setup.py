904ece45f19e983826437d96a5dc4784e977fa58
davidfraser <davidfraser@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-06-24 14:12:19 +0000

    Let's call this version 0.2
    git-svn-id: https://trac-hacks.org/svn@8152 7322e99d-02ea-0310-aa39-e9a107903beb

--- wikicreolerendererplugin/anyrelease/setup.py
+++ wikicreolerendererplugin/anyrelease/setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 PACKAGE = 'WikiCreoleRendererPlugin'
-VERSION = '0.1'
+VERSION = '0.2'
 
 setup(  name=PACKAGE, version=VERSION,
         author = "David Fraser",
