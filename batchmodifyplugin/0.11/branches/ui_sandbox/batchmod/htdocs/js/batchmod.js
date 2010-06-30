a89d4608025b2471dc11380c261ca54a2a56b028
CuriousCurmudgeon <CuriousCurmudgeon@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-06-30 00:03:57 +0000

    refs #7303  * General code cleanup  * Moved most of the jQuery code to batchmod.js. There is still a little bit left that is built with Genshi. That won't be needed by the time I'm done though.  * Commented most of the jQuery code. It's now clear what each block is doing.  * Removed some apparently useless code dealing with columns.
    git-svn-id: https://trac-hacks.org/svn@8170 7322e99d-02ea-0310-aa39-e9a107903beb

--- batchmodifyplugin/0.11/branches/ui_sandbox/batchmod/htdocs/js/batchmod.js
+++ batchmodifyplugin/0.11/branches/ui_sandbox/batchmod/htdocs/js/batchmod.js
@@ -1,7 +1,40 @@
-(function($){
+jQuery(document).ready(function($){
 	
-	$("#batchmod-fieldset select[name^=add_batchmod_field]").change(function() {
-		alert("test")
-    }).next("div.inlinebuttons").remove();
+	//Add a new column with checkboxes for each ticket.
+	//Selecting a ticket marks it for inclusion in the batch. 
+	$("table.listing tr td.id").each(function() {
+    	tId=$(this).text().substring(1); 
+      	$(this).before('<td><input type="checkbox" name="selectedTicket" class="bmod_selector" value="'+tId+'"/></td>');
+    });
+  
+  	//Add a checkbox at the top of the column to select ever ticket in the group.
+  	$("table.listing tr th.id").each(function() { 
+		$(this).before('<th class="bmod_selector"><input type="checkbox" name="bmod_toggleGroup" /></th>');
+    });
 
-})(jQuery);
+    //Add the click behavior for the group toggle. 
+  	$("input[name='bmod_toggleGroup']").click(function() { 
+    	$("tr td input.bmod_selector",$(this).parents("table.listing")).attr("checked",this.checked);
+  	});
+  
+  	//By default the comment field is enabled.
+  	$("input#bmod_flag_comment").click(function() {enableControl("bmod_value_comment",this.checked);} );
+  
+  	//At least one ticket must be selected to submit the batch.
+  	$("form#batchmod-form").submit(function() {
+    	var selectedTix=[];    
+    	$("input[name=selectedTicket]:checked").each( function(){ selectedTix.push(this.value);} ); 
+    	$("input[name=selectedTickets]").val(selectedTix);
+    	if(selectedTix.length == 0){
+	        alert("No tickets selected to modify");
+    	    return false;
+    	}
+  	});
+  
+  	//Collapse the form by default
+  	$("#batchmod-fieldset").toggleClass("collapsed");
+  
+  	$("#add_batchmod_field").change(function() {
+		alert("test")
+	});
+});
