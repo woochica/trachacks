2408112c18fbf6b88e96e0af0c26ad11cce1a07f
bjornharrtell <bjornharrtell@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-08-01 17:01:26 +0000

    Moved current source to trunk and added maintainer notice
    git-svn-id: https://trac-hacks.org/svn@8320 7322e99d-02ea-0310-aa39-e9a107903beb

--- gridmodifyplugin/trunk/setup.py
+++ gridmodifyplugin/trunk/setup.py
@@ -11,6 +11,7 @@ setup(
     name=PACKAGE, version=VERSION,
     description='Allows grid modification of tickets',
     author="Abbywinters.com", author_email="trac-dev@abbywinters.com",
+    maintainer = "Björn Harrtell", maintainer_email = "bjorn@wololo.org",
     license='BSD', url='http://trac-hacks.org/wiki/GridModifyPlugin',
     packages = ['gridmod'],
     package_data={
