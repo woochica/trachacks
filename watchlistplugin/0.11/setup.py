2a6cf356c3b4cb8fff89a0a9efa187cdbece98dd
martin_s <martin_s@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-08-20 13:23:21 +0000

    Removed import of source revisions to avoid installation issues.
    git-svn-id: https://trac-hacks.org/svn@8391 7322e99d-02ea-0310-aa39-e9a107903beb

--- watchlistplugin/0.11/setup.py
+++ watchlistplugin/0.11/setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 from setuptools import setup
-from tracwatchlist.plugin import __revision__ as coderev
 
 __url__      = ur"$URL$"[6:-2]
 __author__   = ur"$Author$"[9:-2]
@@ -12,7 +11,7 @@ rev = str( max( coderev, __revision__ ) )
 
 setup(
     name = 'TracWatchlistPlugin',
-    version = '0.4.' + rev,
+    version = '0.5',
     packages = ['tracwatchlist'],
     author = 'Martin Scharrer',
     author_email = 'martin@scharrer-online.de',
