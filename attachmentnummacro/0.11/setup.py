c4ca797c9ee9a406615f0d5b7c651fce2d4bc3f0
martin_s <martin_s@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-08-20 13:22:36 +0000

    Removed import of source revisions to avoid installation issues.
    git-svn-id: https://trac-hacks.org/svn@8379 7322e99d-02ea-0310-aa39-e9a107903beb

--- attachmentnummacro/0.11/setup.py
+++ attachmentnummacro/0.11/setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 from setuptools import setup
-from tracattachmentnum.attachmentnum import __revision__ as coderev
 
 __url__      = ur"$URL$"[6:-2]
 __author__   = ur"$Author$"[9:-2]
@@ -12,7 +11,7 @@ rev = str( max( coderev, __revision__ ) )
 
 setup(
     name         = 'TracAttachmentNumMacro',
-    version      = '0.6.' + rev,
+    version      = '0.7',
     packages     = ['tracattachmentnum'],
     author       = 'Martin Scharrer',
     author_email = 'martin@scharrer-online.de',
