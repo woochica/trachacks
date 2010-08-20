2f1fd4e64fccfd57fddda117c21433d8e1a6fee4
martin_s <martin_s@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-08-20 13:22:44 +0000

    Removed import of source revisions to avoid installation issues.
    git-svn-id: https://trac-hacks.org/svn@8381 7322e99d-02ea-0310-aa39-e9a107903beb

--- googlestaticmapmacro/0.11/setup.py
+++ googlestaticmapmacro/0.11/setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 from setuptools import setup
-from tracgooglestaticmap.macro import __revision__ as coderev
 
 __url__      = ur"$URL$"[6:-2]
 __author__   = ur"$Author$"[9:-2]
@@ -12,7 +11,7 @@ rev = str( max( coderev, __revision__ ) )
 
 setup(
     name = 'TracGoogleStaticMapMacro',
-    version = '0.3.' + rev,
+    version = '0.4',
     packages = ['tracgooglestaticmap'],
     author = 'Martin Scharrer',
     author_email = 'martin@scharrer-online.de',
