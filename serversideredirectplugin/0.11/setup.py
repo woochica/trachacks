2be9b10d5b133842c8f09345ed37b446856cfe83
martin_s <martin_s@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-08-23 11:12:11 +0000

    Removed outdated variable `rev` from setup file.
    git-svn-id: https://trac-hacks.org/svn@8428 7322e99d-02ea-0310-aa39-e9a107903beb

--- serversideredirectplugin/0.11/setup.py
+++ serversideredirectplugin/0.11/setup.py
@@ -7,8 +7,6 @@ __author__   = ur"$Author$"[9:-2]
 __revision__ = ur"$Rev$"[6:-2]
 __date__     = ur"$Date$"[7:-2]
 
-rev = max(coderev, __revision__)
-
 setup(
     name         = 'TracServerSideRedirectPlugin',
     version      = '0.4',
