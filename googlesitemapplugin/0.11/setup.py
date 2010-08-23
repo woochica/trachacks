ac06df475d3ad4b638e7d4c15fbbe716ca9448e3
martin_s <martin_s@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-08-23 11:11:45 +0000

    Removed outdated variable `rev` from setup file.
    git-svn-id: https://trac-hacks.org/svn@8422 7322e99d-02ea-0310-aa39-e9a107903beb

--- googlesitemapplugin/0.11/setup.py
+++ googlesitemapplugin/0.11/setup.py
@@ -7,8 +7,6 @@ __author__   = ur"$Author$"[9:-2]
 __revision__ = ur"$Rev$"[6:-2]
 __date__     = ur"$Date$"[7:-2]
 
-rev = max(__revision__, codereva, coderevb)
-
 setup(
     name = 'TracGoogleSitemapPlugin',
     version = '1.0',
