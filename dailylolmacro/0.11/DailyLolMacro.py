03ce42f6e5f075bfc089586d7c7de91c21182274
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-08-27 19:22:18 +0000

    Add revision info to macro, and add missing dependencies. Refs #6797, Fixes #6574.
    git-svn-id: https://trac-hacks.org/svn@8462 7322e99d-02ea-0310-aa39-e9a107903beb

--- dailylolmacro/0.11/DailyLolMacro.py
+++ dailylolmacro/0.11/DailyLolMacro.py
@@ -1,3 +1,10 @@
+import urllib2
+from genshi.builder import tag
+from trac.wiki.macros import WikiMacroBase
+
+revison="$Rev$"
+url="$URL$"
+
 class DailyLolMacro(WikiMacroBase):
     """Display a picture of a cat with a funny caption.
     Usage:
@@ -6,19 +13,18 @@ class DailyLolMacro(WikiMacroBase):
     }}}
     """
 
-    def expand_macro(self, formatter, name, args):
-        import urllib2
+    def expand_macro(self, formatter, name, content):        
 
-        website = urllib2.urlopen("http://www.icanhascheezburger.com")
+        website = urllib2.urlopen("http://icanhascheezburger.com/")
         website_html = website.read()
+        print website_html
 
         beg_pos = -1
-        beg_pos = website_html.find('<img title', beg_pos+1)
-        beg_pos = website_html.find('src=', beg_pos)
-        end_pos = website_html.find('"', beg_pos+5)
-        raw_url = website_html[beg_pos+5:end_pos]
+        beg_pos = website_html.find('<img src="', beg_pos+1)
+        end_pos = website_html.find('"', beg_pos+10)
+        raw_url = website_html[beg_pos+10:end_pos]
 
         attr = {}
-        result = tag.img(src=raw_url, **attr)
+        out = tag.img(src=raw_url, **attr)
 
-        return result
+        return out
