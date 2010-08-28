3977daf0aabe9d11ac9a45132e871539c44b4b74
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-08-28 02:04:19 +0000

    Cleaned up indentation.
    git-svn-id: https://trac-hacks.org/svn@8476 7322e99d-02ea-0310-aa39-e9a107903beb

--- pledgiemacro/0.11/TracPledgieMacro/TracPledgieMacro.py
+++ pledgiemacro/0.11/TracPledgieMacro/TracPledgieMacro.py
@@ -2,12 +2,13 @@
     Author: Stephan
 """
 
+from StringIO import StringIO
+
+from genshi.builder import tag
 from trac.core import *
+from trac.util.html import escape,Markup
 from trac.wiki.api import parse_args
 from trac.wiki.macros import WikiMacroBase
-from StringIO import StringIO
-from trac.util.html import escape,Markup
-from genshi.builder import tag
 
 class PledgieMacro(WikiMacroBase):
     """ Displays the pledgie badge
@@ -18,19 +19,18 @@ class PledgieMacro(WikiMacroBase):
 	args = content.split(',')
 
         # HTML arguments used in Pledgie URL (future maybe...)
-        hargs = {
-            'key'    : self.env.config.get('pledgie', 'campaign_id', None)
-            }
+        hargs = { 'key': self.env.config.get('pledgie', 'campaign_id', None)}
 
         if not content:
-            raise TracError("No campaign id given! The campaign id can be found in the pledgie url, e.g. http://pledgie.com/campaigns/ID\n")
+            raise TracError("No campaign id given! The campaign id can " \
+                            "be found in the pledgie url, e.g. " \
+                            "http://pledgie.com/campaigns/ID\n")
 
-        return tag.a(
-                    tag.img(
-                          src    = "http://www.pledgie.com/campaigns/" + args[0] + ".png"
-#                        title  = Markup.escape(title, quotes=True),
-#                        alt    = Markup.escape(alt,   quotes=True),
+        return tag.a(tag.img(src = "http://www.pledgie.com/campaigns/" \
+                                   + args[0] + ".png"
+#                            title = Markup.escape(title, quotes=True),
+    #                        alt = Markup.escape(alt,   quotes=True),
                     ),
                     class_ = "pledgiemacro",
                     href   = "http://www.pledgie.com/campaigns/" + args[0]
-               )
+                    )
