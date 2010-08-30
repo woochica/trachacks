ed58fa126ae077ae8b2fa89651d1ad19097ef85a
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-08-30 05:22:19 +0000

    Adding macro to repository that was previously attached to the project's wiki page.
    git-svn-id: https://trac-hacks.org/svn@8518 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ ticketmilestonedisplaymacro/0.11/tm.py
@@ -0,0 +1,27 @@
+"""
+Lists all tickets matching a given regular expression
+
+This macro takes one parameter, the regexp
+"""
+
+import time
+import re
+from StringIO import StringIO
+
+def execute(hdf, args, env):
+    db = env.get_db_cnx()
+    cursor = db.cursor()
+
+    sql = "SELECT id, milestone FROM ticket WHERE id = '%s'" % args
+
+    cursor.execute(sql)
+
+    buf = StringIO()
+
+    row = cursor.fetchone()
+    if row == None:
+      buf.write('Ticket with ID "%s" was not found' % args)
+    else:
+      buf.write('[<a href="%s">Ticket #%s</a>, <a href="%s">Milestone: %s</a>]' % (env.href.ticket(row[0]), row[0], env.href.milestone(row[1]), row[1]))
+    
+    return buf.getvalue()
