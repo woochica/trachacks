f44287c0dbfd7fa04b847848d74794b187c26c67
kostialopuhin <kostialopuhin@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-09-01 15:05:33 +0000

    better handling of server timeouts
    git-svn-id: https://trac-hacks.org/svn@8585 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracajaxcommentsplugin/0.11/ajaxcomments/htdocs/js/comments.js
+++ tracajaxcommentsplugin/0.11/ajaxcomments/htdocs/js/comments.js
@@ -1,23 +1,28 @@
 // settings
-var refresh_interval = 5000; // ms
-var request_timeout = 9000; // ms
+var refresh_interval = 5000; // 5 s
+var request_timeout = 12000; // 12 s
 
 var ticket_url;
 var warning_sel = '#warning.system-message', form_sel = '#propertyform';
 var submit_button_sel = ':submit[name=submit]', submit_button_text;
 var buttons_sel = form_sel + ' div.buttons';
 var properties_section_sel = '#properties';
+var ticket_sel = '#ticket';
+var attach_sel = '#attachments';
+var changelog_sel = '#changelog';
+var last_comment_sel = changelog_sel + ' .change p:last';
+var comment_sel = '#comment';
 
 function update_ui(clear_form, responseText, statusText) {
     if (statusText == 'success') {
 	var $response = $(responseText);
-        var ticket_sel = '#ticket', attach_sel = '#attachments', changelog_sel = '#changelog';
 	var selectors = [ticket_sel, attach_sel, changelog_sel];
 	var update_changetime = function ($resp) {
 	    var input_sel = 'input[name=ts]';
 	    $(input_sel).replaceWith($(input_sel, $resp));
 	};
 	if ($(warning_sel, $response)[0]) {
+	    // there is a warning in response - comment added after the last update
 	    $.get(ticket_url, update_ui_get);
 	    var $warning = $(warning_sel, $response);
 	    $warning.html($warning.html() +
@@ -28,6 +33,14 @@ function update_ui(clear_form, responseText, statusText) {
 	} else if (clear_form) {
 	    // remove warning from current page, if there was no warning in $reponse
 	    $(warning_sel).remove(); 
+	} else if (
+	    // there is a warning and the last comment in response is the same as
+	    // the comment we entered - most likely we submitted the comment, but
+	    // anyway the text won't be lost
+	    $(warning_sel)[0] && comments_seem_equal(
+		$(last_comment_sel, $response).text(), $(comment_sel).val())) {
+	    clear_form = true; // than we can clear the form, and remove warning
+	    $(warning_sel).remove();
 	}
 	if (clear_form)
 	    selectors.push(form_sel);
@@ -40,12 +53,38 @@ function update_ui(clear_form, responseText, statusText) {
 	}
 
 	if (clear_form) {
+	    // Add the toolbar to all <textarea> elements on the page with the class
+	    // 'wikitext': from wikitoolbar.js FIXME
+	    if (window.addWikiFormattingToolbar != undefined) {
+		$("textarea.wikitext").each(function() { addWikiFormattingToolbar(this) });
+	    }
 	    setup_form_handlers();
 	} else { // update only ticket change time
 	    var input_sel = 'input[name=ts]';
 	    $(input_sel).replaceWith($(input_sel, $response));
 	}
     }
+}
+
+function comments_seem_equal(s1, s2) {
+    // Compare formatted and unformatted comments
+    // we remove trac formatting from strings and compare them
+    return strip_formatting(s1) == strip_formatting(s2);
+}
+
+var trac_formatting_symbols = ' \'"-=+*[]/{}~,.^!&,@#%:;|()\\`\n\t\r<>';
+var trac_fs_regexp_str = '';
+for (var i = 0; i < trac_formatting_symbols.length; i++) {
+    trac_fs_regexp_str += '\\' + trac_formatting_symbols[i];
+}
+var trac_fs_regexp = new RegExp('[' + trac_fs_regexp_str + ']', 'g');
+
+
+function strip_formatting(s) {
+    // return copy of s without trac formatting symbols
+    s = s.replace(/\[\[[^\]]*\]\]/g, ''); // remove [[...]]
+    s = s.replace(/\[[^\]]*\]/g, ''); // remove [...]
+    return s.replace(trac_fs_regexp, ''); // remove other symbols
 }
 
 function update_ui_get(responseText, statusText) {
@@ -62,7 +101,7 @@ function refresh() {
     if (has_focus) {
 	refresh_on = true;
 	has_focus = false; // some action has to set has_focus to true
-	$.get(ticket_url, update_ui_get);	
+	$.get(ticket_url, update_ui_get);
 	setTimeout(refresh, refresh_interval);
     } else { // stop refreshing if there was no activity in refresh_interval
 	refresh_on = false;
