380a9e140262788874b8975c175e878e7b4f6340
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-09-02 06:01:35 +0000

    Added maintainer to `setup.py`. Refs #7589.
    git-svn-id: https://trac-hacks.org/svn@8593 7322e99d-02ea-0310-aa39-e9a107903beb

--- fivestarvoteplugin/0.11/setup.py
+++ fivestarvoteplugin/0.11/setup.py
@@ -8,6 +8,8 @@ setup(
 	package_data={'fivestarvote' : ['htdocs/js/*.js', 'htdocs/css/*.css', 'htdocs/css/*.png']},
     	author='Dav Glass',
       	author_email='dav.glass@yahoo.com',
+        maintainer = 'Ryan J Ollos',
+        maintainer_email = 'ryano@physiosonics.com',
     	license='BSD',
     	url='http://trac-hacks.org/wiki/FiveStarVotePlugin',
     	description='A 5 star plugin for voting on Trac resources.',
