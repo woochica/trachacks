1e3e2256022cc2fa7158cb8e135618c40102e53a
martin_s <martin_s@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-09-07 15:52:32 +0000

    dev/tracwatchlist/htdocs/js/watchlist.js:: Checked in forgotten file related to [8669].
    git-svn-id: https://trac-hacks.org/svn@8672 7322e99d-02ea-0310-aa39-e9a107903beb

--- watchlistplugin/dev/tracwatchlist/htdocs/js/watchlist.js
+++ watchlistplugin/dev/tracwatchlist/htdocs/js/watchlist.js
@@ -20,7 +20,8 @@ $(function(){
 });
 /*! */
 
-function wlremoveempty( tdiv ) {
+function wlremoveempty( child ) {
+    tdiv = $(child).parents('div.watchlist-parent').get(0);
     pdiv = $(tdiv).parent("div");
     $(tdiv).remove();
     $(pdiv).find('h2').removeClass('foldable');
@@ -30,10 +31,6 @@ function wlremoveempty( tdiv ) {
 
 function wldeleterow(tr, table) {
   $(tr).remove();
-}
-
-function wldeleterow(tr, table) {
-  $(table).dataTable().fnDeleteRow(tr);
 }
 
 function wlremmsgbox() {
@@ -74,94 +71,6 @@ jQuery(document).ready(function() {
         return false;
     });
   });
-
-  var asWikiNames = [];
-  $("table#wikilist").each(function(){
-    $(this).find('tbody td.name').each(function () {
-        asWikiNames.push( $(this).text().replace(/^\s*|\s*$/g,'') );
-      });
-  });
-  $("#wikis .remfromwatch input[name=resid]")
-      .autocomplete(asWikiNames);
-  $("#wikis .addtowatch input[name=resid]")
-      .autocomplete("./watchlist?action=search&realm=wiki");
-
-  var asTicketIds = [];
-  $("table#ticketlist").each(function(){
-    $(this).find('tbody td.id').each(function () {
-        asTicketIds.push( $(this).text().replace(/^\s*#|\s*$/g,'') );
-      });
-  });
-  $("#tickets .remfromwatch input[name=resid]")
-      .autocomplete(asTicketIds);
-  $("#tickets .addtowatch input[name=resid]")
-      .autocomplete("./watchlist?action=search&realm=ticket");
-  asWikiNames = [];
-  asTicketIds = [];
-
-
-  $("table.watchlist").each(function(){
-    var aoColumns = [];
-    $(this).find('thead th').each( function () {
-      if ( $(this).hasClass( 'nosorting' ) ) {
-        aoColumns.push( { "bSortable": false, "bSearchable": false } );
-      } else {
-        aoColumns.push( null );
-      }
-    });
-    /*
-    if (aoColumns[0] == null) {
-      aoColumns[0] = {};
-    }
-    aoColumns[0]['sWidth'] = $(this).find('thead th.name').css('width');
-    */
-    $(this).dataTable({
-    "bStateSave": true,
-    "aoColumns": aoColumns,
-    //"bAutoWidth": false,
-    //"bJQueryUI": true,
-    "sPaginationType": "full_numbers",
-    "bPaginate": true,
-    "sDom": 'ilpfrt',
-    "sPagePrevious": "&lt;",
-    "aLengthMenu": [[10, 25, 50, 100, -1], [10, 25, 50, 100, "&#8734;"]],
-    //"sPaginationType": "full_numbers",
-    "fnHeaderCallback": function ( nRow, aaData, iStart, iEnd, aiDisplay ) {
-         if (aaData.length == 0) {
-            pdiv = $(nRow).parents('div.watchlist-parent').get(0);
-            wlremoveempty( pdiv );
-         }
-    },
-    "oLanguage": {
-      "sLengthMenu": "_MENU_",
-      "sZeroRecords": "./.",
-      "sInfo": "_START_-_END_ / _TOTAL_",
-      "sInfoEmpty": "./.",
-      "sInfoFiltered": "(_MAX_)",
-      "sProcessing": "...",
-      "sInfoPostFix": "",
-      "sSearch": "",
-      "sUrl": "",
-      "oPaginate": {
-        "sFirst":    "|&lt;",
-        "sPrevious": "&lt;",
-        "sNext":     "&gt;",
-        "sLast":     "&gt;|"
-      },
-      "fnInfoCallback": null
-    },
-   });
-  });
-  /*
-  $("#wikilist").tablesorter({widthFixed:true,headers: {
-    4: {sorter:false}, 5: {sorter:false}, 6: {sorter:false}
-  }
-  }).tablesorterPager({container:$("#wikipager")}); 
-  $("#ticketlist").tablesorter({widthFixed:true,headers: {
-    4: {sorter:false}
-  }
-  }).tablesorterPager({container:$("#ticketpager")});
-  */
 
   $(".foldable").enableFolding(false, false, true);
 //  $("table.watchlist").each(function() { wlchecktable(this); });
