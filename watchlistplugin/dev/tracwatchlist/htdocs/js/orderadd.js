3ef67c0564b5f281d42411d19d6684e91a539034
martin_s <martin_s@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-09-09 19:45:03 +0000

    dev/tracwatchlist/htdocs/js/orderadd.js:: Added missing JS-code for columns selections in the preferences menu.
    git-svn-id: https://trac-hacks.org/svn@8729 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ watchlistplugin/dev/tracwatchlist/htdocs/js/orderadd.js
@@ -0,0 +1,37 @@
+function orderadd_buttons(orderadd, select1, select2 ) {
+  $(orderadd).find("button[name=up]").click(function(){
+    $(select1).find("option:selected").each(function(){
+      prev = $(this).prev();
+      $(this).insertBefore(prev);
+    });
+  });
+  $(orderadd).find("button[name=down]").click(function(){
+    $(select1).find("option:selected").each(function(){
+      next = $(this).next();
+      $(this).insertAfter(next);
+    });
+  });
+  $(orderadd).find("button[name=right]").click(function(){
+    $(select2).append( $(select1).find("option:selected") );
+  });
+  $(orderadd).find("button[name=left]").click(function(){
+    $(select1).append( $(select2).find("option:selected") );
+  });
+}
+$(document).ready(function(){
+  $("#preferences form").submit(function(){
+    $(".orderadd").each(function(){
+      var text = [];
+      $(this).find("select.active_columns option").each(function(){
+        text.push( $(this).val() );
+      });
+      $(this).find("input.choosen_columns").val( text.join(',') );
+    });
+    return true;
+  });
+  $(".orderadd").each(function(){
+    select1 = $(this).find("select.active_columns");
+    select2 = $(this).find("select.available_columns");
+    orderadd_buttons(this, select1, select2 );
+  });
+});
