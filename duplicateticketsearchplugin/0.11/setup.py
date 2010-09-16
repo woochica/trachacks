41076bcfd2d875ab670271a444da132517d40e00
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-09-16 03:10:11 +0000

    Add URL and other info to `setup.py`. Refs #7662.
    git-svn-id: https://trac-hacks.org/svn@8895 7322e99d-02ea-0310-aa39-e9a107903beb

--- duplicateticketsearchplugin/0.11/setup.py
+++ duplicateticketsearchplugin/0.11/setup.py
@@ -3,13 +3,15 @@ from setuptools import setup
 setup(
     name = 'DuplicateTicketSearch', 
     version = '1.0.1',
+    author = 'gregmac',
+    description = 'Adds XMLRPC-based check from ticket entry page for potential duplicate tickets.',
+    keywords = 'trac duplicate ticket search plugin',
+    url = 'http://trac-hacks.org/wiki/DuplicateTicketSearchPlugin',
     packages = ['duplicateticketsearch'],
     package_data = { 'duplicateticketsearch' : ['htdocs/css/*.css', 'htdocs/js/*'] },
     entry_points = """
         [trac.plugins]
         DuplicateTicketSearch = duplicateticketsearch
     """,
-    author = 'gregmac',
-    install_requires = ['TracXMLRPC'], 
-    description = 'Adds XMLRPC-based check from ticket entry page for potential duplicate tickets.', 
+    install_requires = ['TracXMLRPC'],  
 )
