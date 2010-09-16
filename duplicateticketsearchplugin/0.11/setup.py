fc4f4ed85f644f98cd2220cc8132a69279d44c8c
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-09-16 03:05:44 +0000

    Added requirement to have XML-RPC installed. Fixes #7662.
    git-svn-id: https://trac-hacks.org/svn@8894 7322e99d-02ea-0310-aa39-e9a107903beb

--- duplicateticketsearchplugin/0.11/setup.py
+++ duplicateticketsearchplugin/0.11/setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
 
 setup(
-    name='DuplicateTicketSearch', 
-    version='1.0',
-    packages=['duplicateticketsearch'],
+    name = 'DuplicateTicketSearch', 
+    version = '1.0.1',
+    packages = ['duplicateticketsearch'],
     package_data = { 'duplicateticketsearch' : ['htdocs/css/*.css', 'htdocs/js/*'] },
     entry_points = """
         [trac.plugins]
         DuplicateTicketSearch = duplicateticketsearch
     """,
-    author = 'gregmac', 
-    description = 'Adds XMLRPC-based check from ticket entry page for potential duplicates', 
+    author = 'gregmac',
+    install_requires = ['TracXMLRPC'], 
+    description = 'Adds XMLRPC-based check from ticket entry page for potential duplicate tickets.', 
 )
