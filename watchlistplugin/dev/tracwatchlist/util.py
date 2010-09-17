3fb8adad95d37aad1f73886ac8ef9b65eac1c1b7
martin_s <martin_s@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-09-17 14:50:28 +0000

    util.py:: Added utility file with numeric range decoding methods.
    git-svn-id: https://trac-hacks.org/svn@8932 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ watchlistplugin/dev/tracwatchlist/util.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+ Watchlist Plugin for Trac
+ Copyright (c) 2008-2010  Martin Scharrer <martin@scharrer-online.de>
+ This is Free Software under the BSD license.
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+__url__      = ur"$URL$"[6:-2]
+__author__   = ur"$Author$"[9:-2]
+__revision__ = int("0" + ur"$Rev$"[6:-2].strip('M'))
+__date__     = ur"$Date$"[7:-2]
+
+from  trac.core  import  *
+
+
+def decode_range( str ):
+    """Decodes given string with integer ranges like `a-b,c-d` and yields a list 
+       of tuples: [(a,b),(c,d)] in this ranges."""
+    for irange in unicode(str).split(','):
+        try:
+            index = irange.index('-')
+        except:
+            a = b = irange
+        else:
+            b = irange[index+1:]
+            a = irange[:index]
+        try:
+            a = int(a)
+        except:
+            a = None
+        try:
+            b = int(b)
+        except:
+            b = None
+        if not (a is None and b is None):
+            yield (a,b)
+
+
+def decode_range_sql( str ):
+    """Decodes given string with ranges like `a-b,c-d` and returns a SQL 
+       command fragment to much this ranges."""
+    cmd = []
+    for (a,b) in decode_range( str ):
+        if a is None:
+            if b is None:
+                continue
+            cmd.append( ' ( %%(var)s <= %i ) ' % (b) )
+        elif b is None:
+            cmd.append( ' ( %%(var)s >= %i ) ' % (a) )
+        else:
+            cmd.append( ' ( %%(var)s BETWEEN %i AND %i ) ' % (a,b) )
+    return ' OR '.join(cmd)
+
+
