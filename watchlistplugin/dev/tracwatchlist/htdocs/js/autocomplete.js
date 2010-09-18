14f0956c8637adbcc7e38edac8d1521b9b7c726c
martin_s <martin_s@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-09-18 16:18:21 +0000

    tracwatchlist/htdocs/js/autocomplete.js:: Updated autocomplete to use AJAX     calls for watched resources.
    git-svn-id: https://trac-hacks.org/svn@8957 7322e99d-02ea-0310-aa39-e9a107903beb

--- watchlistplugin/dev/tracwatchlist/htdocs/js/autocomplete.js
+++ watchlistplugin/dev/tracwatchlist/htdocs/js/autocomplete.js
@@ -3,53 +3,59 @@
  * */
 
 jQuery(document).ready(function() {
-  // Autocomplete Wiki Names
-  var asWikiNames = [];
-  $("table#wikilist tbody td.name").each(function () {
-      asWikiNames.push( $(this).text().replace(/^\s*|\s*$/g,'') );
-  });
-  $("#wikis .remfromwatch input[name=resid]")
-      .autocomplete(asWikiNames);
-  $("#wikis tfoot th.name input")
-      .autocomplete(asWikiNames);
-  ///
-  
-  // Wiki author names
-  var osAuthorsHash = new Object();
-  var asAuthors = new Array();
-  $("table#wikilist tbody td.author").each(function () {
-      osAuthorsHash[ $(this).text().replace(/^\s*|\s*$/g,'') ] = 1;
-  });
-  for (key in osAuthorsHash) {
-    asAuthors.push(key);
-  }
-  $("#wikis tfoot th.author input")
-      .autocomplete(asAuthors);
-  osAuthorsHash = 0;
-  asAuthors     = 0;
-  ///
+    // Wiki author names
+    var osAuthorsHash = new Object();
+    var asAuthors = new Array();
+    $("table#wikilist tbody td.author").each(function () {
+        osAuthorsHash[ $(this).text().replace(/^\s*|\s*$/g,'') ] = 1;
+    });
+    for (key in osAuthorsHash) {
+        asAuthors.push(key);
+    }
+    if (asAuthors) {
+        $("#wikis tfoot th.author input").autocomplete(asAuthors);
+    }
+    ///
 
-  // Autocomplete Ticket Ids
-  var asTicketIds = [];
-  $("table#ticketlist tbody td.id").each(function () {
-      asTicketIds.push( $(this).text().replace(/^\s*#|\s*$/g,'') );
-  });
-  $("#tickets .remfromwatch input[name=resid]")
-      .autocomplete(asTicketIds);
-  $("#tickets tfoot th.id input")
-      .autocomplete(asTicketIds);
-  asWikiNames = 0;
-  asTicketIds = 0;
-  ///
+    // Ticket author names
+    var osAuthorsHash = new Object();
+    var asAuthors = new Array();
+    $("table#ticketlist tbody td.author").each(function () {
+        osAuthorsHash[ $(this).text().replace(/^\s*|\s*$/g,'') ] = 1;
+    });
+    for (key in osAuthorsHash) {
+        asAuthors.push(key);
+    }
+    if (asAuthors) {
+        $("#tickets tfoot th.author input").autocomplete(asAuthors);
+    }
+    osAuthorsHash = null;
+    asAuthors     = null;
+    ///
 
-  // AJAX calls
-  var options = {delay:750, matchCase:true, matchSubset:true,
-        selectFirst:false, multiple:true, multipleSeparator:',', cacheLength:10, max:100 }
-  $("#tickets .addtowatch input[name=resid]")
-      .autocomplete("./watchlist?action=search&realm=ticket", options);
-  $("#wikis .addtowatch input[name=resid]")
-      .autocomplete("./watchlist?action=search&realm=wiki", options);
-  ///
+    // AJAX calls
+    var options = {
+        delay:750,
+        matchCase:true,
+        matchSubset:true,
+        selectFirst:false,
+        multiple:true,
+        multipleSeparator:',',
+        cacheLength:10,
+        max:100,
+    }
+    // Autocomplete Wiki Names
+    $("#wikis .addtowatch input[name=resid]")
+        .autocomplete("./watchlist?action=search&realm=wiki", options);
+    $("#wikis .remfromwatch input[name=resid],#wikis tfoot th.name input")
+        .autocomplete("./watchlist?action=search&realm=wiki&group=watched", options);
+    ///
+    // Autocomplete Ticket Ids
+    $("#tickets .addtowatch input[name=resid]")
+        .autocomplete("./watchlist?action=search&realm=ticket", options);
+    $("#tickets .remfromwatch input[name=resid]")
+        .autocomplete("./watchlist?action=search&realm=ticket&group=watched", options);
+    ///
 });
 
 
