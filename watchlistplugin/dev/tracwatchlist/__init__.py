ab05b873eeb7d12348396f4133ad627d035844ed
martin_s <martin_s@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-09-24 09:40:59 +0000

    dev/setup.py::  dev/tracwatchlist/render/__init__.py::  dev/tracwatchlist/__init__.py::     Added/updated copyright headers.
    git-svn-id: https://trac-hacks.org/svn@9096 7322e99d-02ea-0310-aa39-e9a107903beb

--- watchlistplugin/dev/tracwatchlist/__init__.py
+++ watchlistplugin/dev/tracwatchlist/__init__.py
@@ -1,2 +1,27 @@
 # -*- coding: utf-8 -*-
+"""
+= Watchlist Plugin for Trac =
+Plugin Website:  http://trac-hacks.org/wiki/WatchlistPlugin
+Trac website:    http://trac.edgewall.org/
+
+Copyright (c) 2008-2010 by Martin Scharrer <martin@scharrer-online.de>
+All rights reserved.
+
+The i18n support was added by Steffen Hoffmann <hoff.st@web.de>.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+For a copy of the GNU General Public License see
+<http://www.gnu.org/licenses/>.
+
+$Id$
+"""
 from tracwatchlist import *
--- watchlistplugin/dev/tracwatchlist/render/__init__.py
+++ watchlistplugin/dev/tracwatchlist/render/__init__.py
@@ -3,20 +3,65 @@
  This file is part of the Watchlist Plugin for Trac.
  Copyright (c) 2008-2010  Martin Scharrer <martin@scharrer-online.de>
 
- NOTE:
- Code taken and minimal modified from trac.ticket.web_ui.TicketModule._render_property_diff
- (from both Trac 0.11.7 and 0.12) which is licensed under the modified BSD license:
-     Copyright (C) 2003-2009 Edgewall Software
-     Copyright (C) 2003-2005 Jonas Borgström <jonas@edgewall.com>
-     Author: Jonas Borgström <jonas@edgewall.com>
-     See http://trac.edgewall.org/wiki/TracLicense
+ IMPORTANT NOTE:
+ Code taken and minimal modified from
+ trac.ticket.web_ui.TicketModule._render_property_diff
+ (from both Trac 0.11.7 and 0.12) which is licensed under the
+ modified BSD license:
+
+    Copyright (C) 2003-2009 Edgewall Software
+    Copyright (C) 2003-2005 Jonas Borgström <jonas@edgewall.com>
+    Author: Jonas Borgström <jonas@edgewall.com>
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
+
+    1. Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in
+    the documentation and/or other materials provided with the
+    distribution.
+    3. The name of the author may not be used to endorse or promote
+    products derived from this software without specific prior
+    written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE AUTHOR `AS IS'' AND ANY EXPRESS
+    OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+    ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+    DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+    GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+    WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
  Notable changes:
+    * Converted to stand-alone function which can be called by plug-ins
+    * Added Trac version check to use one definition for Trac 0.11 and
+      another for Trac 0.12.
     * Added special format for attachments
 
  The contribution of Martin Scharrer is licenced under the modified BSD licence
- and the the GPL (v3 or later) licence. Either licence can be used for
+ (see above) and the the GPL (v3 or later) licence. Either licence can be used for
  derivatives.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    For a copy of the GNU General Public License see
+    <http://www.gnu.org/licenses/>.
 
  i18n notice:
  This file uses the same domain as trac itself, not the one for the WatchlistPlugin.
@@ -88,8 +133,12 @@ try:
                     'EMAIL_VIEW' in req.perm(resource_new or ticket.resource)):
                 old = obfuscate_email_address(old)
                 new = obfuscate_email_address(new)
+        # Added by MS
+        # The `wtag_` is the `tag_` from tracwatchlist.translation, e.g. 
+        # using its translation domain.
         if field == 'attachment':
             rendered = wtag_("%(value)s added", value=tag.em(new))
+        # changed 'if' to 'elif':
         elif old and not new:
             rendered = tag_("%(value)s deleted", value=tag.em(old))
         elif new and not old:
@@ -149,8 +198,10 @@ except ImportError:
                     'EMAIL_VIEW' in req.perm(resource_new or ticket.resource)):
                 old = obfuscate_email_address(old)
                 new = obfuscate_email_address(new)
+        # Added by MS
         if field == 'attachment':
             rendered = tag(tag.em(new), " added")
+        # changed 'if' to 'elif':
         elif old and not new:
             rendered = tag(tag.em(old), " deleted")
         elif new and not old:
