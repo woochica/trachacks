8f626f46dbead1d511d9bd4ed03aaf8581cfba97
doki_pen <doki_pen@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-09-25 21:07:23 +0000

    Adds subscription UI
    git-svn-id: https://trac-hacks.org/svn@9118 7322e99d-02ea-0310-aa39-e9a107903beb

--- announcerplugin/trunk/announcer/pref.py
+++ announcerplugin/trunk/announcer/pref.py
@@ -31,15 +31,19 @@
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 # ----------------------------------------------------------------------------
 
+import re
+from pkg_resources import resource_filename
+
 from trac.core import Component, implements, ExtensionPoint
 from trac.prefs.api import IPreferencePanelProvider
 from trac.web.chrome import ITemplateProvider, add_stylesheet, Chrome
-from trac.web import IRequestHandler
 
-from pkg_resources import resource_filename
-
-from announcer.api import IAnnouncementPreferenceProvider, \
-                          _, tag_, N_
+from announcer.api import _, tag_, N_
+from announcer.api import IAnnouncementDistributor
+from announcer.api import IAnnouncementFormatter
+from announcer.api import IAnnouncementPreferenceProvider
+from announcer.api import INewAnnouncementSubscriber
+from announcer.model import Subscription
 
 def truth(v):
     if v in (False, 'False', 'false', 0, '0', ''):
@@ -48,16 +52,16 @@ def truth(v):
 
 class AnnouncerPreferences(Component):
     implements(IPreferencePanelProvider, ITemplateProvider)
-    
+
     preference_boxes = ExtensionPoint(IAnnouncementPreferenceProvider)
-    
+
     def get_htdocs_dirs(self):
         return [('announcer', resource_filename(__name__, 'htdocs'))]
 
     def get_templates_dirs(self):
         resource_dir = resource_filename(__name__, 'templates')
         return [resource_dir]
-        
+
     def get_preference_panels(self, req):
         yield ('announcer', _('Announcements'))
 
@@ -71,7 +75,7 @@ class AnnouncerPreferences(Component):
                         continue
                     if (boxname == 'legacy' or boxname == 'joinable_groups') and not req.perm.has_permission('TICKET_VIEW'):
                         continue
-                    yield ((boxname, boxlabel) + 
+                    yield ((boxname, boxlabel) +
                         pr.render_announcement_preference_box(req, boxname))
 
     def render_preference_panel(self, req, panel, path_info=None):
@@ -83,5 +87,86 @@ class AnnouncerPreferences(Component):
             )))
         add_stylesheet(req, 'announcer/css/announcer_prefs.css')
         return 'prefs_announcer.html', {"boxes": streams}
-        
-        
+
+class SubscriptionManagementPanel(Component):
+    implements(IPreferencePanelProvider)
+    implements(ITemplateProvider)
+
+    subscribers = ExtensionPoint(INewAnnouncementSubscriber)
+    distributors = ExtensionPoint(IAnnouncementDistributor)
+    formatters = ExtensionPoint(IAnnouncementFormatter)
+
+    def __init__(self):
+        self.post_handlers = {
+            'add-rule': self._add_rule,
+            'delete-rule': self._delete_rule,
+        }
+
+    def get_htdocs_dirs(self):
+        return [('announcer', resource_filename(__name__, 'htdocs'))]
+
+    def get_templates_dirs(self):
+        resource_dir = resource_filename(__name__, 'templates')
+        return [resource_dir]
+
+    def get_preference_panels(self, req):
+        yield ('subscriptions', _('Subscriptions'))
+
+    def render_preference_panel(self, req, panel, path_info=None):
+        if req.method == 'POST':
+            method_arg = req.args.get('method', '')
+            m = re.match('^([^_]+)_(.+)', method_arg)
+            if m:
+                method, arg = m.groups()
+                method_func = self.post_handlers.get(method)
+                if method_func:
+                    method_func(arg, req)
+                else:
+                    #error
+                    pass
+            else:
+                #default save
+                pass
+            req.redirect(req.href.prefs('subscriptions'))
+
+        data = {'rules':{}, 'subscribers':[]}
+
+        desc_map = {}
+
+        data['formatters'] = ('text/plain', 'text/html')
+        data['selected_format'] = 'text/plain'
+        data['adverbs'] = ('always', 'never')
+
+        for i in self.subscribers:
+            data['subscribers'].append({
+                'class': i.__class__.__name__,
+                'description': i.description()
+            })
+            desc_map[i.__class__.__name__] = i.description()
+
+        for i in self.distributors:
+            for j in i.transports():
+                data['rules'][j] = []
+                for r in Subscription.find_by_sid(self.env, req.session.sid):
+                    data['rules'][j].append({
+                        'id': r['id'],
+                        'adverb': r['adverb'],
+                        'description': desc_map[r['class']],
+                        'priority': r['priority']
+                    })
+
+        add_stylesheet(req, 'announcer/css/announcer_prefs.css')
+        return "prefs_announcer_manage_subscriptions.html", dict(data=data)
+
+    def _add_rule(self, arg, req):
+        rule = Subscription(self.env)
+        rule['sid'] = req.session.sid
+        rule['authenticated'] = True
+        rule['distributor'] = arg
+        rule['format'] = req.args['format-%s'%arg]
+        rule['adverb'] = req.args['new-adverb-%s'%arg]
+        rule['class'] = req.args['new-rule-%s'%arg]
+        Subscription.add(self.env, rule)
+
+    def _delete_rule(self, arg, req):
+        Subscription.delete(self.env, arg)
