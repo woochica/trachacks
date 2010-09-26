ebf6393ad0ea3fe90099f940c7e29373c12afe1b
doki_pen <doki_pen@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-09-26 18:51:21 +0000

    Combine default resolvers into one file
    git-svn-id: https://trac-hacks.org/svn@9135 7322e99d-02ea-0310-aa39-e9a107903beb

--- announcerplugin/trunk/announcer/resolvers.py
+++ announcerplugin/trunk/announcer/resolvers.py
@@ -2,13 +2,13 @@
 #
 # Copyright (c) 2008, Stephen Hansen
 # Copyright (c) 2009, Robert Corsaro
-# 
+#
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
-#     * Redistributions of source code must retain the above copyright 
+#
+#     * Redistributions of source code must retain the above copyright
 #       notice, this list of conditions and the following disclaimer.
 #     * Redistributions in binary form must reproduce the above copyright
 #       notice, this list of conditions and the following disclaimer in the
@@ -16,7 +16,7 @@
 #     * Neither the name of the <ORGANIZATION> nor the names of its
 #       contributors may be used to endorse or promote products derived from
 #       this software without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 # "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 # LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -30,14 +30,28 @@
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 # ----------------------------------------------------------------------------
 
+from trac.config import Option
 from trac.core import Component, implements
 from trac.util.compat import sorted
 
 from announcer.api import IAnnouncementAddressResolver
+from announcer.api import IAnnouncementPreferenceProvider
+from announcer.api import _
+
+class DefaultDomainEmailResolver(Component):
+    implements(IAnnouncementAddressResolver)
+
+    default_domain = Option('announcer', 'email_default_domain', '',
+        """Default host/domain to append to address that do not specify one""")
+
+    def get_address_for_name(self, name, authenticated):
+        if self.default_domain:
+            return '%s@%s' % (name, self.default_domain)
+        return None
 
 class SessionEmailResolver(Component):
     implements(IAnnouncementAddressResolver)
-    
+
     def get_address_for_name(self, name, authenticated):
         db = self.env.get_db_cnx()
         cursor = db.cursor()
@@ -52,3 +66,36 @@ class SessionEmailResolver(Component):
         if result:
             return result[0]
         return None
+
+class SpecifiedEmailResolver(Component):
+    implements(IAnnouncementAddressResolver, IAnnouncementPreferenceProvider)
+
+    def get_address_for_name(self, name, authenticated):
+        db = self.env.get_db_cnx()
+        cursor = db.cursor()
+        cursor.execute("""
+            SELECT value
+              FROM session_attribute
+             WHERE sid=%s
+               AND authenticated=1
+               AND name=%s
+        """, (name,'announcer_specified_email'))
+        result = cursor.fetchone()
+        if result:
+            return result[0]
+        return None
+
+    # IAnnouncementDistributor
+    def get_announcement_preference_boxes(self, req):
+        if req.authname != "anonymous":
+            yield "emailaddress", _("Announcement Email Address")
+
+    def render_announcement_preference_box(self, req, panel):
+        cfg = self.config
+        sess = req.session
+        if req.method == "POST":
+            opt = req.args.get('specified_email', '')
+            sess['announcer_specified_email'] = opt
+        specified = sess.get('announcer_specified_email', '')
+        data = dict(specified_email = specified,)
+        return "prefs_announcer_emailaddress.html", data
