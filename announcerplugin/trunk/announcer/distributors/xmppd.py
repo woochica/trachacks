0c883668bf8f358b60ed5376f34c4566f3cedd79
doki_pen <doki_pen@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-09-29 19:00:07 +0000

    Moves Xmpp preference table to it's own comp.
    So we can disable it and eventually remove it.
    
    git-svn-id: https://trac-hacks.org/svn@9211 7322e99d-02ea-0310-aa39-e9a107903beb

--- announcerplugin/trunk/announcer/distributors/xmppd.py
+++ announcerplugin/trunk/announcer/distributors/xmppd.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (c) 2010, Robert Corsaro
-# 
+#
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
-#     * Redistributions of source code must retain the above copyright 
+#
+#     * Redistributions of source code must retain the above copyright
 #       notice, this list of conditions and the following disclaimer.
 #     * Redistributions in binary form must reproduce the above copyright
 #       notice, this list of conditions and the following disclaimer in the
@@ -15,7 +15,7 @@
 #     * Neither the name of the <ORGANIZATION> nor the names of its
 #       contributors may be used to endorse or promote products derived from
 #       this software without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 # "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 # LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -50,30 +50,28 @@ from announcer.util.settings import SubscriptionSetting
 
 class XmppDistributor(Component):
     """Distribute announcements to XMPP clients."""
-    
-    implements(IAnnouncementDistributor, IAnnouncementPreferenceProvider)
+
+    implements(IAnnouncementDistributor)
 
     formatters = ExtensionPoint(IAnnouncementFormatter)
-    producers = ExtensionPoint(IAnnouncementProducer)
-    distributors = ExtensionPoint(IAnnouncementDistributor)
 
     resolvers = OrderedExtensionsOption('announcer', 'xmpp_resolvers',
         IAnnouncementAddressResolver, 'SpecifiedXmppResolver',
-        """Comma seperated list of xmpp resolver components in the order 
-        they will be called.  If an xmpp address is resolved, the remaining 
+        """Comma seperated list of xmpp resolver components in the order
+        they will be called.  If an xmpp address is resolved, the remaining
         resolvers will no be called.
         """)
 
-    default_format = Option('announcer', 'default_xmpp_format', 
+    default_format = Option('announcer', 'default_xmpp_format',
             'text/plain',
             """Default format for xmpp messages.""")
 
     server = Option('xmpp', 'server', None,
         """XMPP server hostname to use for jabber notifications.""")
- 
+
     port = IntOption('xmpp', 'port', 5222,
         """XMPP server port to use for jabber notification.""")
- 
+
     user = Option('xmpp', 'user', 'trac@localhost',
         """Sender address to use in xmpp message.""")
 
@@ -83,16 +81,16 @@ class XmppDistributor(Component):
     password = Option('xmpp', 'password', None,
         """Password for XMPP server.""")
 
-    use_threaded_delivery = BoolOption('announcer', 'use_threaded_delivery', 
-            False, 
-            """If true, the actual delivery of the message will occur 
-            in a separate thread.  Enabling this will improve responsiveness 
-            for requests that end up with an announcement being sent over 
-            email. It requires building Python with threading support 
-            enabled-- which is usually the case. To test, start Python and 
+    use_threaded_delivery = BoolOption('announcer', 'use_threaded_delivery',
+            False,
+            """If true, the actual delivery of the message will occur
+            in a separate thread.  Enabling this will improve responsiveness
+            for requests that end up with an announcement being sent over
+            email. It requires building Python with threading support
+            enabled-- which is usually the case. To test, start Python and
             type 'import threading' to see if it raises an error.
             """)
-    
+
     def __init__(self):
         self.connections = {}
         self.delivery_queue = None
@@ -105,7 +103,7 @@ class XmppDistributor(Component):
             thread = DeliveryThread(self.delivery_queue, self.send)
             thread.start()
         return self.delivery_queue
-    
+
     # IAnnouncementDistributor
     def transports(self):
         yield "xmpp"
@@ -154,7 +152,7 @@ class XmppDistributor(Component):
                 self.log.debug("XmppDistributor found the " \
                         "address '%s' for '%s (%s)' via: %s"%(
                         addr, name, authed and \
-                        'authenticated' or 'not authenticated', 
+                        'authenticated' or 'not authenticated',
                         rslvr.__class__.__name__))
                 # ok, we found an addr, add the message
                 msgdict.setdefault(fmt, set()).add((name, authed, addr))
@@ -178,13 +176,13 @@ class XmppDistributor(Component):
                 formats[style] = f
         self.log.debug(
             "XmppDistributor has found the following formats capable "
-            "of handling '%s' of '%s': %s"%(transport, realm, 
+            "of handling '%s' of '%s': %s"%(transport, realm,
                 ', '.join(formats.keys())))
         if not formats:
             self.log.error("XmppDistributor is unable to continue " \
                     "without supporting formatters.")
         return formats
-                    
+
     def _get_preferred_format(self, sid, realm=None):
         if realm:
             name = 'xmpp_format_%s'%realm
@@ -218,7 +216,7 @@ class XmppDistributor(Component):
         cl = Client(server, port=self.port, debug=[])
         if not cl.connect():
             raise IOError("Couldn't connect to xmpp server %s"%server)
-        if not cl.auth(jid.getNode(), self.password, 
+        if not cl.auth(jid.getNode(), self.password,
                 resource=self.resource):
             cl.Connection.disconnect()
             raise IOError("Xmpp auth erro using %s to %s"%(jid, server))
@@ -226,10 +224,17 @@ class XmppDistributor(Component):
         for recip in recipients:
             cl.send(Message(recip[2], message))
 
-    # IAnnouncementDistributor
+
+class XmppPreferencePanel(Component):
+    implements(IAnnouncementPreferenceProvider)
+
+    formatters = ExtensionPoint(IAnnouncementFormatter)
+    producers = ExtensionPoint(IAnnouncementProducer)
+    distributors = ExtensionPoint(IAnnouncementDistributor)
+
     def get_announcement_preference_boxes(self, req):
         yield "xmpp", "XMPP Formats"
-        
+
     def render_announcement_preference_box(self, req, panel):
         supported_realms = {}
         for producer in self.producers:
@@ -241,16 +246,16 @@ class XmppDistributor(Component):
                                 if realm not in supported_realms:
                                     supported_realms[realm] = set()
                                 supported_realms[realm].add(style)
-            
+
         settings = {}
         for realm in supported_realms:
             name = 'xmpp_format_%s'%realm
-            settings[realm] = SubscriptionSetting(self.env, name, 
+            settings[realm] = SubscriptionSetting(self.env, name,
                 self.xmpp_format_setting.default)
         if req.method == "POST":
             for realm, setting in settings.items():
                 name = 'xmpp_format_%s'%realm
-                setting.set_user_setting(req.session, req.args.get(name), 
+                setting.set_user_setting(req.session, req.args.get(name),
                     save=False)
             req.session.save()
         prefs = {}
@@ -260,15 +265,17 @@ class XmppDistributor(Component):
             realms = supported_realms,
             preferences = prefs,
         )
-        return "prefs_announcer_xmpp.html", data    
-    
+        return "prefs_announcer_xmpp.html", data
+
+
 class DeliveryThread(Thread):
+
     def __init__(self, queue, sender):
         Thread.__init__(self)
         self._sender = sender
         self._queue = queue
         self.setDaemon(True)
-        
+
     def run(self):
         while 1:
             sendfrom, recipients, message = self._queue.get()
