ed5cf8e4a8c74369704a51f3d4e1c8152a6a61d4
hasienda <hasienda@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-10-13 18:52:57 +0000

    AccountManagerPlugin: Don't care about Python2.3 compatibility for future development, refs #4830.
    Hint: Trac 0.13dev finally dropped even Python2.4 compatibility to past
    the limitation, that `with` statements are not supported before Python2.5 .
    
    git-svn-id: https://trac-hacks.org/svn@9289 7322e99d-02ea-0310-aa39-e9a107903beb

--- accountmanagerplugin/trunk/acct_mgr/admin.py
+++ accountmanagerplugin/trunk/acct_mgr/admin.py
@@ -17,7 +17,6 @@ from trac.perm import IPermissionRequestor, PermissionSystem
 from trac.util.datefmt import format_datetime
 from trac.web.chrome import ITemplateProvider
 from trac.admin import IAdminPanelProvider
-from trac.util.compat import sorted
 
 from acct_mgr.api import AccountManager
 from acct_mgr.web_ui import _create_user
