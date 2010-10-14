fca24fd78e7e84e0fca007d67ea8980bb2775b6b
tbressure <tbressure@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-10-14 22:12:42 +0000

    add support for schedule type daily
    git-svn-id: https://trac-hacks.org/svn@9305 7322e99d-02ea-0310-aa39-e9a107903beb

--- traccronplugin/trunk/src/traccron/traccron.py
+++ traccronplugin/trunk/src/traccron/traccron.py
@@ -9,6 +9,7 @@ from trac.core import *
 from trac.admin import IAdminPanelProvider
 from trac.web.chrome import ITemplateProvider
 from threading import Timer
+from time import  time, localtime
 
 
 
@@ -18,9 +19,21 @@ class ICronTask(Interface):
     """
     
     def wake_up(self):
+        """
+        Call by the scheduler when the task need to be executed
+        """
         raise NotImplementedError
     
     def getId(self):
+        """
+        Return the key to use in trac.ini to cinfigure this task
+        """
+        raise NotImplementedError
+    
+    def getDescription(self):
+        """
+        Return the description of this task to be used in the admin panel.
+        """
         raise NotImplementedError
 
 
@@ -35,21 +48,31 @@ class Core(Component):
     cron_tack_list = ExtensionPoint(ICronTask)
     
     
+    
     def __init__(self,*args,**kwargs):
         """
 	    Intercept the instanciation to start the ticker
         """        
         Component.__init__(self, *args, **kwargs)
+        self.ticker = None
         self.cronconf = CronConfig(self.env)
-        self.webUi = WebUi(self.env)        
-        self.apply_config()
+        self.supported_schedule_type = [DailyScheduler(self.getCronConf())]
+        self.webUi = WebUi(self)        
+        self.apply_config()        
+
         
-    def apply_config(self):
+    def apply_config(self, wait=False):
         """
         Read configuration and apply it
         """
-        if self.getCronConf().get_ticker_enabled():
-        	self.ticker = Ticker(self.getCronConf().get_ticker_enabled(), self.check_task)
+        # stop existing ticker if any
+        if self.ticker is not None:
+            self.ticker.cancel(wait=wait)
+
+        if self.getCronConf().get_ticker_enabled():            
+        	self.ticker = Ticker(self.env,self.getCronConf().get_ticker_interval(), self.check_task)
+
+            
 
     def getCronConf(self):
         """
@@ -57,17 +80,30 @@ class Core(Component):
         """
         return self.cronconf
     
+    def getTaskList(self):
+        """
+        Return the list of existing task
+        """
+        return self.cron_tack_list
+    
+    def getSupportedScheduleType(self):
+        """
+        Return the list of supported schedule type
+        """
+        return self.supported_schedule_type
+    
     def check_task(self):
         """
         Check if any task need to be executed. This method is called by the Ticker.
         """
         # store current time
-        
+        currentTime = localtime(time())        
         for task in self.cron_tack_list:
-            # test current time with task planing
-            
-            # run task if needed
-            pass
+            # test current time with task planing            
+            for schedule in self.supported_schedule_type:
+                # run task if needed
+                if schedule.isTriggerTime(task, currentTime):
+                    task.wape_up()                       
 
         
     # IAdminPanel interface
@@ -94,12 +130,13 @@ class CronConfig():
     """
     This class read and write configuration for TracCronPlugin
     """
+    
     TRACCRON_SECTION = "traccron"
     
-    TICKER_ENABLED_KEY = "ticker.enabled"
+    TICKER_ENABLED_KEY = "ticker_enabled"
     TICKER_ENABLED_DEFAULT = "False"
     
-    TICKER_INTERVAL_KEY = "ticker.interval"
+    TICKER_INTERVAL_KEY = "ticker_interval"
     TICKER_INTERVAL_DEFAULT = 1 #minutes
     
 
@@ -111,14 +148,38 @@ class CronConfig():
 
     def set_ticker_enabled(self, value):
         self.env.config.set(self.TRACCRON_SECTION, self.TICKER_ENABLED_KEY, value)
+    
+    def get_ticker_interval(self):
+        return self.env.config.getint(self.TRACCRON_SECTION, self.TICKER_INTERVAL_KEY, self.TICKER_INTERVAL_DEFAULT)
+    
+    def set_ticker_interval(self, value):
+        self.env.config.set(self.TRACCRON_SECTION, self.TICKER_INTERVAL_KEY, value)
+    
+    def get_schedule_value(self, task, schedule_type):
+        self.env.config.get(self.TRACCRON_SECTION, task.getId() + "." + schedule_type.getId(), None)
 
+    def set_schedule_value(self, task, schedule_type, value):
+        self.env.config.set(self.TRACCRON_SECTION, task.getId() + "." + schedule_type.getId(), value)
+    
+    def set_value(self, key, value):
+        self.env.config.set(self.TRACCRON_SECTION, key, value)
+    
+    def remove_value(self, key):
+        self.env.config.remove(self.TRACCRON_SECTION, key)
+    
+    def save(self):
+        self.env.config.save()
 
 class WebUi(IAdminPanelProvider, ITemplateProvider):
     """
     Class that deal with Web stuff. It is the both the controller and the page builder.
     """
-    def __init__(self, env):
-        self.env = env
+    def __init__(self, core):        
+        self.env = core.env
+        self.cron_task_list = core.getTaskList()
+        self.cronconf = core.getCronConf()
+        self.all_schedule_type = core.getSupportedScheduleType()
+        self.core = core
     
     def get_admin_panels(self, req):
         if ('TRAC_ADMIN' in req.perm) :
@@ -131,10 +192,54 @@ class WebUi(IAdminPanelProvider, ITemplateProvider):
         data = {}
         
         if req.method == 'POST':
-            if 'save' in req.args:                                
+            if 'save' in req.args:          
+                
+                arg_name_list = [self.cronconf.TICKER_ENABLED_KEY,self.cronconf.TICKER_INTERVAL_KEY]
+                for task in self.cron_task_list:                            
+                    task_id = task.getId()                                            
+                    for schedule in self.all_schedule_type:
+                        arg_name_list.append(task_id + "." + schedule.getId())        
+                
+                for arg_name in arg_name_list:
+                    arg_value = req.args.get(arg_name,"").strip()
+                    self.env.log.debug("receive req arg "+ arg_name + "=[" + arg_value + "]")
+                    if (arg_value == ""):
+                        self.cronconf.remove_value(arg_name)                        
+                    else:
+                        self.cronconf.set_value(arg_name, arg_value)                  
+                
+                self.cronconf.save()
+                self.core.apply_config(wait=True)
                 req.redirect(req.abs_href.admin(category, page))
         else:            
+            
+            data.update({
+                          self.cronconf.TICKER_ENABLED_KEY:self.cronconf.get_ticker_enabled(),
+                          self.cronconf.TICKER_INTERVAL_KEY: self.cronconf.get_ticker_interval()                         
+                          })
+            
+            task_list = []
+            
+            for task in self.cron_task_list:
+                task_data = {}
+                
+                task_data['id'] = task.getId()
+                task_data['description'] = task.getDescription()
+                
+                all_schedule_value = {}
+                for schedule in self.all_schedule_type:
+                    value = self.cronconf.get_schedule_value(task, schedule)
+                    if value :
+                        all_schedule_value[schedule.getId()] = value
+                    else:
+                        all_schedule_value[schedule.getId()] = ""
+                task_data['schedule_list'] = all_schedule_value                        
+                                        
+                task_list.append(task_data)
+            
+            data['task_list'] = task_list
             return 'cron_admin.html', data
+                
 
     def get_htdocs_dirs(self):
         return []
@@ -145,7 +250,64 @@ class WebUi(IAdminPanelProvider, ITemplateProvider):
         return [resource_filename(__name__, 'templates')]
 
 
+    # internal method
+    
 
+class SchedulerType():
+    """
+    Define a sort of scheduling. Base class for any scheduler type implementation
+    """   
+    
+    def __init__(self, cronconf):
+        self.cronconf = cronconf
+        
+    def getId(self):
+        """
+        Return the id to use in trac.ini for this schedule type
+        """
+        raise NotImplementedError
+    
+    def isTriggerTime(self, task, currentTime):
+        """
+        Test is accordingly to this scheduler and given currentTime, is time to fire the task
+        """
+        # read the configuration value for the task
+        schedule_value = self._get_task_schedule_value(task)
+        return self.compareTime(currentTime, schedule_value)
+    
+    def compareTime(self, currentTime, schedule_value):
+         """
+         Test is accordingly to this scheduler, given currentTime and schedule value,
+         is time to fire the task.
+         currentTime is a structure computed by time.localtime(time())
+         scheduled_value is the value of the configuration in trac.ini      
+         """
+         raise NotImplementedError
+    
+    def _get_task_schedule_value(self, task):
+        return self.cronconf.get_schedule_value(task, self)
+    
+
+
+
+class DailyScheduler(SchedulerType):
+    """
+    Scheduler that trigger a task one a day based uppon a defined time
+    """
+    
+    def __init__(self, cronconf):
+        SchedulerType.__init__(self, cronconf)
+    
+    def getId(self):
+        return "daily"
+    
+    def compareTime(self, currentTime, schedule_value):        
+        # compare value with current
+        if schedule_value:
+            return schedule_value == str(currentTime.tm_hour) + str(currentTime.tm_min)
+        else:
+            return False
+         
 
 class Ticker():
     """
@@ -157,11 +319,11 @@ class Ticker():
         """
         Create a new Ticker.
         env : the trac environnement
-        interval: interval in second
+        interval: interval in minute
         callback: the function callback to call o every wake-up
         """
         self.env = env
-        self.interval
+        self.interval = interval
         self.callback = callback
         self.timer = None
         self.create_new_timer()
@@ -176,18 +338,24 @@ class Ticker():
             if ( wait ):
                 self.timer.join()            
         
-        self.timer = Timer(self.interval, self.wake_up)
+        self.timer = Timer(self.interval * 60 , self.wake_up)
         self.timer.start()
 
     def wake_up(self):
         self.callback()
         self.create_new_timer()
+        
+    
+    def cancel(self, wait=False):
+        self.timer.cancel()
+        if (wait):
+            self.timer.join()
 
 
 class HeartBeatTask(Component,ICronTask):
     """
     This is a simple task for testing purpose.
-    It only write a trace in log a debug levem
+    It only write a trace in log a debug level
     """
     
     implements(ICronTask)
@@ -197,5 +365,9 @@ class HeartBeatTask(Component,ICronTask):
     
     def getId(self):
         return "heart_beat"
+    
+    def getDescription(self):
+        return self.__doc__
+        
     
     
