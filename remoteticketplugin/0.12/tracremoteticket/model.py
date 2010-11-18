c100e7ce3f6a0acaed89ddf6fdd8899fbfee6992
moreati <moreati@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-11-18 15:55:40 +0000

    Add query of remote ticket by XMLRPC
    git-svn-id: https://trac-hacks.org/svn@9443 7322e99d-02ea-0310-aa39-e9a107903beb

--- remoteticketplugin/0.12/tracremoteticket/model.py
+++ remoteticketplugin/0.12/tracremoteticket/model.py
@@ -1,20 +1,41 @@
 from datetime import datetime
+import xmlrpclib
 
 from trac.ticket.api import TicketSystem
 from trac.util.datefmt import utc
+from trac.web.href import Href
+
+from tracremoteticket.api import RemoteTicketSystem
 
 class RemoteTicket(object):
     '''Local proxy for a ticket in a remote Trac system.
     '''
     remote_fields = ['summary', 'status', 'type', 'resolution']
     
-    def __init__(self, env, remote_name, id):
+    def __init__(self, env, remote_name, tkt_id):
         self.env = env
         self.remote_name = remote_name
-        self.id = id
+        self.id = int(tkt_id)
         self.fields = [f for f in TicketSystem(self.env).get_ticket_fields()
                          if f['name'] in RemoteTicket.remote_fields]
         self.values = {}
+        self._fetch_ticket(self.remote_name, self.id)
+        
+    def _fetch_ticket(self, remote_name, tkt_id):
+        # TODO Define and use method
+        rts = RemoteTicketSystem(self.env)
+        remote_trac = rts._intertracs[remote_name]['url']
+        server = xmlrpclib.ServerProxy(Href(remote_trac).rpc())
+        multicall = xmlrpclib.MultiCall(server)
+        
+        try:
+            tkt_vals = server.ticket.get(tkt_id)
+        except xmlrpclib.Error, e:
+            self.env.log.warn('Could not access remote trac %s %s', 
+                              e.args, e.message)
+            return
+        
+        self.values.update(tkt_vals[3])
     
     def __getitem__(self, name):
         return self.values.get(name)
