f318ffa1053d9b4635500e3c04d62e02ef15cc26
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-11-23 01:34:46 +0000

    Updating version numbers on 0.11 branch.
    git-svn-id: https://trac-hacks.org/svn@9498 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracticketstatsplugin/branches/0.11/setup.py
+++ tracticketstatsplugin/branches/0.11/setup.py
@@ -1,8 +1,8 @@
 from setuptools import find_packages, setup
 
 setup(
-	name='Tracticketstats', 
-	version='2.1.1',
+	name = 'Tracticketstats', 
+	version = '2.1.0',
 	maintainer = 'Ryan J Ollos',
 	maintainer_email = 'ryano@physiosonics.com',
 	packages=find_packages(exclude=['*.test*']),
@@ -10,6 +10,7 @@ setup(
 		[trac.plugins]
 		ticketstats = ticketstats
 	""",
+        install_requires = ['Trac >= 0.11'],
 	package_data={'ticketstats': ['templates/*.html']},
 )
 
