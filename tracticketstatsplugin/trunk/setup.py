444eedd8f61ce43813c925dad5b16ea8b08730a0
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-11-23 01:22:40 +0000

    Created a branch for Trac 0.11 and updated version numbers.
    git-svn-id: https://trac-hacks.org/svn@9496 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracticketstatsplugin/trunk/setup.py
+++ tracticketstatsplugin/trunk/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
 	name='Tracticketstats', 
-	version='2.1.1',
+	version='2.2.0',
 	maintainer = 'Ryan J Ollos',
 	maintainer_email = 'ryano@physiosonics.com',
 	packages=find_packages(exclude=['*.test*']),
