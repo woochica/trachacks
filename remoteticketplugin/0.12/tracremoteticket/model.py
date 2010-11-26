f600bcfc8dc4f46df5c5b449d16e5bdf34e9b55f
moreati <moreati@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-11-26 00:12:36 +0000

    Use cached tickets in RemoteTicket instances, force refresh when populating new ticket
    git-svn-id: https://trac-hacks.org/svn@9527 7322e99d-02ea-0310-aa39-e9a107903beb

--- remoteticketplugin/0.12/tracremoteticket/model.py
+++ remoteticketplugin/0.12/tracremoteticket/model.py
@@ -2,7 +2,10 @@ import xmlrpclib
 
 from trac.resource import ResourceNotFound
 from trac.ticket.api import TicketSystem
-from trac.util.datefmt import datetime, parse_date, to_utimestamp, utc
+from trac.util.datefmt import (datetime, timedelta,
+                               from_utimestamp, parse_date, to_utimestamp, 
+                               utc)
+from trac.util.text import empty
 from trac.web.href import Href
 
 from tracremoteticket.api import RemoteTicketSystem
@@ -12,23 +15,65 @@ __all__ = ['RemoteTicket']
 class RemoteTicket(object):
     '''Local proxy for a ticket in a remote Trac system.
     '''
+    # All fields from Trac ticket table except id
     remote_fields = ['time', 'changetime', 'component', #'severity', 
                      'priority', 'owner', 'reporter', 'cc',
                      'version', 'milestone', 'status', 'resolution',
                      'summary', 'description', 'keywords',
                      ]
     
-    def __init__(self, env, remote_name, tkt_id):
+    table_fields  = remote_fields + ['cachetime', 'remote_name', 'id']
+    cachetime_pos = -3
+    
+    def __init__(self, env, remote_name, tkt_id, refresh=False):
         self.env = env
         self.remote_name = remote_name
         self.id = int(tkt_id)
         self.fields = [f for f in TicketSystem(self.env).get_ticket_fields()
                          if f['name'] in RemoteTicket.remote_fields]
+        self.time_fields = [f['name'] for f in self.fields if 
+                                      f['type'] == 'time']
         self.values = {}
-        self._fetch_ticket()
+        
+        if not refresh:
+            self._fetch_ticket()
+        else:
+            self._refresh_ticket()
         
     def _fetch_ticket(self):
-        # TODO Define and use method
+        rts = RemoteTicketSystem(self.env)
+        db = self.env.get_read_db()
+        cursor = db.cursor()
+        
+        # Try to retrieve remote ticket from cache
+        cursor.execute('''SELECT %s FROM remote_tickets 
+                       WHERE remote_name=%%s and id=%%s
+                       ''' % (', '.join(self.table_fields)),
+                       (self.remote_name, self.id))
+        row = cursor.fetchone()
+        
+        # Remote ticket not in cache
+        if not row:
+            self._refresh_ticket()
+        
+        self._cachetime = from_utimestamp(row[self.cachetime_pos])
+        ttl = timedelta(seconds=int(rts.cache_ttl) // 1000, 
+                        microseconds=int(rts.cache_ttl) % 1000 * 1000)
+        
+        # Cached remote ticket is too old
+        if self._cachetime < datetime.now(utc) - ttl:
+            self._refresh_ticket()
+        
+        # Cached ticket is valid, populate instance
+        for name, value in zip(self.remote_fields, row):
+            if name in self.time_fields:
+                self.values[name] = from_utimestamp(value)
+            elif value is None:
+                self.values[name] = empty
+            else:
+                self.values[name] = value
+        
+    def _refresh_ticket(self):
         rts = RemoteTicketSystem(self.env)
         remote_trac = rts.get_remote_trac(self.remote_name)['url']
         xmlrpc_addr = Href(remote_trac).rpc()
@@ -50,7 +95,7 @@ class RemoteTicket(object):
                                    "Remote ticket unavailable")
         
         # Convert from DateTime used by xmlrpclib to datetime used by trac
-        for k in ['time', 'changetime']:
+        for k in self.time_fields:
             tkt_vals[3][k] = parse_date(tkt_vals[3][k].value, utc)
 
         self.values.update(tkt_vals[3])
@@ -68,7 +113,7 @@ class RemoteTicket(object):
                                  if f['name'] in values]
         for name in field_names:
             self[name] = values.get(name, '')
-    
+            
     def save(self, when=None):
         if when is None:
             when = self._cachetime
@@ -76,9 +121,12 @@ class RemoteTicket(object):
         
         # Build the update/insert sequences
         # NB The ordering means the same sequences may be reused in both cases
-        field_names = self.remote_fields + ['cachetime', 'remote_name', 'id']
+        field_names = self.table_fields
         values_dict = dict(self.values)
-        values_dict.update({'cachetime': when_ts, 
+        values_dict.update({'time': to_utimestamp(values_dict['time']),
+                            'changetime': 
+                                    to_utimestamp(values_dict['changetime']),
+                            'cachetime': when_ts, 
                             'remote_name': self.remote_name, 
                             'id': self.id})
         values = [values_dict[name] for name in field_names]
