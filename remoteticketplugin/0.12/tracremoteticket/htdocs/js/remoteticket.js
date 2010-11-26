3dfd9b4e798af67234c6b07886317574eb341c75
moreati <moreati@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-11-26 12:26:48 +0000

    Fix jslint errors and extraction of ticket id when url has ?query or #anchor
    git-svn-id: https://trac-hacks.org/svn@9530 7322e99d-02ea-0310-aa39-e9a107903beb

--- remoteticketplugin/0.12/tracremoteticket/htdocs/js/remoteticket.js
+++ remoteticketplugin/0.12/tracremoteticket/htdocs/js/remoteticket.js
@@ -1,10 +1,9 @@
-$(function() {
-    $('#newlinked :submit[name="create"]').bind('click', function() {
-        site = $('#remote-site :selected').val();
-        href = location.href;
+$(document).ready(function () {
+    $('#newlinked :submit[name="create"]').bind('click', function () {
+        var site = $('#remote-site :selected').val();
         
         // Direct form submission according to project drop down
-        form = $('#newlinked');
+        var form = $('#newlinked');
         form.attr('action', site);
         
         // If the selected project is this project set the linked_val
@@ -12,10 +11,13 @@ $(function() {
         // of this page
         if (site.charAt(0) === '/' ||
                 site.substring(0, site.lastIndexOf('/')) ===
-                href.substring(0, href.lastIndexOf('/'))) {
-            $('#linked-val').val(href.substring(href.lastIndexOf('/')+1));
+                location.href.substring(0, location.href.lastIndexOf('/'))
+                ) {
+            var tkt_id = location.pathname.substring(
+                            location.pathname.lastIndexOf('/') + 1);
+            $('#linked-val').val(tkt_id);
         } else {
-            $('#linked-val').val(href);
+            $('#linked-val').val(location.href);
         }
     });
 });
