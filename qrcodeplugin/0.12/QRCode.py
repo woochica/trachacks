8cc22bd259419dae11dfbd6cef9c21a19fb3e1e1
datamgmt <datamgmt@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-11-30 14:45:50 +0000

    Initial version
    git-svn-id: https://trac-hacks.org/svn@9558 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ qrcodeplugin/0.12/QRCode.py
@@ -0,0 +1,29 @@
+import urllib2
+from genshi.builder import tag
+from trac.wiki.macros import WikiMacroBase
+from trac.resource import get_resource_url
+
+revison="$Rev$"
+url="$URL$"
+
+class QRCodeMacro(WikiMacroBase):
+    """Display a QR Code for the current wiki page URL
+    Usage:
+    {{{
+    [[QRCode]]
+    }}}
+    """
+
+    def expand_macro(self, formatter, name, content):        
+        
+        qrc_url = 'http://chart.apis.google.com/chart?cht=qr&chs=120x120&chl='
+        req_url = get_resource_url(self.env, formatter.context.resource, formatter.req.abs_href)
+#        raw_url = qrc_url + req_url
+
+        out = '<a href="' + req_url + '"><img src="' + qrc_url + req_url + '" border="0"></a>'
+#        attr = {}
+#        out = tag.img(src=raw_url, **attr)
+#        out = raw_url
+
+        return out
+
