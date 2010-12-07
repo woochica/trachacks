be1ad319def61ebdd6f0c7158ba25eacab44754b
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-12-07 00:41:50 +0000

    Fix Trac display issue. Fixes #6264.
    git-svn-id: https://trac-hacks.org/svn@9625 7322e99d-02ea-0310-aa39-e9a107903beb

--- autocompleteusersplugin/0.11/autocompleteusers/htdocs/js/autocomplete_ticket.js
+++ autocompleteusersplugin/0.11/autocompleteusers/htdocs/js/autocomplete_ticket.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+jQuery(document).ready(function($) {
 	$("#action_reassign_reassign_owner").autocomplete("../users", { 
 		formatItem: formatItem 
 		    }); 
--- autocompleteusersplugin/0.11/autocompleteusers/htdocs/js/autocomplete_ticket_cc.js
+++ autocompleteusersplugin/0.11/autocompleteusers/htdocs/js/autocomplete_ticket_cc.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+jQuery(document).ready(function($) {
 	$("#field-cc").autocomplete("../users", {
 		multiple: true,
 		    formatItem: formatItem});
