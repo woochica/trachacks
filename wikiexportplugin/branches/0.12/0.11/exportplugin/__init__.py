ac2c4aaedde20ec7360aa99c7189852469daae2e
sfhynx <sfhynx@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-12-15 21:26:43 +0000

    Branch created to work with Trac 0.12, Python 2.6 and OOo3.2
    git-svn-id: https://trac-hacks.org/svn@9680 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ wikiexportplugin/branches/0.12/0.11/exportplugin/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+ Copyright (C) 2008 General de Software de Canarias.
+ 
+ Author: Claudio Manuel Fernández Barreiro <claudio.sphinx@gmail.com>
+'''
+
+from export import *
