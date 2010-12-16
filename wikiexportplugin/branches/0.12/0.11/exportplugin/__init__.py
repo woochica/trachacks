dcc4931b82e6832fab2a02bec60d3152421b54b8
sfhynx <sfhynx@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-12-16 11:48:00 +0000

    Directory malformed
    git-svn-id: https://trac-hacks.org/svn@9693 7322e99d-02ea-0310-aa39-e9a107903beb

--- wikiexportplugin/branches/0.12/0.11/exportplugin/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-'''
- Copyright (C) 2008 General de Software de Canarias.
- 
- Author: Claudio Manuel Fernández Barreiro <claudio.sphinx@gmail.com>
-'''
-
-from export import *
