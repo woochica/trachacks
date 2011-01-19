e4b070ff74671705d441b733970ea1d8d30e36a1
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2011-01-19 02:01:41 +0000

    Bumped plugin version number to 0.2dev and added minimum required version for Trac.
    git-svn-id: https://trac-hacks.org/svn@9773 7322e99d-02ea-0310-aa39-e9a107903beb

--- acronymsplugin/0.11/setup.py
+++ acronymsplugin/0.11/setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
 setup(name='TracAcronyms',
-      version='0.1',
+      version='0.2dev',
       packages=['tracacronyms'],
       author='Alec Thomas',
       description='Auto-generated acronyms from a table in a Wiki page.',
       url='http://trac-hacks.org/wiki/AcronymsPlugin',
       license='BSD',
-      entry_points = {'trac.plugins': ['tracacronyms = tracacronyms']})
+      entry_points = {'trac.plugins': ['tracacronyms = tracacronyms']},
+      install_requires = ['Trac >= 0.11'])
