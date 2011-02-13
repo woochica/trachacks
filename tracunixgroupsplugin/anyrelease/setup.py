f0707dc2aab3911ff2a670d13cafa3964c65a684
lord <lord@7322e99d-02ea-0310-aa39-e9a107903beb>
2011-02-13 23:12:37 +0000

    initial implementation
    git-svn-id: https://trac-hacks.org/svn@9858 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ tracunixgroupsplugin/anyrelease/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+
+from setuptools import setup, find_packages
+
+PACKAGE = 'TracUnixGroups'
+VERSION = '0.11'
+
+setup(
+    name = PACKAGE,
+    version = VERSION,
+
+    packages = find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data = True,
+
+    author = "Vadim Zaliva",
+    author_email = "lord@crocodile.org",
+    description = "TracUnixGroups - use system group membership in trac.",
+    long_description = "The TracUnixGroups plugin allows Trac to use the" \
+        " OS group membership for permissions assignment.",
+    license = "BSD",
+    keywords = "trac group permission plugin",
+    url = "https://trac-hacks.org/wiki/TracUnixGroupsPlugin",
+
+    entry_points = {
+        'trac.plugins': [
+            '%s = %s' % (PACKAGE, 'unixgroups')
+        ],
+    },
+
+    zip_safe = True
+)
---
+++ tracunixgroupsplugin/anyrelease/unixgroups/unixgroups.py
@@ -0,0 +1,21 @@
+# -*- coding: iso-8859-1 -*-
+
+# UnixGroups plugin v0.1
+
+from trac.core import *
+from trac.perm import IPermissionGroupProvider
+from subprocess import Popen,PIPE
+
+__all__ = ['UnixGroups']
+
+class UnixGroups(Component):
+    implements(IPermissionGroupProvider)
+
+    def get_permission_groups(self, username):
+        groups = []
+	groups_str = Popen(["groups", username], stdout=PIPE).communicate()[0]
+	groups = ["@%s"% x.strip() for x in groups_str.strip().split()]
+
+        self.env.log.debug('unixgroups found for %s = %s' % (username, ','.join(groups)))
+
+        return groups
