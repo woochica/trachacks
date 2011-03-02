89a006f2ccf44853d6391f68e0be136dadf41583
ChristianM <ChristianM@7322e99d-02ea-0310-aa39-e9a107903beb>
2011-03-02 14:42:42 +0000

    correct mail-address and url
    git-svn-id: https://trac-hacks.org/svn@9919 7322e99d-02ea-0310-aa39-e9a107903beb

--- motdplugin/0.12/setup.py
+++ motdplugin/0.12/setup.py
@@ -11,9 +11,9 @@ setup(
       'tracmotd': [ 'htdocs/*.css', 'htdocs/*.js' ]
     },
     author = 'Christian Masopust',
-    author_email = 'christian.masopust@siemens.com',
+    author_email = 'christian.masopust@chello.at',
     description = 'MOTD Trac Plugin',
-    url = 'http://www.trac-hacks.org/wiki/MessageOfTheDayPlugin',
+    url = 'http://trac-hacks.org/wiki/MotdPlugin',
     license = 'GPLv3',
     zip_safe = False,
     keywords = 'trac plugin motd',
