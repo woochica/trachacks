5370a3f827cea37c65eb3c3cdf98b38f331c32ce
netjunki <netjunki@7322e99d-02ea-0310-aa39-e9a107903beb>
2011-03-13 03:28:12 +0000

    updated the version... there were enough changes to justify it.
    git-svn-id: https://trac-hacks.org/svn@9950 7322e99d-02ea-0310-aa39-e9a107903beb

--- scrippetmacro/0.12/setup.py
+++ scrippetmacro/0.12/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name = 'TracScrippetsMacro',
-    version = '0.2',
+    version = '0.3',
     packages = ['scrippets'],
     package_data={ 'scrippets' : [ 'htdocs/css/*.css' ] },
     author = "Benjamin Lau",
