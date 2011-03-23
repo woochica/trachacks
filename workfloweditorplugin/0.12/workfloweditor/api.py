40450211463f0b7c9ef321caf71a3273418826cb
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2011-03-23 19:43:11 +0000

    Fix error in case that locale information is not received. Note: I wasn't able to reproduce the error, but the code changes are logical. Thanks for szaffarano for the patch. Refs #6645.
    git-svn-id: https://trac-hacks.org/svn@9979 7322e99d-02ea-0310-aa39-e9a107903beb

--- workfloweditorplugin/0.12/workfloweditor/api.py
+++ workfloweditorplugin/0.12/workfloweditor/api.py
@@ -13,10 +13,10 @@ class LocaleUtil:
         if req.environ.has_key('HTTP_ACCEPT_LANGUAGE'):
             locale_array = req.environ['HTTP_ACCEPT_LANGUAGE'].split(",")
         
-        if (len(locale_array) > 0):
+        if locale_array and (len(locale_array) > 0):
             locale = locale_array[0].strip()
         
-        if (len(locale) > 2):
+        if locale and (len(locale) > 2):
             locale = locale[0:2];
         
         return locale
