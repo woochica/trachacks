f5709307f6eb53a221bb46d00ae6f4395fe5e025
dan <dan@7322e99d-02ea-0310-aa39-e9a107903beb>
2011-04-04 20:01:59 +0000

    Created 0.12 repo
    git-svn-id: https://trac-hacks.org/svn@10020 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ awesomeattachmentsplugin/0.12/trunk/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='AwesomeAttachmentsPlugin', version='0.2',
+    packages=['awesome'],
+    package_data={ 'awesome': [ 'htdocs/images/*', 'htdocs/js/*', 'htdocs/css/*' ]},
+    entry_points = """
+        [trac.plugins]
+        awesome = awesome.ticketuploadmodule
+    """,
+)
