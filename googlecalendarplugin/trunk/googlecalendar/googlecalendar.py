a48a4ac9ef9a9dbed155b0c34b5561a36e11d798
StefanSimroth <StefanSimroth@7322e99d-02ea-0310-aa39-e9a107903beb>
2011-04-04 10:25:43 +0000

    Applied permissions patch from alfons@meteologica.com fixes #8512
    git-svn-id: https://trac-hacks.org/svn@10016 7322e99d-02ea-0310-aa39-e9a107903beb

--- googlecalendarplugin/trunk/googlecalendar/googlecalendar.py
+++ googlecalendarplugin/trunk/googlecalendar/googlecalendar.py
@@ -1,24 +1,32 @@
 from trac.core import *
 from trac.util.html import html
+from trac.perm import PermissionSystem, IPermissionRequestor
 from trac.web import IRequestHandler
 from trac.web.chrome import INavigationContributor, ITemplateProvider
 from pkg_resources import resource_filename
 
 class GoogleCalendar(Component):
-    implements(INavigationContributor, IRequestHandler, ITemplateProvider)
+    implements(INavigationContributor, IRequestHandler, IPermissionRequestor, ITemplateProvider)
 
     # INavigationContributor methods
     def get_active_navigation_item(self, req):
         return 'calendar'
     def get_navigation_items(self, req):
-        yield ('mainnav', 'calendar',
-            html.A('Calendar', href= req.href.calendar()))
+	if 'GOOGLECALENDAR_VIEW' in req.perm:
+		yield ('mainnav', 'calendar',
+				html.A('Calendar', href= req.href.calendar()))
 
+
+    # IPermissionRequestor methods
+    def get_permission_actions(self):
+        return ['GOOGLECALENDAR_VIEW']
+	    
     # IRequestHandler methods
     def match_request(self, req):
         return req.path_info == "/calendar"
 
     def process_request(self, req):
+	req.perm.require('GOOGLECALENDAR_VIEW')	
         req.hdf['title'] = 'Calendar'
 	calendar_url = self.config.get('googlecalendar', 'calendar_url')
 	if calendar_url == '':
