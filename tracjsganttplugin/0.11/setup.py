a3818e77168308b51864fb77a3f79f1911ddde6b
ChrisNelson <ChrisNelson@7322e99d-02ea-0310-aa39-e9a107903beb>
2011-04-05 16:17:13 +0000

    Merge all local changes to TH branch.  Refs #8574, #8642.
    Increase version to 0.4.
    
    
    git-svn-id: https://trac-hacks.org/svn@10043 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracjsganttplugin/0.11/setup.py
+++ tracjsganttplugin/0.11/setup.py
@@ -8,7 +8,7 @@ setup(
     author = 'Chris Nelson',
     author_email = 'Chris.Nelson@SIXNET.com',
     description = 'Trac plugin displaying jsGantt charts in Trac',
-    version = '0.3',
+    version = '0.4',
     url = 'http://trac-hacks.org/wiki/TracJsGanttPlugin',
     license='BSD',
     packages=['tracjsgantt'],
