9e279ecca1e25e8e1b50bafbe200422b6a7b73d4
helend <helend@7322e99d-02ea-0310-aa39-e9a107903beb>
2011-05-16 03:43:55 +0000

    BUG FIX: can not work with TracWysiwygPlugin.
    git-svn-id: https://trac-hacks.org/svn@10193 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracsuposeplugin/0.12/setup.py
+++ tracsuposeplugin/0.12/setup.py
@@ -1,6 +1,25 @@
 from setuptools import setup
 
-setup(name='tracsupose',
+extra = {}
+
+try:
+    from trac.util.dist  import  get_l10n_cmdclass
+    cmdclass = get_l10n_cmdclass()
+    if cmdclass:
+        extra['cmdclass'] = cmdclass
+        extractors = [
+            ('**.py',                'python', None),
+            ('**/templates/**.html', 'genshi', None),
+        ]
+        extra['message_extractors'] = {
+            'tractags': extractors,
+        }
+# i18n is implemented to be optional here
+except ImportError:
+    pass
+
+
+setup(name='TracSupose',
       version='0.0.1',
       packages=['tracsupose'],
       package_data={'tracsupose' : [
@@ -11,4 +30,5 @@ setup(name='tracsupose',
       url="http://trac-hacks.org/wiki/TracSupose",
       license='BSD',
       scripts=['update-index'],
-      entry_points = {'trac.plugins': ['tracsupose = tracsupose']})
+      entry_points = {'trac.plugins': ['tracsupose = tracsupose']},
+	  **extra)
