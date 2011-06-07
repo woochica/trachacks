86b56da1208a51b8606f75530fbae83ebe8680a4
bobbysmith007 <bobbysmith007@7322e99d-02ea-0310-aa39-e9a107903beb>
2011-06-07 20:48:37 +0000

    added a timeline filter to remove information about private fields from the timeline
    git-svn-id: https://trac-hacks.org/svn@10267 7322e99d-02ea-0310-aa39-e9a107903beb

--- timingandestimationplugin/branches/trac0.12-Permissions/timingandestimationplugin/tande_filters.py
+++ timingandestimationplugin/branches/trac0.12-Permissions/timingandestimationplugin/tande_filters.py
@@ -2,6 +2,7 @@ from trac.web.api import ITemplateStreamFilter
 from trac.perm import IPermissionRequestor
 from trac.core import *
 from genshi.core import *
+import genshi
 from genshi.builder import tag
 
 from genshi.filters.transform import Transformer
@@ -14,9 +15,15 @@ from trac.mimeview.api import Context
 from trac.resource import Resource
 from trac.web.chrome import  Chrome
 
-import sys 
+import sys, re
 if sys.version_info < (2, 4, 0): 
     from sets import Set as set
+
+# also in blackmagic ... not sure how to guarantee that one of them 
+# will be run in time other than to do it in both places
+def textOf(self, **keys):
+    return self.render('text', None, **keys)
+Stream.textOf = textOf
 
 ## MONKEY PATCH THE QUERY MODULE CSV EXPORT FN TO ENFORCE PERMISSIONS
 def new_csv_export(self, req, query, sep=',', mimetype='text/plain'):
@@ -77,6 +84,20 @@ class TicketFormatFilter(Component):
         stream = remove_header(stream, "hours")
         return stream 
 
+def denied_fields(comp, req):
+    fields = comp.config.getlist(csection, 'fields', [])
+    for field in fields:
+        comp.log.debug('found : %s' % field)
+        perms = comp.config.getlist(csection, '%s.permission' % field, [])
+        comp.log.debug('read permission config: %s has %s' % (field, perms))
+        for (perm, denial) in [s.split(":") for s in perms] :
+            perm = perm.upper()
+            comp.log.debug('testing permission: %s:%s should act= %s' %
+                           (field, perm, (not req.perm.has_permission(perm) or perm == "ALWAYS")))
+            if (not req.perm.has_permission(perm) or perm == "ALWAYS") \
+                    and denial.lower() in ["remove","hide"]:
+                label = comp.env.config.get('ticket-custom',field+'.label', field).lower().strip()
+                yield (field, label)
 
 class QueryColumnPermissionFilter(Component):
     """ Filtering the stream to remove """
@@ -99,38 +120,67 @@ class QueryColumnPermissionFilter(Component):
                         yield kind, data, pos        
             return column_helper
 
-        fields = self.config.getlist(csection, 'fields', [])
-        for field in fields:
-            self.log.debug('found : %s' % field)
-            perms = self.config.getlist(csection, '%s.permission' % field, [])
-            self.log.debug('read permission config: %s has %s' % (field, perms))
-            for (perm, denial) in [s.split(":") for s in perms] :
-                perm = perm.upper()
-                self.log.debug('testing permission: %s:%s should act= %s' %
-                               (field, perm, (not req.perm.has_permission(perm) or perm == "ALWAYS")))
-                if (not req.perm.has_permission(perm) or perm == "ALWAYS") and denial.lower() in ["remove","hide"]:
-                    # remove from the list of addable 
-                    stream = stream | Transformer(
-                        '//select[@id="add_filter"]/option[@value="%s"]' % field
-                        ).replace(" ")
+        for (field, label) in denied_fields(self, req):
+            # remove from the list of addable 
+            stream = stream | Transformer(
+                '//select[@id="add_filter"]/option[@value="%s"]' % field
+                ).replace(" ")
 
-                    # remove from the list of columns
-                    stream = stream | Transformer(
-                        '//fieldset[@id="columns"]/div/label'
-                        ).filter(make_col_helper(field))
+            # remove from the list of columns
+            stream = stream | Transformer(
+                '//fieldset[@id="columns"]/div/label'
+                ).filter(make_col_helper(field))
                     
-                    #remove from the results table
-                    stream = stream | Transformer(
-                        '//th[@class="%s"]' % field
-                        ).replace(" ")
-                    stream = stream | Transformer(
-                        '//td[@class="%s"]' % field
-                        ).replace(" ")
+            # remove from the results table
+            stream = stream | Transformer(
+                '//th[@class="%s"]' % field
+                ).replace(" ")
+            stream = stream | Transformer(
+                '//td[@class="%s"]' % field
+                ).replace(" ")
+            
+            # remove from the filters
+            stream = stream | Transformer(
+                '//tr[@class="%s"]' % field
+                ).replace(" ")
+        return stream
 
-                    # remove from the filters
-                    stream = stream | Transformer(
-                        '//tr[@class="%s"]' % field
-                        ).replace(" ")
+commasRE = re.compile(r',\s(,\s)+', re.I)
+class TimelinePermissionFilter(Component):
+    """ Filtering the stream to remove fields from the timeline of changes """
+    implements(ITemplateStreamFilter)
+    
+    ## ITemplateStreamFilter
+    
+    def filter_stream(self, req, method, filename, stream, data):
+        if not filename == "timeline.html":
+            self.log.debug('Not a timeline, returning')
+            return stream
+        denied = [label for (field, label) in denied_fields(self, req)]
+        def helper(field_stream):
+            try:
+                s = Stream(field_stream)
+                # without None as the second value we get str instead of unicode
+                # and that causes things to break sometimes
+                f = s.select('//text()').textOf(strip_markup=True).lower()
+                self.log.debug('Timeline Filter: is %r in %r, skip?%r',
+                               f, denied, f in denied )
+                if f not in denied: #if we are the field just skip it
+                #identity stream filter
+                    for kind, data, pos in s:
+                        yield kind, data, pos
+            except Exception, e:
+                self.log.exception('Timeline: Stream Filter Exception');
+                raise e
 
+        def comma_cleanup(stream):
+            text = Stream(stream).textOf()
+            self.log.debug( 'Timeline: Commas %r %r' , text, commasRE.sub( text, ', ' ) );
+            text = commasRE.sub( ', ' , text)
+            for kind, data, pos in tag(text):
+                yield kind, data, pos
+
+        stream = stream | Transformer('//dd[@class="editedticket"]/i').filter(helper)
+        stream = stream | Transformer('//dd[@class="editedticket"]/text()').filter(comma_cleanup)
                     
         return stream
