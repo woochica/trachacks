6bd1a83a1eb98693720b65393903260ac836e24b
hasienda <hasienda@7322e99d-02ea-0310-aa39-e9a107903beb>
2011-07-06 00:04:53 +0000

    TagsPlugin: Allow Unicode wiki page names for `ListTaggedMacro` macro, refs #8352.
    Thank you very much for the report including a possible solution alongside.
    
    git-svn-id: https://trac-hacks.org/svn@10385 7322e99d-02ea-0310-aa39-e9a107903beb

--- tagsplugin/tags/0.6/tractags/macros.py
+++ tagsplugin/tags/0.6/tractags/macros.py
@@ -76,8 +76,8 @@ class ListTaggedMacro(WikiMacroBase):
                                         resource, 'compact')
 
         ul = builder.ul(class_='taglist')
-        for resource, tags in sorted(query_result,
-                                     key=lambda r: str(r[0].id)):
+        # Beware: Resources can be Unicode strings.
+        for resource, tags in sorted(query_result, key=lambda r: r[0].id):
             tags = sorted(tags)
             if tags:
                 rendered_tags = [
