f298a5f8a3cdc6f0a34b868d45bd1316a72f6acd
hasienda <hasienda@7322e99d-02ea-0310-aa39-e9a107903beb>
2011-07-10 21:42:55 +0000

    AccountManagerPlugin: Second: Improve redirect loop protection - again, refs #3233 and #8963.
    We already knew a lot about infinite loops starting from '/login' (see #3233),
    but for Trac 0.11 obviously this has been not enough.  Still I wonder, why
    no one complained about it recently.
    
    Hint: Trac 0.11 doesn't preserve the referer very well, but I'm reluctant to
    fix this in AccountManager.  Better use a current Trac, if you dislike it.
    
    git-svn-id: https://trac-hacks.org/svn@10421 7322e99d-02ea-0310-aa39-e9a107903beb

--- accountmanagerplugin/trunk/acct_mgr/web_ui.py
+++ accountmanagerplugin/trunk/acct_mgr/web_ui.py
@@ -664,6 +664,16 @@ class LoginModule(auth.LoginModule):
     # overrides
     def _do_login(self, req):
         if not req.remote_user:
+            if req.method == 'GET':
+                # Trac before 0.12 has known weak redirect loop protection.
+                # Adding redirect fix from Trac 0.12, and especially avert
+                # from 'self._redirect_back', when we see a 'GET' here.
+                referer = req.get_header('Referer')
+                # Steer clear of requests going nowhere or loop to self
+                if referer is None or \
+                        referer.startswith(str(req.abs_href()) + '/login'):
+                    referer = req.abs_href()
+                req.redirect(referer)
             self._redirect_back(req)
         res = auth.LoginModule._do_login(self, req)
         if req.args.get('rememberme', '0') == '1':
