4bbd146cb2feeab644ac31b5315c8be9811b408c
cboos <cboos@7322e99d-02ea-0310-aa39-e9a107903beb>
2011-08-02 12:42:52 +0000

    Prepare for 0.13 version of graphviz plugin (need a similar fix as for #T10199)
    git-svn-id: https://trac-hacks.org/svn@10527 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ graphvizplugin/0.13/setup.py
@@ -0,0 +1,64 @@
+"""
+$Id$
+$HeadURL$
+
+Copyright (c) 2005, 2006, 2008 Peter Kropf. All rights reserved.
+
+Python egg setup file for the graphviz trac wiki processor.
+"""
+
+
+__revision__  = '$LastChangedRevision$'
+__id__        = '$Id$'
+__headurl__   = '$HeadURL$'
+__docformat__ = 'restructuredtext'
+__version__   = '0.7.6dev'
+
+from setuptools import setup, find_packages
+
+setup (
+    name = 'graphviz',
+    version = __version__,
+    packages = find_packages(),
+    package_data = { 'graphviz' : ['examples/*',],
+    },
+    entry_points={'trac.plugins': 'graphviz = graphviz'},
+    author = "Peter Kropf",
+    author_email = "pkropf@gmail.com",
+    keywords = "trac graphviz",
+    url = "http://trac-hacks.org/wiki/GraphvizPlugin",
+    description = "Graphviz plugin for Trac 0.11",
+    long_description = """
+    The graphviz wiki processor is a plugin for Trac that allows the
+dynamic generation of diagrams by the various graphviz programs. The
+text of a wiki page can contain the source text for graphviz and the
+web browser will show the resulting image.""",
+    license = """Copyright (C) 2005, 2006, 2008 Peter Kropf
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+ 1. Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+ 2. Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in
+    the documentation and/or other materials provided with the
+    distribution.
+ 3. The name of the author may not be used to endorse or promote
+    products derived from this software without specific prior
+    written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
+OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.""",
+)
