ceadb5d43fd9743a1837e5e1603400c84a41ac1e
osimons <osimons@7322e99d-02ea-0310-aa39-e9a107903beb>
2011-09-22 09:02:18 +0000

    TagsPlugin: Adding some slightly more elaborate web_ui test cases to ensure that basic request handling and permissions work.
    References #9194.
    
    
    git-svn-id: https://trac-hacks.org/svn@10671 7322e99d-02ea-0310-aa39-e9a107903beb

--- tagsplugin/trunk/tractags/tests/web_ui.py
+++ tagsplugin/trunk/tractags/tests/web_ui.py
@@ -11,7 +11,11 @@ import shutil
 import tempfile
 
 from trac.test import EnvironmentStub, Mock
+from trac.perm import PermissionSystem, PermissionCache, PermissionError
+from trac.web.href import Href
+from trac.web.session import DetachedSession
 
+from tractags.api import TagSystem
 from tractags.model import TagModelProvider
 from tractags.web_ui import TagTemplateProvider, TagRequestHandler
 
@@ -43,14 +47,62 @@ class TagRequestHandlerTestCase(unittest.TestCase):
         self.env.path = tempfile.mkdtemp()
         TagModelProvider(self.env).environment_created()
         
+        self.tag_s = TagSystem(self.env)
         self.tag_rh = TagRequestHandler(self.env)
+        
+        perm_system = PermissionSystem(self.env)
+        self.anonymous = PermissionCache(self.env, 'anonymous')
+        self.reader = PermissionCache(self.env, 'reader')
+        perm_system.grant_permission('reader', 'TAGS_VIEW')
+        self.writer = PermissionCache(self.env, 'writer')
+        perm_system.grant_permission('writer', 'TAGS_MODIFY')
+        self.admin = PermissionCache(self.env, 'admin')
+        perm_system.grant_permission('admin', 'TAGS_ADMIN')
+        
+        self.href = Href('/trac')
+        self.abs_href = Href('http://example.org/trac')
     
     def tearDown(self):
         shutil.rmtree(self.env.path)
     
-    def test_init(self):
-        # Empty test just to confirm that setUp and tearDown works
-        pass
+    def test_matches(self):
+        req = Mock(path_info='/tags',
+                   authname='reader',
+                   perm=self.reader
+                  )
+        self.assertEquals(True, self.tag_rh.match_request(req))
+    
+    def test_matches_no_permission(self):
+        req = Mock(path_info='/tags',
+                   authname='anonymous',
+                   perm=self.anonymous
+                  )
+        self.assertEquals(False, self.tag_rh.match_request(req))
+    
+    def test_get_main_page(self):
+        req = Mock(path_info='/tags',
+                   args={},
+                   authname='reader',
+                   perm=self.reader,
+                   href=self.href,
+                   method='GET',
+                   chrome={},
+                   session=DetachedSession(self.env, 'reader'),
+                   locale='',
+                   tz=''
+                )
+        template, data, content_type = self.tag_rh.process_request(req)
+        self.assertEquals('tag_view.html', template)
+        self.assertEquals(None, content_type)
+        self.assertEquals(['tag_body', 'tag_query', 'tag_realms', 'title'],
+                          sorted(data.keys()))
+    
+    def test_get_main_page_no_permission(self):
+        req = Mock(path_info='/tags',
+                   authname='anonymous',
+                   perm=self.anonymous
+                )
+        self.assertRaises(PermissionError, self.tag_rh.process_request, req)
 
 
 def test_suite():
