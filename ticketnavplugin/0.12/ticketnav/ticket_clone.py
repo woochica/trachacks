4d4168e3c61aef72f0b18781a90fed15b8be98d4
framay <framay@7322e99d-02ea-0310-aa39-e9a107903beb>
2011-09-28 15:00:46 +0000

    adjusted ticket clone, so it is translated through messages.pot and has now wiki-text instead of HTML
    git-svn-id: https://trac-hacks.org/svn@10700 7322e99d-02ea-0310-aa39-e9a107903beb

--- ticketnavplugin/0.12/ticketnav/ticket_clone.py
+++ ticketnavplugin/0.12/ticketnav/ticket_clone.py
@@ -3,29 +3,42 @@ from trac.web.api import ITemplateStreamFilter
 
 from genshi.builder import tag
 from genshi.filters import Transformer
+from trac.util.translation import domain_functions
+from trac.config import Option
+import pkg_resources
 
-revision = "$Rev: 8149 $"
-url = "$URL: http://svn.edgewall.org/repos/trac/tags/trac-0.12.2/sample-plugins/ticket_clone.py $"
+_, tag_, N_, add_domain = domain_functions('ticketnav', '_', 'tag_', 'N_', 'add_domain')
 
 class SimpleTicketCloneButton(Component):
     """Add a 'Clone' button to the ticket box. 
-    
-    This button is located next to the 'Reply' to description button,
-    and pressing it will send a request for creating a new ticket
-    which will be based on the cloned one.
-    """
+
+~~This button is located next to the 'Reply' to description button,
+and~~ pressing it will send a request for creating a new ticket
+which will be based on the cloned one.
+
+Copied from both `trac-0.12.2/sample-plugins/ticket_clone.py` 
+and from `TRAC/tracopt/ticket/clone.py`. Adjusted position 
+of Clone-Button and translated text. 
+"""
        
     implements(ITemplateStreamFilter)
 
     # ITemplateStreamFilter methods
+    xpathToPosition = Option('trac', 'clone_xpath', '//h1[@id="trac-ticket-title"]', 
+                             """xpath to postion of clone button""")
 
+    def __init__(self):
+            # bind the 'tracnav' catalog to the locale directory 
+            locale_dir = pkg_resources.resource_filename(__name__, 'locale') 
+            add_domain(self.env.path, locale_dir)
+        
     def filter_stream(self, req, method, filename, stream, data):
         if filename == 'ticket.html':
             ticket = data.get('ticket')
             if ticket and ticket.exists and \
                     'TICKET_CREATE' in req.perm(ticket.resource):
 #                filter = Transformer('//h3[@id="comment:description"]')
-                filter = Transformer('//h1[@id="trac-ticket-title"]')
+                filter = Transformer( self.config.get('trac', 'clone_xpath') )
                 return stream | filter.after(self._clone_form(req, ticket, data))
         return stream
 
@@ -34,20 +47,21 @@ class SimpleTicketCloneButton(Component):
         for f in data.get('fields', []):
             name = f['name']
             if name == 'summary':
-                fields['summary'] = "%s (geklont von %s)" % (ticket['summary'], ticket.id)
+#                fields['summary'] = "%(summary)s (geklont von %s)" % (ticket['summary'], ticket.id)
+                fields['summary'] = _("%(summary)s (cloned from %(id)s)",
+                                  summary=ticket['summary'], id=ticket.id)
             elif name == 'description':
-                fields['description'] = 'Geklont von Ticket <a href="%s/%s/ticket/%s">%s</a> (%s)<hr><br/> %s' % \
-                    (self.env.project_url, self.env.project_name, 
-                     ticket.id, ticket.id, ticket['summary'], ticket['description'])
+                fields['description'] = \
+                    _("Cloned from #%(id)s:\n----\n%(description)s",
+                      id=ticket.id, description=ticket['description'])
             else:
                 fields[name] = ticket[name]
         return tag.form(
             tag.div(
-                tag.input(type="submit", name="clone", value="Klonen",
-                    title="Create a copy of this ticket"),
+                tag.input(type="submit", name="clone", value=_('Clone'),
+                    title=_('Create a copy of this ticket')),
                 [tag.input(type="hidden", name='field_'+n, value=v) for n, v in
                     fields.items()],
                 tag.input(type="hidden", name='preview', value=''),
                 class_="inlinebuttons"),
             method="post", action=req.href.newticket())
-
