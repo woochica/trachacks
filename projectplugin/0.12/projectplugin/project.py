b55dde9975f0b5cfafeb5cd7500b35994b3c4ede
framay <framay@7322e99d-02ea-0310-aa39-e9a107903beb>
2011-09-30 13:14:17 +0000

    initial commit
    git-svn-id: https://trac-hacks.org/svn@10712 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ projectplugin/0.12/projectplugin/project.py
@@ -0,0 +1,268 @@
+import re
+import pkg_resources
+from pkg_resources import resource_filename
+
+from operator import attrgetter
+from genshi.filters import Transformer
+from genshi.path import Path
+from trac.core import *
+from trac.admin.api import IAdminPanelProvider
+from trac.web.chrome import ITemplateProvider, add_stylesheet
+from trac.web.api import IRequestHandler, ITemplateStreamFilter, IRequestFilter
+from trac.config import Option
+from trac.ticket.api import ITicketChangeListener, ITicketManipulator
+from trac.perm import IPermissionRequestor, PermissionSystem
+from trac.util.translation import domain_functions
+
+_, tag_, N_, add_domain = domain_functions('projectplugin',  '_', 'tag_', 'N_', 'add_domain')
+
+class ProjectAdmin(Component):
+    """Adds project number to ticket.
+    
+    User which have right `CONTROLLER_ADMIN` (or `TRAC_ADMIN`) can edit list of default project numbers in admin pane.
+    
+    Editing project number manually:
+    - when creating a ticket, manually typed project numbers will be '''skipped''' !
+    """
+    implements(IAdminPanelProvider, ITemplateProvider, ITicketChangeListener, IPermissionRequestor, IRequestFilter)
+    #ITemplateStreamFilter , IRequestHandler, ITicketManipulator
+    
+    changed_fields = ['type', 'milestone']
+    
+    #===========================================================================
+    # TODO: maybe using class CustomField from settings.py
+    #===========================================================================
+    def _init_config(self):
+        section = 'ticket-custom'
+        fld_name = 'projectplugin'
+        
+        if self.config and not self.config.get(section, fld_name):
+            self.log.debug( "adding custom-ticket %s" % fld_name )
+            self.config.set(section, fld_name, 'text')
+            self.config.set(section, fld_name + '.label', 'Project')
+            self.config.set(section, fld_name + '.order', '40')
+            self.config.set(section, fld_name + '.value', '')
+            self.config.save()
+            self.log.info( "custom-ticket fields added to trac.ini" )
+    
+    # ITemplateProvider methods
+    def get_templates_dirs(self):
+        from pkg_resources import resource_filename
+        return [resource_filename(__name__,'templates')]
+    
+    def get_htdocs_dirs(self):
+        return [('pp_htdocs', resource_filename(__name__, 'htdocs'))]  
+    
+    #IPermissionRequestor
+    def get_permission_actions(self):
+        yield ("CONTROLLER_ADMIN")
+
+    def checkPermissions(self, req):
+        for permission in ("TRAC_ADMIN", "CONTROLLER_ADMIN"):
+            if permission in PermissionSystem(self.env).get_user_permissions(req.authname):
+                return True
+        return False
+    
+#===============================================================================
+# ITicketChangeListener
+#===============================================================================
+    def ticket_created(self, ticket):
+        self._save_project_no(ticket, self.changed_fields)
+        return
+
+    def ticket_changed(self, ticket, comment, author, old_values):
+        if 'projectplugin' in old_values:
+            return
+        
+        flds = []
+        for cf in self.changed_fields:
+            if cf in old_values:
+                flds.append(cf)
+        self._save_project_no(ticket, flds)
+        return
+
+    def ticket_deleted(self, ticket):
+        """Called when a ticket is deleted."""
+        return
+    
+    def pre_process_request(self, req, handler):
+        return handler
+
+    # for ClearSilver templates
+    def post_process_request(self, req, template, data, content_type):
+        if template == 'ticket.html':
+            self._init_config()
+            if not self.checkPermissions(req):
+                add_stylesheet(req, 'pp_htdocs/prj_style.css')
+        return template, data, content_type
+    
+    # overridden from IAdminPanelProvider
+    def get_admin_panels(self, req):
+        """Return a list of available admin panels.
+        
+        The items returned by this function must be tuples of the form
+        `(category, category_label, page, page_label)`.
+        """
+        # TODO: add new permission !!!
+        if req.perm.has_permission('TRAC_ADMIN') or req.perm.has_permission("CONTROLLER_ADMIN"):
+            yield ('ticket', _('Ticket System'), 'projectplugin', _('Project'))
+
+    # overridden from IAdminPanelProvider
+    def render_admin_panel(self, req, category, page, path_info):
+        """Process a request for an admin panel.
+        
+        This function should return a tuple of the form `(template, data)`,
+        where `template` is the name of the template to use and `data` is the
+        data to be passed to the template.
+        """
+        errors = []
+        if req.args and 'projectname' in req.args:
+            errors = self._save({'projectname': req.args['projectname'], 
+                        'milestone': req.args['milestone'],
+                        'type': req.args['type'] })
+
+        return self._print_view(errors)
+        
+    
+    def _save_project_no(self, ticket, changed_fields):
+        """Save project number to corresponding ticket. """
+        projects, max_order = self._get_projects()
+        project_no = self.config.get('project-plugin', 'default-project')
+        
+        for prj in projects:
+            if prj['field'] in changed_fields:
+                val = ticket.get_value_or_default(prj['field'])
+                if val == prj['value']:
+                    project_no = prj['project']
+                    break
+        
+        db = self.env.get_db_cnx()
+        myCursor = db.cursor()
+         
+        sql_select = "select value from ticket_custom" \
+            " where ticket=%s and name='%s'" % (ticket.id, 'projectplugin')
+        
+        sql_update = "update ticket_custom " \
+              " set value = '%s'"\
+              " where ticket=%s and name='%s'" % (project_no, ticket.id, 'projectplugin')
+              
+        sql_insert = "insert into ticket_custom (ticket, name, value)" \
+              " values(%s, '%s', '%s') " % (ticket.id, 'projectplugin', project_no)
+        
+        sql = sql_select
+        sql_type = "select"
+        try:
+            myCursor.execute(sql)
+            sql = sql_insert
+            sql_type = "inserted"
+            for row in myCursor:
+                if row[0] and row[0] == project_no:
+                    self.log.info('project no %s already committed for ticket %s' % (project_no, ticket.id))
+                    return []
+                else:
+                    sql = sql_update
+                    sql_type = "updated"
+            myCursor.execute(sql)
+            db.commit()
+            self.log.info('%s project no %s for ticket %s' % (sql_type, project_no, ticket.id))
+            return []
+        except Exception, e:
+            self.log.error ( "Error executing SQL Statement \n ( %s ) \n %s" % (sql, e))
+            return ['projectplugin', e]
+        finally:
+            db.close()
+        
+    def _save(self, new_project):
+        """Save project to config file (`trac.ini`). """
+        if new_project:
+            if not new_project['type'] and not new_project['milestone']:
+                return ['Type or milestone is not set; one of it has to be selected']
+            elif not new_project['projectname']:
+                return ['Project name is not set, but mandatory']
+        else:
+            return []
+        
+        projects, max_order = self._get_projects()
+        #=======================================================================
+        # # enhencement: make list editable
+        # if new_project.has_key('order'):
+        #    for prj in projects:
+        #        if prj['order'] == new_project['order']:
+        #            # project already exists, pls update in config
+        #            pass
+        #=======================================================================
+        fld_name = None
+        if new_project['type']:
+            fld_name = 'type'
+        elif new_project['milestone']:
+            fld_name = 'milestone'
+            
+        field = 'order.%s.%s' % (max_order, fld_name)
+        value = '%s;%s' % (new_project['type'] or new_project['milestone'], new_project['projectname'])
+        self.log.info( 'saved new default project number: field: %s, project: %s' % ( field, value ) )
+        self.config.set('project-plugin', field, value)
+        self.config.save()
+        return []
+    
+    def _get_projects(self):
+        if 'project-plugin' in self.config:
+            entries = []
+            max_order = 1
+            for optname, value in self.config.options('project-plugin'):
+                row_fld = optname.split('.')
+                row_val = value.split(';')
+                if len(row_fld) == 3 and len(row_val) == 2:
+                    entries.append( {'order': row_fld[1],
+                           'field': row_fld[2],
+                           'value': row_val[0],
+                           'project': row_val[1]} )
+#            print "entries: %s" % entries
+            max_order = len(entries) + 1
+            if max_order > 0:
+                sorted(entries, key=lambda k: k['order'])
+            return entries, max_order
+        else:
+            return [], 1
+                    
+    def _print_view(self, errors):
+        projects, max_order = self._get_projects()
+        data = {'ticket_types': self._get_enums('ticket_type'),
+                'milestones': self._get_milestones(),
+                'projects': projects,
+                'errors': errors }
+        
+#        print "data is: %s" % data
+        return 'project-admin.html', data
+    
+    def _get_enums(self, enum_name):
+        if not enum_name:
+            return []
+        
+        db = self.env.get_db_cnx()
+        myCursor = db.cursor()
+        sql = "select type, name, value from enum where type='%s' order by value" % enum_name
+        sqlResult = []
+        
+        try:
+            myCursor.execute(sql)
+            for row in myCursor:
+                sqlResult.append({'type': row[0], 'name': row[1], 'value': row[2]})
+        except Exception, e:
+            self.log.error ( "Error executing SQL Statement \n ( %s ) \n %s" % (sql, e))
+            
+        return sqlResult
+    
+    def _get_milestones(self):
+        db = self.env.get_db_cnx()
+        myCursor = db.cursor()
+        sql = "select name from milestone where completed=0 order by name"
+        sqlResult = []
+        
+        try:
+            myCursor.execute(sql)
+            for row in myCursor:
+                sqlResult.append(row[0])
+        except Exception, e:
+            self.log.error ( "Error executing SQL Statement \n ( %s ) \n %s" % (sql, e))
+            
+        return sqlResult
