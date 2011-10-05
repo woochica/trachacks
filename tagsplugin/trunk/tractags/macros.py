18ae2ac7f9cc9c6d553f9a3e4e9bc02e254618a9
hasienda <hasienda@7322e99d-02ea-0310-aa39-e9a107903beb>
2011-10-05 21:04:30 +0000

    TagsPlugin: Move Genshi builder instructions to fragment template, refs #4503 and #9064.
    Now all list formats and even the warning message about obsolete !TagsQuery
    arguments are integrated into a HTML fragment template.  Various goodies like
    differently shaded odd/even rows and 'no results message' have been added.
    Oddly squeezed !ListTagged tables are gone as well - all due to re-using big
    parts of the !TicketQuery code from Trac core.
    
    git-svn-id: https://trac-hacks.org/svn@10738 7322e99d-02ea-0310-aa39-e9a107903beb

--- tagsplugin/trunk/tractags/macros.py
+++ tagsplugin/trunk/tractags/macros.py
@@ -22,7 +22,7 @@ from trac.util import embedded_numbers
 from trac.util.compat import sorted, set
 from trac.util.presentation import Paginator
 from trac.util.text import shorten_line, to_unicode
-from trac.web.chrome import add_link, add_stylesheet
+from trac.web.chrome import Chrome, add_link, add_stylesheet
 from trac.wiki.api import IWikiMacroProvider, parse_args
 
 from tractags.api import TagSystem, _
@@ -125,6 +125,7 @@ class TagWikiMacros(TagTemplateProvider):
     items_per_page = Option('tags', 'listtagged_items_per_page', 100,
         doc="""Number of tagged resources displayed per page in tag queries,
             by default""")
+    items_per_page = as_int(items_per_page, 100)
     supported_cols = frozenset(['realm', 'id', 'description', 'tags'])
 
     def __init__(self):
@@ -174,14 +175,10 @@ class TagWikiMacros(TagTemplateProvider):
                                 caseless_sort=self.caseless_sort)
 
         elif name == 'ListTagged':
-            message = None
             if _OBSOLETE_ARGS_RE.search(content):
-                message = builder.div(builder.p(Markup(_("""
-                    You seem to be using an old Tag query. Try using the
-                    <a href="%(url)s">new syntax</a> in your
-                    <strong>ListTagged</strong> macro.
-                    """, url=req.href('tags')))),
-                    class_='central system-message warning')
+                data = {'warning': 'obsolete_args'}
+            else:
+                data = {'warning': None}
             args, kw = parse_args(content)
             cols = 'cols' in kw and kw['cols'] or self.default_cols
             format = 'format' in kw and kw['format'] or self.default_format
@@ -201,11 +198,14 @@ class TagWikiMacros(TagTemplateProvider):
                     realms = set(all_realms)-set(self.exclude_realms)
             if not realms:
                 return ''
+            else:
+                self.all_realms = all_realms
+                self.query = query
+                self.realms = list(realms)
             query = '(%s) (%s)' % (query, ' or '.join(['realm:%s' % (r)
                                                        for r in realms]))
             self.env.log.debug('LISTTAGGED_QUERY: ' + query)
             query_result = tag_system.query(req, query)
-            add_stylesheet(req, 'tags/css/tractags.css')
 
             def _link(resource):
                 if resource.realm == 'ticket':
@@ -232,21 +232,16 @@ class TagWikiMacros(TagTemplateProvider):
                     labels = {'id': 'Id', 'description': 'Description'}
                 labels['realm'] = _('Realm')
                 labels['tags'] = _('Tags')
-                headers = [{'name': col,
-                            'label': labels.get(col)}
+                self.env.log.debug(str(labels))
+                headers = [{'label': labels.get(col)}
                            for col in cols]
-                container = builder.table(class_='wiki')
-                thead = builder.thead()
-                for col in cols:
-                    for header in headers:
-                        if header['name'] == col:
-                            thead(builder.th(header['label']))
-                container(thead)
-            else:
-                container = builder.ul(class_='taglist')
+                data.update({'cols': cols,
+                             'headers': headers})
+
             results = sorted(query_result, key=lambda r: \
                              embedded_numbers(to_unicode(r[0].id)))
             results = self._paginate(req, results)
+            rows = []
             for resource, tags in results:
                 desc = tag_system.describe_tagged_resource(req, resource)
                 tags = sorted(tags)
@@ -254,65 +249,85 @@ class TagWikiMacros(TagTemplateProvider):
                     rendered_tags = [_link(resource('tag', tag))
                                      for tag in tags]
                     if 'oldlist' == format:
-                        item = builder.li(_link(resource), ' ', desc, ' (',
-                                          rendered_tags[0],
-                                          [(' ', tag)
-                                           for tag in rendered_tags[1:]], ')'
-                               )
+                        resource_link = _link(resource)
                     else:
                         context=formatter.context
                         desc = desc or \
                                get_resource_description(self.env, resource,
                                                         context=context)
-                        link = builder.a(desc,
-                                         href=get_resource_url(self.env,
-                                                               resource,
-                                                               context.href)
-                               )
+                        resource_link = builder.a(desc, href=get_resource_url(
+                                            self.env, resource, context.href))
                         if 'table' == format:
-                            item = builder.tr()
+                            cells = []
                             for col in cols:
                                 if col == 'id':
-                                    item(builder.td(_link(resource)))
+                                    cells.append(_link(resource))
                                 # Don't duplicate links to resource in both.
                                 elif col == 'description' and 'id' in cols:
-                                    item(builder.td(desc))
+                                    cells.append(desc)
                                 elif col == 'description':
-                                    item(builder.td(link))
+                                    cells.append(resource_link)
                                 elif col == 'realm':
-                                    item(builder.td(resource.realm))
+                                    cells.append(resource.realm)
                                 elif col == 'tags':
-                                    item(builder.td([(tag, ' ')
+                                    cells.append(
+                                        builder([(tag, ' ')
                                                  for tag in rendered_tags]))
-                        else:
-                            item = builder.li(link)
-                else:
-                    item = builder.li(_link(resource), ' ', desc)
-                container(item)
-            container = builder(message, container)
-            page_nav = self._pageindex(req, results)
-            results = builder.span('(', results.displayed_items(), ')',
-                                   class_="numresults")
-            return builder(builder.h2(Markup(_("Results %(results)s",
-                                      results=results)),
-                                      class_="report-result"),
-                           page_nav, container, page_nav)
+                            rows.append({'cells': cells})
+                            continue
+                rows.append({'desc': desc,
+                             'rendered_tags': None,
+                             'resource_link': _link(resource)})
+            data.update({'format': format,
+                         'paginator': results,
+                         'results': rows,
+                         'tags_url': req.href('tags')})
+
+            # Work around a bug in trac/templates/layout.html, that causes a
+            # TypeError for the wiki macro call, if we use add_link() alone.
+            add_stylesheet(req, 'common/css/search.css')
+
+            return Chrome(self.env).render_template(
+                req, 'listtagged_results.html', data, 'text/html', True)
+
+    def get_href(self, req, per_page=None, page=None):
+        """Prepare href objects for pager links.
+
+        Generate form-related arguments, strip arguments with default values.
+        """
+        form_realms = None
+        if req.path_info == '/tags':
+            form_realms = {}
+            # Prepare realm arguments to keep form data consistent.
+            for realm in self.realms:
+                form_realms[realm] = 'on'
+            realms = None
+        elif set(self.realms) == set(self.all_realms):
+            realms = None
+        else:
+            realms = self.realms
+        if per_page == self.items_per_page:
+            per_page = None
+        if page == 1:
+            page = None
+        return req.href(req.path_info, form_realms, q=self.query,
+                        realms=realms, listtagged_per_page=per_page,
+                        listtagged_page=page)
 
     def _paginate(self, req, results):
-        query = req.args.get('q', None)
+        self.query = req.args.get('q', None)
         current_page = as_int(req.args.get('listtagged_page'), 1)
         items_per_page = as_int(req.args.get('listtagged_per_page'), None)
         if items_per_page is None:
-            items_per_page = as_int(self.items_per_page, 100)
+            items_per_page = self.items_per_page
         result = Paginator(results, current_page - 1, items_per_page)
 
         pagedata = []
         shown_pages = result.get_shown_pages(21)
         for page in shown_pages:
-            page_href = req.href(req.path_info, q=query, listtagged_page=page,
-                                 listtagged_per_page=items_per_page)
+            page_href = self.get_href(req, items_per_page, page)
             pagedata.append([page_href, None, str(page),
-                             _("page %(num)s", num=str(page))])
+                             _("Page %(num)d", num=page)])
 
         attributes = ['href', 'class', 'string', 'title']
         result.shown_pages = [dict(zip(attributes, p)) for p in pagedata]
@@ -321,41 +336,11 @@ class TagWikiMacros(TagTemplateProvider):
                                'string': str(result.page + 1), 'title': None}
 
         if result.has_next_page:
-            next_href = req.href(req.path_info, q=query,
-                                 listtagged_page=current_page + 1,
-                                 listtagged_per_page=items_per_page)
+            next_href = self.get_href(req, items_per_page, current_page + 1)
             add_link(req, 'next', next_href, _('Next Page'))
 
         if result.has_previous_page:
-            prev_href = req.href(req.path_info, q=query,
-                                 listtagged_page=current_page - 1,
-                                 listtagged_per_page=items_per_page)
+            prev_href = self.get_href(req, items_per_page, current_page - 1)
             add_link(req, 'prev', prev_href, _('Previous Page'))
         return result
-
-    def _pageindex(self, req, results):
-        index = builder.div()
-        if results.has_more_pages:
-            index(class_="paging")
-        if results.has_previous_page:
-            prevlink = req.chrome['links']['prev'][0]
-            index(builder.span(builder.a(Markup('&larr;'),
-                                         href=prevlink['href'],
-                                         title=prevlink['title']),
-                               class_="previous"))
-        for page in results.shown_pages:
-            current_page = results.current_page
-            if page['string'] == current_page['string']:
-                index(builder.span(builder.span(current_page['string'],
-                                                class_=current_page['class'])))
-            else:
-                index(builder.span(builder.a(page['string'], href=page['href'],
-                                             title=page['title'])))
-        if results.has_next_page:
-            nextlink = req.chrome['links']['next'][0]
-            index(builder.span(builder.a(Markup('&rarr;'),
-                                         href=nextlink['href'],
-                                         title=nextlink['title']),
-                               class_="next"))
-        return index
 
