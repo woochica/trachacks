524e7c76d0df6c145474aec9528f2fae3cde19b2
hasienda <hasienda@7322e99d-02ea-0310-aa39-e9a107903beb>
2011-10-07 21:40:16 +0000

    DataSaverPlugin: Make sure to always include a 'locale' dir in zipped plugins, refs #9249.
    Just some improved i18n code missing for this plugin, done elsewhere before.
    
    git-svn-id: https://trac-hacks.org/svn@10746 7322e99d-02ea-0310-aa39-e9a107903beb

--- datasaverplugin/trunk/setup.py
+++ datasaverplugin/trunk/setup.py
@@ -4,18 +4,22 @@ from setuptools import find_packages, setup
 
 extra = {}
 
-from trac.util.dist import get_l10n_js_cmdclass
-cmdclass = get_l10n_js_cmdclass()
-if cmdclass:
-    # includes some 'install_lib' overrides,
-    # i.e. 'compile_catalog' before 'bdist_egg' 
-    extra['cmdclass'] = cmdclass
-    extractors = [
-        ('**.py', 'python', None),
-    ]
-    extra['message_extractors'] = {
-        'datasaver': extractors,
-    }
+try:
+    from trac.util.dist import get_l10n_js_cmdclass
+    cmdclass = get_l10n_js_cmdclass()
+    if cmdclass:
+        # includes some 'install_lib' overrides,
+        # i.e. 'compile_catalog' before 'bdist_egg' 
+        extra['cmdclass'] = cmdclass
+        extractors = [
+            ('**.py', 'python', None),
+        ]
+        extra['message_extractors'] = {
+            'datasaver': extractors,
+        }
+# i18n is implemented to be optional here
+except ImportError:
+    pass
 
 
 PACKAGE = 'DataSaverPlugin'
@@ -27,11 +31,15 @@ setup(
     version=VERSION,
     author='Rich Harkins',
     author_email='rich@worldsinfinite.com',
+    maintainer = 'Steffen Hoffmann',
+    maintainer_email = 'hoff.st@web.de',
     license='GPL', url='http://trac-hacks.org/wiki/DataSaverPlugin',
-    install_requires = ['Babel>= 0.9.5', 'Trac >= 0.12dev'],
+    install_requires = ['Genshi >= 0.5', 'Trac >= 0.11'],
+    extras_require = {'Babel': 'Babel>= 0.9.5', 'Trac': 'Trac >= 0.12'},
     packages=find_packages(exclude=['*.tests*']),
     package_data={'datasaver': ['htdocs/*.js', 'htdocs/lang_js/*.js',
-                                'htdocs/*.css', 'locale/*/LC_MESSAGES/*.mo']},
+                                'htdocs/*.css', 'locale/*/LC_MESSAGES/*.mo',
+                                'locale/.placeholder']},
     entry_points="""
         [trac.plugins]
         datasaver = datasaver.datasaver
