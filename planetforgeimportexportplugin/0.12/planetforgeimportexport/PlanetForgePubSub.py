5f21200e3b5353289f037ea1133e5e488c43bcb7
zerodeux <zerodeux@7322e99d-02ea-0310-aa39-e9a107903beb>
2011-10-13 17:33:43 +0000

    add ticket listener. Call hub when new ticket is created.
    git-svn-id: https://trac-hacks.org/svn@10767 7322e99d-02ea-0310-aa39-e9a107903beb

--- planetforgeimportexportplugin/0.12/planetforgeimportexport/PlanetForgePubSub.py
+++ planetforgeimportexportplugin/0.12/planetforgeimportexport/PlanetForgePubSub.py
@@ -20,10 +20,57 @@ from trac.ticket.model import Ticket
 
 from urlparse import urlparse
 import json
+import urllib
+import urllib2
 
+debug=False
+
+
+#peer to notify
+peer='http://hub.coclico.bearstech.com/'
+#peer='http://127.0.0.1:8001/'
+
+#use htaccess ?
+use_htaccess = False
+
+#htacess credential
+htaccess = {
+    'login' : '',
+    'pwd' : '',
+}
+
+#Functions to call
+functions = { #'status' : 'status/index.php',
+              #'owner' : 'owner/index.php',
+              'create' : 'pubsubhub/ticket/create',
+              }
+
+def notify(field, args) :
+    #args['token'] = token
+    if debug :
+        log = open('/tmp/ticket_listener', 'a')
+        log.writelines('in \n')
+    if not functions.has_key(field) :
+        return
+    url = peer + functions[field]
+    postData=json.dumps(args)
+    if use_htaccess :
+        passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
+        passman.add_password(None, url, htaccess['login'], htaccess['pwd'])
+        authhandler = urllib2.HTTPBasicAuthHandler(passman)
+        opener = urllib2.build_opener(authhandler)
+    else :
+        opener = urllib2.build_opener()
+    req =  urllib2.Request(url, postData, {'Content-Type': 'application/json'})
+    urllib2.install_opener(opener)
+    res = urllib2.urlopen(req)
+    if debug :
+        log.writelines('url : %s\nPOST data : %s\nresponse : %s\ninfo : %s'%(url, str(args), res.read(), res.info()))
+        log.writelines('out \n')
+        log.close()
 
 class PlanetForgePubSub(Component):
-    implements(IAdminCommandProvider, IAdminPanelProvider, ITemplateProvider)
+    implements(IAdminCommandProvider, IAdminPanelProvider, ITemplateProvider, ITicketChangeListener)
 
 
     # CLI part (trac-admin /path/to/trac planetforge pubsub out hub.planetforge.com:4559
@@ -53,3 +100,58 @@ class PlanetForgePubSub(Component):
         req.perm.require('TRAC_ADMIN')
         return './pubsub.html', {}
 
+
+    def _getRepr(self, ticket) :
+        return {'authtoken':self.env.base_url + ticket.values['reporter'] + str(ticket.id),
+                'status': ticket.values['status'],
+                'description': ticket.values['description'],
+                'id': ticket.id,
+                'reporter': ticket.values['reporter'],
+                'cc': ticket.values['cc'],
+                'component': ticket.values['component'],
+                'summary': ticket.values['summary'],
+                'priority': ticket.values['priority'],
+                'owner': ticket.values['owner'],
+                'version': ticket.values['version'] if ticket.values.has_key('version') else 0,
+                'milestone': ticket.values['milestone'] if ticket.values.has_key('milestone') else '',
+                'keywords': ticket.values['keywords'],
+                'type': ticket.values['type']}
+
+
+    def ticket_created(self, ticket):
+        res = self._getRepr(ticket)
+        notify('create', res)
+        """Called when a ticket is created."""
+        return
+
+        
+    def ticket_changed(self, ticket, comment, author, old_values):
+        """Called when a ticket is modified.
+        
+        `old_values` is a dictionary containing the previous values of the
+        fields that have changed.
+        """
+        
+        if old_values.has_key('status') :
+            data = {'id' : ticket.id,
+                    'base_url' : self.env.base_url,
+                    'old_status' : old_values['status'],
+                    'new_status' : ticket.values['status'],
+                    'comment' : comment.encode('utf-8'),
+                    }
+            notify('status', data)
+
+        elif old_values.has_key('owner') :
+            data = {'id' : ticket.id,
+                    'base_url' : self.env.base_url,
+                    'old_owner' : old_values['owner'],
+                    'new_owner' : ticket.values['owner'],
+                    'comment' : comment.encode('utf-8'),
+                    }
+            notify('owner', data)
+        return
+        
+	
+    def ticket_deleted(self, ticket):
+        """Called when a ticket is deleted."""
+        return
