e89dd7627b1c86a3ee8e59b02cbc3da4301a5ae0
j98789 <j98789@7322e99d-02ea-0310-aa39-e9a107903beb>
2011-10-19 19:37:17 +0000

    changed filename
    git-svn-id: https://trac-hacks.org/svn@10803 7322e99d-02ea-0310-aa39-e9a107903beb

--- traccvsntintegrationplugin/hook/ProjectConfig.py
+++ traccvsntintegrationplugin/hook/ProjectConfig.py
@@ -13,9 +13,9 @@ import time
 class ProjectConfig():
     def __init__(self):
         # edit these string constants to match your configuration
-        self.repos          = '/repositories_cvs'                      # cvsnt repository name
-        self.call_db        = 'c:/jeroen_cvs/CVSROOT/loginfo_call.db'  # path to new database file
-        self.changeset_db   = 'c:/jeroen_cvs/CVSROOT/changesets.db'    # path to new database file
-        self.tracpath       = 'c:/temp/trac/scripts'                   # path where trac was installed
-        self.tracprojfolder = 'c:/temp/trac/test'                      # trac project folder
-        self.tracprojname   = 'test'                                   # trac project name
+        self.repos          = '/repositories_cvs'                            # cvsnt repository name
+        self.call_db        = 'c:/jeroen_cvs/CVSROOT/loginfo_hook_calls.db'  # path to new database file
+        self.changeset_db   = 'c:/jeroen_cvs/CVSROOT/changesets.db'          # path to new database file
+        self.tracpath       = 'c:/temp/trac/scripts'                         # path where trac was installed
+        self.tracprojfolder = 'c:/temp/trac/test'                            # trac project folder
+        self.tracprojname   = 'test'                                         # trac project name
