f1a250e44438b2e6b2bf4c08107e2f63f3071215
zitune <zitune@7322e99d-02ea-0310-aa39-e9a107903beb>
2011-11-02 17:56:16 +0000

    update setup.
    git-svn-id: https://trac-hacks.org/svn@10873 7322e99d-02ea-0310-aa39-e9a107903beb

--- mailtotracplugin/0.12/plugin/setup.py
+++ mailtotracplugin/0.12/plugin/setup.py
@@ -1,27 +1,23 @@
 from setuptools import find_packages, setup
 
-version='0.5'
+version='1'
 
 setup(name='mail2trac',
       version=version,
-      description="mail2trac",
-      author='Jeff Hammel',
-      author_email='jhammel@openplans.org',
+      description="mail2trac allow to respond to ticket via mail",
+      author='Olivier ANDRE',
+      author_email='oandre@bearstech.com',
       url='http://trac-hacks.org/wiki/MailToTracPlugin',
-      keywords='trac plugin',
-      license="GPL",
+      keywords='trac plugin email',
+      license="GPLv3",
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests*']),
       include_package_data=True,
       package_data={ 'mail2trac': ['templates/*', 'htdocs/*'] },
       zip_safe=False,
-      entry_points = """
-      [console_scripts]
-      mail2trac = mail2trac.email2trac:main
 
+      entry_points = """
       [trac.plugins]
-      email2ticket = mail2trac.email2ticket
-      emailposthandler = mail2trac.web
-      emailfilters = mail2trac.filters
+      mail2trac = mail2trac
       """,
       )
 
