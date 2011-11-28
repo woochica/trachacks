48d436c4a92a25933da5c5ae2d725d3c36d29f3b
framay <framay@7322e99d-02ea-0310-aa39-e9a107903beb>
2011-11-28 07:45:46 +0000

    creating folder `trunk`, since future changes will be made for Trac version 0.13
    git-svn-id: https://trac-hacks.org/svn@10950 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ projectplugin/trunk/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ProjectPlugin', 
+    version='0.4.0',
+    author = 'Franz Mayer, Gefasoft AG',
+    author_email = 'franz.mayer@gefasoft.de',
+    description = 'Adds project information to each ticket.',
+    url = 'http://www.gefasoft-muenchen.de',
+    packages=find_packages(exclude=['*.tests*']),
+    entry_points = """
+        [trac.plugins]
+        projectplugin = projectplugin
+    """,
+    package_data={'projectplugin': ['htdocs/*.css',
+                                    'templates/*.*']}
+)
