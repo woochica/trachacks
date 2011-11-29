ba2f711ebaf4b73a069df5c51e1dc07b4f2f63c3
crossroad <crossroad@7322e99d-02ea-0310-aa39-e9a107903beb>
2011-11-29 13:12:29 +0000

    added setup.py file and directory restructuring
    git-svn-id: https://trac-hacks.org/svn@10958 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ simplemultiprojectplugin/trunk/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='SimpleMultiProject',
+    version='0.0.1',
+    packages=['simplemultiproject'],
+    package_data={
+        'simplemultiproject' : [
+            'templates/*.html',
+            'htdocs/css/*.css'
+        ]
+    },
+    author = 'Christopher Paredes',
+    author_email='jesuchristopher@gmail.com',
+    license='GPL',
+    url='http://trac-hacks.org/wiki/SimpleMultiProject',
+    description='Simple Project',
+    long_description='Simple Project',
+    keywords='Simple Project',
+    entry_points = {'trac.plugins': ['simplemultiproject = simplemultiproject']}
+)
