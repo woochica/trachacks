2aaf171f3c20b90fb39d6665db4106349bf06323
hasienda <hasienda@7322e99d-02ea-0310-aa39-e9a107903beb>
2011-12-16 20:26:03 +0000

    AccountManagerPlugin: Allow relative URL for `HttpAuthStore` configuration, refs #8925 and #9618.
    Add more sophisticated logic to deal with various `authentication_url` values
    other than full absolute URLs as suggested by Dennis McRitchie.
    Minor differences to the original patch include enforced 79 char/line limit
    including use of multi-line strings, modified comments and the rename of
    user -> username according to previous changes, starting with [10288].
    
    git-svn-id: https://trac-hacks.org/svn@11045 7322e99d-02ea-0310-aa39-e9a107903beb

--- accountmanagerplugin/trunk/acct_mgr/http.py
+++ accountmanagerplugin/trunk/acct_mgr/http.py
@@ -1,6 +1,7 @@
 # -*- coding: utf8 -*-
 #
 # Copyright (C) 2005 Matthew Good <trac@matt-good.net>
+# Copyright (C) 2011 Dennis McRitchie
 #
 # "THE BEER-WARE LICENSE" (Revision 42):
 # <trac@matt-good.net> wrote this file.  As long as you retain this notice you
@@ -11,9 +12,11 @@
 
 from urllib2 import build_opener, HTTPBasicAuthHandler, \
                     HTTPDigestAuthHandler, HTTPPasswordMgrWithDefaultRealm
+from urlparse import urlparse
 
 from trac.core import Component, implements
 from trac.config import Option
+from trac.web.href import Href
 
 from acct_mgr.api import IPasswordStore, _, N_
 
@@ -24,17 +27,45 @@ class HttpAuthStore(Component):
     auth_url = Option('account-manager', 'authentication_url', '',
         doc = N_("URL of the HTTP authentication service"))
 
-    def check_password(self, user, password):
+    def check_password(self, username, password):
+        self.log.debug("Trac.ini authentication_url = '%s'" % self.auth_url)
+        # Nothing to do, if URL is absolute.
+        if self.auth_url.startswith('http://') or \
+                self.auth_url.startswith('https://'):
+            authUrl = self.auth_url
+        # Handle server-relative URLs.
+        elif self.auth_url.startswith('/'):
+            # Prepend the Trac server component.
+            pr = urlparse(self.env.abs_href())
+            href = Href(pr[0] + '://' + pr[1])
+            authUrl = href(self.auth_url)
+        elif '/' in self.auth_url:
+            # URLs with path like 'common/authFile' or 'site/authFile'.
+            authUrl = self.env.abs_href.chrome(self.auth_url)
+        else:
+            # Bare file name option value like 'authFile'.
+            authUrl = self.env.abs_href.chrome('common', self.auth_url)
+        self.log.debug("Final auth_url = '%s'" % authUrl)
+
         acctmgr = HTTPPasswordMgrWithDefaultRealm()
-        acctmgr.add_password(None, self.auth_url, user, password)
+        acctmgr.add_password(None, authUrl, username, password)
         try:
             build_opener(HTTPBasicAuthHandler(acctmgr),
-                         HTTPDigestAuthHandler(acctmgr)).open(self.auth_url)
-        except IOError:
+                         HTTPDigestAuthHandler(acctmgr)).open(authUrl)
+        except IOError,e:
+            if hasattr(e, 'code') and e.code == 404:
+                self.log.debug("""HttpAuthStore page not found; we are
+                               authenticated nonetheless""")
+                return True
+            if hasattr(e, 'code') and e.code == 401:
+                self.log.debug("HttpAuthStore authentication failed")
             return None
-        except ValueError:
+        except ValueError, e:
+            self.log.debug("""HttpAuthStore: 'authentication_url' specifies
+                           an invalid URL""")
             return None
         else:
+            self.log.debug("HttpAuthStore page exists; we are authenticated")
             return True
 
     def get_users(self):
