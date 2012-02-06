0faf22c0478948c0b717629b2ef153d3bbcf647f
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-02-06 04:47:43 +0000

    Refs #9770: Re-implemented the patch by ebencheung, with minor changes. The images are now stored in a cache directory, rather than in the session data. ebencheung had described the issue in which session data was limited by Apache, and the graphs failed to render. Caching files to disk seems like a better approach and this is similar to how GraphvizPlugin works. Some code review will be done in the future to compare this implementation with that of the GraphvizPlugin.
    Minor changes from the original patch are:
     * The cache directory can be specified in `trac.ini`. The default is `$TRACENV/cache/plantuml`.
     * A `_read_img_from_file` method was added.
     * Minor coding style changes.
     
    The biggest concern I have with this implementation is that the cache directory is never cleaned. I'm not sure of the best way to handle this, but as a first step I'll take a look at if/how the GraphvizPlugin handles this issue.
    
    git-svn-id: https://trac-hacks.org/svn@11255 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracplantumlplugin/0.11/plantuml/macro.py
+++ tracplantumlplugin/0.11/plantuml/macro.py
@@ -1,16 +1,13 @@
 # -*- coding: utf-8 -*-
 
-import base64
 import hashlib
 import os
-import pickle
 import re
-from datetime import datetime
 from subprocess import Popen, PIPE
 
-from trac.core import *
 from trac.config import Option
-from trac.web import IRequestHandler, RequestDone
+from trac.core import implements
+from trac.web import IRequestHandler
 from trac.wiki.api import parse_args
 from trac.wiki.formatter import format_to_html, system_message
 from trac.wiki.macros import WikiMacroBase
@@ -24,8 +21,15 @@ class PlantUMLMacro(WikiMacroBase):
     
     implements(IRequestHandler)
 
-    plantuml_jar = Option("plantuml", "plantuml_jar", "", "Path to PlantUML .jar file")
-    java_bin = Option("plantuml", "java_bin", "java", "Path to the Java binary file") 
+    plantuml_jar = Option('plantuml', 'plantuml_jar', '', 'Path to PlantUML .jar file')
+    java_bin = Option('plantuml', 'java_bin', 'java', 'Path to the Java binary file')
+    img_dir = Option('plantuml', 'img_dir', 'cache/plantuml',
+                     'Path relative to project environment in which images are cached')
+    
+    def __init__(self):
+        self.abs_img_dir = os.path.join(os.path.abspath(self.env.path), self.img_dir)
+        if not os.path.isdir(self.abs_img_dir):
+            os.makedirs(self.abs_img_dir)
 
     def expand_macro(self, formatter, name, content):
         if content is None:
@@ -36,31 +40,16 @@ class PlantUMLMacro(WikiMacroBase):
             return system_message("plantuml.jar not found: %s" % self.plantuml_jar)
 
         args, _ = parse_args(content)
-        source = args[0].encode('utf-8').strip()
+        markup = args[0].encode('utf-8').strip()
+        img_id = hashlib.sha1(markup).hexdigest()
 
-        try:
-            graphs = pickle.loads(base64.b64decode(formatter.req.session.get('plantuml',"")))
-        except:
-            graphs = {}
-            
-        img_id = hashlib.sha1(source).hexdigest()            
-            
-        #Clean old images
-        for key, graph in graphs.items():
-            if (datetime.now() - graph[1]).seconds > 3600:
-                del graphs[key]              
-
-        if not graphs.has_key(img_id):
-            
+        if not self._is_img_existing(img_id):
             cmd = "%s -jar -Djava.awt.headless=true \"%s\" -charset UTF-8 -pipe" % (self.java_bin, self.plantuml_jar)
             p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-            (stdout, stderr) = p.communicate(input=source)
+            (img_data, stderr) = p.communicate(input=markup)
             if p.returncode != 0:
-                return system_message("Error running plantuml: %s" % stderr)
-
-            graphs[img_id] = (stdout, datetime.now())
-        
-        formatter.req.session['plantuml'] = base64.b64encode(pickle.dumps(graphs))
+                return system_message("Error running plantuml: %s" % stderr)            
+            self._write_img_to_file(img_id, img_data)
 
         out = "{{{\n#!html\n<img src='%s' alt='PlantUML Diagram' />\n}}}\n" % formatter.href("plantuml", id=img_id)
         return format_to_html(self.env, formatter.context, out)
@@ -70,14 +59,26 @@ class PlantUMLMacro(WikiMacroBase):
         return re.match(r'/plantuml?$', req.path_info)
     
     def process_request(self, req):
-        graphs = pickle.loads(base64.b64decode(req.session.get('plantuml', None)))
-        
-        img, tstamp = graphs[req.args.get('id')]
-
-        #file = req.args.get('file', None)
-        #tfile = open(file, "rb")
-        req.send(img, 'image/png', status=200)
-        #tfile.close()
-        #os.unlink(file)
+        img_id = req.args.get('id')
+        img_data = self._read_img_from_file(img_id)
+        req.send(img_data, 'image/png', status=200)
         return ""
-        #raise RequestDone
+
+    # Internal
+    def _get_img_path(self, img_id):
+        img_path = os.path.join(self.abs_img_dir, img_id)
+        img_path += '.png'
+        return img_path
+
+    def _is_img_existing(self, img_id):
+        img_path = self._get_img_path(img_id)
+        return os.path.isfile(img_path)
+
+    def _write_img_to_file(self, img_id, data):
+        img_path = self._get_img_path(img_id)
+        open(img_path, 'wb').write(data)
+
+    def _read_img_from_file(self, img_id):
+        img_path = self._get_img_path(img_id)
+        img_data = open(img_path, 'rb').read()
+        return img_data
