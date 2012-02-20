5011dfe20585432c06f24ca11e72dc9e970bd8b4
dkgdkg <dkgdkg@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-02-20 05:03:46 +0000

    update version number; add my name to the author list; change author e-mail to my own, since i am now the author of the majority of the plugin in addition to being responsible for it upstream
    git-svn-id: https://trac-hacks.org/svn@11290 7322e99d-02ea-0310-aa39-e9a107903beb

--- sensitiveticketsplugin/trunk/setup.py
+++ sensitiveticketsplugin/trunk/setup.py
@@ -1,12 +1,12 @@
 from setuptools import find_packages, setup
 
-version='0.11r1'
+version='0.12~rc1'
 
 setup(name='sensitivetickets',
       version=version,
       description="A trac plugin that lets you mark tickets as 'sensitive' with a check box.  Those tickets can only be seen with permission.  This plugin is based on the example plugin http://trac.edgewall.org/browser/trunk/sample-plugins/permissions/vulnerability_tickets.py",
-      author='Jeff Hammel and Sebastian Benthall',
-      author_email='jhammel@openplans.org',
+      author='Jeff Hammel, Sebastian Benthall, Rowan Wookey, Daniel Kahn Gillmor',
+      author_email='dkg@fifthhorseman.net',
       maintainer = 'Daniel Kahn Gillmor',
       maintainer_email = 'dkg@fifthhorseman.net',
       url='http://trac-hacks.org/wiki/SensitiveTicketsPlugin',
