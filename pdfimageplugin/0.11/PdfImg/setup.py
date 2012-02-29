5e5dc93cde1118c17b679e2e7116c3c93e2c81d7
ursaw <ursaw@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-02-29 19:04:40 +0000

    * added support for repository * output according #9842
    git-svn-id: https://trac-hacks.org/svn@11340 7322e99d-02ea-0310-aa39-e9a107903beb

--- pdfimageplugin/0.11/PdfImg/setup.py
+++ pdfimageplugin/0.11/PdfImg/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name = 'PdfImg',
-    version = '0.0.2',
+    version = '0.0.3',
 
     packages = ['PdfImg'],
     include_package_data = True,
@@ -14,11 +14,11 @@ setup(
 
     author = "Urs Wihlfahrt",
     author_email = "trac-hacks@wihlfahrt.net",
-    description = "Display PDF-Files and vectorgraphics as images in wiki-pages",
-    long_description = " Insert PDFs or vectorgraphics like SVGs as PNG-Image into a wikipage. \
-    The handling and the parameters are inspired by LaTeX includegraphics. ",
+    description = "Display PDF-files and vector graphics as images in wiki-pages",
+    long_description = "Insert PDFs or vector graphics like SVGs as PNG-Image into a wiki-page. \
+    The handling and the parameters are inspired by LaTeX includegraphics and the trac ImageMacro.",
     license = "GPLv2",
-    keywords = "trac 0.11 0.12 pdf svg vectorgraphics ",
+    keywords = "trac 0.11 0.12 pdf svg vectorgraphics",
     url = "http://trac-hacks.org/wiki/PdfImagePlugin",
     entry_points = {
         'trac.plugins': [
