6ee10e30b92d008ec8fff38403a5be1eccf71250
hasienda <hasienda@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-02-29 20:49:12 +0000

    AccountManagerPlugin: Don't give away account/user details without elevated permission, refs #6616 and #9852.
    `USER_VIEW` permission is required, where anonymous users could learn about
    sensitive information like existing accounts/users.  This permission
    shouldn't be granted lightly in publicly available Trac applications,
    because it has the potential to encourage efficient brute-force attacks
    without the need to guess existing accounts.
    
    git-svn-id: https://trac-hacks.org/svn@11347 7322e99d-02ea-0310-aa39-e9a107903beb

--- accountmanagerplugin/trunk/acct_mgr/macros.py
+++ accountmanagerplugin/trunk/acct_mgr/macros.py
@@ -2,25 +2,37 @@
 #
 # Copyright (C) 2012 Steffen Hoffmann <hoff.st@web.de>
 
-from genshi.builder import tag
+from genshi.builder import Markup, tag
 from pkg_resources import resource_filename
 
 from trac.core import Component, implements
-from trac.perm import PermissionSystem
+from trac.perm import IPermissionRequestor, PermissionSystem
 from trac.util.compat import sorted
 from trac.web.chrome import Chrome, ITemplateProvider
 from trac.wiki.api import IWikiMacroProvider, parse_args
 from trac.wiki.formatter import format_to_oneliner
 
 from acct_mgr.admin import fetch_user_data
-from acct_mgr.api import AccountManager
+from acct_mgr.api import AccountManager, _
 from acct_mgr.guard import AccountGuard
 from acct_mgr.util import get_pretty_dateinfo
+
+
+MSG_NO_PERM = tag.p(Markup(_("(required %(perm)s missing)",
+                             perm=tag.strong('USER_VIEW'))), class_='hint')
+
 
 class AccountManagerWikiMacros(Component):
     """Provides wiki macros related to Trac accounts/authenticated users."""
 
-    implements(ITemplateProvider, IWikiMacroProvider)
+    implements(IPermissionRequestor, ITemplateProvider, IWikiMacroProvider)
+
+    # IPermissionRequestor methods
+
+    def get_permission_actions(self):
+        action = 'USER_VIEW'
+        actions = [action, ('ACCTMGR_USER_ADMIN', action)]
+        return actions
 
     # ITemplateProvider methods
 
@@ -91,6 +103,8 @@ parameters are:
                 else:
                     users = list(set(users) - set(locked))
             elif 'visit' in kw.keys() or 'visit' in args:
+                if not 'USER_VIEW' in req.perm:
+                    return MSG_NO_PERM
                 cols = []
                 data = {}
                 data['accounts'] = fetch_user_data(env, req)
@@ -104,6 +118,8 @@ parameters are:
                        req, 'user_table.html', data, 'text/html', True)
             if kw.get('format') == 'count' or 'count' in args:
                 return tag(len(users))
+            if not 'USER_VIEW' in req.perm:
+                return MSG_NO_PERM
             if 'email' in args or 'name' in args:
                 # Replace username with full name, add email if available.
                 for username, name, email in self.env.get_known_users():
