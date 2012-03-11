594e60d38956c346e4844a5ae48b76b4e2e96ad6
martin_s <martin_s@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-03-11 10:43:50 +0000

    Reverted minimising of CSS/JS files.
    git-svn-id: https://trac-hacks.org/svn@11380 7322e99d-02ea-0310-aa39-e9a107903beb

--- watchlistplugin/0.12/tracwatchlist/htdocs/js/orderadd.js
+++ watchlistplugin/0.12/tracwatchlist/htdocs/js/orderadd.js
@@ -1,4 +1,72 @@
-/* Javascript code for Trac Watchlist Plugin
+/*! Javascript code for Trac Watchlist Plugin
  * $Id$
  * */
-function orderadd_buttons(b,c,a){$(b).find("button[name=up]").click(function(){$(b).data("modified",1);$(c).find("option:selected").each(function(){prev=$(this).prev();$(this).insertBefore(prev)})});$(b).find("button[name=down]").click(function(){$(b).data("modified",1);$(c).find("option:selected").each(function(){next=$(this).next();$(this).insertAfter(next)})});$(b).find("button[name=right]").click(function(){$(b).data("modified",1);$(a).append($(c).find("option:selected"))});$(b).find("button[name=left]").click(function(){$(b).data("modified",1);$(c).append($(a).find("option:selected"))})}function wlprefsubmit(){}function wlresettodefault(){}$(document).ready(function(){$("#preferences form").submit(function(){$(".orderadd").each(function(){var a=[];$(this).find("select.active_fields option").each(function(){a.push($(this).val())});$(this).find("input.choosen_fields").val(a.join(","))});wlprefsubmit();return true});$("#preferences form input[type=reset]").click(function(){$(".orderadd select").each(function(){$(this).empty();$(this).append($(this).data("original options"))});return true});$("#preferences form #resettodefault").click(function(){form=$("#preferences form");$(form).find("input[name=action]").val("defaultsettings");wlresettodefault();$(form).submit();return false});$(".orderadd select").each(function(){$(this).data("original options",$(this).find("option").clone())});$(".orderadd").each(function(){realm=$(this).attr("id").replace("_fields","");$(this).data("realm",realm);select1=$(this).find("select.active_fields");select2=$(this).find("select.available_fields");orderadd_buttons(this,select1,select2)})});
+
+function orderadd_buttons(orderadd, select1, select2 ) {
+  $(orderadd).find("button[name=up]").click(function(){
+    $(orderadd).data('modified', 1);
+    $(select1).find("option:selected").each(function(){
+      prev = $(this).prev();
+      $(this).insertBefore(prev);
+    });
+  });
+  $(orderadd).find("button[name=down]").click(function(){
+    $(orderadd).data('modified', 1);
+    $(select1).find("option:selected").each(function(){
+      next = $(this).next();
+      $(this).insertAfter(next);
+    });
+  });
+  $(orderadd).find("button[name=right]").click(function(){
+    $(orderadd).data('modified', 1);
+    $(select2).append( $(select1).find("option:selected") );
+  });
+  $(orderadd).find("button[name=left]").click(function(){
+    $(orderadd).data('modified', 1);
+    $(select1).append( $(select2).find("option:selected") );
+  });
+}
+
+// The following functions will be overwritten by 'dynamictables.js' if loaded
+function wlprefsubmit(){}
+function wlresettodefault(){}
+///
+
+$(document).ready(function(){
+  $("#preferences form").submit(function(){
+    $(".orderadd").each(function(){
+      var text = [];
+      $(this).find("select.active_fields option").each(function(){
+        text.push( $(this).val() );
+      });
+      $(this).find("input.choosen_fields").val( text.join(',') );
+    });
+    wlprefsubmit();
+    return true;
+  });
+  $("#preferences form input[type=reset]").click(function(){
+    $(".orderadd select").each(function(){
+        $(this).empty();
+        $(this).append( $(this).data('original options') );
+    });
+    return true;
+  });
+  $("#preferences form #resettodefault").click(function(){
+    form = $("#preferences form");
+    $(form).find("input[name=action]").val("defaultsettings");
+    wlresettodefault();
+    $(form).submit();
+    return false;
+  });
+  $(".orderadd select").each(function(){
+      $(this).data('original options', $(this).find("option").clone() );
+  });
+  $(".orderadd").each(function(){
+    realm = $(this).attr("id").replace("_fields","");
+    $(this).data('realm', realm);
+    select1 = $(this).find("select.active_fields");
+    select2 = $(this).find("select.available_fields");
+    orderadd_buttons(this, select1, select2 );
+  });
+});
+
--- watchlistplugin/0.12/tracwatchlist/htdocs/js/watchlist.js
+++ watchlistplugin/0.12/tracwatchlist/htdocs/js/watchlist.js
@@ -1,7 +1,108 @@
-/* Javascript code for Trac Watchlist Plugin 
+/*! Javascript code for Trac Watchlist Plugin 
  * $Id$
  * */
-/* From http://chris-barr.com/entry/disable_text_selection_with_jquery/  */
-$(function(){$.extend($.fn.disableTextSelect=function(){return this.each(function(){if($.browser.mozilla){$(this).css("MozUserSelect","none")}else{if($.browser.msie){$(this).bind("selectstart",function(){return false})}else{$(this).mousedown(function(){return false})}}})});$(".noSelect").disableTextSelect()});
-/* */
-function wlremoveempty(b){tdiv=$(b).parents("div.watchlist-parent").get(0);pdiv=$(tdiv).parent("div");$(tdiv).remove();$(pdiv).find("h2").removeClass("foldable");$(pdiv).find("p.noentrymessage").css("display","block");wlremmsgbox()}function wldeleterow(c,b){$(c).remove()}function wlresettodefault(){}function wlremmsgbox(){$("#message-box").remove()}jQuery(document).ready(function(){$("input.ackmsg").each(function(){$(this).disableTextSelect();$(this).click(function(){$(this).parents("div.system-message").hide(5).remove();return false})});$("td.unwatch a.plainlink").each(function(){$(this).click(function(){tr=$(this).parents("tr").get(0);table=$(tr).parents("table").get(0);$.ajax({url:$(this).attr("href")+"&async=true",success:function(b,c){wldeleterow(tr,table)}});return false})});$("td.notify a.plainlink").each(function(){$(this).click(function(){a=this;chkbox=$(this).children("input");$.ajax({url:$(this).attr("href")+"&async=true",success:function(b,c){if($(chkbox).attr("checked")){$(a).attr("href",$(a).attr("href").replace("action=notifyoff","action=notifyon"));$(chkbox).removeAttr("checked")}else{$(a).attr("href",$(a).attr("href").replace("action=notifyon","action=notifyoff"));$(chkbox).attr("checked","checked")}}});return false})});$(".foldable").enableFolding(false,false,true);$("a.newwindow").click(function(){window.open(this.href);return false});$("td a.more").click(function(){$(this).hide();$(this).parent().find(".moretext,.less").show()});$("td a.less").click(function(){$(this).hide();$(this).parent().find(".moretext").hide();$(this).parent().find(".more").show()});$("div#ctxtnav a").click(function(){$($(this).attr("href")+" div.collapsed").removeClass("collapsed");return true});if(location.hash){$(location.hash+" div.collapsed").removeClass("collapsed")}});
+
+
+/*! From http://chris-barr.com/entry/disable_text_selection_with_jquery/  */
+$(function(){
+  $.extend($.fn.disableTextSelect = function() {
+    return this.each(function(){
+      if($.browser.mozilla){//Firefox
+        $(this).css('MozUserSelect','none');
+      }else if($.browser.msie){//IE
+        $(this).bind('selectstart',function(){return false;});
+      }else{//Opera, etc.
+        $(this).mousedown(function(){return false;});
+      }
+    });
+  });
+  $('.noSelect').disableTextSelect();//No text selection on elements with a class of 'noSelect'
+});
+/*! */
+
+function wlremoveempty( child ) {
+    tdiv = $(child).parents('div.watchlist-parent').get(0);
+    pdiv = $(tdiv).parent("div");
+    $(tdiv).remove();
+    $(pdiv).find('h2').removeClass('foldable');
+    $(pdiv).find('p.noentrymessage').css('display','block');
+    wlremmsgbox();
+}
+
+// The following functions will be overwritten by 'dynamictables.js' if loaded
+function wldeleterow(tr, table) {
+  $(tr).remove();
+}
+
+function wlresettodefault() {}
+///
+
+function wlremmsgbox() {
+  $('#message-box').remove();
+}
+
+jQuery(document).ready(function() {
+  $("input.ackmsg").each(function() {
+    $(this).disableTextSelect();
+    $(this).click(function() {
+      $(this).parents('div.system-message').hide(5).remove();
+      return false;
+    });
+  });
+
+  $("td.unwatch a.plainlink").each(function() {
+    $(this).click(function() {
+        tr    = $(this).parents('tr').get(0);
+        table = $(tr).parents('table').get(0);
+        $.ajax({ url: $(this).attr('href') + '&async=true', success: function (data, textStatus) {
+            wldeleterow(tr, table);
+          }
+        });
+        return false;
+    });
+  });
+  $("td.notify a.plainlink").each(function() {
+    $(this).click(function() {
+        a = this;
+        chkbox = $(this).children('input');
+        $.ajax({ url: $(this).attr('href') + '&async=true', success: function (data, textStatus) {
+          if ($(chkbox).attr('checked')) {
+            $(a).attr('href', $(a).attr('href').replace('action=notifyoff', 'action=notifyon') )
+            $(chkbox).removeAttr('checked');
+          }
+          else {
+            $(a).attr('href', $(a).attr('href').replace('action=notifyon', 'action=notifyoff') )
+            $(chkbox).attr('checked','checked');
+          }
+        } } );
+        return false;
+    });
+  });
+
+  $(".foldable").enableFolding(false, false, true);
+//  $("table.watchlist").each(function() { wlchecktable(this); });
+
+  $("a.newwindow").click(function(){
+      window.open(this.href);
+      return false;
+  });
+  $("td a.more").click(function(){
+      $(this).hide();
+      $(this).parent().find(".moretext,.less").show();
+  });
+  $("td a.less").click(function(){
+      $(this).hide();
+      $(this).parent().find(".moretext").hide();
+      $(this).parent().find(".more").show();
+  });
+  // Unfold section if context navigation link to it was used:
+  $("div#ctxtnav a").click(function(){
+      $( $(this).attr("href") + ' div.collapsed' ).removeClass("collapsed");
+      return true;
+  });
+  // Unfold section if targeted directly by the URL
+  if ( location.hash ) {
+    $( location.hash + ' div.collapsed' ).removeClass("collapsed");
+  }
+});
+
