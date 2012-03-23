92b819fea309bbbf040e4bbdc1ba632b08a456fb
ChrisNelson <ChrisNelson@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-03-23 17:52:20 +0000

    Up version to 0.10.  Refs #9648.
    git-svn-id: https://trac-hacks.org/svn@11429 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracjsganttplugin/0.11/setup.py
+++ tracjsganttplugin/0.11/setup.py
@@ -8,7 +8,7 @@ setup(
     author = 'Chris Nelson',
     author_email = 'Chris.Nelson@SIXNET.com',
     description = 'Trac plugin displaying jsGantt charts in Trac',
-    version = '0.9',
+    version = '0.10',
     url = 'http://trac-hacks.org/wiki/TracJsGanttPlugin',
     license='BSD',
     packages=['tracjsgantt'],
