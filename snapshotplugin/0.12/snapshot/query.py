043fb4789edb57248ecf67f3707e810090fafac2
matobaa <matobaa@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-04-12 14:56:13 +0000

    SnapshowPlugin: Short style query string for page name; to avoid question-mark.
    git-svn-id: https://trac-hacks.org/svn@11481 7322e99d-02ea-0310-aa39-e9a107903beb

--- snapshotplugin/0.12/snapshot/query.py
+++ snapshotplugin/0.12/snapshot/query.py
@@ -24,7 +24,9 @@ a \"page_name\" URL Parameter uses for default name if exists."""
     def filter_stream(self, req, method, filename, stream, data):
         if filename != 'query.html':
             return stream
-        query_string = data['query'].to_string().replace("\n", "")
+        query_string = 'query:' + '&'.join(['%s=%s' % (key, '|'.join([cond for cond in values])) 
+                                            for constraint in data['query'].constraints
+                                            for key, values in constraint.items()])
         page_name = 'report_resource' in data and \
             'report:%s' % data['report_resource'].id or query_string 
         if 'page_name' in req.args:
@@ -40,7 +42,7 @@ a \"page_name\" URL Parameter uses for default name if exists."""
         for (group_name, tickets) in data['groups']:
             text += '|| group: %s\n' % group_name
             for ticket in tickets:
-                text += '|| %s || %s\n' % ( ticket['href'] ,
+                text += '|| %s || %s\n' % (ticket['href'] ,
                     ' || '.join([self.formatter(ticket[col]) for col in cols]))
         text += '}}}'
         div = tag.div(tag.input(value='Save as wiki:', type='submit'),
@@ -85,8 +87,8 @@ class Report(Component):
                 ticket = {}
                 for value in row:
                     ticket[value['header']['col']] = value['value']
-                ticket['href'] = get_resource_url(self.env, Resource('ticket',ticket.get('ticket',0)), self.env.href)
-                text += '|| %s || %s\n' % ( ticket['href'] ,
+                ticket['href'] = get_resource_url(self.env, Resource('ticket', ticket.get('ticket', 0)), self.env.href)
+                text += '|| %s || %s\n' % (ticket['href'] ,
                     ' || '.join([self.formatter(col, ticket[col]) for col in cols]))
         text += '}}}'
         div = tag.div(tag.input(value='Save as wiki:', type='submit'),
