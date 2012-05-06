d507ef0801cadd071bb44037008eb705b32c658c
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-05-06 23:09:30 +0000

    Refs #4162: Created 0.11 branch from r10462.
    git-svn-id: https://trac-hacks.org/svn@11534 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ includemacro/branches/0.11/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+import os.path
+
+from setuptools import setup
+
+setup(
+    name = 'TracIncludeMacro',
+    version = '2.1',
+    packages = ['includemacro'],
+
+    author = 'Noah Kantrowitz',
+    author_email = 'noah@coderanger.net',
+    description = 'Include the contents of external URLs and other Trac objects in a wiki page.',
+    long_description = open(os.path.join(os.path.dirname(__file__), 'README')).read(),
+    license = 'BSD',
+    keywords = 'trac 0.11 plugin wiki include macro',
+    url = 'http://trac-hacks.org/wiki/IncludeMacro',
+    classifiers = [
+        'Framework :: Trac',
+    ],
+    
+    install_requires = ['Trac'],
+
+    entry_points = {
+        'trac.plugins': [
+            'includemacro.macros = includemacro.macros',
+        ]
+    }
+)
