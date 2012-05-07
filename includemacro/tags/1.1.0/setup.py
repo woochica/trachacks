a8b50ffc00f0a39249a9b31b9424d397be3a4879
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-05-07 03:20:08 +0000

    Tagging 1.1.0.
    git-svn-id: https://trac-hacks.org/svn@11538 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ includemacro/tags/1.1.0/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+
+from setuptools import setup
+
+setup(
+    name = 'TracIncludeMacro',
+    version = '1.1',
+    packages = ['includemacro'],
+
+    author = 'Noah Kantrowitz',
+    author_email = 'noah@coderanger.net',
+    description = 'Include the contents of external URLs and other Trac objects in a wiki page.',
+    license = 'BSD',
+    keywords = 'trac 0.10 plugin wiki include macro',
+    url = 'http://trac-hacks.org/wiki/IncludeMacro',
+    classifiers = [
+        'Framework :: Trac',
+    ],
+
+    entry_points = {
+        'trac.plugins': [
+            'includemacro.macros = includemacro.macros',
+        ]
+    }
+)
