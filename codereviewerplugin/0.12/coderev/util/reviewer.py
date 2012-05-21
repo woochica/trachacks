316e79c37c059badb8ca6ab28c14eceb6b194d92
robguttman <robguttman@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-05-21 21:54:57 +0000

    reviewer.py enhancement (in support of nagios checks).
    git-svn-id: https://trac-hacks.org/svn@11591 7322e99d-02ea-0310-aa39-e9a107903beb

--- codereviewerplugin/0.12/coderev/util/reviewer.py
+++ codereviewerplugin/0.12/coderev/util/reviewer.py
@@ -12,39 +12,54 @@ class Reviewer(object):
     been fully reviewed.  Works in conjunction with the Trac CodeReviewer
     plugin and its database tables."""
     
-    def __init__(self, trac_env, repo_dir, target_ref, data_file):
+    def __init__(self, trac_env, repo_dir, target_ref, data_file, verbose=True):
         self.env = Environment(trac_env)
         self.repo_dir = repo_dir.rstrip('/')
-        self.reponame = os.path.basename(repo_dir).lower()
+        self.reponame = os.path.basename(self.repo_dir).lower()
         self.target_ref = target_ref
         self.data_file = data_file
+        self.verbose = verbose
     
     def get_next_changeset(self, save=True):
-        """Return the next changeset and saves it as the current changeset
-        when save is True."""
-        current_ref = self.get_current_changeset()
-        
-        # extract changesets in order from current to target ref
-        cmds = ['cd %s' % self.repo_dir,
-                'git rev-list --reverse %s..%s' % (current_ref,self.target_ref)]
-        changesets = self._execute(' && '.join(cmds)).splitlines()
-        print "\n%d changesets from current %s to target %s" % \
-                (len(changesets),current_ref,self.target_ref)
-        
-        # find last reviewed
-        next = current_ref
-        for changeset in changesets:
-            print '.',
+        """Return the next reviewed changeset and save it as the current
+        changeset when save is True."""
+        next = self.get_current_changeset()
+        for changeset in self._get_changesets():
+            if self.verbose:
+                print '.',
             if not self.is_reviewed(changeset):
                 return self.set_current_changeset(next, save)
             next = changeset
         return self.set_current_changeset(next, save)
+    
+    def get_blocking_changeset(self):
+        """Return the next blocking changeset."""
+        # find last *not* reviewed
+        for changeset in self._get_changesets():
+            if not self.is_reviewed(changeset):
+                return changeset
+        return None
+    
+    def _get_changesets(self):
+        """Extract changesets in order from current to target ref."""
+        current_ref = self.get_current_changeset()
+        cmds = ['cd %s' % self.repo_dir,
+                'git rev-list --reverse %s..%s' % (current_ref,self.target_ref)]
+        changesets = self._execute(' && '.join(cmds)).splitlines()
+        if self.verbose:
+            print "\n%d changesets from current %s to target %s" % \
+                    (len(changesets),current_ref,self.target_ref)
+        return changesets
+    
+    def get_review(self, changeset):
+        """Return all tickets referenced directly by a changeset."""
+        return CodeReview(self.env, self.reponame, changeset)
         
     def is_reviewed(self, changeset):
         """Returns True if all of the given changeset's tickets are fully
         reviewed.  Fully reviewed means that the ticket has no pending
         reviews and the last review has passed."""
-        review = CodeReview(self.env, self.reponame, changeset)
+        review = self.get_review(changeset)
         for ticket in review.tickets:
             if not self._is_fully_reviewed(ticket):
                 return False
@@ -63,14 +78,16 @@ class Reviewer(object):
         for review in CodeReview.get_reviews(self.env, ticket):
             # are any in pending?
             if review.encode(review.status) == 'PENDING':
-                print "\nticket #%s has a PENDING review" % ticket,
-                print "for changeset %s" % review.changeset
+                if self.verbose:
+                    print "\nticket #%s has a PENDING review" % ticket,
+                    print "for changeset %s" % review.changeset
                 return False
             
         # has last review passed?
         if review and review.encode(review.status) != "PASSED":
-            print "\nticket #%s's last changeset %s = %s (not PASSED)" % \
-                    (ticket,review.changeset,review.status)
+            if self.verbose:
+                print "\nticket #%s's last changeset %s = %s (not PASSED)" % \
+                        (ticket,review.changeset,review.status)
             return False
         
         return True
@@ -92,8 +109,9 @@ class Reviewer(object):
             if data['current'] != changeset:
                 data['current'] = changeset
                 self._set_data(data)
-                print "setting current changeset to %s" % changeset
-            else:
+                if self.verbose:
+                    print "setting current changeset to %s" % changeset
+            elif self.verbose:
                 print "current changeset already is %s" % changeset
         return changeset
     
