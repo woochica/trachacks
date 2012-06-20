1d2b2dddc8b95b4436562f3dcfa828b7cc9d55a3
thomasd <thomasd@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-06-20 03:52:52 +0000

    - added BSD-license (refs #10105)  - added maintainer falkb
    git-svn-id: https://trac-hacks.org/svn@11665 7322e99d-02ea-0310-aa39-e9a107903beb

--- groupticketfieldsplugin/0.12/trunk/setup.py
+++ groupticketfieldsplugin/0.12/trunk/setup.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2012 Thomas Doering
+# All rights reserved.
 #
-from setuptools import setup
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.from setuptools import setup
 
 setup(
     name='GroupTicketFields',
@@ -17,6 +20,9 @@ setup(
     },
     author = 'thomasd',
     author_email='tdoering@baumer.com',
+    maintainer = 'falkb',
+    maintainer_email='fbrettschneider@baumer.com',
+    license = "BSD 3-Clause",
     description='Group Ticket Fields',
     long_description='Group Ticket Fields',
     keywords='Group Ticket Fields',
