cc07906f64f3cc7714f920f8bc6bbb963ee61cae
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-06-20 18:21:52 +0000

    Refs #9773: Removed unused imported of `sets` module, which was resulting in a deprecation warning with Python 2.6.
    git-svn-id: https://trac-hacks.org/svn@11674 7322e99d-02ea-0310-aa39-e9a107903beb

--- downloadsplugin/0.11/tracdownloads/tags.py
+++ downloadsplugin/0.11/tracdownloads/tags.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import sets
-
 from tracdownloads.api import *
 from trac.core import *
 from trac.resource import *
--- downloadsplugin/0.12/tracdownloads/tags.py
+++ downloadsplugin/0.12/tracdownloads/tags.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Deprecated as for Python 2.6.
-try:
-    import sets
-except:
-    pass
-
 # Trac imports.
 from trac.core import *
 from trac.resource import Resource
