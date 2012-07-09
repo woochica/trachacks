d5626484dcae3c2d1a741968b11108ef784af2c0
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-07-09 12:52:45 +0000

    Refs #10107: Branching to support Trac 1.0. Bump DateFieldPlugin version to `3.0.0dev`.
    git-svn-id: https://trac-hacks.org/svn@11717 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ datefieldplugin/1.0/datefield/htdocs/js/datefield.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function($) {
+    jQuery('.datepick').datepicker({
+        firstDay: datefield['first_day'],
+        dateFormat: datefield['format'],
+        showOn: "both",
+        weekHeader: 'W',
+        showWeek: datefield['show_week'],
+        showButtonPanel: datefield['show_panel'],
+        numberOfMonths: datefield['num_months'],
+        changeMonth: datefield['change_month'],
+        changeYear: datefield['change_year'],
+        buttonImage: datefield['calendar'],
+        buttonImageOnly: true
+    });
+});
---
+++ datefieldplugin/1.0/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+
+from setuptools import setup
+
+setup(
+    name = 'TracDateField',
+    version = '3.0.0',
+    packages = ['datefield'],
+    package_data = { 'datefield': ['htdocs/css/*.css', 
+        'htdocs/js/*.js', 'htdocs/css/images/*.png' ] },
+    author = 'Noah Kantrowitz',
+    author_email = 'noah@coderanger.net',
+    description = 'Add custom date fields to Trac tickets.',
+    license = 'BSD',
+    keywords = 'trac plugin ticket',
+    url = 'http://trac-hacks.org/wiki/DateFieldPlugin',
+    classifiers = [
+        'Framework :: Trac',
+    ],
+    install_requires = ['Trac >= 0.12'],
+    entry_points = {
+        'trac.plugins': [
+            'datefield.filter = datefield.filter',
+        ]
+    },
+)
