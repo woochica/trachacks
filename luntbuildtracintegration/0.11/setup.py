f6e001303c879d6b4472b3f09de4d0a1bdbaf23a
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-07-13 00:13:33 +0000

    Refs #10108: Reorganized repository to trunk/branches structure and bumped version.
    git-svn-id: https://trac-hacks.org/svn@11723 7322e99d-02ea-0310-aa39-e9a107903beb

--- luntbuildtracintegration/0.11/setup.py
+++ luntbuildtracintegration/0.11/setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='LuntbuildTracIntegration', version='1.1',
+    name='LuntbuildTracIntegration', version='1.2',
     author="David Roussel",
     description="A trac plugin to add Luntbuild builds to the trac timeline",
     packages=find_packages(exclude=['*.tests*']),
