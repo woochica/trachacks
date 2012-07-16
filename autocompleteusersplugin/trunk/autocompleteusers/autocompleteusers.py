6127ffa5328daef2fae3b9e60a2436be3b419e0c
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-07-16 01:46:49 +0000

    Refactoring to support transition to jQuery UI 1.8.
    git-svn-id: https://trac-hacks.org/svn@11729 7322e99d-02ea-0310-aa39-e9a107903beb

--- autocompleteusersplugin/trunk/autocompleteusers/autocompleteusers.py
+++ autocompleteusersplugin/trunk/autocompleteusers/autocompleteusers.py
@@ -16,14 +16,15 @@ from trac.web.chrome import (
 
 import fnmatch
 
+USER=0; NAME=1; EMAIL=2 # indices
+
 class AutocompleteUsers(Component):
 
     implements(IRequestFilter, IRequestHandler,
                ITemplateProvider, ITemplateStreamFilter)
-    
+
     selectfields = ListOption('autocomplete', 'fields', default='',
                               doc='select fields to transform to autocomplete text boxes')
-    prefix = 'autocompleteusers' # prefix for htdocs: /chrome/prefix/...    
 
     # IRequestHandler methods
 
@@ -31,20 +32,71 @@ class AutocompleteUsers(Component):
         return req.path_info.rstrip('/') == '/users'
 
     def process_request(self, req):
+        
+        users = self._get_users(req)
 
-        query = req.args.get('q', '').lower() 
+        users = ['%s|%s|%s' % (user[USER],
+                               user[EMAIL] and '&lt;%s&gt; ' % user[EMAIL] or '',
+                               user[NAME])
+                 for value, user in sorted(users)] # value unused (placeholder need for sorting)
 
-        ### user names, email addresses, full names
-        users = []
-        USER=0; NAME=1; EMAIL=2 # indices 
+        req.send('\n'.join(users).encode('utf-8'), 'text/plain')
 
+    # ITemplateProvider methods
+
+    def get_htdocs_dirs(self):
+        from pkg_resources import resource_filename
+        return [('autocomplete', resource_filename(__name__, 'htdocs'))]
+
+    def get_templates_dirs(self):
+        return []
+
+    # IRequestFilter methods
+
+    def pre_process_request(self, req, handler):
+        return handler
+
+    def post_process_request(self, req, template, data, content_type):
+        if template == 'ticket.html':
+            add_stylesheet(req, 'autocomplete/css/autocomplete.css')
+            add_script(req, 'autocomplete/js/autocomplete.js')
+            add_script(req, 'autocomplete/js/format_item.js')
+            restrict_owner = self.env.config.getbool('ticket', 'restrict_owner')
+            if req.path_info.rstrip() == '/newticket':
+                add_script(req, 'autocomplete/js/autocomplete_newticket_cc.js')
+                if not restrict_owner:
+                    add_script(req, 'autocomplete/js/autocomplete_newticket.js')
+            else:
+                add_script(req, 'autocomplete/js/autocomplete_ticket_cc.js')
+                if not restrict_owner:
+                    add_script(req, 'autocomplete/js/autocomplete_ticket.js')
+        return template, data, content_type
+
+    # ITemplateStreamFilter methods
+
+    def filter_stream(self, req, method, filename, stream, data):
+        if filename == 'ticket.html':
+            fields = [field['name'] for field in data['ticket'].fields 
+                      if field['type'] == 'select']
+            fields = set(sum([fnmatch.filter(fields, pattern)
+                              for pattern in self.selectfields], []))
+            
+        return stream
+    
+    # Private methods
+    
+    def _get_users(self, req):
         # instead of known_users, could be
         # perm = PermissionSystem(self.env)
         # owners = perm.get_users_with_permission('TICKET_MODIFY')
         # owners.sort()
         # see: http://trac.edgewall.org/browser/trunk/trac/ticket/default_workflow.py#L232
 
-        for user_data in self.env.get_known_users(): 
+        query = req.args.get('q', '').lower()
+
+        # user names, email addresses, full names
+        users = []
+        for user_data in self.env.get_known_users():
             user_data = [user is not None and Chrome(self.env).format_author(req, user) or ''
                          for user in user_data]
             for index, field in enumerate((USER, EMAIL, NAME)): # ordered by how they appear
@@ -59,52 +111,4 @@ class AutocompleteUsers(Component):
                         users.append((2-index, user_data)) # 2-index is the sort key
                         break
 
-            
-        users = ['%s|%s|%s' % (user[USER], 
-                               user[EMAIL] and '&lt;%s&gt; ' % user[EMAIL] or '',
-                               user[NAME])
-                 for value, user in sorted(users)] # value unused (placeholder need for sorting)
-
-        req.send('\n'.join(users).encode('utf-8'), 'text/plain')
-
-
-    # ITemplateProvider methods
-
-    def get_htdocs_dirs(self):
-        from pkg_resources import resource_filename
-        return [(self.prefix, resource_filename(__name__, 'htdocs'))]
-
-    def get_templates_dirs(self):
-        return []
-
-    # IRequestFilter methods
-
-    def pre_process_request(self, req, handler):
-        return handler
-
-    def post_process_request(self, req, template, data, content_type):
-        if template == 'ticket.html':
-            add_stylesheet(req, '%s/css/autocomplete.css' % self.prefix)
-            add_script(req, '%s/js/autocomplete.js' % self.prefix)
-            add_script(req, '%s/js/format_item.js' % self.prefix)
-            restrict_owner = self.env.config.getbool('ticket', 'restrict_owner')
-            if req.path_info.rstrip() == '/newticket':
-                add_script(req, '%s/js/autocomplete_newticket_cc.js' % self.prefix)
-                if not restrict_owner:
-                    add_script(req, '%s/js/autocomplete_newticket.js' % self.prefix)
-            else:
-                add_script(req, '%s/js/autocomplete_ticket_cc.js' % self.prefix)
-                if not restrict_owner:
-                    add_script(req, '%s/js/autocomplete_ticket.js' % self.prefix)
-        return template, data, content_type
-
-    # ITemplateStreamFilter methods
-
-    def filter_stream(self, req, method, filename, stream, data):
-        if filename == 'ticket.html':
-            fields = [field['name'] for field in data['ticket'].fields 
-                      if field['type'] == 'select']
-            fields = set(sum([fnmatch.filter(fields, pattern)
-                              for pattern in self.selectfields], []))
-            
-        return stream
+        return users
