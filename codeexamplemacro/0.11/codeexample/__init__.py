db77e6ae2a2bad3ae7fd7f8c0164790b224cfc98
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-07-25 07:01:47 +0000

    Part of [11766].
    git-svn-id: https://trac-hacks.org/svn@11775 7322e99d-02ea-0310-aa39-e9a107903beb

--- codeexamplemacro/0.11/codeexample/__init__.py
+++
@@ -1,1 +0,0 @@
-from code_example_processor import *
--- codeexamplemacro/0.11/codeexample/tests/__init__.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from codeexample.tests import test_all
-
-
-def suite():
-    suite = unittest.TestSuite()
-    suite.addTest(test_all.suite())
-    return suite
