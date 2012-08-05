f9d28a0f9da6e733c9d96d63dbca2d54b1103a9e
jonny <jonny@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-08-05 14:49:21 +0000

    Initial checkin.
    git-svn-id: https://trac-hacks.org/svn@11903 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ tableplugin/0.11/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+PACKAGE = 'TablePlugin'
+VERSION = 0.1
+
+setup(name=PACKAGE,
+      version=VERSION,
+      packages=['table'],
+      entry_points = """
+      [trac.plugins]
+      table = table
+      """,
+)
