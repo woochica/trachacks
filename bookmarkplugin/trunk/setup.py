7c3476624dff3cf41b34a9b8420fc05dd2b5dc6e
saigon <saigon@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-08-06 12:16:49 +0000

    fixes #10205 clarify license BSD to 3-clause BSD
    git-svn-id: https://trac-hacks.org/svn@11907 7322e99d-02ea-0310-aa39-e9a107903beb

--- bookmarkplugin/trunk/setup.py
+++ bookmarkplugin/trunk/setup.py
@@ -13,7 +13,7 @@ setup(
     author='Yoshiyuki Sugimoto',
     maintainer = 'Yoshiyuki Sugimoto',
     maintainer_email = 's.yosiyuki@gmail.com',
-    license='BSD',
+    license='3-clause BSD',
     url='http://trac-hacks.org/wiki/BookmarkPlugin',
     description='A plugin bookmark Trac resources.',
     entry_points = {'trac.plugins': ['tracbookmark = tracbookmark']},
