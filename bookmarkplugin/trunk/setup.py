189f0a479aca4a90b86363a3b4bcfc5c2b7932c9
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-08-06 22:34:02 +0000

    Refs #10205: Added copy of 3-Clause BSD license and added headers to every Python source file.
    git-svn-id: https://trac-hacks.org/svn@11908 7322e99d-02ea-0310-aa39-e9a107903beb

--- bookmarkplugin/trunk/setup.py
+++ bookmarkplugin/trunk/setup.py
@@ -1,4 +1,12 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+# Copyright (C) 2010-2012 Yoshiyuki Sugimoto <s.yosiyuki@gmail.com>
+# Copyright (C) 2012 Jun Omae <jun66j5@gmail.com>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
 
 from setuptools import setup
 
