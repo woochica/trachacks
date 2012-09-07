9c6c01363ddd856c3547685be21144fd2af817d4
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-09-07 00:22:03 +0000

    * Fixed issue in which there would always be a blank line at the start of the code block.  * Making use of `WikiProcessor._processor_re` and eliminated duplicate variable in `LinenoMacro` class.  * Added `setup.cfg` and configured to tag the egg with the SVN revision.  * Added an example to the documentation that is displayed on the WikiMacros page.  * Refactored `LinenoMacro.py`, removed unused imports and alphabetized methods.
    git-svn-id: https://trac-hacks.org/svn@11944 7322e99d-02ea-0310-aa39-e9a107903beb

--- linenomacro/trunk/lineno/LinenoMacro.py
+++ linenomacro/trunk/lineno/LinenoMacro.py
@@ -7,43 +7,59 @@
 # This software is licensed as described in the file COPYING, which
 # you should have received as part of this distribution.
 
-from genshi.builder import tag
-from trac.core import *
+from trac.core import implements
 from trac.mimeview.api import Mimeview
 from trac.web.chrome import ITemplateProvider, add_stylesheet
 from trac.wiki.macros import WikiMacroBase
+from trac.wiki.parser import WikiParser
 
-import inspect
-import re
-
-_processor_re = re.compile('#\!([\w+-][\w+-/]*)')
 
 class LinenoMacro(WikiMacroBase):
-    """Wiki processor that prints line-numbered code listings"""
+    """Wiki processor that prints line numbers for code blocks.
+       
+       Example:
+       
+       {{{
+       {{{
+       #!Lineno
+       #!java
+       class A {
+           public static void main(String[] args) {
+           }
+       }
+       }}}
+       }}}
+       
+       {{{
+       #!Lineno
+       #!java
+       class A {
+           public static void main(String[] args) {
+           }
+       }
+       }}}
+    """
     
     implements(ITemplateProvider)
     
-    # ITemplateProvider
+    def expand_macro(self, formatter, name, content):
+        add_stylesheet(formatter.req, 'lineno/css/lineno.css')
+        
+        mt = 'txt'
+        match = WikiParser._processor_re.match(content)
+        if match:
+            mt = match.group(1)
+            content = content[match.end(1)+1:]
+        
+        mimeview = Mimeview(formatter.env)
+        mimetype = mimeview.get_mimetype(mt) or mimeview.get_mimetype('txt')        
+        return mimeview.render(formatter.context, mimetype,
+                               content, annotations=['lineno'])
+    
+    ### ITemplateProvider
     def get_templates_dirs(self):
         return []
     
     def get_htdocs_dirs(self):
         from pkg_resources import resource_filename
         return [('lineno', resource_filename(__name__, 'htdocs'))]
-    
-    def expand_macro(self, formatter, name, content):
-        add_stylesheet(formatter.req, 'lineno/css/lineno.css')
-        mt = 'txt'
-        match = _processor_re.search(content)
-        if match:
-            mt = match.group().strip()[2:]
-            content = content[match.end():]
-        
-        mimetype = Mimeview(formatter.env).get_mimetype(mt)
-        if not mimetype:
-            mimetype = Mimeview(formatter.env).get_mimetype('txt')
-        
-        annotations = ['lineno']
-        return Mimeview(self.env).render(formatter.context,
-                                         mimetype, content,
-                                         None, None, annotations)
--- linenomacro/trunk/setup.py
+++ linenomacro/trunk/setup.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+# Copyright (C) 2008-2009 Adamansky Anton <anton@adamansky.com>
+# Copyright (C) 2012 Ryan J Ollos <ryan.j.ollos@gmail.com>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
 
 from setuptools import find_packages, setup
 
 setup(
-    name='Lineno', version='1.1',
+    name = 'Lineno',
+    version = '1.1',
     author = 'Adamansky Anton',
     author_email = 'anton@adamansky.com',
     description = 'Prints line numbered code listings',
     license = 'Apache License, Version 2.0',
     url = 'http://trac-hacks.org/wiki/LinenoMacro',
-    packages=['lineno'],
-    package_data={ 'lineno' : [ 'htdocs/css/*.css' ] },
+    packages = find_packages(exclude=['tests*']),
+    package_data = { 'lineno' : [ 'htdocs/css/*.css' ] },
     entry_points = """
         [trac.plugins]
         lineno = lineno.LinenoMacro
