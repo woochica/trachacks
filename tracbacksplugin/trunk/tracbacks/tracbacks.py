47b26d850fec0bd5279ca7f9d19b8c5dd8e1bf45
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-09-12 05:37:50 +0000

    Fixes #9809: (0.2dev) Changed license from GPLv3+ to 3-Clause BSD.
    git-svn-id: https://trac-hacks.org/svn@11980 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracbacksplugin/trunk/tracbacks/tracbacks.py
+++ tracbacksplugin/trunk/tracbacks/tracbacks.py
@@ -1,31 +1,11 @@
 # -*- coding: utf-8 -*-
+#
 # Copyright (C) 2008 The Open Planning Project
 #
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 3
-# of the License, or (at your option) any later version.
+# All rights reserved.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the
-# Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor,
-# Boston, MA  02110-1301
-# USA
-
-"""
-TracBacks Plugin
-author: Mel Chua <mchua@openplans.org>
-version: 0.1
-
-Detects when a ticket A is referenced in another ticket B and adds
-a comment "TracBack: #B" to ticket A.
-"""
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
 
 import re
 
