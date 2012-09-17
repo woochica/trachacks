875f425e056a2b63c5ec47963a9f288e96bfafbb
falkb <falkb@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-09-17 06:21:48 +0000

    Refs #10084: applied the Heiko Böttger's patch of [comment:10], reset of component if not available anymore after project switch. Thank you very much Heiko!
    git-svn-id: https://trac-hacks.org/svn@12006 7322e99d-02ea-0310-aa39-e9a107903beb

--- simplemultiprojectplugin/trunk/simplemultiproject/htdocs/filter_milestones.js
+++ simplemultiprojectplugin/trunk/simplemultiproject/htdocs/filter_milestones.js
@@ -10,7 +10,7 @@ function smp_updateSelect(id, newOptions, selectedOption)
         options = select.attr('options');
     }
     select.empty();
-    
+
     var addedOptions = [];
     if (newOptions) {
         $.each(newOptions, function(val, text) {
@@ -23,7 +23,7 @@ function smp_updateSelect(id, newOptions, selectedOption)
     if (selectedOption && $.inArray(selectedOption, addedOptions) == -1) {
         options[options.length] = new Option(selectedOption, selectedOption, true, true);
     }
-    
+
     // call custom event trigger to allow other plugins to notify the change
     $(field_id).trigger("onUpdate");
 }
@@ -46,25 +46,27 @@ function smp_onProjectChange(project)
     // components
     var components = smp_all_components;
     var cur_comp = $('#field-component option:selected').text();
-    var filtered_components = []
-    
+    var filtered_components = [];
+    var cur_comp_valid = false;
+
     for (var i = 0; i < components.length; i++) {
         var comp = components[i];
         if (!smp_component_projects[comp] || $.inArray(project, smp_component_projects[comp]) != -1) {
-            filtered_components.push(comp)
+            filtered_components.push(comp);
+            cur_comp_valid = cur_comp_valid || (comp == cur_comp);
         }
     }
 
-    if (!cur_comp || cur_comp == "") {
+    if (!cur_comp || cur_comp == "" || !cur_comp_valid) {
         cur_comp = smp_default_component;
     }
-    
+
     smp_updateSelect('component', filtered_components, cur_comp);
 
     // versions
-    smp_updateSelect('version', smp_project_versions[project], smp_default_version)
+    smp_updateSelect('version', smp_project_versions[project], smp_default_version);
 }
 
 jQuery(document).ready(function($) {
-    smp_onProjectChange()
+    smp_onProjectChange();
 });
