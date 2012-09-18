332f28964b6be647d53aa7cd11fc733f799aa5ef
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-09-18 13:18:34 +0000

    Fixes #10335: Changed license from GPLv2 to 3-Clause BSD.
    git-svn-id: https://trac-hacks.org/svn@12018 7322e99d-02ea-0310-aa39-e9a107903beb

--- defaultccplugin/trunk/setup.py
+++ defaultccplugin/trunk/setup.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+# Copyright (C) 2009 Jean-Guilhem Rouel <jean-guilhem.rouel@ercim.org>
+# Copyright (C) 2009 Vivien Lacourba <vivien.lacourba@ercim.org>
+# Copyright (C) 2012 Ryan J Ollos <ryan.j.ollos@gmail.com>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+#
 
 from setuptools import setup
 
@@ -10,11 +19,13 @@ setup(
     include_package_data = True,
     author = "Jean-Guilhem Rouel",
     author_email = "jean-guilhem.rouel@ercim.org",
+    maintainer="Ryan J Ollos",
+    maintainer_email="ryan.j.ollos@gmail.com",
     description = "Automatically adds a default CC list to new tickets.",
     long_description = "Automatically adds a default CC list when a new ticket is created," \
         "based on its initial component." \
         "CC lists can be configured per component through the component admin UI",
-    license = "GPLv2",
+    license = "3-Clause BSD",
     keywords = "trac CC ticket component",
     url = "http://trac-hacks.org/wiki/DefaultCCPlugin",
     classifiers = [
