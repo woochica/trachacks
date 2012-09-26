9fde3604a39a1196155065a0af9e74b8851fae8a
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-09-26 03:21:12 +0000

    Fixes #8764:  * Made compatible with SpamFilterPlugin 0.2 through at least 0.7.1 (latest at this time).  * Made the minimum required version of TagsPlugin to be 0.6.  * Cleaned up and organized `setup.py`.
    git-svn-id: https://trac-hacks.org/svn@12058 7322e99d-02ea-0310-aa39-e9a107903beb

--- discussionplugin/0.11/setup.py
+++ discussionplugin/0.11/setup.py
@@ -12,30 +12,33 @@ if sys.version_info >= (3,):
     print "DiscussionPlugin doesn't support Python 3 (yet)"
     sys.exit(1)
 
-setup(
-  name = 'TracDiscussion',
-  version = '0.8',
-  packages = ['tracdiscussion', 'tracdiscussion.db'],
-  package_data = {'tracdiscussion' : ['templates/*.html', 'templates/*.txt',
-    'templates/*.rss', 'htdocs/css/*.css', 'htdocs/js/*.js', 'htdocs/*.png']},
-  entry_points = {'trac.plugins': ['TracDiscussion.admin = tracdiscussion.admin',
-    'TracDiscussion.ajax = tracdiscussion.ajax',
-    'TracDiscussion.api = tracdiscussion.api',
-    'TracDiscussion.core = tracdiscussion.core',
-    'TracDiscussion.init = tracdiscussion.init',
-    'TracDiscussion.notification = tracdiscussion.notification',
-    'TracDiscussion.search = tracdiscussion.search',
-    'TracDiscussion.spamfilter = tracdiscussion.spamfilter [SpamFilter]',
-    'TracDiscussion.tags = tracdiscussion.tags [Tags]',
-    'TracDiscussion.timeline = tracdiscussion.timeline',
-    'TracDiscussion.wiki = tracdiscussion.wiki']},
-  install_requires = ['Trac'],
-  extras_require = {'SpamFilter' : ['TracSpamFilter'],
-                    'Tags' : ['TracTags']},
-  keywords = 'trac discussion e-mail',
-  author = 'Radek Bartoň',
-  author_email = 'blackhex@post.cz',
-  url = 'http://trac-hacks.org/wiki/DiscussionPlugin',
-  description = 'Discussion forum plugin for Trac',
-  license = '''GPL'''
-)
+setup(name='TracDiscussion',
+      version='0.8',
+      author='Radek Bartoň',
+      author_email='blackhex@post.cz',
+      license='''GPL''',
+      url='http://trac-hacks.org/wiki/DiscussionPlugin',
+      description='Discussion forum plugin for Trac',
+      keywords='trac discussion e-mail',
+      packages=['tracdiscussion', 'tracdiscussion.db'],
+      package_data={'tracdiscussion' : [
+          'templates/*.html', 'templates/*.txt', 'templates/*.rss',
+          'htdocs/css/*.css', 'htdocs/js/*.js', 'htdocs/*.png']
+      },
+      entry_points={'trac.plugins': [
+          'TracDiscussion.admin = tracdiscussion.admin',
+          'TracDiscussion.ajax = tracdiscussion.ajax',
+          'TracDiscussion.api = tracdiscussion.api',
+          'TracDiscussion.core = tracdiscussion.core',
+          'TracDiscussion.init = tracdiscussion.init',
+          'TracDiscussion.notification = tracdiscussion.notification',
+          'TracDiscussion.search = tracdiscussion.search',
+          'TracDiscussion.spamfilter = tracdiscussion.spamfilter[spamfilter]',
+          'TracDiscussion.tags = tracdiscussion.tags[tags]',
+          'TracDiscussion.timeline = tracdiscussion.timeline',
+          'TracDiscussion.wiki = tracdiscussion.wiki']
+      },
+      install_requires=[],
+      extras_require={'spamfilter' : ['TracSpamFilter >= 0.2'],
+                      'tags' : ['TracTags >= 0.6']}
+      )
--- discussionplugin/0.11/tracdiscussion/spamfilter.py
+++ discussionplugin/0.11/tracdiscussion/spamfilter.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 
 # Spamfilter imports.
-from tracspamfilter.api import FilterSystem, RejectContent
+from trac.util import arity
+from tracspamfilter.api import RejectContent
+try: #SpamFilter < 0.7
+    from tracspamfilter.api import FilterSystem
+except ImportError: #SpamFilter 0.7+
+    from tracspamfilter.filtersystem import FilterSystem
 
 # Local imports.
 from tracdiscussion.api import *
@@ -18,15 +23,20 @@ class DiscussionSpamFilter(Component):
     def filter_topic(self, context, topic):
         # Test topic for spam.
         try:
-            FilterSystem(self.env).test(context.req, topic['author'], [(None,
-              topic['author']), (None, topic['subject']), (None,
-              topic['body'])])
+            if arity(FilterSystem.test) == 4: #SpamFilter < 0.3.2 or >= 0.7.0
+                FilterSystem(self.env).test(context.req, topic['author'],
+                    [(None, topic['author']), (None, topic['subject']),
+                     (None, topic['body'])])
+            else: #SpamFilter >= 0.3.2 or < 0.7.0
+                FilterSystem(self.env).test(context.req, topic['author'],
+                    [(None, topic['author']), (None, topic['subject']),
+                     (None, topic['body'])], context.req.remote_addr)
         except RejectContent, error:
             # Topic contains spam.
-            return (False, error.message)
+            return False, error.message
 
         # Topic is fine.
-        return (True, topic)
+        return True, topic
 
     def filter_message(self, context, message):
         # Test message for spam.
@@ -35,7 +45,7 @@ class DiscussionSpamFilter(Component):
               message['author']), (None, message['body'])])
         except RejectContent, error:
             # Message contains spam.
-            return (False, error.message)
+            return False, error.message
 
         # Message is fine.
-        return (True, message)
+        return True, message
