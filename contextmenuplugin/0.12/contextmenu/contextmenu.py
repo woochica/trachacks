b36667fa680e68b27ee7aeb9d9a60e9f9c729784
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-10-06 01:44:58 +0000

    Refs #10439: Moved license description from source files to the added `COPYING` file.
    git-svn-id: https://trac-hacks.org/svn@12117 7322e99d-02ea-0310-aa39-e9a107903beb

--- contextmenuplugin/0.12/contextmenu/contextmenu.py
+++ contextmenuplugin/0.12/contextmenu/contextmenu.py
@@ -1,38 +1,13 @@
-# coding: utf-8
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 #
 # Copyright (c) 2010, Logica
-# 
 # All rights reserved.
-# 
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-# 
-#     * Redistributions of source code must retain the above copyright 
-#       notice, this list of conditions and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above copyright
-#       notice, this list of conditions and the following disclaimer in the
-#       documentation and/or other materials provided with the distribution.
-#     * Neither Logica nor the names of its contributors may be used to 
-#       endorse or promote products derived from this software without 
-#       specific prior written permission.
-# 
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-# ----------------------------------------------------------------------------
-'''
-Created on 17 Jun 2010
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+#
 
-@author: penmark
-'''
 from genshi.builder import tag
 from genshi.core import Markup
 from genshi.filters.transform import Transformer
