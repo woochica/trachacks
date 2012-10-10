dcb79868a1654d3b51797436ececa6955015a6fb
mhenke <mhenke@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-10-10 13:34:10 +0000

    Fixed #10463 and #10454
    git-svn-id: https://trac-hacks.org/svn@12140 7322e99d-02ea-0310-aa39-e9a107903beb

--- privatereportsplugin/0.12/privatereports/privatereports.py
+++ privatereportsplugin/0.12/privatereports/privatereports.py
@@ -2,7 +2,8 @@ from types import ListType, StringTypes
 
 from trac.core import *
 
-from trac.perm import PermissionSystem, IPermissionPolicy, IPermissionGroupProvider
+from trac.perm import PermissionSystem, IPermissionPolicy, IPermissionGroupProvider, IPermissionRequestor
+
 from trac.env import IEnvironmentSetupParticipant
 from trac.admin import IAdminPanelProvider
 from trac.web.chrome import ITemplateProvider
@@ -16,32 +17,26 @@ from genshi.input import HTML
 from pkg_resources import resource_filename
 
 class PrivateReports(Component):
-
 	group_providers = ExtensionPoint(IPermissionGroupProvider)
-
-	implements(ITemplateStreamFilter, IEnvironmentSetupParticipant, IAdminPanelProvider, ITemplateProvider, IRequestFilter)
+	implements(ITemplateStreamFilter, IEnvironmentSetupParticipant, IAdminPanelProvider, ITemplateProvider, IRequestFilter, IPermissionRequestor)
 
 	# IRequestFilter 
 	def pre_process_request(self, req, handler):
 		if handler is not ReportModule(self.env):
 			return handler	
-			
 		url = req.path_info	
 		user = req.authname
-		
 		find = url.rfind('/')
 		report_id= url[find+1:]
-		
 		try:
 			report_id = int(report_id)
 		except:
 			return handler
-		
 		if self._has_permission(user,report_id):
 			return handler
 		else:
 			raise TracError('You don\'t have the permission to access this report', 'No Permission')
-		
+
 	def post_process_request(self, req, template, data, content_type):
 		return template, data, content_type
 	
@@ -59,72 +54,46 @@ class PrivateReports(Component):
 
 	def render_admin_panel(self, req, cat, page, path_info):
 		if page == 'privatereports':
-
 			reports = self._get_reports()
-
 			data = {
 				'reports': reports
 			}
-
 			if req.method == 'POST':
-
 				report_id = req.args.get('report_id')
 				try:
 					report_id = int(report_id)
 				except:
 					req.redirect(self.env.href('/admin/privatereports/privatereports'))
-
 				if req.args.get('add'):
-
 					newpermission = req.args.get('newpermission')
 					if newpermission == None or newpermission.isupper() == False:
 						req.redirect(self.env.href('/admin/privatereports/privatereports'))
-
-					report_permissions = self._get_report_permissions(report_id)
-					
-					if report_permissions or report_permissions == []:
-						report_permissions.append(newpermission)
-						self._alter_report_permissions(report_id, report_permissions)
-					else:
-						report_permissions = [newpermission]
-						self._insert_report_permissions(report_id, report_permissions)
-
-					data['report_permissions'] = report_permissions or ''
+					self._insert_report_permission(report_id, newpermission)
+					data['report_permissions'] = self._get_report_permissions(report_id) or ''
 					data['show_report'] = report_id
-
 				elif req.args.get('remove'):
-
 					arg_report_permissions = req.args.get('report_permissions')
 					if arg_report_permissions == None:
 						req.redirect(self.env.href('/admin/privatereports/privatereports'))
-
 					report_permissions = self._get_report_permissions(report_id)
 					report_permissions = set(report_permissions)
-
 					if type(arg_report_permissions) in StringTypes:
 						toremove = set([arg_report_permissions])
 					elif type(arg_report_permissions) == ListType:
 						toremove = set(arg_report_permissions)
 					else:
 						req.redirect(self.env.href('/admin/privatereports/privatereports'))
-
 					report_permissions = report_permissions - toremove
-
 					self._alter_report_permissions(report_id, report_permissions)
-
 					data['report_permissions'] = report_permissions or ''
 					data['show_report'] = report_id
-
 				elif req.args.get('show'):
-
 					report_permissions = self._get_report_permissions(report_id)
 					data['report_permissions'] = report_permissions or ''
 					data['show_report'] = report_id
-
 			else:
 				report_permissions = self._get_report_permissions(reports[0][1])
 				data['report_permissions'] = report_permissions or ''
-
 			return 'admin_privatereports.html', data
 
 	# IEnvironmentSetupParticipant
@@ -136,7 +105,7 @@ class PrivateReports(Component):
 	def environment_needs_upgrade(self, db):
 		cursor = db.cursor()
 		try:
-			cursor.execute('SELECT * FROM private_report')
+			cursor.execute('SELECT report_id, permission FROM private_report')
 			cursor.close ()
 			return False
 		except:
@@ -145,9 +114,17 @@ class PrivateReports(Component):
 			return True
 
 	def upgrade_environment(self, db):
-		cursor = db.cursor()
 		try:
-			cursor.execute('CREATE TABLE private_report(report_id integer, permissions text)')
+			cursor = db.cursor()
+			cursor.execute('DROP TABLE IF EXISTS private_report')
+			cursor.close ()
+			db.commit()
+		except:
+			cursor.connection.rollback()
+			cursor.close ()
+		try:
+			cursor = db.cursor()
+			cursor.execute('CREATE TABLE private_report(report_id integer, permission text)')
 			cursor.close ()
 			db.commit()
 		except:
@@ -158,66 +135,65 @@ class PrivateReports(Component):
 	def filter_stream(self, req, method, filename, stream, data):
 		if not filename == 'report_list.html':
 			return stream
-			
 		user = req.authname
-
 		buffer = StreamBuffer()
-
 		def check_report_permission():
 			delimiter = '</tr>'
-
 			reportstream = str(buffer)
-
 			reports_raw = reportstream.split(delimiter)
 			reportstream = ''
-
 			for report in reports_raw:
 				if report != None and len(report) != 0:
-
 					# determine the report id
 					s = report.find('/report/')
 					if s == -1:
 						continue
-
 					e = report.find('\"',s)
 					if e == -1:
 						continue
 					report_id = report[s+len('/report/'):e]
-
 					if self._has_permission(user,report_id):
 						reportstream += report
-
 			return HTML(reportstream)
-
 		return stream | Transformer('//tbody/tr') \
 		.copy(buffer) \
 		.replace(check_report_permission)
+	
+	# IPermissionRequestor
+	def get_permission_actions(self):
+		db = self.env.get_db_cnx()
+		cursor = db.cursor()
+		sql = 'SELECT permission FROM private_report GROUP BY permission'
+		self.log.debug(sql)
+		cursor.execute(sql)
+		rperms = []
+		try:
+			for permission in cursor.fetchall():
+				rperms.append(permission[0])
+		except:
+			pass
+		cursor.close()
+		return tuple(rperms)
 
 	# Internal
 	def _has_permission(self, user, report_id):
 		report_permissions = self._get_report_permissions(report_id)
-		
 		if report_permissions == None or report_permissions == []:
 			return True
-			
 		perms = PermissionSystem(self.env)
-
 		report_permissions = set(report_permissions)
 		user_perm = set(perms.get_user_permissions(user))
 		groups = set(self._get_user_groups(user))
-		
 		user_perm.update(groups)
-		
 		if report_permissions.intersection(user_perm) != set([]):
 			return True
-			
 		return False
 		
 	def _get_user_groups(self, user):
 		subjects = set([user])
 		for provider in self.group_providers:
 			subjects.update(provider.get_permission_groups(user) or [])
-
+		
 		groups = []
 		db = self.env.get_db_cnx()
 		cursor = db.cursor()
@@ -227,86 +203,52 @@ class PrivateReports(Component):
 		for action in rows:
 			if action[0].isupper():
 				groups.append(action[0])
-			
+		cursor.close ()
 		return groups
 	
 	def _get_reports(self):
 		db = self.env.get_db_cnx()
-
 		cursor = db.cursor()
-
-		sql = 'SELECT title,id FROM report'
+		sql = 'SELECT title, id FROM report'
 		self.log.debug(sql)
-
 		cursor.execute(sql)
 		reports = cursor.fetchall()
 		cursor.close ()
-
 		return reports
 
-
-	def _insert_report_permissions(self,report_id, permissions):
+	def _insert_report_permission(self,report_id, permission):
 		db = self.env.get_db_cnx()
-
-		permission_str = ''
-		for permission in permissions:
-			permission_str = permission_str + '|' + permission
-
-		permission_str = permission_str[1:]
-
 		cursor = db.cursor()
-
-		sql = 'INSERT INTO private_report(report_id, permissions) VALUES(%d,\'%s\')' % \
-		(int(report_id), str(permission_str))
+		sql = 'INSERT INTO private_report(report_id, permission) VALUES(%d,\'%s\')' % \
+		(int(report_id), str(permission))
 		self.log.debug(sql)
-
 		cursor.execute(sql)
 		cursor.close ()
 		db.commit()
 
 	def _alter_report_permissions(self,report_id, permissions):
 		db = self.env.get_db_cnx()
-
-		permission_str = ''
-		for permission in permissions:
-			permission_str = permission_str + '|' + permission
-
-		permission_str = permission_str[1:]
-
 		cursor = db.cursor()
-
-		sql = 'UPDATE private_report SET permissions = \'%s\' WHERE report_id=%d' % \
-		(str(permission_str),int(report_id))
+		sql = 'DELETE FROM private_report WHERE report_id=%d' % \
+		(int(report_id))
 		self.log.debug(sql)
-
 		cursor.execute(sql)
 		cursor.close ()
 		db.commit()
+		for permission in permissions:
+			self._insert_report_permission(report_id, permission)
 
 	def _get_report_permissions(self,report_id):
 		db = self.env.get_db_cnx()
-
 		cursor = db.cursor()
-
-		sql = 'SELECT permissions FROM private_report WHERE report_id=%d' % \
+		sql = 'SELECT permission FROM private_report WHERE report_id=%d GROUP BY permission' % \
 		(int(report_id))
 		self.log.debug(sql)
-
 		cursor.execute(sql)
+		rperms = []
 		try:
-			permissions = cursor.fetchone()[0]
+			for permission in cursor.fetchall():
+				rperms.append(permission[0])
 		except:
-			permissions = None
-
-		# no entry available
-		if permissions == None:
-			return None
-
-		# no permission set but an existing entry
-		if len(permissions) == 0:
-			return []
-
-		permissions = str(permissions)
-
-		# list of permissions
-		return permissions.split('|')
+			pass
+		return rperms
--- privatereportsplugin/0.12/setup.py
+++ privatereportsplugin/0.12/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name='privatereports',
-	version='0.2',
+	version='0.3',
 	packages=['privatereports'],
 
 	author='Michael Henke',
