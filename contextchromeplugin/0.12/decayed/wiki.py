22800acdf821768e72e769905e0ae62a78ff1689
matobaa <matobaa@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-10-18 14:54:01 +0000

    ContextChromePlugin: add new "decayed wiki" feature.
    git-svn-id: https://trac-hacks.org/svn@12196 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ contextchromeplugin/0.12/decayed/wiki.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from genshi.filters.transform import Transformer
+from sys import maxint
+from trac.config import ListOption
+from trac.core import Component, implements
+from trac.web.api import ITemplateStreamFilter
+import datetime
+import sys
+
+class DecayedWiki(Component):
+    """ Indicate how old the wiki page is. colors can set in [wiki]-decayed_colors in trac.ini """
+    implements(ITemplateStreamFilter)
+
+    default_colors = {
+          86400 * 1: '#ffffff',
+          86400 * 7: '#eeeeee',
+          86400 * 31: '#dddddd',
+          86400 * 365: '#aaaaaa',
+          maxint: '#777777'}
+    colors = ListOption('wiki', 'decay_colors',
+        "86400:#ffffff, 604800:#eeeeee, 2678400:#dddddd, 31536000:#aaaaaa, 2147483647: #777777",
+        doc=
+        """ List of (age_in_seconds: color) value pairs
+            e.g. "86400:#ffffff, 604800:#cccccc, 2147483647: !#777777";
+            
+            means that the page modified in a day shows on white,
+            in a week shows on light grey, and so on.
+            (Provided by !%s)""" % "DecayedPlugin.Wiki")
+
+    # ITemplateStreamFilter methods
+    def filter_stream(self, req, method, filename, stream, data):
+        if data and 'context' in data and \
+                data['context'].resource.realm == 'wiki' and \
+                'page' in data and data['page'].time:
+            page = data['page']
+            delta = (datetime.datetime.now(page.time and page.time.tzinfo or None) - page.time).total_seconds()
+            try:
+                colors = [color.split(':') for color in self.config.getlist('wiki', 'decay_colors')]
+                self.log.debug("colors getlist: %s" % colors)
+                colors = map(lambda kv: (int(kv[0]), kv[1]), colors)
+            except:
+                errorinfo = sys.exc_info()
+                self.log.warning('Parse Error - use defaults, at trac.ini [wiki]/decay_colors;  %s', errorinfo)
+                colors = self.default_colors.items() #fallback
+            color = 'white' # centinel; avoid undefined error with empty list
+            for value, color in sorted(colors):
+                if value > delta:
+                    break
+            self.log.debug('wiki decay: {wiki:%s, delta:%s, color:%s}' % (page.name, delta, color))
+            return stream | Transformer('//body').attr('style', 'background-color: %s' % color)
+        return stream
--- contextchromeplugin/0.12/setup.py
+++ contextchromeplugin/0.12/setup.py
@@ -5,12 +5,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='ContextChrome',
-    version='0.1',
+    version='0.2',
     license='Modified BSD',
     author='MATOBA Akihiro',
     author_email='matobaa+trac-hacks@gmail.com',
     url='http://trac-hacks.org/wiki/matobaa',
-    description='Add context-aware css attribute',
+    description='Add context-aware css attribute or style',
     zip_safe=True,
     packages=find_packages(exclude=['*.tests']),
     #package_data={
@@ -19,6 +19,7 @@ setup(
     entry_points={
         'trac.plugins': [
             'ContextChrome = contextchrome.style',
+            'DecayedWiki = decayed.wiki'
         ]
     },
 )
