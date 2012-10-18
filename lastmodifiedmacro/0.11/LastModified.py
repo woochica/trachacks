1e19d97b919064af22145233a46aae8a1bc3dd4c
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-10-18 21:17:53 +0000

    Refs #8527:  * Formatted source code.  * Added `author`, `author_email` and `url`, which show on the plugin admin panel.
    git-svn-id: https://trac-hacks.org/svn@12199 7322e99d-02ea-0310-aa39-e9a107903beb

--- lastmodifiedmacro/0.11/LastModified.py
+++ lastmodifiedmacro/0.11/LastModified.py
@@ -32,30 +32,28 @@ from trac.wiki.formatter import Formatter
 from trac.wiki.macros import WikiMacroBase, parse_args
 
 revision = "$Rev$"
-url = "$URL$"
+url = "http://trac-hacks.org/wiki/LastModifiedMacro"
+author = "Steven N. Severinghaus"
+author_email = "<sns@severinghaus.org>"
 
 class LastModifiedMacro(WikiMacroBase):
-
-    """Displays the last modified time of the specified wiki page."""
-    revision = "$Rev$"
-    url = "$URL$"
 
     def expand_macro(self, formatter, name, content):
 
         args, _ = parse_args(content)
         
         if not args:
-            page_name = get_resource_name( self.env, formatter.resource )
-            mode='normal'
+            page_name = get_resource_name(self.env, formatter.resource)
+            mode = 'normal'
         elif len(args) == 1:
-            page_name=args[0]
-            mode='normal'
+            page_name = args[0]
+            mode = 'normal'
         else:
-            page_name=args[0]
-            mode='delta'
+            page_name = args[0]
+            mode = 'delta'
 
-        db=self.env.get_db_cnx()
-        cursor=db.cursor()
+        db = self.env.get_db_cnx()
+        cursor = db.cursor()
         cursor.execute("SELECT author, time FROM wiki WHERE name = '%s' "
                        "ORDER BY version DESC LIMIT 1" % page_name)
         row = cursor.fetchone()
@@ -63,7 +61,7 @@ class LastModifiedMacro(WikiMacroBase):
             out = StringIO('cannot find "' + page_name + '"')
             return Markup(out.getvalue())
 
-        username   = row[0]
+        username = row[0]
         time_int = row[1]
         
         # see if there's a fullname associated with username
@@ -76,40 +74,39 @@ class LastModifiedMacro(WikiMacroBase):
             author = row[0]        
 
         last_mod = datetime.fromtimestamp(time_int, utc)
-        now      = datetime.now(utc)
-        elapsed  = now - last_mod
-        if mode=='delta':
-            if elapsed.days==0:
-                if elapsed.seconds/3600>1.5:
-                    count=elapsed.seconds/3600
-                    unit='hour'
-                elif elapsed.seconds/60>1.5:
-                    count=elapsed.seconds/60
-                    unit='minute'
+        now = datetime.now(utc)
+        elapsed = now - last_mod
+        if mode == 'delta':
+            if elapsed.days == 0:
+                if elapsed.seconds / 3600 > 1.5:
+                    count = elapsed.seconds / 3600
+                    unit = 'hour'
+                elif elapsed.seconds / 60 > 1.5:
+                    count = elapsed.seconds / 60
+                    unit = 'minute'
                 else:
-                    count=elapsed.seconds
-                    unit='second'
-            elif elapsed.days/3650>1.5:
-                count=elapsed.days/3650
-                unit='decade'
-            elif elapsed.days/365>1.5:
-                count=elapsed.days/365
-                unit='year'
-            elif elapsed.days/30>1.5:
-                count=elapsed.days/30
-                unit='month'
-            elif elapsed.days/7>1.5:
-                count=elapsed.days/7
-                unit='week'
+                    count = elapsed.seconds
+                    unit = 'second'
+            elif elapsed.days / 3650 > 1.5:
+                count = elapsed.days / 3650
+                unit = 'decade'
+            elif elapsed.days / 365 > 1.5:
+                count = elapsed.days / 365
+                unit = 'year'
+            elif elapsed.days / 30 > 1.5:
+                count = elapsed.days / 30
+                unit = 'month'
+            elif elapsed.days / 7 > 1.5:
+                count = elapsed.days / 7
+                unit = 'week'
             else:
-                count=elapsed.days
-                unit='day'
-            text =""+repr(count)+" "+unit
-            if (count != 1 and count != -1): text+="s"
+                count = elapsed.days
+                unit = 'day'
+            text = "" + repr(count) + " " + unit
+            if (count != 1 and count != -1): text += "s"
         else:
             text = format_datetime(last_mod, '%c')
             text = format_datetime(last_mod, '%m/%d %k:%M')
 
         out = StringIO(text)
         return Markup(out.getvalue())
-
