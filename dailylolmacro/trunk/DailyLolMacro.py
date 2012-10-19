0155500f2d983b4c3a9e7b14255ed3724df78c04
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-10-19 01:24:05 +0000

    * Set license as 3-Clause BSD with permission of original author.  * Renamed `0.11` directory to `trunk`.
    git-svn-id: https://trac-hacks.org/svn@12211 7322e99d-02ea-0310-aa39-e9a107903beb

--- dailylolmacro/trunk/DailyLolMacro.py
+++ dailylolmacro/trunk/DailyLolMacro.py
@@ -1,9 +1,19 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2009-2010 Brian Lynch <blynch1@gmail.com>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+#
+
 import urllib2
 from genshi.builder import tag
 from trac.wiki.macros import WikiMacroBase
 
-revison="$Rev$"
-url="$URL$"
+revison = "$Rev$"
+url = "http://trac-hacks.org/wiki/DailyLolMacro"
+license = "3-Clause BSD"
 
 class DailyLolMacro(WikiMacroBase):
     """Display a picture of a cat with a funny caption.
@@ -19,9 +29,9 @@ class DailyLolMacro(WikiMacroBase):
         website_html = website.read()
 
         beg_pos = -1
-        beg_pos = website_html.find('<img src="', beg_pos+1)
-        end_pos = website_html.find('"', beg_pos+10)
-        raw_url = website_html[beg_pos+10:end_pos]
+        beg_pos = website_html.find('<img src="', beg_pos + 1)
+        end_pos = website_html.find('"', beg_pos + 10)
+        raw_url = website_html[beg_pos + 10:end_pos]
 
         attr = {}
         out = tag.img(src=raw_url, **attr)
