612f93cf18c7b0e869047969d1331efd2e8afed8
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-10-24 02:56:50 +0000

    Fixes #10527:  * Clarified license as 3-Clause BSD and added license text to source code. Change made with permission of original author, Max Stewart (superafroman).
    git-svn-id: https://trac-hacks.org/svn@12251 7322e99d-02ea-0310-aa39-e9a107903beb

--- ticketvalidatorplugin/0.11/ticketvalidator/admin.py
+++ ticketvalidatorplugin/0.11/ticketvalidator/admin.py
@@ -1,23 +1,11 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2008 Max Stewart
+# Copyright (C) 2008 Max Stewart <max.e.stewart@gmail.com>
 # All rights reserved.
 #
-# This file is part of the TicketValidator plugin for Trac
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
 #
-# TicketValidator is free software: you can redistribute it and/or 
-# modify it under the terms of the GNU General Public License as 
-# published by the Free Software Foundation, either version 3 of 
-# the License, or (at your option) any later version.
-#
-# TicketValidator is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with TicketValidator.  If not, see 
-# <http://www.gnu.org/licenses/>.
 
 from trac.admin import IAdminPanelProvider
 from trac.core import Component, implements
