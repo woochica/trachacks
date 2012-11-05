166ed9b5007737d642267b0b35404e0e2adf7639
jun66j5 <jun66j5@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-11-05 10:57:29 +0000

    iniadmin: bump the version to `0.3` and add new maintainer to `setup.py`
    git-svn-id: https://trac-hacks.org/svn@12307 7322e99d-02ea-0310-aa39-e9a107903beb

--- iniadminplugin/0.11/setup.py
+++ iniadminplugin/0.11/setup.py
@@ -1,9 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from setuptools import setup
 
 setup(name='IniAdmin',
-      version='0.2',
+      version='0.3',
       packages=['iniadmin'],
       author='Alec Thomas',
+      maintainer='Jun Omae',
+      maintainer_email='jun66j5@gmail.com',
       description='Expose all TracIni options using the Trac config option API',
       url='http://trac-hacks.org/wiki/IniAdminPlugin',
       license='BSD',
