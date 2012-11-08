b7d80a1daf0b56350deb0ba40464b34b182f7a73
hasienda <hasienda@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-11-08 00:45:33 +0000

    TracAnnouncer: Update AccountManagerPlugin messaging support, refs #7759, #7977, #8740, #8927, #9090 and #9204.
    This long-standing regression is fixed now, while associated message templates
    are rather bare-bone ones yet and formatting could be improved significantly.
    
    git-svn-id: https://trac-hacks.org/svn@12312 7322e99d-02ea-0310-aa39-e9a107903beb

--- announcerplugin/trunk/announcer/opt/acct_mgr/announce.py
+++ announcerplugin/trunk/announcer/opt/acct_mgr/announce.py
@@ -1,26 +1,30 @@
 #-*- coding: utf-8 -*-
 #
 # Copyright (c) 2010, Robert Corsaro
-# Copyright (c) 2010, Steffen Hoffmann
+# Copyright (c) 2010,2012, Steffen Hoffmann
 # 
 # This software is licensed as described in the file COPYING, which
 # you should have received as part of this distribution.
 #
 
-from trac.core import *
-from trac.web.chrome import Chrome
-
 from genshi.template import NewTextTemplate, TemplateLoader
 
-from announcer.api import AnnouncementSystem, AnnouncementEvent
+from trac.config import BoolOption, ListOption
+from trac.core import Component, implements
+from trac.perm import PermissionCache
+from trac.web.chrome import Chrome
+
+from announcer.api import AnnouncementEvent, AnnouncementSystem
+from announcer.api import IAnnouncementDefaultSubscriber 
 from announcer.api import IAnnouncementFormatter, IAnnouncementSubscriber
-from announcer.api import IAnnouncementPreferenceProvider
+from announcer.api import IAnnouncementSubscriptionFilter
 from announcer.api import _
 from announcer.distributors.mail import IAnnouncementEmailDecorator
+from announcer.model import Subscription
 from announcer.util.mail import set_header, next_decorator
-from announcer.util.settings import BoolSubscriptionSetting
 
 from acct_mgr.api import IAccountChangeListener
+
 
 class AccountChangeEvent(AnnouncementEvent):
     def __init__(self, category, username, password=None, token=None):
@@ -29,18 +33,33 @@ class AccountChangeEvent(AnnouncementEvent):
         self.password = password
         self.token = token
 
+
 class AccountManagerAnnouncement(Component):
     """Send announcements on account changes."""
 
     implements(
-        IAccountChangeListener,
-        IAnnouncementSubscriber,
-        IAnnouncementFormatter,
+        IAccountChangeListener, # from AccountManagerPlugin
         IAnnouncementEmailDecorator,
-        IAnnouncementPreferenceProvider
+        IAnnouncementFormatter,
+        IAnnouncementDefaultSubscriber,
+        IAnnouncementSubscriber,
+        IAnnouncementSubscriptionFilter
     )
 
-    # IAccountChangeListener interface
+    categories = ('created', 'change', 'delete', 'reset', 'verify')
+
+    default_on = BoolOption("announcer", "always_notify_user_admins", True,
+        """Sent user account notification to admin users per default, so they
+        may opt-out individually instead of requiring everyone to opt-in.
+        """)
+    default_distributor = ListOption("announcer",
+        "always_notify_user_admins_distributor", "email",
+        doc="""Comma-separated list of distributors to send the message to
+        by default.  ex. email, xmpp
+        """)
+
+    # IAccountChangeListener methods
+
     def user_created(self, username, password):
         self._notify('created', username, password)
 
@@ -51,12 +70,24 @@ class AccountManagerAnnouncement(Component):
         self._notify('delete', username)
 
     def user_password_reset(self, username, email, password):
+        """User password has been reset.
+
+        Note, that this is no longer final, and the old password could still
+        be recovered before first successful login with the new password.
+        """
         self._notify('reset', username, password)
 
     def user_email_verification_requested(self, username, token):
         self._notify('verify', username, token=token)
 
-    # IAnnouncementSubscriber interface
+    # IAnnouncementDefaultSubscriber method
+    def default_subscriptions(self):
+        if self.default_on:
+            for d in self.default_distributor:
+                yield (self.__class__.__name__, d, 101, 'always')
+
+    # IAnnouncementSubscriber methods
+
     def subscriptions(self, event):
         if event.realm == 'acct_mgr':
             for subscriber in self._get_membership(event):
@@ -65,12 +96,47 @@ class AccountManagerAnnouncement(Component):
                 yield subscriber
 
     def matches(self, event):
-        yield
+        if event.realm != 'acct_mgr':
+            return
+        if not event.category in self.categories:
+            return
+
+        klass = self.__class__.__name__
+        for i in Subscription.find_by_class(self.env, klass):
+            yield i.subscription_tuple()
 
     def description(self):
-        return 'notify me an account changes NOT IMPLEMENTED'
+        return _(
+            """notify me on user account changes (`ACCTMGR_USER_ADMIN`
+            required)""")
 
-    # IAnnouncementFormatter interface
+    def requires_authentication(self):
+        # Unauthenticated users must never see that.
+        return True
+
+    # IAnnouncementSubscriptionFilter method
+    def filter_subscriptions(self, event, subscriptions):
+        if event.realm != 'acct_mgr':
+            return subscriptions
+
+        # Make subscriptions available only for admins.
+        action = 'ACCTMGR_USER_ADMIN'
+        for subscription in subscriptions:
+            sid, auth = subscription[1:3]
+            # PermissionCache already takes care of sid = None
+            if not auth:
+                sid = 'anonymous'
+            perm = PermissionCache(self.env, sid)
+            if perm.has_permission(action):
+                yield subscription
+            else:
+                self.log.debug(
+                    "Filtering %s because of AccountManagerAnnouncement rule"
+                    % sid
+                )
+
+    # IAnnouncementFormatter methods
+
     def styles(self, transport, realm):
         if realm == 'acct_mgr':
             yield 'text/plain'
@@ -83,34 +149,16 @@ class AccountManagerAnnouncement(Component):
         if realm == 'acct_mgr' and style == 'text/plain':
             return self._format_plaintext(event)
 
-    # IAnnouncementEmailDecorator
+    # IAnnouncementEmailDecorator method
     def decorate_message(self, event, message, decorates=None):
         if event.realm == "acct_mgr":
             prjname = self.env.project_name
             subject = '[%s] %s: %s' % (prjname, event.category, event.username)
-            set_header(message, 'Subject', subject) 
+            set_header(message, 'Subject', subject)
         return next_decorator(event, message, decorates)
 
-    # IAnnouncementPreferenceProvider interface
-    def get_announcement_preference_boxes(self, req):
-        if req.authname == "anonymous" and 'email' not in req.session:
-            return
-        yield "acct_mgr_subscription", _("Account Manager Subscription")
+    # Private methods
 
-    def render_announcement_preference_box(self, req, panel):
-        settings = self._settings()
-        if req.method == "POST":
-            for k, setting in settings.items():
-                setting.set_user_setting(req.session, 
-                        value=req.args.get('acct_mgr_%s_subscription'%k),
-                        save=False)
-            req.session.save()
-        data = {}
-        for k, setting in settings.items():
-            data[k] = setting.get_user_setting(req.session.sid)[1]
-        return "prefs_announcer_acct_mgr_subscription.html", data
-
-    # private methods
     def _notify(self, category, username, password=None, token=None):
         try:
             announcer = AnnouncementSystem(self.env)
@@ -120,20 +168,6 @@ class AccountManagerAnnouncement(Component):
         except Exception, e:
             self.log.exception("Failure creating announcement for account "
                                "event %s: %s", username, category)
-
-    def _settings(self):
-        ret = {}
-        for n in ('created', 'change', 'delete'):
-            ret[n] = BoolSubscriptionSetting(self.env, "acct_mgr_%s"%n, None)
-        return ret
-
-    def _get_membership(self, event):
-        settings = self._settings()
-        if event.category in settings.keys():
-            for result in settings[event.category].get_subscriptions():
-                yield result
-        elif event.category in ('verify', 'reset'):
-            yield ('email', event.username, True, None)
 
     def _format_plaintext(self, event):
         acct_templates = {
@@ -169,7 +203,7 @@ class AccountManagerAnnouncement(Component):
             dirs += provider.get_templates_dirs()
         templates = TemplateLoader(dirs, variable_lookup='lenient')
         template = templates.load(acct_templates[event.category], 
-                cls=NewTextTemplate)
+                                  cls=NewTextTemplate)
         if template:
             stream = template.generate(**data)
             output = stream.render('text')
