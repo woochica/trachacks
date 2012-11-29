1b153d2e5fa9633ca07c912b9770a17429d5199b
jun66j5 <jun66j5@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-11-29 17:26:08 +0000

    Fixed broken communication between client on `tracd` using `HTTP/1.1` if sending `401 Unauthorized`. Sends `Connection: close` header in this case.
    Closes #8558.
    
    
    git-svn-id: https://trac-hacks.org/svn@12394 7322e99d-02ea-0310-aa39-e9a107903beb

--- httpauthplugin/trunk/httpauth/filter.py
+++ httpauthplugin/trunk/httpauth/filter.py
@@ -52,6 +52,8 @@ class HTTPAuthFilter(Component):
         req.send_header('Cache-control', 'no-cache')
         req.send_header('Expires', 'Fri, 01 Jan 1999 00:00:00 GMT')
         req.send_header('Content-Length', str(len(auth_req_msg)))
+        if req.get_header('Content-Length'):
+            req.send_header('Connection', 'close')
         req.end_headers()
 
         if req.method != 'HEAD':
