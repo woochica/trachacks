b8f831bf72a18c110237b1289cce17d71bbdab51
hasienda <hasienda@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-12-11 00:05:47 +0000

    AccountManagerPlugin: Late supply of unit test file missed out in [12415].
    git-svn-id: https://trac-hacks.org/svn@12428 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ accountmanagerplugin/trunk/acct_mgr/tests/model.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2012 Steffen Hoffmann <hoff.st@web.de>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+#
+# Author: Steffen Hoffmann <hoff.st@web.de>
+
+import shutil
+import tempfile
+import time
+import unittest
+
+from Cookie  import SimpleCookie as Cookie
+
+from trac.test  import EnvironmentStub, Mock
+from trac.web.session  import Session
+
+from acct_mgr.model  import delete_user, email_associated, email_verified, \
+                            del_user_attribute, get_user_attribute, \
+                            set_user_attribute, last_seen, user_known, \
+                            prime_auth_session
+
+
+class ModelTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.env = EnvironmentStub(default_data=True, enable=['trac.*'])
+        self.env.path = tempfile.mkdtemp()
+        self.db = self.env.get_db_cnx()
+
+    def tearDown(self):
+        self.db.close()
+        # Really close db connections.
+        self.env.shutdown()
+        shutil.rmtree(self.env.path)
+
+    # Helpers
+
+    def _create_session(self, user, authenticated=1, name='', email=''):
+        args = dict(username=user, name=name, email=email)
+        incookie = Cookie()
+        incookie['trac_session'] = '123456'
+        self.req = Mock(authname=bool(authenticated) and user or 'anonymous',
+                        args=args, base_path='/',
+                        chrome=dict(warnings=list()),
+                        href=Mock(prefs=lambda x: None),
+                        incookie=incookie, outcookie=Cookie(),
+                        redirect=lambda x: None)
+        self.req.session = Session(self.env, self.req)
+        self.req.session.save()
+
+    def test_last_seen(self):
+        user = 'user'
+
+        # Use basic function.
+        self.assertEqual(last_seen(self.env), [])
+        self._create_session(user)
+        # Devel: Not fail-safe, will produce random false-negatives.
+        now = time.time()
+        self.assertEqual(last_seen(self.env), [(user, int(now))])
+
+        # Use 1st optional kwarg.
+        self.assertEqual(last_seen(self.env, user), [(user, int(now))])
+        user = 'anotheruser'
+        self.assertEqual(last_seen(self.env, user), [])
+        # Don't care for anonymous session IDs.
+        self._create_session(user, False)
+        self.assertEqual(last_seen(self.env, user), [])
+
+
+    def test_user_known(self):
+        user = 'user'
+        self.assertFalse(user_known(self.env, user))
+        # Don't care for anonymous session IDs.
+        self._create_session(user, False)
+        self.assertFalse(user_known(self.env, user))
+        self._create_session(user)
+        self.assertTrue(user_known(self.env, user))
+
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(ModelTestCase, 'test'))
+    return suite
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')
