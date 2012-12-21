8065bb60774c16e6bf5724f68a7733b2f9391256
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-12-21 13:55:49 +0000

    Refs #10693: Workaround for `select.val()` not working on IE8 with Trac 0.11.7. This appears to be due to the `option` elements not having a `value`, an issue that was fixed in later versions of jQuery. Patch by Jun Omae (jun66j5).
    git-svn-id: https://trac-hacks.org/svn@12457 7322e99d-02ea-0310-aa39-e9a107903beb

--- adminenumlistplugin/trunk/adminenumlistplugin/htdocs/adminenumlist.js
+++ adminenumlistplugin/trunk/adminenumlistplugin/htdocs/adminenumlist.js
@@ -6,7 +6,7 @@ http://www.lukedingle.com/javascript/sortable-table-rows-with-jquery-draggable-r
 
 (C) Stepan Riha, 2009
 */
-  
+
 
 jQuery(document).ready(function ($) {
 
@@ -15,26 +15,26 @@ jQuery(document).ready(function ($) {
 	// http://bugs.jquery.com/ticket/11921
 	if(!$.isFunction($.curCSS))
 		$.curCSS = $.css;
-	
+
 	var mouseY, lastY = 0;
-	
+
 	//IE Doesn't stop selecting text when mousedown returns false we need to check
 	// That onselectstart exists and return false if it does
 	var hasOnSelectStart = document.onselectstart !== undefined;
 
-	// Indicates whether we're dragging a row	
+	// Indicates whether we're dragging a row
 	var dragging = false, unsaved_changes = false;
-	
+
 	// Disable the Apply changes button until there is a change
 	$('#enumtable div input[name="apply"]').attr('disabled', true)
 	$('#enumtable tbody tr input[name="default"]').click(function(){
 		$('#enumtable div input[name="apply"]').attr('disabled', false)
 	});
-	
+
 	// Insert Revert changes button after the Apply changes button
 	$('<input type="submit" name="revert" value="Revert changes" disabled="disabled"/>')
 		.insertAfter('#enumtable div input[name="apply"]');
-	
+
 	// Prompt with a dialog if leaving the page with unsaved changes to the list
 	var support_beforeunload = jQuery.event.special.beforeunload !== undefined;
 	var beforeunload = function() {
@@ -51,13 +51,13 @@ jQuery(document).ready(function ($) {
 		window.onbeforeunload = beforeunload;
 		$(window).bind('unload', function() { window.onbeforeunload = null; });
 	}
-	
+
 	// Don't prompt with a dialog if the Apply or Revert changes button is pressed
 	var button_pressed
 	$('#enumtable div.buttons input').click(function(){
 		button_pressed = $(this).attr('name');
 	})
-	
+
 	$('#enumtable').submit(function(){
 		if(button_pressed === 'apply' || button_pressed === 'revert') {
 			if (support_beforeunload)
@@ -71,12 +71,12 @@ jQuery(document).ready(function ($) {
 			return false;
 		}
 	});
-	
+
 	// This keep track of current vertical coordinates
 	$().mousemove(function(e) {
 		mouseY = e.pageY;
 	});
-	
+
 	// Suppress behavior on elements that should capture mouse
 	$('#enumlist input, #enumlist a, #enumlist select').mousedown(function (e) {
 		// Suppress drag behavior for controls and links
@@ -87,19 +87,19 @@ jQuery(document).ready(function ($) {
 	// Begin drag
 	$('#enumlist tbody tr').mousedown(function (e) {
 		// Suppress drag on elements that should capture mouse
-	 	if(mouseY == 0)
-	 		return;
-	 	
-	 	dragging = true;
-	 	
-	 	// Remember starting mouseY
+		if(mouseY == 0)
+			return;
+
+		dragging = true;
+
+		// Remember starting mouseY
 		lastY = mouseY;
-	
+
 		var tr = $(this);
-	
+
 		// This is just for flashiness. It fades the TR element out to an opacity of 0.2 while it is being moved.
 		tr.find('td').fadeTo('fast', 0.2);
-		
+
 		// jQuery has a fantastic function called mouseenter() which fires when the mouse enters
 		// This code fires a function each time the mouse enters over any TR inside the tbody -- except $(this) one
 		$('tr', tr.parent() ).not(tr).bind('mouseenter', function(e){
@@ -114,7 +114,7 @@ jQuery(document).ready(function ($) {
 				lastY = e.pageY - 1;
 			}
 		});
-	
+
 		// When mouse is released, unhook events and update values
 		$('body').mouseup(function () {
 			//Fade the TR element back to full opacity
@@ -123,36 +123,37 @@ jQuery(document).ready(function ($) {
 			$('tr', tr.parent()).unbind('mouseenter');
 			// Remove this mouseup function until next time
 			$('body').unbind('mouseup');
-	
+
 			// Make text selectable for IE again with
 			// The workaround for IE based browers
 			if (hasOnSelectStart)
 				$(document).unbind('selectstart');
-			
+
 			updateValues(tr);
-						
+
 			dragging = false;
 		});
-	
-	
-		
+
+
+
 	// Preventing the default action and returning false will Stop any text in the table from being
 	// highlighted (this can cause problems when dragging elements)
-		
+
 	e.preventDefault();
-	
+
 	// The workaround for IE browsers
 	if (hasOnSelectStart)
 			$(document).bind('selectstart', function () { return false; });
 		return false;
-	
+
 	}).css('cursor', 'move');
-	
-	// When user changes a select value, reorder rows	
+
+	// When user changes a select value, reorder rows
 	$('#enumlist select').change(function (e) {
 		// Move ($this) in the right position
 		var tr = $(this).parents('tr')[0];
-		var val = $(this).val();
+        // select.val() does not work on IE8 with Trac 0.11.7 (#10693)
+		var val = $(':selected', this).val();
 		if(val == 1) {
 			$('#enumlist tbody').prepend(tr);
 		} else {
@@ -165,14 +166,15 @@ jQuery(document).ready(function ($) {
 		}
 		updateValues(tr);
 	});
-	
+
 	// Set select values based on the row they're in and highlight those that have changed
 	function updateValues (tr) {
 		var position = 1;
 		var trSelect = $('select', $(tr))[0];
 		$('#enumlist tbody tr select').each(function () {
 			var select = $(this);
-			if(select.val() != position) {
+            // select.val() does not work on IE8 with Trac 0.11.7 (#10693)
+			if($(':selected', select).val() != position) {
 				select.val(position);
 				select.not(trSelect).parent().effect('highlight', {}, 1000);
 				unsaved_changes = true;
@@ -187,5 +189,5 @@ jQuery(document).ready(function ($) {
 			$('#enumtable div input[name="apply"]').attr('disabled', false);
 		}
 	}
-	
+
 });
