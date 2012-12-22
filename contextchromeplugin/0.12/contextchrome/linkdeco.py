6fa1860de0db98dfd06454a53379d5157601fb2c
matobaa <matobaa@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-12-22 06:08:42 +0000

    ConteContextChromePlugin: add new WikiLinkNewDecolator feature.
    git-svn-id: https://trac-hacks.org/svn@12465 7322e99d-02ea-0310-aa39-e9a107903beb

--- contextchromeplugin/0.12/contextchrome/linkdeco.py
+++ contextchromeplugin/0.12/contextchrome/linkdeco.py
@@ -3,10 +3,15 @@
 
 from trac.core import Component, implements
 from trac.ticket.api import TicketSystem
-from trac.web.api import IRequestHandler
-from trac.config import ListOption
+from trac.web.api import IRequestHandler, IRequestFilter
+from trac.config import ListOption, IntOption
 from trac.util import Ranges
 from genshi.builder import Element
+from trac.wiki.api import WikiSystem
+from trac.wiki.model import WikiPage
+from datetime import datetime
+from trac.web.chrome import ITemplateProvider, add_stylesheet
+from pkg_resources import ResourceManager
 
 class TicketLinkDecorator(Component):
     """ set css-class to ticket link as ticket field value. field name can set in [ticket]-decorate_fields in trac.ini"""
@@ -55,3 +60,68 @@ class TicketLinkDecorator(Component):
         
     def process_request(self, req):
         pass
+    
+
+class WikiLinkNewDecolator(Component):
+    """ set \"new\" css-class to wiki link if the page is young. age can set in [wiki]-wiki_new_info_second in trac.ini"""
+    implements(IRequestHandler)
+
+    wrapped = None
+    
+    wiki_new_info_day = IntOption('wiki', 'wiki_new_info_second', '432000',
+        doc=u"""age in seconds to add new icon. (Provided by !ContextChrome.!WikiLinkNewDecolator) """)
+
+    def __init__(self):
+        Component.__init__(self)
+        if not self.wrapped: self.wrap()
+  
+    def wrap(self):
+        wikisystem = self.compmgr[WikiSystem]
+        def _format_link(*args, **kwargs): # hook method
+            element = self.wrapped(*args, **kwargs)
+            if isinstance(element, Element):
+                class_ = element.attrib.get('class')
+                if class_ and element.attrib.get('href'): # existing ticket
+                    deco = self.get_deco(*args, **kwargs) or []
+                    element.attrib = element.attrib | [('class', ' '.join(deco + [class_]))]
+            return element
+        self.wrapped = wikisystem._format_link
+        wikisystem._format_link = _format_link
+
+    def get_deco(self, formatter, ns, pagename, label, ignore_missing,
+                     original_label=None):
+        wikipage = WikiPage(self.env, pagename)
+        if not wikipage.time:
+            return
+        now = datetime.now(formatter.req.tz)
+        delta = now - wikipage.time
+        limit = self.config.getint('wiki', 'wiki_new_info_second')
+        if limit < delta.days * 86400 + delta.seconds:
+            return
+        return ['new']
+
+    # IRequestHandler Methods
+    def match_request(self, req):
+        return False
+        
+    def process_request(self, req):
+        pass
+    
+class InternalStylesheet(Component):
+    """ Use internal stylesheet. Off to use your own site.css for \".new\" css-class."""
+    implements(IRequestFilter, ITemplateProvider)
+
+    # IRequestFilter methods
+    def pre_process_request(self, req, handler):
+        add_stylesheet(req, "contextchrome/css/newicon.css")
+        return handler
+    
+    def post_process_request(self, req, template, data, content_type):
+        return template, data, content_type
+    
+    # ITemplateProvider methods
+    def get_htdocs_dirs(self):
+        return [('contextchrome', ResourceManager().resource_filename(__name__, 'htdocs'))]
+
+    def get_templates_dirs(self):
+        return []
