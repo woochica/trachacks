6caf9a83d1495b03e1361f9e4c1078f9f660985a
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-12-26 07:02:45 +0000

    Bumped version to 2.2dev.v. Added text of 3-Clause BSD license.
    git-svn-id: https://trac-hacks.org/svn@12478 7322e99d-02ea-0310-aa39-e9a107903beb

--- peerreviewplugin/trunk/setup.py
+++ peerreviewplugin/trunk/setup.py
@@ -1,29 +1,34 @@
 #!/usr/bin/env python
-# -*- coding: iso-8859-1 -*-
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2005-2006 Team5
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+#
 
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
-    name = 'TracCodeReview',
-    version = '2.1-toddler',
-    packages = ['codereview'],
-    package_data={ 'codereview' : ['templates/*.cs',
-                                   'templates/*.html',
-                                   'htdocs/images/*.*',
-                                   'htdocs/js/*.js' ] },
-    author = "Team5",
-    author_email = "UNKNOWN",
-    maintainer = "Sebastian Marek",
-    maintainer_email = "smarek@plus.net",
-    description = "Framework for realtime code review within Trac.",
-    license = "BSD",
-    keywords = "trac plugin code peer review",
-    url = "http://trac-hacks.org/wiki/PeerReviewPlugin",
-
-    entry_points = {
+    name='TracCodeReview',
+    version='2.2',
+    author="Team5",
+    author_email="UNKNOWN",
+    maintainer="Sebastian Marek",
+    maintainer_email="smarek@plus.net",
+    description="Framework for code review in Trac.",
+    license="3-Clause BSD",
+    keywords="trac plugin code peer review",
+    url="http://trac-hacks.org/wiki/PeerReviewPlugin",
+    packages=find_packages(exclude=['*.tests']),
+    package_data={'codereview': ['templates/*.html',
+                                 'htdocs/images/*.png',
+                                 'htdocs/images/*.gif',
+                                 'htdocs/js/*.js']},
+    entry_points={
         'trac.plugins': [
             'codereview = codereview',
         ],
     },
-
 )
