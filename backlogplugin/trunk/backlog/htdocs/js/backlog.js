9615ef56b085d05c00fab166f12557eb8bf5f467
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-12-27 18:45:50 +0000

    Converted tabs to spaces in !JavaScript file.
    git-svn-id: https://trac-hacks.org/svn@12489 7322e99d-02ea-0310-aa39-e9a107903beb

--- backlogplugin/trunk/backlog/htdocs/js/backlog.js
+++ backlogplugin/trunk/backlog/htdocs/js/backlog.js
@@ -1,61 +1,61 @@
 jQuery(document).ready(function($) {
-	var changes_saved = true;
+    var changes_saved = true;
 
-	window.onbeforeunload = warnNotSaved;
+    window.onbeforeunload = warnNotSaved;
 
-	// TODO: only enable remove_closed if there are closed tickets
-	$("#remove_closed").attr('disabled', false);
+    // TODO: only enable remove_closed if there are closed tickets
+    $("#remove_closed").attr('disabled', false);
 
-	$(".backlog_tickets").sortable({
-		update : function() {
-			list_changed();
-		},
-		connectWith : '.backlog_tickets'
-	}).disableSelection();
+    $(".backlog_tickets").sortable({
+        update : function() {
+            list_changed();
+        },
+        connectWith : '.backlog_tickets'
+    }).disableSelection();
 
-	$("#save_order").click(function(){
-		updateOrder();
-	});
+    $("#save_order").click(function(){
+        updateOrder();
+    });
 
-	$("#reset_order").click(function(){
-		changes_saved = true;
-		$('#ticket_order').attr('value', '');
-		$('#tickets_out').attr('value', '');
-		return true;
-	});
+    $("#reset_order").click(function(){
+        changes_saved = true;
+        $('#ticket_order').attr('value', '');
+        $('#tickets_out').attr('value', '');
+        return true;
+    });
 
-	$("#remove_closed").click(function(){
-		if (changes_saved) {
-			$('#remove_tickets').attr('value', true);
-			return true;
-		}
-		if (confirm('You have unsaved changes to the ticket order. Do you want to save these changes now?')) {
-			$('#remove_tickets').attr('value', 'true');
-			return updateOrder();
-		} else {
-			return false;
-		}
-	});
+    $("#remove_closed").click(function(){
+        if (changes_saved) {
+            $('#remove_tickets').attr('value', true);
+            return true;
+        }
+        if (confirm('You have unsaved changes to the ticket order. Do you want to save these changes now?')) {
+            $('#remove_tickets').attr('value', 'true');
+            return updateOrder();
+        } else {
+            return false;
+        }
+    });
 
-	function list_changed() {
-		changes_saved = false;
-		$('#save_order').attr('disabled', false);
-		$('#reset_order').attr('disabled', false);
-	}
+    function list_changed() {
+        changes_saved = false;
+        $('#save_order').attr('disabled', false);
+        $('#reset_order').attr('disabled', false);
+    }
 
-	function updateOrder() {
-		changes_saved = true;
-		var order = $('#backlog').sortable('toArray');
-		$('#ticket_order').attr('value', order.toString());
-		var out = $('#notbacklog').sortable('toArray');
-		$('#tickets_out').attr('value', out.toString());
-		return true;
-	}
+    function updateOrder() {
+        changes_saved = true;
+        var order = $('#backlog').sortable('toArray');
+        $('#ticket_order').attr('value', order.toString());
+        var out = $('#notbacklog').sortable('toArray');
+        $('#tickets_out').attr('value', out.toString());
+        return true;
+    }
 
-	function warnNotSaved() {
-		if (changes_saved)
-			return null;
-		return 'You have changed the backlog, but you have not saved the changes. If you leave before saving your changes will be lost.'
-	}
+    function warnNotSaved() {
+        if (changes_saved)
+            return null;
+        return 'You have changed the backlog, but you have not saved the changes. If you leave before saving your changes will be lost.'
+    }
 });
 
