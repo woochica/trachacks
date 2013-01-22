d2a036a801c2199bc9998a588593b1adfeec5dae
zshahan <zshahan@7322e99d-02ea-0310-aa39-e9a107903beb>
2013-01-22 19:25:14 +0000

    db api updates
    git-svn-id: https://trac-hacks.org/svn@12558 7322e99d-02ea-0310-aa39-e9a107903beb

--- traccomponentaliasplugin/1.0/setup.py
+++ traccomponentaliasplugin/1.0/setup.py
@@ -4,18 +4,18 @@ from setuptools import find_packages, setup
 
 setup(
     name='TracComponentAliasPlugin',
-    version='0.1.1',
+    version='0.1.2',
     keywords='trac plugin ticket component alias',
     author='Zack Shahan',
     author_email='zshahan@dig-inc.net',
     url='null',
     description='Trac Component Alias Plugin',
     long_description="""
-This plugin for Trac 0.12/1.0 provides a way to map components to a friendly name in the ticket form.
+This plugin for Trac 1.0 provides a way to map components to a friendly name in the ticket form.
 """,
     license='BSD',
 
-    install_requires=['Trac >= 0.12dev'],
+    install_requires=['Trac >= 0.13dev'],
 
     packages=['traccomponentalias'],
 
