894a5ee5596b4e5dc1f1f5094fc90d27c9845d96
zshahan <zshahan@7322e99d-02ea-0310-aa39-e9a107903beb>
2013-02-18 18:07:06 +0000

    added license info.
    git-svn-id: https://trac-hacks.org/svn@12631 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracticketweightplugin/1.0/tracticketweight/api.py
+++ tracticketweightplugin/1.0/tracticketweight/api.py
@@ -1,8 +1,9 @@
-'''
-Created on Feb 4, 2013
-
-@author: Zack Shahan
-'''
+# -*- coding: utf-8 -*-
+# Copyright (C) 2012 Zack Shahan
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
 
 from __future__ import division
 from trac.core import Component, implements
