f235daaa9b507463f9ded31474df3fa6f4f7b8ae
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2013-02-19 23:54:09 +0000

    Refs #10831: Added license header to `setup.py`. Added URL to `setup.py`.
    git-svn-id: https://trac-hacks.org/svn@12638 7322e99d-02ea-0310-aa39-e9a107903beb

--- traccomponentaliasplugin/trunk/setup.py
+++ traccomponentaliasplugin/trunk/setup.py
@@ -1,4 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2012 Zack Shahan <zshahan@dig-inc.net>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
 
 from setuptools import find_packages, setup
 
@@ -8,12 +15,12 @@ setup(
     keywords='trac plugin ticket component alias',
     author='Zack Shahan',
     author_email='zshahan@dig-inc.net',
-    url='null',
+    url='http://trac-hacks.org/wiki/TracComponentAliasPlugin',
     description='Trac Component Alias Plugin',
     long_description="""
 This plugin for Trac 1.0 provides a way to map components to a friendly name in the ticket form.
 """,
-    license='BSD',
+    license='BSD 3-Clause',
 
     install_requires=['Trac >= 0.13dev'],
 
