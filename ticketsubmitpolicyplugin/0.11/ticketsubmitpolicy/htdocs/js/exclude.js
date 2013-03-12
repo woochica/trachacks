6291b25748ed2e98ead6bb7e2f4e4aa8ea6ebef8
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2013-03-12 23:00:58 +0000

    Code formatting changes.
    git-svn-id: https://trac-hacks.org/svn@12712 7322e99d-02ea-0310-aa39-e9a107903beb

--- ticketsubmitpolicyplugin/0.11/ticketsubmitpolicy/htdocs/js/exclude.js
+++ ticketsubmitpolicyplugin/0.11/ticketsubmitpolicy/htdocs/js/exclude.js
@@ -1,29 +1,25 @@
 function exclude(policy, excludedfields) {
 
-
-    if (condition(policy)) {
-        for (var i=0; i != excludedfields.length; i++) {
-            $("#field-" + excludedfields[i]).hide();
-        }
+  if (condition(policy)) {
+    for (var i = 0; i != excludedfields.length; i++) {
+      $("#field-" + excludedfields[i]).hide();
     }
-    else {
-        for (var i=0; i != excludedfields.length; i++) {
-            $("#field-" + excludedfields[i]).show();
-        }
+  }
+  else {
+    for (var i = 0; i != excludedfields.length; i++) {
+      $("#field-" + excludedfields[i]).show();
     }
-
+  }
 
 }
 
 function excludeSubmit(policy, excludedfields) {
 
-    if (condition(policy)) {
-
-        for (var i=0; i != excludedfields.length; i++) {
-            ("#field-" + excludedfields[i]).val("");
-        }
-
+  if (condition(policy)) {
+    for (var i = 0; i != excludedfields.length; i++) {
+      ("#field-" + excludedfields[i]).val("");
     }
-    
-    return true;
+  }
+
+  return true;
 }
--- ticketsubmitpolicyplugin/0.11/ticketsubmitpolicy/htdocs/js/requires.js
+++ ticketsubmitpolicyplugin/0.11/ticketsubmitpolicy/htdocs/js/requires.js
@@ -1,29 +1,26 @@
 function requires(policy, requiredfields) {
-    var missing = [];
-    if (condition(policy)) {
-        for ( var i=0; i != requiredfields.length; i++ ) {
-            var field=$("#field-" + requiredfields[i]).val();
-            if (!field) {
-                missing.push(requiredfields[i]);
-            }
-        }
-
-        if (missing.length != 0) {
-
-            if (missing.length == 1) {
-                prestring = missing[0] + " is a required field ";
-                poststring = "Please provide this value.";
-            }
-            else {
-                prestring = missing.join(", ") + " are required fields ";
-                poststring = "Please provide these values.";
-            }
-            
-            return prestring + "for tickets where " + policytostring(policy) + ".\n" + poststring;
-        }
+  var missing = [];
+  if (condition(policy)) {
+    for (var i = 0; i != requiredfields.length; i++) {
+      var field = $("#field-" + requiredfields[i]).val();
+      if (!field) {
+        missing.push(requiredfields[i]);
+      }
     }
 
-    return true;
+    if (missing.length != 0) {
+
+      if (missing.length == 1) {
+        prestring = missing[0] + " is a required field ";
+        poststring = "Please provide this value.";
+      }
+      else {
+        prestring = missing.join(", ") + " are required fields ";
+        poststring = "Please provide these values.";
+      }
+
+      return prestring + "for tickets where " + policytostring(policy) + ".\n" + poststring;
+    }
+  }
+  return true;
 }
-
-
