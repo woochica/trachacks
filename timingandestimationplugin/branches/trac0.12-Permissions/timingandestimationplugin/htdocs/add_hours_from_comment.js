abe1bc08e78fb10ff048e63f5aa5dba93afdf245
bobbysmith007 <bobbysmith007@7322e99d-02ea-0310-aa39-e9a107903beb>
2013-03-27 20:12:20 +0000

    more JS refactor to modernize and simplify re #10977
    git-svn-id: https://trac-hacks.org/svn@12822 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ timingandestimationplugin/branches/trac0.12-Permissions/timingandestimationplugin/htdocs/add_hours_from_comment.js
@@ -0,0 +1,33 @@
+(function(){
+var btnTemp = $('<div class="inlinebuttons"><input type="submit" title="add time elapsed since this comment" value="Add Time" class="add-hours-btn"></div>');
+
+function add_buttons(){
+  $(".change .trac-ticket-buttons").each(function(i,e){
+    var ts = timestamp_from_change(this);
+    if(ts)$(this).append(btnTemp.clone().click(
+      function(){TandE_onClickOfADateElement(ts);}
+    ));
+  });
+}
+
+function timestamp_from_change(el){
+  el = $(el);
+  if(!el.is(".change")) el = $(el.parents(".change"));
+  var id=el.attr("id"), r = /trac-change-\d+-(\d+)/, m=id.match(r),
+    ts = m && m[1];
+  if(ts) return new Date(Math.floor(Number(ts/1000)));
+  return null;
+}
+
+function TandE_onClickOfADateElement(commenttime) {
+  var now = new Date();
+  var minutesDiff = (now - commenttime) / 1000 / 60;
+  var fifteens = Math.floor(minutesDiff / 15);
+  var remainder = minutesDiff % 15;
+  var hours = (fifteens * 0.25) + ((remainder > 5) ? 0.25 : 0.0);
+  $('#field-hours')[0].value = hours;
+  return false;
+}
+
+$(document).ready(add_buttons);
+})(); // end of scoping function
