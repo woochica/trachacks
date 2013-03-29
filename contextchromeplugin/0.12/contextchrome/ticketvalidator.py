cbb35afa73ce66d882ee276b57941ff38f05da45
matobaa <matobaa@7322e99d-02ea-0310-aa39-e9a107903beb>
2013-03-29 00:14:59 +0000

    ContextChromePlugin: added Support for TracTicketConditionalValidatePlugin. closes #10953.
    git-svn-id: https://trac-hacks.org/svn@12836 7322e99d-02ea-0310-aa39-e9a107903beb

--- contextchromeplugin/0.12/contextchrome/ticketvalidator.py
+++ contextchromeplugin/0.12/contextchrome/ticketvalidator.py
@@ -12,6 +12,16 @@ from trac.ticket.default_workflow import get_workflow_config
 from trac.web.api import ITemplateStreamFilter, IRequestHandler
 from trac.web.chrome import add_script
 try:
+    from tracticketconditionalvalidatefield.api import TicketsValidator
+except:
+    class TicketsValidator(Component):
+        pass  # fake it
+try:
+    from tracticketvalidator.ticketvalidator import TicketValidator
+except:
+    class TicketValidator(Component):
+        pass  # fake it
+try:
     import json
 except:
     from tracrpc.json_rpc import json
@@ -29,16 +39,29 @@ class TicketValidatorDecolator(Component):
         conditionkeys = ['status', 'type']
         conditionkeys.sort()
         rules = {'status=new&type=*': {'summary': sentinel}}
+        ''' compatible with TracTicketConditionalValidateFieldsPlugin '''
+        if self.compmgr.is_enabled(TicketsValidator):
+            types = [k for k, v in self.config.options('ticket_validate') if k.find('.') == -1]
+            for ticket_type in types:
+                fields = self.config.getlist('ticket_validate', ticket_type)
+                condition = 'status=*&type=%s' % ticket_type
+                if condition not in rules:
+                    rules[condition] = {}
+                rule = rules.get(condition)
+                for field in fields:
+                    rule[field] = self.config.get('ticket_validate', '%s.tip' % field, default=sentinel)
+
         ''' compatible with TracTicketValidatorPlugin '''
-        keys = [k[:-4] for k, v in self.config.options('ticketvalidator') if k.endswith('.rule')]  # includes dot
-        for key in keys:
-            values = dict([(k[len(key):], v) for k, v in self.config.options('ticketvalidator') if k.startswith(key)])
-            condition = '&'.join(['%s=%s' % (ck, values.get(ck, '*')) for ck in conditionkeys])
-            if condition not in rules:
-                rules[condition] = {}
-            rule = rules.get(condition)
-            field = values.get('field', key[:-1])
-            rule[field] = values.get('tip', sentinel)
+        if self.compmgr.is_enabled(TicketValidator):
+            keys = [k[:-4] for k, v in self.config.options('ticketvalidator') if k.endswith('.rule')]  # includes dot
+            for key in keys:
+                values = dict([(k[len(key):], v) for k, v in self.config.options('ticketvalidator') if k.startswith(key)])
+                condition = '&'.join(['%s=%s' % (ck, values.get(ck, '*')) for ck in conditionkeys])
+                if condition not in rules:
+                    rules[condition] = {}
+                rule = rules.get(condition)
+                field = values.get('field', key[:-1])
+                rule[field] = values.get('tip', sentinel)
         return rules
 
     # IRequestHandler methods
