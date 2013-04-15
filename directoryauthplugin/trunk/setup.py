6325a4bd09b01f769875add48ee52a78a78ed9e5
sandinak <sandinak@7322e99d-02ea-0310-aa39-e9a107903beb>
2013-04-15 17:47:22 +0000

    checking in fix for groups
    git-svn-id: https://trac-hacks.org/svn@12971 7322e99d-02ea-0310-aa39-e9a107903beb

--- directoryauthplugin/trunk/setup.py
+++ directoryauthplugin/trunk/setup.py
@@ -13,7 +13,7 @@
 from setuptools import setup, find_packages
 
 PACKAGE = 'TracDirectoryAuth'
-VERSION = '1.0.2'
+VERSION = '1.0.1'
 
 setup(  name=PACKAGE, version=VERSION,
         author = 'John Hampton',
