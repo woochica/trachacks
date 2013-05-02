817a0022330e5c1f82cf0229f98248cbd7f44fe9
matobaa <matobaa@7322e99d-02ea-0310-aa39-e9a107903beb>
2013-05-02 02:31:36 +0000

    StatusHistoStatusHistoryChartMacro: Enhanced, "report" parameter can be used for y-axis field name.
    git-svn-id: https://trac-hacks.org/svn@13030 7322e99d-02ea-0310-aa39-e9a107903beb

--- statushistorychartmacro/1.0/statushistorychart/statushistorychart.py
+++ statushistorychartmacro/1.0/statushistorychart/statushistorychart.py
@@ -7,8 +7,10 @@
 # This software is licensed as described in the file COPYING, which
 # you should have received as part of this distribution.
 
+from genshi.builder import tag
 from pkg_resources import ResourceManager
 from trac.core import Component, implements, TracError
+from trac.ticket.api import TicketSystem
 from trac.ticket.query import Query
 from trac.web.chrome import add_script, ITemplateProvider, add_script_data
 from trac.wiki.api import IWikiMacroProvider
@@ -56,18 +58,19 @@ class Macro(Component):
    }}}"""
 
     def expand_macro(self, formatter, name, content, args=None):
+        # add scripts
         def lte_ie8(req):
             user_agent = formatter.req.get_header('user-agent')
             msie = user_agent.find('MSIE ')
             return (msie != -1) and user_agent[msie + 5:msie + 6] in ['6', '7', '8']
-        query = None
-        status_list = ['new', 'assigned', 'accepted', 'closed', '*']
-        default_status = status_list.index('*')
         if lte_ie8(formatter.req):
             add_script(formatter.req, "statushistorychart/js/flot/excanvas.js")
         add_script(formatter.req, "statushistorychart/js/flot/jquery.flot.js")
         add_script(formatter.req, "statushistorychart/js/flot/jquery.flot.time.js")
         add_script(formatter.req, "statushistorychart/js/enabler.js")
+        # from macro parameters
+        query = None
+        status_list = ['new', 'assigned', 'accepted', 'closed']  # default
         if(content):
             # replace parameters
             for match in reversed([match for match in re_param.finditer(content)]):
@@ -83,10 +86,24 @@ class Macro(Component):
                         content = content.replace(param_name, param_value)
             # execute query
             query = Query.from_string(formatter.env, content)
-            status_list = query.format and query.format.split('/') or status_list
-            if '*' not in status_list:
-                status_list.append('*')
-            default_status = status_list.index('*')
+        field = query and query.id or 'status'  # stopgap implementation; I use 'report' for y-axis parameter
+        field = filter(lambda x: x['name'] == field, TicketSystem(self.env).fields)
+        if len(field) <= 0:
+            return tag.div(tag.strong('Error: Macro %s failed' % name),
+                       tag.pre("%s is not field name" % query.id), class_="system-message")
+        field = field[0]
+        custom = 'custom' in field and field['custom'] or None
+        status_list = query and query.format and query.format.split('/') or \
+                      'options' in field and field['options'] or status_list
+        if field['name'] == 'status' and not (query and query.format):
+            def isprime(item):
+                primaries = ['new', 'assigned', 'accepted', 'closed']
+                return (item in primaries and [primaries.index(item)] or [len(primaries)])[0]
+            status_list.sort(key=isprime)
+        if '*' not in status_list:
+            status_list.append('*')
+        default_status = status_list.index('*')
+        # execute query for ids of ticket
         if(query and len(query.constraint_cols) > 0):
             result = query.execute() or [{'id':-1}]  # Sentinel for no result
             cond = "ticket.id in (%s)" % ', '.join([str(t['id']) for t in result])
@@ -98,17 +115,23 @@ class Macro(Component):
             cond = "ticket.id in (%s)" % ', '.join(tickets)
         else:
             raise TracError("Currently %sMacro without content is not supported." % name)
+        # execute query for value changes of each ticket
+        join_clause_dummy = ''
+        join_clause = "JOIN ticket_custom ON ticket.id = ticket_custom.ticket and ticket_custom.name = '%s'"
         changes = [row for row in formatter.env.get_read_db().execute("""
-                SELECT id, time, null, 'new'
+                SELECT id, time, null, %s
                     FROM ticket
+                    %s
                     WHERE %s
                 UNION
                 SELECT id, ticket_change.time, oldvalue, newvalue
                     FROM ticket
                     JOIN ticket_change ON ticket.id = ticket_change.ticket
-                    WHERE %s AND field='status'
+                    WHERE %s AND field='%s'
                 ORDER BY id, time
-                """ % (cond, cond))]
+                """ % (custom and "'\uFEFF'" or field['name'],  # ZERO WIDTH NO-BREAK SPACE; uses for mark of invalid data
+                       custom and (join_clause % field['name']) or join_clause_dummy,
+                       cond, cond, field['name']))]
         # transform (ticket, time, status)* ==> (ticket <>- status)*
         tid = 0  # position of ticket id
         tickets = {}
@@ -116,12 +139,15 @@ class Macro(Component):
             ticket = tickets.get(change[tid])  # slot is exist, or
             if ticket is None:
                 ticket = tickets[change[tid]] = []  # create new slot and use it
-            ticket.append(change)
+            ticket.append(list(change))
         # group status_list
 #        groupstats = self.compmgr[DefaultTicketGroupStatsProvider]
 #        ticket_groups = groupstats._get_ticket_groups()
 #        for group in ticket_groups:
 #            pass
+        for tid in tickets:
+            if len(tickets[tid]) > 1:
+                tickets[tid][0][3] = tickets[tid][1][2]  # override last value with initial value
         # generate data
         data = []
         # points
