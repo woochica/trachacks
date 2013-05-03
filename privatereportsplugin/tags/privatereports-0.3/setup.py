8dab202685e74de690aaf852a606bbac85fcb9da
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2013-05-03 23:05:05 +0000

    Refs #11049: Tagging version 0.3 of plugin.
    git-svn-id: https://trac-hacks.org/svn@13043 7322e99d-02ea-0310-aa39-e9a107903beb

---
+++ privatereportsplugin/tags/privatereports-0.3/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+setup(
+	name='privatereports',
+	version='0.3',
+	packages=['privatereports'],
+
+	author='Michael Henke',
+	author_email='michael.henke@she.net',
+	description="A trac plugin that lets you control which groups and users can see a report",
+	license="GPL",
+
+	keywords='trac plugin security report group user',
+	url='http://trac-hacks.org/wiki/PrivateReportsPlugin',
+	
+	classifiers = [
+		'Framework :: Trac',
+	],
+	
+	zip_safe=True,
+	package_data=	{'privatereports': 
+						[
+							'templates/*.html'
+						]
+					},
+
+	install_requires = ['Trac'],
+	
+	entry_points = {
+		'trac.plugins': [
+			'privatereports = privatereports',
+		],
+	},
+)
