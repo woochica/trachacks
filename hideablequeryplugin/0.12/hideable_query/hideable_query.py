cd2a1b699e98e7d9df7f909d6598c8f8c5c18dda
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2013-05-08 08:48:25 +0000

    Refs #11053: Replaced tabs with spaces in source code.
    git-svn-id: https://trac-hacks.org/svn@13057 7322e99d-02ea-0310-aa39-e9a107903beb

--- hideablequeryplugin/0.12/hideable_query/hideable_query.py
+++ hideablequeryplugin/0.12/hideable_query/hideable_query.py
@@ -9,67 +9,66 @@ from genshi.input import HTML
 
 class HideableQuery(Component):
 
-	implements(IRequestFilter, IPermissionRequestor, ITemplateStreamFilter)
-	
-	query_permission = Option(
-		'hideable_query', 
-		'query_permission',
-		default='QUERY_VIEW',
+    implements(IRequestFilter, IPermissionRequestor, ITemplateStreamFilter)
+
+    query_permission = Option(
+        'hideable_query',
+        'query_permission',
+        default='QUERY_VIEW',
         doc='The permission which users need to see the query site')
-		
-	query_hidden_redirect = Option(
-		'hideable_query', 
-		'query_hidden_redirect',
-		default='/report',
+
+    query_hidden_redirect = Option(
+        'hideable_query',
+        'query_hidden_redirect',
+        default='/report',
         doc='The site to which the user will be redirected if he has not the query_permission and tries to access the query site')
-	
-	# IPermissionRequestor methods
-	def get_permission_actions(self):
-		group_actions = [self.query_permission]
-		return group_actions
-	
-	# IRequestFilter methods
-	def pre_process_request(self, req, handler):
-		if req.path_info.find('/query') == -1:
-			return handler
-			
-		if self.query_permission in PermissionSystem(self.env).get_user_permissions(req.authname):
-			return handler
-		else:
-			if self.query_hidden_redirect == '':
-				return handler
-			else:
-				req.redirect(self.env.href(self.query_hidden_redirect))
-			return None
-	
-	def post_process_request(self, req, template, data, content_type):
-		return template, data, content_type
-		
-	# ITemplateStreamFilter methods
-	def filter_stream(self, req, method, filename, stream, data):
-		if filename != 'query.html' and filename != 'report_list.html' and filename != 'report_view.html':
-			return stream
-		
-		hasquerypermission = self.query_permission in PermissionSystem(self.env).get_user_permissions(req.authname)
-		
-		buffer = StreamBuffer()
-		
-		def replace_query_link():
-			if hasquerypermission:
-				return buffer
-			else:
-				return HTML('<div id="ctxtnav" class="nav"></div>')
-				
-		def replace_filter_box():
-			if hasquerypermission:
-				return buffer
-			else:
-				return HTML('')
-		
-		return stream | Transformer('//div[@id="ctxtnav" and @class="nav"]') \
-		.copy(buffer) \
-		.replace(replace_query_link).end() \
-		.select('//form[@id="query" and @method="post" and @action]') \
-		.copy(buffer) \
-		.replace(replace_filter_box)
-		
+
+    # IPermissionRequestor methods
+    def get_permission_actions(self):
+        group_actions = [self.query_permission]
+        return group_actions
+
+    # IRequestFilter methods
+    def pre_process_request(self, req, handler):
+        if req.path_info.find('/query') == -1:
+            return handler
+
+        if self.query_permission in PermissionSystem(self.env).get_user_permissions(req.authname):
+            return handler
+        else:
+            if self.query_hidden_redirect == '':
+                return handler
+            else:
+                req.redirect(self.env.href(self.query_hidden_redirect))
+            return None
+
+    def post_process_request(self, req, template, data, content_type):
+        return template, data, content_type
+
+    # ITemplateStreamFilter methods
+    def filter_stream(self, req, method, filename, stream, data):
+        if filename != 'query.html' and filename != 'report_list.html' and filename != 'report_view.html':
+            return stream
+
+        hasquerypermission = self.query_permission in PermissionSystem(self.env).get_user_permissions(req.authname)
+
+        buffer = StreamBuffer()
+
+        def replace_query_link():
+            if hasquerypermission:
+                return buffer
+            else:
+                return HTML('<div id="ctxtnav" class="nav"></div>')
+
+        def replace_filter_box():
+            if hasquerypermission:
+                return buffer
+            else:
+                return HTML('')
+
+        return stream | Transformer('//div[@id="ctxtnav" and @class="nav"]') \
+        .copy(buffer) \
+        .replace(replace_query_link).end() \
+        .select('//form[@id="query" and @method="post" and @action]') \
+        .copy(buffer) \
+        .replace(replace_filter_box)
--- hideablequeryplugin/0.12/setup.py
+++ hideablequeryplugin/0.12/setup.py
@@ -1,27 +1,27 @@
 from setuptools import setup
 
 setup(
-	name='hideable_query',
-	version='0.1',
-	packages=['hideable_query'],
+    name='hideable_query',
+    version='0.1',
+    packages=['hideable_query'],
 
-	author='Michael Henke',
-	author_email='michael.henke@she.net',
-	description="A trac plugin that lets you hide the query site and the links to it",
-	license="GPL",
+    author='Michael Henke',
+    author_email='michael.henke@she.net',
+    description="A trac plugin that lets you hide the query site and the links to it",
+    license="GPL",
 
-	keywords='trac plugin security hide query report',
-	url='http://trac-hacks.org/wiki/HideableQueryPlugin',
-	
-	classifiers = [
+    keywords='trac plugin security hide query report',
+    url='http://trac-hacks.org/wiki/HideableQueryPlugin',
+
+    classifiers=[
         'Framework :: Trac',
     ],
 
-	install_requires = ['Trac'],
-	
-	entry_points = {
-		'trac.plugins': [
-			'hideable_query = hideable_query',
-		],
-	},
+    install_requires=['Trac'],
+
+    entry_points={
+        'trac.plugins': [
+            'hideable_query = hideable_query',
+        ],
+    },
 )
