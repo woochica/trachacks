13be8dd7d9e6c4ca2e699f36e4ee9718df5785bb
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2013-05-08 08:49:31 +0000

    Fixes #11053: Changed license to BSD 3-Clause with permission of author. Added license text and file headers containing license info.
    git-svn-id: https://trac-hacks.org/svn@13059 7322e99d-02ea-0310-aa39-e9a107903beb

--- hideablequeryplugin/0.12/hideable_query/hideable_query.py
+++ hideablequeryplugin/0.12/hideable_query/hideable_query.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2010 Michael Henke <michael.henke@she.net>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+#
+
 from trac.core import Component, implements
 from trac.perm import PermissionSystem, IPermissionRequestor
 from trac.config import Option
--- hideablequeryplugin/0.12/setup.py
+++ hideablequeryplugin/0.12/setup.py
@@ -1,14 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2010 Michael Henke <michael.henke@she.net>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+#
+
 from setuptools import setup
 
 setup(
     name='hideable_query',
-    version='0.1',
+    version='0.2',
     packages=['hideable_query'],
 
     author='Michael Henke',
     author_email='michael.henke@she.net',
-    description="A trac plugin that lets you hide the query site and the links to it",
-    license="GPL",
+    description=""""A trac plugin that lets you hide the query site and
+                    the links to it""",
+    license="BSD 3-Clause",
 
     keywords='trac plugin security hide query report',
     url='http://trac-hacks.org/wiki/HideableQueryPlugin',
