6d7bceb550563181baae5eb5199ebc8f264e8208
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2013-05-10 05:12:39 +0000

    PEP-0008 changes.
    git-svn-id: https://trac-hacks.org/svn@13072 7322e99d-02ea-0310-aa39-e9a107903beb

--- simpleticketplugin/trunk/setup.py
+++ simpleticketplugin/trunk/setup.py
@@ -4,23 +4,24 @@
 from setuptools import setup
 
 setup(
-    name = 'TracSimpleTicket',
-    version = '5.0',
-    packages = ['simpleticket'],
-    author = 'Noah Kantrowitz',
-    author_email = 'noah+tracplugins@coderanger.net',
-    maintainer = 'Ryan J Ollos',
-    maintainer_email = 'ryan.j.ollos@gmail.com',
-    description = 'Restricted ticket entry form for Trac',
-    long_description = 'A Trac plugin that provides a configurable ticket entry form, with selected fields hidden from the user.',
-    license = 'BSD',
-    keywords = 'trac plugin restricted ticket',
-    url = 'http://trac-hacks.org/wiki/SimpleTicketPlugin',
-    classifiers = [
+    name='TracSimpleTicket',
+    version='5.0',
+    packages=['simpleticket'],
+    author='Noah Kantrowitz',
+    author_email='noah+tracplugins@coderanger.net',
+    maintainer='Ryan J Ollos',
+    maintainer_email='ryan.j.ollos@gmail.com',
+    description='Restricted ticket entry form for Trac',
+    long_description="""A Trac plugin that provides a configurable ticket
+        entry form, with selected fields hidden from the user.""",
+    license='BSD 3-Clause',
+    keywords='trac plugin restricted ticket',
+    url='http://trac-hacks.org/wiki/SimpleTicketPlugin',
+    classifiers=[
         'Framework :: Trac',
-    ],    
-    install_requires = ['Trac'],    
-    entry_points = {
+    ],
+    install_requires=['Trac'],
+    entry_points={
         'trac.plugins': [
             'simpleticket.web_ui = simpleticket.web_ui',
         ],
--- simpleticketplugin/trunk/simpleticket/web_ui.py
+++ simpleticketplugin/trunk/simpleticket/web_ui.py
@@ -1,38 +1,48 @@
 # Created by Noah Kantrowitz on 2008-03-11.
 # Copyright (c) 2008 Noah Kantrowitz. All rights reserved.
 
-from trac.config import ListOption, BoolOption
-from trac.core import *
-from trac.perm import IPermissionRequestor, PermissionSystem
+from trac.config import BoolOption, ListOption
+from trac.core import Component, implements
+from trac.perm import IPermissionRequestor
 from trac.web.api import IRequestFilter
 
+
 class SimpleTicketModule(Component):
-    """A request filter to implement the SimpleTicket reduced ticket entry form."""
-    
+    """A request filter to implement the SimpleTicket reduced ticket
+    entry form."""
+    implements(IPermissionRequestor, IRequestFilter)
+
     fields = ListOption('simpleticket', 'fields', default='',
-                         doc='Fields to hide for the simple ticket entry form.')
-    
+                        doc="""Fields to hide for the simple ticket entry
+                               form.""")
+
     show_only = BoolOption('simpleticket', 'show_only', default=False,
-                           doc='If True, show only the specified fields rather than hiding the specified fields')
+                           doc="""If True, show only the specified fields
+                                  rather than hiding the specified fields""")
 
-    implements(IRequestFilter, IPermissionRequestor)
-
-    # IRequestFilter methods
-    def pre_process_request(self, req, handler):
-        return handler
-            
-    def post_process_request(self, req, template, data, content_type):
-        if req.path_info == '/newticket':
-            do_filter = 'TICKET_CREATE_SIMPLE' in req.perm and not 'TRAC_ADMIN' in req.perm            
-            
-            self.log.debug('SimpleTicket: Filtering new ticket form for %s', req.authname)
-            if do_filter:
-                if self.show_only:
-                    data['fields'] = [f for f in data['fields'] if f['name'] in self.fields and f is not None]
-                else: 
-                    data['fields'] = [f for f in data['fields'] if f['name'] not in self.fields and f is not None]
-
-        return template, data, content_type
-    # IPermissionRequestor methods
+    ### IPermissionRequestor methods
     def get_permission_actions(self):
         yield 'TICKET_CREATE_SIMPLE', ['TICKET_CREATE']
+
+    ### IRequestFilter methods
+    def pre_process_request(self, req, handler):
+        return handler
+
+    def post_process_request(self, req, template, data, content_type):
+        if req.path_info == '/newticket':
+            do_filter = 'TICKET_CREATE_SIMPLE' in req.perm and \
+                        not 'TRAC_ADMIN' in req.perm
+            
+            self.log.debug('SimpleTicket: Filtering new ticket form for %s',
+                           req.authname)
+            if do_filter:
+                if self.show_only:
+                    data['fields'] = [f for f in data['fields']
+                                      if f['name'] in self.fields
+                                      and f is not None]
+                else: 
+                    data['fields'] = [f for f in data['fields']
+                                      if f['name'] not in self.fields
+                                      and f is not None]
+
+        return template, data, content_type
