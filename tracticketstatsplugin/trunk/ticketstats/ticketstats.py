cdfdb5e3c9e0d36c894c74ff06c0c1b3337365e3
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2013-05-13 03:24:45 +0000

    Fixed PEP-0008 violations.
    git-svn-id: https://trac-hacks.org/svn@13102 7322e99d-02ea-0310-aa39-e9a107903beb

--- tracticketstatsplugin/trunk/ticketstats/ticketstats.py
+++ tracticketstatsplugin/trunk/ticketstats/ticketstats.py
@@ -26,6 +26,7 @@ DEFAULT_DAYS_BACK = 30 * 6
 DEFAULT_INTERVAL = 30
 # ************************
 
+
 class TicketStatsPlugin(Component):
     implements(INavigationContributor, IRequestHandler, ITemplateProvider)
 
@@ -39,10 +40,10 @@ class TicketStatsPlugin(Component):
 
     default_interval = IntOption('ticketstats', 'default_interval',
                                  default=DEFAULT_INTERVAL,
-                                 doc='Number of days between each data point' \
+                                 doc='Number of days between each data point'
                                      ' (resolution) by default')
 
-    # ==[ INavigationContributor methods ]==
+    ### INavigationContributor methods
 
     def get_active_navigation_item(self, req):
         return 'ticketstats'
@@ -52,12 +53,12 @@ class TicketStatsPlugin(Component):
             yield ('mainnav', 'ticketstats',
                    tag.a('Ticket Stats', href=req.href.ticketstats()))
 
-    # ==[ Helper functions ]==
-    def _get_num_closed_tix(self, from_date, at_date, milestone, req):
-        '''
-        Returns an integer of the number of close ticket
-        events counted between from_date to at_date.
-        '''
+    ### Internal methods
+
+    def _get_num_closed_tix(self, from_date, at_date, milestone):
+        """Returns an integer of the number of close ticket events counted
+        between from_date to at_date."""
+
         status_map = {'new': 0,
                       'reopened': 0,
                       'assigned': 0,
@@ -67,31 +68,34 @@ class TicketStatsPlugin(Component):
         count = 0
 
         ma_milestone_str = ""
-        if milestone != None:
+        if milestone is not None:
             ma_milestone_str = " AND t.milestone = \"%s\" " % milestone
 
         db = self.env.get_db_cnx()
         cursor = db.cursor()
 
         # TODO clean up this query
-        cursor.execute(
-            "SELECT t.id, tc.field, tc.time, tc.oldvalue, tc.newvalue, t.priority FROM enum p, ticket_change tc INNER JOIN ticket t ON t.id = tc.ticket AND tc.time > %s AND tc.time <= %s WHERE p.name = t.priority AND p.type = 'priority' %s ORDER BY tc.time"
-            % (
-            to_utimestamp(from_date), to_utimestamp(at_date), ma_milestone_str))
+        cursor.execute("""
+            SELECT t.id, tc.field, tc.time, tc.oldvalue, tc.newvalue,
+              t.priority
+            FROM enum p, ticket_change tc
+              INNER JOIN ticket t ON t.id = tc.ticket AND tc.time > %s
+                AND tc.time <= %s
+            WHERE p.name = t.priority AND p.type = 'priority' %s
+            ORDER BY tc.time""" % (to_utimestamp(from_date),
+                                   to_utimestamp(at_date), ma_milestone_str))
 
-        for id, field, time, old, status, priority in cursor:
+        for tid, field, time, old, status, priority in cursor:
             if field == 'status':
                 if status in ('new', 'assigned', 'reopened', 'closed', 'edit'):
                     count += status_map[status]
 
         return count
 
+    def _get_num_open_tix(self, at_date, milestone):
+        """Returns an integer of the number of tickets currently open on that
+        date."""
 
-    def _get_num_open_tix(self, at_date, milestone, req):
-        '''
-        Returns an integer of the number of tickets
-        currently open on that date.
-        '''
         status_map = {'new': 0,
                       'reopened': 1,
                       'assigned': 0,
@@ -101,33 +105,41 @@ class TicketStatsPlugin(Component):
         count = 0
 
         ma_milestone_str = ""
-        if milestone != None:
+        if milestone is not None:
             ma_milestone_str = " AND t.milestone = \"%s\" " % milestone
 
         db = self.env.get_db_cnx()
         cursor = db.cursor()
 
         # TODO clean up this query
-        cursor.execute(
-            "SELECT t.type AS type, owner, status, time FROM ticket t, enum p WHERE p.name = t.priority AND p.type = 'priority' AND t.time <= %s %s"
-            % (to_utimestamp(at_date), ma_milestone_str))
+        cursor.execute("""
+            SELECT t.type AS type, owner, status, time
+            FROM ticket t, enum p
+            WHERE p.name = t.priority AND p.type = 'priority'
+              AND t.time <= %s %s
+            """ % (to_utimestamp(at_date), ma_milestone_str))
 
         for rows in cursor:
             count += 1
 
         # TODO clean up this query
-        cursor.execute(
-            "SELECT t.id, tc.field, tc.time, tc.oldvalue, tc.newvalue, t.priority FROM enum p, ticket_change tc INNER JOIN ticket t ON t.id = tc.ticket AND tc.time > 0 AND tc.time <= %s WHERE p.name = t.priority AND p.type = 'priority' %s ORDER BY tc.time"
-            % (to_utimestamp(at_date), ma_milestone_str))
+        cursor.execute("""
+            SELECT t.id, tc.field, tc.time, tc.oldvalue, tc.newvalue,
+              t.priority
+            FROM enum p, ticket_change tc
+              INNER JOIN ticket t ON t.id = tc.ticket AND tc.time > 0
+                AND tc.time <= %s WHERE p.name = t.priority
+                AND p.type = 'priority' %s
+            ORDER BY tc.time""" % (to_utimestamp(at_date), ma_milestone_str))
 
-        for id, field, time, old, status, priority in cursor:
+        for tid, field, time, old, status, priority in cursor:
             if field == 'status':
                 if status in ('new', 'assigned', 'reopened', 'closed', 'edit'):
                     count += status_map[status]
 
         return count
 
-    # ==[ IRequestHandle methods ]==
+    ### IRequestHandler methods
 
     def match_request(self, req):
         return re.match(r'/ticketstats(?:_trac)?(?:/.*)?$', req.path_info)
@@ -167,11 +179,11 @@ class TicketStatsPlugin(Component):
 
             at_date = parse_date(grab_at_date)
             at_date = datetime.combine(at_date,
-                                       time(11, 59, 59, 0, utc)) # Add tzinfo
+                                       time(11, 59, 59, 0, utc))  # Add tzinfo
 
             from_date = parse_date(grab_from_date)
             from_date = datetime.combine(from_date,
-                                         time(0, 0, 0, 0, utc)) # Add tzinfo
+                                         time(0, 0, 0, 0, utc))  # Add tzinfo
 
             graph_res = int(grab_resolution)
 
@@ -182,65 +194,65 @@ class TicketStatsPlugin(Component):
             from_date = at_date - timedelta(self.default_days_back)
             graph_res = self.default_interval
 
-            at_date_str = format_date(at_date)
-            from_date_str = format_date(from_date)
-
         count = []
 
         # Calculate 0th point
         last_date = from_date - timedelta(graph_res)
-        last_num_open = self._get_num_open_tix(last_date, milestone, req)
+        last_num_open = self._get_num_open_tix(last_date, milestone)
 
         # Calculate remaining points
-        for cur_date in daterange(from_date, at_date, graph_res):
-            num_open = self._get_num_open_tix(cur_date, milestone, req)
+        for cur_date in date_range(from_date, at_date, graph_res):
+            num_open = self._get_num_open_tix(cur_date, milestone)
             num_closed = self._get_num_closed_tix(last_date, cur_date,
-                                                  milestone, req)
-            datestr = format_date(cur_date)
+                                                  milestone)
+            date_str = format_date(cur_date)
             if graph_res != 1:
-                datestr = "%s thru %s" % (format_date(last_date), datestr)
-            count.append({'date': datestr,
+                date_str = "%s thru %s" % (format_date(last_date), date_str)
+            count.append({'date': date_str,
                           'new': num_open - last_num_open + num_closed,
                           'closed': num_closed,
                           'open': num_open})
             last_num_open = num_open
             last_date = cur_date
 
-        # if chartdata is requested, raw text is returned rather than data object
-        # for templating
-        if (not req_content == None) and (req_content == "chartdata"):
-            jsdstr = '{"chartdata": [\n'
+        # if chart data is requested, raw text is returned rather than data
+        # object for templating
+        if req_content is not None and req_content == "chartdata":
+            js_data = '{"chartdata": [\n'
             for x in count:
-                jsdstr += '{"date": "%s",' % x['date']
-                jsdstr += ' "new_tickets": %s,' % x['new']
-                jsdstr += ' "closed": %s,' % x['closed']
-                jsdstr += ' "open": %s},\n' % x['open']
-            jsdstr = jsdstr[:-2] + '\n]}'
-            req.send(jsdstr.encode('utf-8'))
+                js_data += '{"date": "%s",' % x['date']
+                js_data += ' "new_tickets": %s,' % x['new']
+                js_data += ' "closed": %s,' % x['closed']
+                js_data += ' "open": %s},\n' % x['open']
+            js_data = js_data[:-2] + '\n]}'
+            req.send(js_data.encode('utf-8'))
             return
         else:
             db = self.env.get_db_cnx()
-            showall = req.args.get('show') == 'all'
+            show_all = req.args.get('show') == 'all'
             milestone_list = [m.name for m in
-                              Milestone.select(self.env, showall, db)]
-            if milestone == None:
+                              Milestone.select(self.env, show_all, db)]
+            if milestone is None:
                 milestone_num = 0
             elif milestone in milestone_list:
                 milestone_num = milestone_list.find(milestone)
             else:
                 milestone_num = 0
 
-            data = {}
-            data['ticket_data'] = count
-            data['start_date'] = format_date(from_date)
-            data['end_date'] = format_date(at_date)
-            data['resolution'] = str(graph_res)
-            data['baseurl'] = req.base_url
-            data['milestones'] = milestone_list
-            data['cmilestone'] = milestone_num
-            data['yui_base_url'] = self.yui_base_url
+            data = {
+                'ticket_data': count,
+                'start_date': format_date(from_date),
+                'end_date': format_date(at_date),
+                'resolution': str(graph_res),
+                'baseurl': req.base_url,
+                'milestones': milestone_list,
+                'cmilestone': milestone_num,
+                'yui_base_url': self.yui_base_url
+            }
 
             return 'greensauce.html', data, None
+
+    ### ITemplateProvider methods
 
     def get_htdocs_dirs(self):
         return []
@@ -251,8 +263,9 @@ class TicketStatsPlugin(Component):
         return [resource_filename(__name__, 'templates')]
 
 
-def daterange(begin, end, delta=timedelta(1)):
-    """Stolen from: http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/574441
+def date_range(begin, end, delta=timedelta(1)):
+    """Stolen from:
+    http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/574441
 
     Form a range of dates and iterate over them.  
 
@@ -282,5 +295,3 @@ def daterange(begin, end, delta=timedelta(1)):
     while test(begin):
         yield begin
         begin += delta
-
-        #[EOF]
