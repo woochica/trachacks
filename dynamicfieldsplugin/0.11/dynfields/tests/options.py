d7c76594a740955dff386a3d3b0199578bf81194
rjollos <rqTkzckbm0CUTEJqsQVX7kLmCHsur67zbkxfywbzVT7Rsbx0yHcqcMeGagsJoAE1>
2013-05-24 19:30:25 -0400

    Refs #10476: Added failing unit test for `ticket-custom` section specified in an inherited ini file.

--- dynamicfieldsplugin/0.11/dynfields/tests/options.py
+++ dynamicfieldsplugin/0.11/dynfields/tests/options.py
@@ -11,6 +11,7 @@ import shutil
 import tempfile
 import unittest
 
+from trac.config import Configuration
 from trac.test import EnvironmentStub
 
 from dynfields.options import Options
@@ -23,18 +24,35 @@ class OptionsTestCase(unittest.TestCase):
                                    enable=['trac.*', 'dynfields.*'],
                                    path=tempfile.mkdtemp())
         self.env.config.filename = os.path.join(self.env.path, 'trac.ini')
-        self.env.config['ticket-custom'].set('version.show_when_type',
-                                             'enhancement')
-        self.env.config['ticket-custom'].set('alwayshidden.clear_on_hide',
-                                             False)
-        self.env.config.save()
 
     def tearDown(self):
         self.env.reset_db()
         shutil.rmtree(self.env.path)
 
     def test_options(self):
+        self.env.config['ticket-custom'].set('version.show_when_type',
+                                             'enhancement')
+        self.env.config['ticket-custom'].set('alwayshidden.clear_on_hide',
+                                             False)
+        self.env.config.save()
         options = Options(self.env)
+
+        self.assertEqual('enhancement', options['version.show_when_type'])
+        self.assertEqual('False', options['alwayshidden.clear_on_hide'])
+
+    def test_options_inherit(self):
+        inherited_config = Configuration(os.path.join(self.env.path,
+                                                      'global_trac.ini'))
+        inherited_config['ticket-custom'].set('version.show_when_type',
+                                              'enhancement')
+        inherited_config['ticket-custom'].set('alwayshidden.clear_on_hide',
+                                              False)
+        inherited_config.save()
+        self.env.config.parents.append(inherited_config)
+        self.env.config['inherit'].set('file', inherited_config.filename)
+        self.env.config.save()
+        options = Options(self.env)
+
         self.assertEqual('enhancement', options['version.show_when_type'])
         self.assertEqual('False', options['alwayshidden.clear_on_hide'])
 
